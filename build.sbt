191591f8ca8b577807aeabd4d065f293afcca1ec
Li Haoyi <haoyi.sg@gmail.com>
2017-10-16 19:36:16 -0700

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+scalaVersion := "2.12.3"
+
+name := "hbt"
+
+organization := "com.lihaoyi"
---
+++ src/main/scala/hbt/Main.scala
@@ -0,0 +1,6 @@
+package hbt
+object Main{
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+}
