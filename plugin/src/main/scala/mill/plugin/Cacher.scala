57a1e99fe924d3a244e7a6d5255c8acede0edcdd
虎鸣 <hepin.p@alibabapictures.com>
2017-12-15 01:47:38 +0800

    ! core change the cacher to look-free version

--- plugin/src/main/scala/mill/plugin/Cacher.scala
+++ plugin/src/main/scala/mill/plugin/Cacher.scala
@@ -1,22 +1,40 @@
 package mill.plugin
 
-import scala.collection.mutable
-import scala.reflect.macros.blackbox.Context
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.annotation.tailrec
+import scala.language.higherKinds
+import scala.reflect.macros.blackbox
 
 
 trait Cacher[C[_]]{
-  private[this] val cacherLazyMap = mutable.Map.empty[sourcecode.Enclosing, C[_]]
+  private[this] val cacherLazyMapRef = new AtomicReference[Map[sourcecode.Enclosing, C[_]]](Map.empty[sourcecode.Enclosing, C[_]])
+
   def wrapCached[T](in: C[T], enclosing: String): C[T]
   protected[this] def cachedTarget[T](t: => C[T])
-                                     (implicit c: sourcecode.Enclosing): C[T] = synchronized{
-    cacherLazyMap.getOrElseUpdate(c, wrapCached(t, c.value)).asInstanceOf[C[T]]
+                                     (implicit c: sourcecode.Enclosing): C[T] = {
+    @tailrec
+    def getOrCacheTarget(ct: C[T] = null.asInstanceOf[C[T]]):C[T] = {
+      val cacherLazyMap = cacherLazyMapRef.get()
+      cacherLazyMap.get(c) match {
+        case Some(cached) => cached.asInstanceOf[C[T]]
+        case None =>
+          val unCached = if (ct == null) wrapCached(t, c.value) else ct
+          if (cacherLazyMapRef.compareAndSet(cacherLazyMap,cacherLazyMap.updated(c,unCached))){
+            unCached
+          }else{
+            getOrCacheTarget(unCached)
+          }
+      }
+    }
+    getOrCacheTarget()
   }
 }
 object Cacher{
-  def impl0[M[_], T: c.WeakTypeTag](c: Context)(t: c.Expr[M[T]]): c.Expr[M[T]] = {
+  def impl0[M[_], T: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[M[T]]): c.Expr[M[T]] = {
     c.Expr[M[T]](wrapCached(c)(t.tree))
   }
-  def wrapCached(c: Context)(t: c.Tree) = {
+  def wrapCached(c: blackbox.Context)(t: c.Tree): c.universe.Tree = {
 
     import c.universe._
     val owner = c.internal.enclosingOwner
