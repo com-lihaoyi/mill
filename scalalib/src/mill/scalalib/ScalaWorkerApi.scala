69d474067446668f29e4b04be8c44d879ffaaf04
Li Haoyi <haoyi.sg@gmail.com>
2018-02-04 03:38:14 -0800

    fix mill_version use site

--- scalalib/src/mill/scalalib/ScalaWorkerApi.scala
+++ scalalib/src/mill/scalalib/ScalaWorkerApi.scala
@@ -19,7 +19,7 @@ object ScalaWorkerApi extends mill.define.ExternalModule {
       val mill.eval.Result.Success(v) = resolveDependencies(
         Seq(Cache.ivy2Local, MavenRepository("https://repo1.maven.org/maven2")),
         "2.12.4",
-        Seq(ivy"com.lihaoyi::mill-scalaworker:" + sys.props("MILL_VERSION"))
+        Seq(ivy"com.lihaoyi::mill-scalaworker:${sys.props("MILL_VERSION")}")
       )
       v.map(_.path)
     }
