511b38a8e463838e652912e1498600f060c9ff33
Robby <robby@k-state.edu>
2018-02-22 17:15:55 -0600

    Added generation of .idea/scala_compiler.xml (Scalac compiler plugins and options).

--- scalalib/src/mill/scalalib/GenIdea.scala
+++ scalalib/src/mill/scalalib/GenIdea.scala
@@ -90,14 +90,22 @@ object GenIdea {
       val externalSources = T.task{
         mod.resolveDeps(allIvyDeps, sources = true)()
       }
-      val Seq(resolvedCp: Loose.Agg[PathRef], resolvedSrcs: Loose.Agg[PathRef]) =
-        evaluator.evaluate(Agg(externalDependencies, externalSources))
+
+      val scalacPluginsIvyDeps = T.task{mod.scalacPluginIvyDeps()}
+      val scalacOptions = T.task{mod.scalacOptions()}
+      val scalacPluginDependencies = T.task{
+        mod.resolveDeps(scalacPluginsIvyDeps)()
+      }
+
+      val Seq(resolvedCp: Loose.Agg[PathRef], resolvedSrcs: Loose.Agg[PathRef], resolvedSp: Loose.Agg[PathRef],
+      scalacOpts: Seq[String]) =
+        evaluator.evaluate(Agg(externalDependencies, externalSources, scalacPluginDependencies, scalacOptions))
           .values
 
-      (path, resolvedCp.map(_.path).filter(_.ext == "jar") ++ resolvedSrcs.map(_.path), mod)
+      (path, resolvedCp.map(_.path).filter(_.ext == "jar") ++ resolvedSrcs.map(_.path), mod,
+        resolvedSp.map(_.path).filter(_.ext == "jar"), scalacOpts)
     }
     val moduleLabels = modules.map(_.swap).toMap
-
 
 
     val allResolved = resolved.flatMap(_._2) ++ buildLibraryPaths
@@ -124,6 +132,13 @@ object GenIdea {
       }
       .toMap
 
+    val compilerSettings = resolved
+      .foldLeft(Map[(Loose.Agg[Path], Seq[String]), Vector[ScalaModule]]()) {
+        (r, q) =>
+          val key = (q._4, q._5)
+          r + (key -> (r.getOrElse(key, Vector()) :+ q._3))
+      }
+
     val fixedFiles = Seq(
       Tuple2(".idea"/"misc.xml", miscXmlTemplate(jdkInfo)),
       Tuple2(".idea"/"scala_settings.xml", scalaSettingsTemplate()),
@@ -140,6 +155,10 @@ object GenIdea {
           for(path <- buildLibraryPaths)
           yield pathToLibName(path)
         )
+      ),
+      Tuple2(
+        ".idea"/"scala_compiler.xml",
+        scalaCompilerTemplate(compilerSettings)
       )
     )
 
@@ -155,7 +174,7 @@ object GenIdea {
       Tuple2(".idea"/'libraries/s"$name.xml", libraryXmlTemplate(name, url))
     }
 
-    val moduleFiles = resolved.map{ case (path, resolvedDeps, mod) =>
+    val moduleFiles = resolved.map{ case (path, resolvedDeps, mod, _, _) =>
       val Seq(
         resourcesPathRefs: Seq[PathRef],
         sourcesPathRef: Seq[PathRef],
@@ -306,5 +325,30 @@ object GenIdea {
         }
       </component>
     </module>
+  }
+  def scalaCompilerTemplate(settings: Map[(Loose.Agg[Path], Seq[String]), Seq[ScalaModule]]) = {
+
+    <project version="4">
+      <component name="ScalaCompilerConfiguration">
+        {
+        for((((plugins, params), mods), i) <- settings.toSeq.zip(1 to settings.size))
+        yield
+          <profile name={s"mill $i"} modules={mods.map(m => moduleName(m.millModuleSegments)).mkString(",")}>
+            <parameters>
+              {
+              for(param <- params)
+              yield <parameter value={param} />
+              }
+            </parameters>
+            <plugins>
+              {
+              for(plugin <- plugins.toSeq)
+              yield <plugin path={plugin.toString} />
+              }
+            </plugins>
+          </profile>
+        }
+      </component>
+    </project>
   }
 }
