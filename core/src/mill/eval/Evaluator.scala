a1da8036f6d6744dc0a9c34cc12f8fb26f6d3daa
Li Haoyi <haoyi.sg@gmail.com>
2018-03-03 12:13:58 -0800

    Split overriden enclosing path on `#` in addition to `.`, since `#` behaves badly in URIs and other places

--- core/src/mill/eval/Evaluator.scala
+++ core/src/mill/eval/Evaluator.scala
@@ -70,7 +70,7 @@ case class Evaluator[T](outPath: Path,
 
         val additional =
           if (finalTaskOverrides == t.ctx.overrides) Nil
-          else Seq(Segment.Label("overriden")) ++ t.ctx.enclosing.split('.').map(Segment.Label)
+          else Seq(Segment.Label("overriden")) ++ t.ctx.enclosing.split("\\.|#| ").map(Segment.Label)
 
         Right(Labelled(t, segments ++ additional))
       case t if goals.contains(t) => Left(t)
