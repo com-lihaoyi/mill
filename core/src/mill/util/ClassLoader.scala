90d4e0bda1bcb917ed6b0b16ec70d0b5de50c6ee
Robby <robby@k-state.edu>
2018-03-10 18:55:31 -0600

    Use java.net.URLClassLoader.

--- core/src/mill/util/ClassLoader.scala
+++ core/src/mill/util/ClassLoader.scala
@@ -1,14 +1,12 @@
 package mill.util
 
-import java.net.URL
+import java.net.{URL, URLClassLoader}
 
 import io.github.retronym.java9rtexport.Export
-
-import scala.reflect.internal.util.ScalaClassLoader.URLClassLoader
 
 object ClassLoader {
   def create(urls: Seq[URL], parent: java.lang.ClassLoader): URLClassLoader = {
     val rtOpt = if (ammonite.util.Util.java9OrAbove) Some(Export.export().toURI.toURL) else None
-    new URLClassLoader(urls ++ rtOpt, parent)
+    new URLClassLoader((urls ++ rtOpt).toArray, parent)
   }
 }
