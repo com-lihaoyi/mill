0f08f451ab63b5a7faa853897fe0d8505ebc4760
rockjam <5min4eq.unity@gmail.com>
2018-03-24 20:54:21 +0300

    move all custom classloading logic into mill.util.ClassLoader

--- core/src/mill/util/ClassLoader.scala
+++ core/src/mill/util/ClassLoader.scala
@@ -6,24 +6,51 @@ import ammonite.ops._
 import io.github.retronym.java9rtexport.Export
 
 object ClassLoader {
+
   def create(urls: Seq[URL], parent: java.lang.ClassLoader)(
       implicit ctx: Ctx.Home): URLClassLoader = {
-    if (ammonite.util.Util.java9OrAbove) {
-      val platformParent =
-        if (parent == null)
-          classOf[ClassLoader]
-            .getMethod("getPlatformClassLoader")
-            .invoke(null)
-            .asInstanceOf[ClassLoader]
-        else parent
+    new URLClassLoader(
+      makeUrls(urls).toArray,
+      refinePlatformParent(parent)
+    )
+  }
 
+  def create(urls: Seq[URL],
+             parent: java.lang.ClassLoader,
+             customFindClass: String => Option[Class[_]])(
+      implicit ctx: Ctx.Home): URLClassLoader = {
+    new URLClassLoader(
+      makeUrls(urls).toArray,
+      refinePlatformParent(parent)
+    ) {
+      override def findClass(name: String): Class[_] = {
+        customFindClass(name).getOrElse(super.findClass(name))
+      }
+    }
+  }
+
+  private def refinePlatformParent(parent: java.lang.ClassLoader): ClassLoader = {
+    if (ammonite.util.Util.java9OrAbove) {
+      if (parent == null)
+        classOf[ClassLoader]
+          .getMethod("getPlatformClassLoader")
+          .invoke(null)
+          .asInstanceOf[ClassLoader]
+      else parent
+    } else {
+      parent
+    }
+  }
+
+  private def makeUrls(urls: Seq[URL])(implicit ctx: Ctx.Home): Seq[URL] = {
+    if (ammonite.util.Util.java9OrAbove) {
       val rtFile = ctx.home / s"rt-${System.getProperty("java.version")}.jar"
       if (!exists(rtFile)) {
         cp(Path(Export.export()), rtFile)
       }
-      new URLClassLoader((urls :+ rtFile.toNIO.toUri.toURL).toArray,platformParent)
+      urls :+ rtFile.toNIO.toUri.toURL
     } else {
-      new URLClassLoader(urls.toArray, parent)
+      urls
     }
   }
 }
