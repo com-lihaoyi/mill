ac12671e906b4ca4b544eb2f41da69e9edb39952
rockjam <5min4eq.unity@gmail.com>
2018-03-27 01:27:31 +0300

    update release script

--- integration/test/resources/play-json/release.sh
+++ integration/test/resources/play-json/release.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
+
 mill release.clean
 mill __.test
 mill release.setReleaseVersion
 mill mill.scalalib.PublishModule/publishAll \
-    $SONATYPE_CREDENTIALS \
-    $GPG_PASSPHRASE \
-    __.publishArtifacts \
-    --release \
-    true \
+    --sonatypeCreds $SONATYPE_CREDENTIALS \
+    --gpgPassphrase $GPG_PASSPHRASE \
+    --publishArtifacts __.publishArtifacts \
+    --release true
 mill release.setNextVersion
