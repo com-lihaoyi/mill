a796f0a1381632560b6251a93466957319065966
Robby <robby@k-state.edu>
2018-03-27 16:38:11 -0500

    Removed ~/.mill before running mill test.

--- ci/test-mill-release.sh
+++ ci/test-mill-release.sh
@@ -9,6 +9,8 @@ ci/publish-local.sh
 
 git clean -xdf
 
+rm -fR ~/.mill
+
 # Second build & run tests using Mill
 
 ~/mill-release -i integration.test "mill.integration.forked.{AcyclicTests,UpickleTests}"
