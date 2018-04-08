5005b18f220571b8236f4a7f8951ce61d1cc5e3a
Li Haoyi <haoyi.sg@gmail.com>
2018-04-08 04:28:23 -0700

    Update HelloWorldTests.scala

--- scalalib/test/src/mill/scalalib/HelloWorldTests.scala
+++ scalalib/test/src/mill/scalalib/HelloWorldTests.scala
@@ -101,7 +101,7 @@ object HelloWorldTests extends TestSuite {
 
   object HelloWorldTypeLevel extends HelloBase{
     object foo extends ScalaModule {
-      def scalaVersion = "2.11.8"
+      def scalaVersion = "2.12.4"
       override def mapDependencies(d: coursier.Dependency) = {
         val artifacts = Set("scala-library", "scala-compiler", "scala-reflect")
         if (d.module.organization != "org.scala-lang" || !artifacts(d.module.name)) d
@@ -119,7 +119,7 @@ object HelloWorldTests extends TestSuite {
 
   object HelloWorldMacros extends HelloBase{
     object core extends ScalaModule {
-      def scalaVersion = "2.11.8"
+      def scalaVersion = "2.12.4"
 
       def ivyDeps = Agg(
         ivy"com.github.julien-truffaut::monocle-macro::1.4.0"
