c6be48d1422139e9c849167155936f1012e2ecfd
rockjam <5min4eq.unity@gmail.com>
2018-05-05 23:38:08 +0300

    add reformatAll command

--- scalalib/src/mill/scalalib/scalafmt/ScalafmtModule.scala
+++ scalalib/src/mill/scalalib/scalafmt/ScalafmtModule.scala
@@ -1,11 +1,11 @@
 package mill.scalalib.scalafmt
 
-import ammonite.ops._
+import ammonite.ops.{exists, ls, pwd}
 import mill._
-import mill.define.{Command, Sources}
+import mill.define._
 import mill.scalalib._
 
-trait ScalafmtModule extends ScalaModule {
+trait ScalafmtModule extends JavaModule {
 
   def reformat(): Command[Unit] = T.command {
     ScalafmtWorkerModule
@@ -23,17 +23,36 @@ trait ScalafmtModule extends ScalaModule {
 
   def scalafmtDeps: T[Agg[PathRef]] = T {
     Lib.resolveDependencies(
-      scalaWorker.repositories,
+      ScalaWorkerModule.repositories,
       Lib.depToDependency(_, "2.12.4"),
       Seq(ivy"com.geirsson::scalafmt-cli:${scalafmtVersion()}")
     )
   }
 
-  private def filesToFormat(sources: Seq[PathRef]) = {
+  protected def filesToFormat(sources: Seq[PathRef]) = {
     for {
       pathRef <- sources if exists(pathRef.path)
       file <- ls.rec(pathRef.path) if file.isFile && file.ext == "scala"
     } yield PathRef(file)
   }
 
+}
+
+object ScalafmtModule extends ExternalModule with ScalafmtModule {
+
+  def reformatAll(sources: mill.main.Tasks[Seq[PathRef]]): Command[Unit] =
+    T.command {
+      val files = Task.sequence(sources.value)().flatMap(filesToFormat)
+      ScalafmtWorkerModule
+        .worker()
+        .reformat(
+          files,
+          scalafmtConfig().head,
+          scalafmtDeps().map(_.path)
+        )
+    }
+
+  implicit def millScoptTargetReads[T] = new mill.main.Tasks.Scopt[T]()
+
+  lazy val millDiscover = Discover[this.type]
 }
