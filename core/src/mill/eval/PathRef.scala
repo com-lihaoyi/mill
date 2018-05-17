78384ff5c26d34107b2f2a044b249ebae899870c
Jim Lawson <ucbjrl@berkeley.edu>
2018-05-17 13:01:33 -0700

    Restrict doc files to *.java/*.scala Support symbolic links

--- core/src/mill/eval/PathRef.scala
+++ core/src/mill/eval/PathRef.scala
@@ -2,7 +2,7 @@ package mill.eval
 
 import java.io.IOException
 import java.nio.file.attribute.BasicFileAttributes
-import java.nio.file.{FileVisitResult, FileVisitor}
+import java.nio.file.{FileVisitOption, FileVisitResult, FileVisitor}
 import java.nio.{file => jnio}
 import java.security.{DigestOutputStream, MessageDigest}
 
@@ -25,8 +25,11 @@ object PathRef{
     val sig = {
       val digest = MessageDigest.getInstance("MD5")
       val digestOut = new DigestOutputStream(DummyOutputStream, digest)
+      // Handle symbolic links.
       jnio.Files.walkFileTree(
         path.toNIO,
+        java.util.EnumSet.of(FileVisitOption.FOLLOW_LINKS),
+        Integer.MAX_VALUE,
         new FileVisitor[jnio.Path] {
           def preVisitDirectory(dir: jnio.Path, attrs: BasicFileAttributes) = {
             digest.update(dir.toAbsolutePath.toString.getBytes)
