43a5b6db9b233da1e6e094c5f8156e9c2bcaea26
Unknown <guilgaly@gmail.com>
2018-05-20 14:31:15 +0200

    dependency updates output formatting

--- scalalib/src/mill/scalalib/dependency/DependencyUpdatesImpl.scala
+++ scalalib/src/mill/scalalib/dependency/DependencyUpdatesImpl.scala
@@ -1,7 +1,11 @@
 package mill.scalalib.dependency
 
 import mill.define._
-import mill.scalalib.dependency.updates.UpdatesFinder
+import mill.scalalib.dependency.updates.{
+  DependencyUpdate,
+  DependencyUpdates,
+  UpdatesFinder
+}
 import mill.scalalib.dependency.versions.VersionsFinder
 import mill.util.Ctx.{Home, Log}
 
@@ -18,17 +22,28 @@ object DependencyUpdatesImpl {
       UpdatesFinder.findUpdates(dependencyVersions, allowPreRelease = false)
     }
 
-    allUpdates.foreach { dependencyUpdates =>
-      println("----------")
-      println(dependencyUpdates.module)
-      println("----------")
-      dependencyUpdates.dependencies.foreach { dependencyUpdate =>
-        println(
-          s"${dependencyUpdate.dependency.module} (${dependencyUpdate.currentVersion})")
-        println(dependencyUpdate.updates)
-        println()
-      }
-      println()
-    }
+    showAllUpdates(allUpdates)
   }
+
+  private def showAllUpdates(updates: Seq[DependencyUpdates]): Unit =
+    updates.foreach { dependencyUpdates =>
+      val module = dependencyUpdates.module.toString
+      val actualUpdates =
+        dependencyUpdates.dependencies.filter(_.updates.nonEmpty)
+      if (actualUpdates.isEmpty) {
+        println(s"No dependency updates found for $module")
+      } else {
+        println(s"Found ${actualUpdates.length} dependency update for $module")
+        showUpdates(actualUpdates)
+      }
+    }
+
+  private def showUpdates(updates: Seq[DependencyUpdate]): Unit =
+    updates.foreach { dependencyUpdate =>
+      val module = s"${dependencyUpdate.dependency.module}"
+      val allVersions =
+        (dependencyUpdate.currentVersion +: dependencyUpdate.updates.toList)
+          .mkString(" -> ")
+      println(s"  $module : $allVersions")
+    }
 }
