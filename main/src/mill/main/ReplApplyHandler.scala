807e470fe3cb359993c8e37f79da2530138748d4
Guillaume Martres <smarter@ubuntu.com>
2018-08-22 17:30:13 +0900

    Make the REPL evaluator accessible from the REPL itself (#413)
    This makes it easier to hack on Mill using Mill itself, the evaluator is
    available using `replApplyHandler.evaluator`

--- main/src/mill/main/ReplApplyHandler.scala
+++ main/src/mill/main/ReplApplyHandler.scala
@@ -91,7 +91,7 @@ object ReplApplyHandler{
 
 }
 class ReplApplyHandler(pprinter0: pprint.PPrinter,
-                       evaluator: Evaluator[_]) extends ApplyHandler[Task] {
+                       val evaluator: Evaluator[_]) extends ApplyHandler[Task] {
   // Evaluate classLoaderSig only once in the REPL to avoid busting caches
   // as the user enters more REPL commands and changes the classpath
   val classLoaderSig = Evaluator.classLoaderSig
