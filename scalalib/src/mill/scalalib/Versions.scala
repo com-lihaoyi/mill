1e2b8fc4adff7b5c6a9cdc41aefcdb0c336f30b2
Li Haoyi <haoyi.sg@gmail.com>
2018-10-18 19:17:38 +0800

    bump ammonite version again

--- scalalib/src/mill/scalalib/Versions.scala
+++ scalalib/src/mill/scalalib/Versions.scala
@@ -2,7 +2,7 @@ package mill.scalalib
 
 object Versions {
   // Keep synchronized with ammonite dependency in core in build.sc
-  val ammonite = "1.3.0"
+  val ammonite = "1.3.1"
   // Keep synchronized with zinc dependency in scalalib.worker in build.sc
   val zinc = "1.2.1"
 }
