c746bdd4d4287525896636696864f9543732d0a1
Li Haoyi <haoyi.sg@gmail.com>
2018-12-19 12:28:19 +0800

    fix

--- scalalib/src/ZincWorkerModule.scala
+++ scalalib/src/ZincWorkerModule.scala
@@ -58,7 +58,7 @@ trait ZincWorkerModule extends mill.Module{
         Left((
           T.ctx(),
           compilerInterfaceClasspath().map(_.path).toArray,
-          scalaCompilerBridgeSourceJar _
+          scalaCompilerBridgeSourceJar(_, _).asSuccess.get.value
         )),
         mill.scalalib.api.Util.grepJar(_, "scala-library", _, sources = false),
         mill.scalalib.api.Util.grepJar(_, "scala-compiler", _, sources = false),
