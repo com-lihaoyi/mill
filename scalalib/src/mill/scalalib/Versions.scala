b44fe2753c62349b3b133d1da0e41a19178233a1
Li Haoyi <haoyi.sg@gmail.com>
2018-12-19 15:08:17 +0800

    bump zinc 1.2.1 -> 1.2.5

--- scalalib/src/mill/scalalib/Versions.scala
+++ scalalib/src/mill/scalalib/Versions.scala
@@ -4,5 +4,5 @@ object Versions {
   // Keep synchronized with ammonite dependency in core in build.sc
   val ammonite = "1.5.0"
   // Keep synchronized with zinc dependency in scalalib.worker in build.sc
-  val zinc = "1.2.1"
+  val zinc = "1.2.5"
 }
