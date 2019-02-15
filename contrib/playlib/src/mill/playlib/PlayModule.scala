c801ed09d657d10cccd0cd4ee4193f4716899196
Jean Helou <jhe@codamens.fr>
2019-02-15 15:02:44 +0100

    Play modules : api only, full play and mono module project.

---
+++ contrib/playlib/src/mill/playlib/PlayModule.scala
@@ -0,0 +1,23 @@
+package mill
+package playlib
+
+import mill.scalalib._
+import api.Versions
+
+trait PlayApiModule extends Dependencies with Router with Server{
+  trait PlayTests extends super.Tests{
+    def testFrameworks = Seq("org.scalatest.tools.Framework")
+    override def ivyDeps = T{
+      playMinorVersion() match {
+        case Versions.PLAY_2_6=>
+          Agg(ivy"org.scalatestplus.play::scalatestplus-play::3.1.2")
+        case Versions.PLAY_2_7=>
+          Agg(ivy"org.scalatestplus.play::scalatestplus-play::4.0.1")
+      }
+    }
+    override def sources = T.sources{ millSourcePath }
+  }
+
+  def start(args: String*) = T.command{ run(args:_*) }
+}
+trait PlayModule extends PlayApiModule with Twirl
