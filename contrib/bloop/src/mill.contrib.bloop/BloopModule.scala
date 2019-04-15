450e03fd392d99195829156eb5d21540eacd6732
Olivier Mélois <olivier.melois@bamtechmedia.com>
2019-04-15 20:19:56 +0200

    BloopModule back to the bare minimum

--- contrib/bloop/src/mill.contrib.bloop/BloopModule.scala
+++ contrib/bloop/src/mill.contrib.bloop/BloopModule.scala
@@ -1,7 +1,5 @@
 package mill.contrib.bloop
 
-import java.io.{PipedInputStream, PipedOutputStream}
-
 import ammonite.ops._
 import bloop.config.Config.{File => BloopFile}
 import bloop.config.ConfigEncoderDecoders._
@@ -12,62 +10,44 @@ import mill.eval.Evaluator
 import mill.modules.Jvm
 import mill.scalalib._
 import mill.scalalib.api.CompilationResult
-import scribe.{LogRecord, LoggerSupport}
 import upickle.core.Visitor
 import upickle.default
 
-import scala.meta.jsonrpc.{JsonRpcClient, Services}
+trait BloopModule extends Module { self: JavaModule =>
 
-// TODO can this be mixed with other scalajs module safely ?
-// TODO can we plugin into "mill clean" ?
-//
-trait BloopModule extends ScalaModule { self =>
+  object bloop extends Module {
 
-  trait Tests extends super.Tests with BloopModule {
-    override def test(args: String*) = {
-      super.test(args: _*)
-    }
-  }
-
-  override def compile = T {
-    val config = bloopConfig().project
-    val cmd = Seq("bloop", "compile", config.name)
-    val classes = Path(config.classesDir)
-    val out = Path(config.out)
-    val analysisFile = out / s"${config.name}-analysis.bin" // TODO does it need polling ?
-    Jvm.runSubprocess(cmd, T.ctx().env, pwd)
-    CompilationResult(analysisFile, PathRef(classes))
-  }
-
-  override def run(args: String*) = T.command {
-    val config = bloopConfig().project
-    val cmd = Seq("bloop", "run", config.name) ++ args // TODO does bloop return a specific exit code when failing to parse params ?
-    Jvm.runSubprocess(cmd, T.ctx().env, pwd)
-  }
-
-  def bloopConfig = T {
-    // Forcing the generation of the config for all modules here
-    val _ = BloopModule.install() // NB: looks like it has to be assigned to be executed
-    BloopModule.bloopConfig(pwd / ".bloop", self)()._1
-  }
-
-  // Converts from a Circe encoder to a uPickle one
-  implicit def circeWriter[T: Encoder]: default.Writer[T] =
-    new default.Writer[T] {
-      override def write0[V](out: Visitor[_, V], v: T) =
-        ujson.circe.CirceJson.transform(Encoder[T].apply(v), out)
+    def compile = T.input {
+      val prj = config().project
+      val cmd = Seq("bloop", "compile", prj.name)
+      val classes = Path(prj.classesDir)
+      val out = Path(prj.out)
+      val analysisFile = out / s"${prj.name}-analysis.bin"
+      Jvm.runSubprocess(cmd, T.ctx().env, pwd)
+      CompilationResult(analysisFile, PathRef(classes))
     }
 
-  // Converts from a Circe decoder to a uPickle one
-  implicit def circeReader[T: Decoder]: default.Reader[T] =
-    new default.Reader.Delegate[Json, T](
-      ujson.circe.CirceJson.map(Decoder[T].decodeJson).map(_.right.get))
+    def config = T {
+      // Forcing the generation of the config for all modules here
+      val _ = BloopModule.install() // NB: looks like it has to be assigned to be executed
+      BloopModule.bloopConfig(pwd / ".bloop", self)()._1
+    }
 
+    // Converts from a Circe encoder to a uPickle one
+    implicit def circeWriter[T: Encoder]: default.Writer[T] =
+      new default.Writer[T] {
+        override def write0[V](out: Visitor[_, V], v: T) =
+          ujson.circe.CirceJson.transform(Encoder[T].apply(v), out)
+      }
+
+    // Converts from a Circe decoder to a uPickle one
+    implicit def circeReader[T: Decoder]: default.Reader[T] =
+      new default.Reader.Delegate[Json, T](
+        ujson.circe.CirceJson.map(Decoder[T].decodeJson).map(_.right.get))
+  }
 }
 
 object BloopModule extends ExternalModule {
-
-  val bloopVersion = "1.2.5"
 
   private val ev = Evaluator.currentEvaluator.get()
   implicit def millScoptEvaluatorReads[T] = new mill.main.EvaluatorScopt[T]()
@@ -91,7 +71,7 @@ object BloopModule extends ExternalModule {
   // from calling module#sources in bloopInstall
   def moduleSourceMap: Target[Map[String, Seq[Path]]] = T {
     Task
-      .traverse(modules)(_.sources)
+      .traverse(modules)(_.allSources)
       .map(
         modules
           .map(j => j.millModuleSegments.toString)
@@ -109,6 +89,7 @@ object BloopModule extends ExternalModule {
 
     val javaConfig =
       module.javacOptions.map(opts => Some(Config.Java(options = opts.toList)))
+
     val scalaConfig = module match {
       case s: ScalaModule =>
         T.task {
@@ -169,12 +150,12 @@ object BloopModule extends ExternalModule {
 
     def transitiveClasspath(m: JavaModule): Task[Seq[Path]] = T.task {
       m.moduleDeps.map(classes) ++
-        m.resources().map(_.path) ++
         m.unmanagedClasspath().map(_.path) ++
         Task.traverse(m.moduleDeps)(transitiveClasspath)().flatten
     }
 
     val classpath = T.task(transitiveClasspath(module)() ++ ivyDepsClasspath())
+    val resources = T.task(module.resources().map(_.path.toNIO).toList)
 
     val project = T.task {
       val mSources = moduleSourceMap()
@@ -192,7 +173,7 @@ object BloopModule extends ExternalModule {
         classpath = classpath().map(_.toNIO).toList,
         out = out(module).toNIO,
         classesDir = classes(module).toNIO,
-        resources = None,
+        resources = Some(resources()),
         `scala` = scalaConfig(),
         java = javaConfig(),
         sbt = None,
@@ -216,47 +197,6 @@ object BloopModule extends ExternalModule {
     val (config, path) = bloopConfig(bloopDir, module)()
     bloop.config.write(config, path.toNIO)
     path
-  }
-
-  /**
-    * Creates the connection to bloop and surfaces a client
-    */
-  def bloopConnection: Worker[JsonRpcClient] = T.worker {
-    import bloop.launcher._
-
-    val launcherIn = new PipedInputStream()
-    val clientOut = new PipedOutputStream(launcherIn)
-
-    val clientIn = new PipedInputStream()
-    val launcherOut = new PipedOutputStream(clientIn)
-
-    val startedServer = scala.concurrent.Promise[Unit]()
-    val charset = java.nio.charset.StandardCharsets.UTF_8
-    val shell = bloop.launcher.core.Shell.default
-    val launcher = new LauncherMain(clientIn,
-                                    clientOut,
-                                    T.ctx().log.outputStream,
-                                    charset,
-                                    shell,
-                                    None,
-                                    startedServer)
-    val skipBspConnection = false
-    launcher.runLauncher(bloopVersion, skipBspConnection, List.empty) match {
-      case LauncherStatus.SuccessfulRun => ()
-      case other                        => throw new Exception("Bloop connection failed with: $other")
-    }
-    val millLogger = T.ctx().log
-    val logger = new LoggerSupport {
-      override def log[M](record: LogRecord[M]): Unit =
-        millLogger.debug(record.message) // might need some more thoughts
-    }
-    import monix.execution.Scheduler.Implicits.global
-    val clientIO = new scala.meta.jsonrpc.InputOutput(launcherIn, launcherOut)
-    val clientServices =
-      (_: scala.meta.jsonrpc.LanguageClient) => Services.empty(logger)
-    val connection =
-      scala.meta.jsonrpc.Connection(clientIO, logger, logger)(clientServices)
-    connection.client
   }
 
   lazy val millDiscover = Discover[this.type]
