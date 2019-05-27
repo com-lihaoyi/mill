de4d5bdaa8a5470e5b46268d28aac99dce2e9bc4
Nils Homer <nilshomer@gmail.com>
2019-05-27 14:51:10 -0700

    Scoverage can output an XML report
    * use the _.scoverage.xmlReport task

--- contrib/scoverage/api/src/ScoverageReportWorkerApi.scala
+++ contrib/scoverage/api/src/ScoverageReportWorkerApi.scala
@@ -4,4 +4,5 @@ import mill.eval.PathRef
 
 trait ScoverageReportWorkerApi {
   def htmlReport(sources: Seq[PathRef], dataDir: String, selfDir: String): Unit
+  def xmlReport(sources: Seq[PathRef], dataDir: String, selfDir: String): Unit
 }
