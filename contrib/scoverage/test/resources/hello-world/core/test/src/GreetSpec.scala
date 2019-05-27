64469e59c4f1e8f4691214b89e1f19c73a0311d3
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2019-05-26 20:04:32 -0400

    Scoverage multiple source folders (#615)
    * Add test the replicates issue #613
    
    * Fixes issue #613

---
+++ contrib/scoverage/test/resources/hello-world/core/test/src/GreetSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest._
+
+class GreetSpec extends WordSpec with Matchers {
+  "Greet" should {
+    "work" in {
+      Greet.greet("Nik", None) shouldBe("Hello, Nik!")
+    }
+  }
+}
