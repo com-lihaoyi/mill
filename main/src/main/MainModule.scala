5114df41e4144df4080e7664608d5a10578b8a80
Tobias Roeser <le.petit.fou@web.de>
2019-07-01 18:52:18 +0200

    Force single-threaded work-queue for all task

--- main/src/main/MainModule.scala
+++ main/src/main/MainModule.scala
@@ -161,11 +161,13 @@ trait MainModule extends mill.Module{
 
   /**
     * Runs multiple tasks in a single call.
-    *
-    *
+    * For compatibility reasons, the tasks are executed single-threaded.
     */
   def all(evaluator: Evaluator, targets: String*) = mill.T.command{
-    MainModule.evaluateTasks(evaluator, targets, multiSelect = true) {res =>
+    MainModule.evaluateTasks(
+      evaluator = if (evaluator.effectiveThreadCount > 1) evaluator.copy(threadCount = Some(1)) else evaluator,
+      targets = targets,
+      multiSelect = true) {res =>
       res.flatMap(_._2)
     }
   }
