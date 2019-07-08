6529614982824739d84837b01a3483e746eb9878
Li Haoyi <haoyi.sg@gmail.com>
2019-07-09 03:30:23 +0800

    .

--- ci/test-mill-dev.sh
+++ ci/test-mill-dev.sh
@@ -13,5 +13,5 @@ rm -rf ~/.mill
 # Second build & run tests
 out/dev/assembly/dest/mill -i main.test.compile
 
-#out/dev/assembly/dest/mill -i all {main,scalalib,scalajslib,contrib.twirllib,contrib.scalapblib}.test
+out/dev/assembly/dest/mill -i all {main,scalalib,scalajslib,contrib.twirllib,contrib.scalapblib}.test
 
