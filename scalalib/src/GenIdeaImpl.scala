fb80d430c9cddacd8167248377d23024f3df08dd
Tobias Roeser <le.petit.fou@web.de>
2019-09-18 11:14:07 +0200

    Use dedicated output dir for mill-build module in IntelliJ IDEA
    This fixes issues with missing generated sources in IntelliJ IDEA after a rebuild.

--- scalalib/src/GenIdeaImpl.scala
+++ scalalib/src/GenIdeaImpl.scala
@@ -451,7 +451,7 @@ case class GenIdeaImpl(evaluator: Evaluator,
   def rootXmlTemplate(libNames: Strict.Agg[String]) = {
     <module type="JAVA_MODULE" version={"" + ideaConfigVersion}>
       <component name="NewModuleRootManager">
-        <output url="file://$MODULE_DIR$/../out"/>
+        <output url="file://$MODULE_DIR$/../out/ideaOutputDir-mill-build"/>
         <content url="file://$MODULE_DIR$/..">
           <excludeFolder url="file://$MODULE_DIR$/../project" />
           <excludeFolder url="file://$MODULE_DIR$/../target" />
