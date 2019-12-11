0500afec559a44810ebfe7461e826e88fa3f311a
Tobias Roeser <le.petit.fou@web.de>
2019-12-11 08:11:57 +0100

    Reformatted new Java files

--- main/client/src/mill/main/client/FileToStreamTailer.java
+++ main/client/src/mill/main/client/FileToStreamTailer.java
@@ -10,88 +10,88 @@ import java.util.Optional;
 
 public class FileToStreamTailer extends Thread implements AutoCloseable {
 
-	private final File file;
-	private final PrintStream stream;
-	private final int intervalMsec;
+    private final File file;
+    private final PrintStream stream;
+    private final int intervalMsec;
 
-	private Optional<BufferedReader> reader = Optional.empty();
-	// if true, we won't read the whole file, but only new lines
-	private boolean ignoreHead = true;
+    private Optional<BufferedReader> reader = Optional.empty();
+    // if true, we won't read the whole file, but only new lines
+    private boolean ignoreHead = true;
 
-	private volatile boolean keepReading = true;
-	private volatile boolean flush = false;
+    private volatile boolean keepReading = true;
+    private volatile boolean flush = false;
 
-	public FileToStreamTailer(File file, PrintStream stream, int intervalMsec) {
-		super("Tail");
-		this.intervalMsec = intervalMsec;
-		setDaemon(true);
-		this.file = file;
-		this.stream = stream;
-	}
+    public FileToStreamTailer(File file, PrintStream stream, int intervalMsec) {
+        super("Tail");
+        this.intervalMsec = intervalMsec;
+        setDaemon(true);
+        this.file = file;
+        this.stream = stream;
+    }
 
-	@Override
-	public void run() {
-		if (isInterrupted()) {
-			keepReading = false;
-		}
-		while (keepReading || flush) {
-			flush = false;
-			try {
-				if (!reader.isPresent()) {
-					try {
-						this.reader = Optional.of(new BufferedReader(new FileReader(file)));
-					} catch (FileNotFoundException e) {
-						// nothing to ignore if file is inially missing
-						ignoreHead = false;
-					}
-				}
-				reader.ifPresent(r -> {
-					// read lines
-					try {
-						String line;
-						while ((line = r.readLine()) != null) {
-							if (!ignoreHead) {
-								stream.println(line);
-							}
-						}
-						// we ignored once
-						this.ignoreHead = false;
-					} catch (FileNotFoundException e) {
-						// File vanished
-						this.reader = Optional.empty();
-						this.ignoreHead = false;
-					} catch (IOException e) {
-						// could not read line or file vanished
-					}
-				});
-			} finally {
-				if (keepReading) {
-					// wait
-					try {
-						Thread.sleep(intervalMsec);
-					} catch (InterruptedException e) {
-						// can't handle anyway
-					}
-				}
-			}
-		}
-	}
+    @Override
+    public void run() {
+        if (isInterrupted()) {
+            keepReading = false;
+        }
+        while (keepReading || flush) {
+            flush = false;
+            try {
+                if (!reader.isPresent()) {
+                    try {
+                        this.reader = Optional.of(new BufferedReader(new FileReader(file)));
+                    } catch (FileNotFoundException e) {
+                        // nothing to ignore if file is inially missing
+                        ignoreHead = false;
+                    }
+                }
+                reader.ifPresent(r -> {
+                    // read lines
+                    try {
+                        String line;
+                        while ((line = r.readLine()) != null) {
+                            if (!ignoreHead) {
+                                stream.println(line);
+                            }
+                        }
+                        // we ignored once
+                        this.ignoreHead = false;
+                    } catch (FileNotFoundException e) {
+                        // File vanished
+                        this.reader = Optional.empty();
+                        this.ignoreHead = false;
+                    } catch (IOException e) {
+                        // could not read line or file vanished
+                    }
+                });
+            } finally {
+                if (keepReading) {
+                    // wait
+                    try {
+                        Thread.sleep(intervalMsec);
+                    } catch (InterruptedException e) {
+                        // can't handle anyway
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public void interrupt() {
-		this.keepReading = false;
-		super.interrupt();
-	}
+    @Override
+    public void interrupt() {
+        this.keepReading = false;
+        super.interrupt();
+    }
 
-	/**
-	 * Force a next read, even if we interrupt the thread.
-	 */
-	public void flush() {
-		this.flush = true;
-	}
+    /**
+     * Force a next read, even if we interrupt the thread.
+     */
+    public void flush() {
+        this.flush = true;
+    }
 
-	@Override
-	public void close() throws Exception {
-		interrupt();
-	}
+    @Override
+    public void close() throws Exception {
+        interrupt();
+    }
 }
--- main/client/test/src/mill/main/client/FileToStreamTailerTest.java
+++ main/client/test/src/mill/main/client/FileToStreamTailerTest.java
@@ -13,146 +13,146 @@ import de.tobiasroeser.lambdatest.junit.FreeSpec;
 
 public class FileToStreamTailerTest extends FreeSpec {
 
-	public FileToStreamTailerTest() {
-		setReporter(new DefaultReporter());
+    public FileToStreamTailerTest() {
+        setReporter(new DefaultReporter());
 
-		test("Handle non-existing file", () -> {
-			ByteArrayOutputStream bas = new ByteArrayOutputStream();
-			final PrintStream ps = new PrintStream(bas);
+        test("Handle non-existing file", () -> {
+            ByteArrayOutputStream bas = new ByteArrayOutputStream();
+            final PrintStream ps = new PrintStream(bas);
 
-			final File file = File.createTempFile("tailer", "");
-			expectTrue(file.delete());
+            final File file = File.createTempFile("tailer", "");
+            expectTrue(file.delete());
 
-			try (
-					final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
-			) {
-				tailer.start();
-				Thread.sleep(200);
-				expectEquals(bas.toString(), "");
-			}
-		});
+            try (
+                final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
+            ) {
+                tailer.start();
+                Thread.sleep(200);
+                expectEquals(bas.toString(), "");
+            }
+        });
 
-		test("Handle non-existing file that appears later", () -> {
-			ByteArrayOutputStream bas = new ByteArrayOutputStream();
-			final PrintStream ps = new PrintStream(bas);
+        test("Handle non-existing file that appears later", () -> {
+            ByteArrayOutputStream bas = new ByteArrayOutputStream();
+            final PrintStream ps = new PrintStream(bas);
 
-			final File file = File.createTempFile("tailer", "");
-			expectTrue(file.delete());
+            final File file = File.createTempFile("tailer", "");
+            expectTrue(file.delete());
 
-			try (
-					final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
-			) {
-				tailer.start();
-				Thread.sleep(100);
-				expectEquals(bas.toString(), "");
+            try (
+                final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
+            ) {
+                tailer.start();
+                Thread.sleep(100);
+                expectEquals(bas.toString(), "");
 
-				try (
-						PrintStream out = new PrintStream(new FileOutputStream(file));
-				) {
-					out.println("log line");
-					expectTrue(file.exists());
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "log line\n");
-				}
-			}
-		});
+                try (
+                    PrintStream out = new PrintStream(new FileOutputStream(file));
+                ) {
+                    out.println("log line");
+                    expectTrue(file.exists());
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "log line\n");
+                }
+            }
+        });
 
-		test("Handle existing and initially empty file", () -> {
-			ByteArrayOutputStream bas = new ByteArrayOutputStream();
-			final PrintStream ps = new PrintStream(bas);
+        test("Handle existing and initially empty file", () -> {
+            ByteArrayOutputStream bas = new ByteArrayOutputStream();
+            final PrintStream ps = new PrintStream(bas);
 
-			final File file = File.createTempFile("tailer", "");
-			expectTrue(file.exists());
+            final File file = File.createTempFile("tailer", "");
+            expectTrue(file.exists());
 
-			try (
-					final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
-			) {
-				tailer.start();
-				Thread.sleep(100);
+            try (
+                final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
+            ) {
+                tailer.start();
+                Thread.sleep(100);
 
-				expectEquals(bas.toString(), "");
+                expectEquals(bas.toString(), "");
 
-				try (
-						PrintStream out = new PrintStream(new FileOutputStream(file));
-				) {
-					out.println("log line");
-					expectTrue(file.exists());
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "log line\n");
-				}
-			}
-		});
+                try (
+                    PrintStream out = new PrintStream(new FileOutputStream(file));
+                ) {
+                    out.println("log line");
+                    expectTrue(file.exists());
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "log line\n");
+                }
+            }
+        });
 
-		test("Handle existing file with old content", () -> {
-			ByteArrayOutputStream bas = new ByteArrayOutputStream();
-			final PrintStream ps = new PrintStream(bas);
+        test("Handle existing file with old content", () -> {
+            ByteArrayOutputStream bas = new ByteArrayOutputStream();
+            final PrintStream ps = new PrintStream(bas);
 
-			final File file = File.createTempFile("tailer", "");
-			expectTrue(file.exists());
+            final File file = File.createTempFile("tailer", "");
+            expectTrue(file.exists());
 
-			try (
-					PrintStream out = new PrintStream(new FileOutputStream(file));
-			) {
-				out.println("old line 1");
-				out.println("old line 2");
-				try (
-						final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
-				) {
-					tailer.start();
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "");
-					out.println("log line");
-					expectTrue(file.exists());
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "log line\n");
-				}
-			}
-		});
+            try (
+                PrintStream out = new PrintStream(new FileOutputStream(file));
+            ) {
+                out.println("old line 1");
+                out.println("old line 2");
+                try (
+                    final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
+                ) {
+                    tailer.start();
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "");
+                    out.println("log line");
+                    expectTrue(file.exists());
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "log line\n");
+                }
+            }
+        });
 
-		test("Handle existing empty file which disappears later and comes back", () -> {
-			pending("Not a requirement");
+        test("Handle existing empty file which disappears later and comes back", () -> {
+            pending("Not a requirement");
 
-			ByteArrayOutputStream bas = new ByteArrayOutputStream();
-			final PrintStream ps = new PrintStream(bas);
+            ByteArrayOutputStream bas = new ByteArrayOutputStream();
+            final PrintStream ps = new PrintStream(bas);
 
-			final File file = File.createTempFile("tailer", "");
-			expectTrue(file.exists());
+            final File file = File.createTempFile("tailer", "");
+            expectTrue(file.exists());
 
-			try (
-					final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
-			) {
-				tailer.start();
-				Thread.sleep(100);
+            try (
+                final FileToStreamTailer tailer = new FileToStreamTailer(file, ps, 10);
+            ) {
+                tailer.start();
+                Thread.sleep(100);
 
-				expectEquals(bas.toString(), "");
+                expectEquals(bas.toString(), "");
 
-				try (
-						PrintStream out = new PrintStream(new FileOutputStream(file));
-				) {
-					out.println("log line 1");
-					out.println("log line 2");
-					expectTrue(file.exists());
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "log line 1\nlog line 2\n");
-				}
+                try (
+                    PrintStream out = new PrintStream(new FileOutputStream(file));
+                ) {
+                    out.println("log line 1");
+                    out.println("log line 2");
+                    expectTrue(file.exists());
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "log line 1\nlog line 2\n");
+                }
 
-				// Now delete file and give some time, then append new lines
+                // Now delete file and give some time, then append new lines
 
-				expectTrue(file.delete());
-				Thread.sleep(100);
+                expectTrue(file.delete());
+                Thread.sleep(100);
 
-				try (
-						PrintStream out = new PrintStream(new FileOutputStream(file));
-				) {
-					out.println("new line");
-					expectTrue(file.exists());
-					Thread.sleep(100);
-					expectEquals(bas.toString(), "log line 1\nlog line 2\nnew line\n");
-				}
+                try (
+                    PrintStream out = new PrintStream(new FileOutputStream(file));
+                ) {
+                    out.println("new line");
+                    expectTrue(file.exists());
+                    Thread.sleep(100);
+                    expectEquals(bas.toString(), "log line 1\nlog line 2\nnew line\n");
+                }
 
-			}
-		});
+            }
+        });
 
 
-	}
+    }
 }
