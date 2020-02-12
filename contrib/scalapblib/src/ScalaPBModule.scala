8c2c58fa803099eea7d2ba0c48f0a57217f8c27b
Alexis Hernandez <alexis22229@gmail.com>
2020-02-12 09:25:12 -0700

    Allow disabling lenses on the ScalaPBModule (#772)
    See https://github.com/lihaoyi/mill/pull/772
    
    Co-Authored-By: Tobias Roeser <le.petit.fou@web.de>
    
    Co-authored-by: Tobias Roeser <le.petit.fou@web.de>

--- contrib/scalapblib/src/ScalaPBModule.scala
+++ contrib/scalapblib/src/ScalaPBModule.scala
@@ -33,6 +33,9 @@ trait ScalaPBModule extends ScalaModule {
 
   def scalaPBSingleLineToProtoString: T[Boolean] = T { false }
 
+  /** ScalaPB enables lenses by default, this option allows you to disable it. */
+  def scalaPBLenses: T[Boolean] = T { true }
+
   def scalaPBProtocPath: T[Option[String]] = T { None }
 
   def scalaPBSources: Sources = T.sources {
@@ -43,6 +46,7 @@ trait ScalaPBModule extends ScalaModule {
     (
       (if (scalaPBFlatPackage()) Seq("flat_package") else Seq.empty) ++
       (if (scalaPBJavaConversions()) Seq("java_conversions") else Seq.empty) ++
+      (if (!scalaPBLenses()) Seq("no_lenses") else Seq.empty) ++
       (if (scalaPBGrpc()) Seq("grpc") else Seq.empty) ++ (
         if (!scalaPBSingleLineToProtoString()) Seq.empty else {
           if (Version(scalaPBVersion()) >= Version("0.7.0"))
