81b84d84008b2e9084464136d3170df473e498b0
eMASTER <emstlk@gmail.com>
2020-02-12 16:54:23 +0000

    Fix merge files in assembly (#768)
    Fixes https://github.com/lihaoyi/mill/issues/671
    
    * Fix merge files in assembly
    
    * Add param assemblySeparator
    
    * Move the separator into Append rule
    
    * The separator will be placed only between entries
    
    See https://github.com/lihaoyi/mill/pull/768

--- main/src/modules/Jvm.scala
+++ main/src/modules/Jvm.scala
@@ -283,10 +283,16 @@ object Jvm {
 
     Assembly.groupAssemblyEntries(inputPaths, assemblyRules).view
       .foreach {
-        case (mapping, AppendEntry(entries)) =>
+        case (mapping, AppendEntry(entries, separator)) =>
           val path = zipFs.getPath(mapping).toAbsolutePath
+          val separated =
+            if (entries.isEmpty) Nil
+            else
+              entries.head +: entries.tail.flatMap { e =>
+                List(JarFileEntry(e.mapping, () => new ByteArrayInputStream(separator.getBytes)), e)
+              }
           val concatenated = new SequenceInputStream(
-            Collections.enumeration(entries.map(_.inputStream).asJava))
+            Collections.enumeration(separated.map(_.inputStream).asJava))
           writeEntry(path, concatenated, append = true)
         case (mapping, WriteOnceEntry(entry)) =>
           val path = zipFs.getPath(mapping).toAbsolutePath
@@ -328,7 +334,7 @@ object Jvm {
     outputStream.close()
     is.close()
   }
-  
+
   def universalScript(shellCommands: String,
                       cmdCommands: String,
                       shebang: Boolean = false): String = {
