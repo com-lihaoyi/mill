8d80ccaa06fa603dbcd2c85c78e63d93ca2f41ae
Gunnar André Saxvik-Lilleaasen <ginnus@outlook.com>
2020-03-18 16:59:49 +0100

    Replaced hardcoded "org.scala-lang" with call to ScalaModule.scalaOrganization.

--- contrib/bloop/src/mill/contrib/bloop/BloopImpl.scala
+++ contrib/bloop/src/mill/contrib/bloop/BloopImpl.scala
@@ -154,7 +154,7 @@ class BloopImpl(ev: () => Evaluator, wd: Path) extends ExternalModule { outer =>
             (s.scalacOptions() ++ pluginOptions).toList
           Some(
             BloopConfig.Scala(
-              organization = "org.scala-lang",
+              organization = s.scalaOrganization(),
               name = "scala-compiler",
               version = s.scalaVersion(),
               options = allScalacOptions,
