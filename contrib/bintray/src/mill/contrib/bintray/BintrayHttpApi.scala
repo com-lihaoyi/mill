96724729df84ffe966f8615b4665eca2a357d8bd
Gunnar André Saxvik-Lilleaasen <ginnus@outlook.com>
2020-03-20 18:14:41 +0100

    Added error logging to BintrayHttpApi.

--- contrib/bintray/src/mill/contrib/bintray/BintrayHttpApi.scala
+++ contrib/bintray/src/mill/contrib/bintray/BintrayHttpApi.scala
@@ -23,48 +23,57 @@ class BintrayHttpApi(
   def now = ZonedDateTime.now(ZoneOffset.UTC)
 
   // https://www.jfrog.com/confluence/display/BT/Bintray+REST+API#BintrayRESTAPI-UploadContent
-  def upload(pkg: String, version: String, path: String, contentType: String, data: Array[Byte]): requests.Response = {
-    log.info(s"Uploading $path")
-    http.put(
-      s"${Paths.upload(pkg, version)}/$path",
-      readTimeout = uploadTimeout,
-      headers = Seq(
-        "Content-Type" -> contentType,
-        "Authorization" -> Auth.basic
-      ),
-      data = data
-    )
-  }
+  def upload(pkg: String, version: String, path: String, contentType: String, data: Array[Byte]): requests.Response =
+    send (s"Uploading $path") {
+      http.put(
+        s"${Paths.upload(pkg, version)}/$path",
+        readTimeout = uploadTimeout,
+        headers = Seq(
+          "Content-Type" -> contentType,
+          "Authorization" -> Auth.basic
+        ),
+        data = data
+      )
+    }
 
   def createVersion(
     pkg: String,
     version: String,
     releaseDate: ZonedDateTime = now,
     description: String = ""
-  ): requests.Response = {
-    log.info(s"Creating version $version")
-    http.post(
-      Paths.version(pkg),
-      headers = Seq(
-        "Content-Type" -> ContentTypes.json,
-        "Authorization" -> Auth.basic
-      ),
-      data = s"""{
-                |  "desc": "$description",
-                |  "released": "${releaseDate.format(DateTimeFormatter.ISO_INSTANT)}",
-                |  "name": "$version"
-                |}""".stripMargin
-    )
-  }
-
-  def publish(pkg: String, version: String): requests.Response = {
-    log.info(s"Publishing version $version")
-    http.post(
-      Paths.publish(pkg, version),
-      headers = Seq(
-        "Authorization" -> Auth.basic
+  ): requests.Response =
+    send (s"Creating version $version") {
+      http.post(
+        Paths.version(pkg),
+        headers = Seq(
+          "Content-Type" -> ContentTypes.json,
+          "Authorization" -> Auth.basic
+        ),
+        data = s"""{
+                  |  "desc": "$description",
+                  |  "released": "${releaseDate.format(DateTimeFormatter.ISO_INSTANT)}",
+                  |  "name": "$version"
+                  |}""".stripMargin
       )
-    )
+    }
+
+  def publish(pkg: String, version: String): requests.Response =
+    send (s"Publishing version $version") {
+      http.post(
+        Paths.publish(pkg, version),
+        headers = Seq(
+          "Authorization" -> Auth.basic
+        )
+      )
+    }
+
+  private def send(description: String)(request: => requests.Response) = {
+    log.info(description)
+    val response = request
+    if (!response.is2xx && !response.is3xx) {
+      log.error(s"$description failed")
+    }
+    response
   }
 
   object Paths {
