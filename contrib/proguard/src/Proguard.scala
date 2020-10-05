81a8b16088830fb695681bf5b15d6630dce29e5d
Stephen Bly <stephen.bly@bamtechmedia.com>
2020-10-05 12:23:28 -0400

    Finish proguard plugin

--- contrib/proguard/src/Proguard.scala
+++ contrib/proguard/src/Proguard.scala
@@ -12,6 +12,9 @@ import os.Path
 import os.PathChunk
 
 trait Proguard extends ScalaModule {
+
+  // This is actually the only available version on Maven currently.
+  // But we should keep it as a configuration in case a new version is released.
   def proguardVersion: T[String] = T { "7.0.0" }
 
   def shrink: T[Boolean] = T { true }
@@ -19,28 +22,34 @@ trait Proguard extends ScalaModule {
   def obfuscate: T[Boolean] = T { true }
   def preverify: T[Boolean] = T { true }
 
+  def javaHome: T[PathRef] = T {
+    PathRef(Path(System.getProperty("java.home")))
+  }
+
   def inJar: T[PathRef] = T { assembly() }
   def outJar: T[PathRef] = T { PathRef(T.dest / "out.jar") }
   def libraryJars: T[Seq[PathRef]] = T {
-    upstreamAssemblyClasspath().toSeq
+    val javaJars = os.list(javaHome().path / "lib", sort = false).filter(_.ext == "jar")
+    javaJars.toSeq.map(PathRef(_))
   }
 
   def proguard: T[PathRef] = T {
     val cmd = os.proc(
-        "java",
-        "-cp",
-        proguardClasspath().map(_.path).mkString(":"),
-        "proguard.ProGuard",
-        "-injars",
-        inJar().path,
-        "-outjars",
-        outJar().path,
-        "-libraryjars",
-        libraryJars().map(_.path),
-        steps(),
-        additionalOptions()
-      )
-    System.out.println("Running command: " + cmd.command.flatMap(_.value).mkString(" "))
+      "java",
+      "-cp",
+      proguardClasspath().map(_.path).mkString(":"),
+      "proguard.ProGuard",
+      steps(),
+      "-injars",
+      inJar().path,
+      "-outjars",
+      outJar().path,
+      "-libraryjars",
+      libraryJars().map(_.path).mkString(":"),
+      entryPoint(),
+      additionalOptions()
+    )
+    System.out.println(cmd.command.flatMap(_.value).mkString(" "))
     cmd.call(stdout = T.dest / "stdout.txt", stderr = T.dest / "stderr.txt")
 
     // the call above already throws an exception on a non-zero exit code,
@@ -60,6 +69,13 @@ trait Proguard extends ScalaModule {
       (if (obfuscate()) Seq() else Seq("-dontobfuscate")) ++
       (if (shrink()) Seq() else Seq("-dontshrink")) ++
       (if (preverify()) Seq() else Seq("-dontpreverify"))
+  }
+
+  def entryPoint: T[String] = T {
+    s"""|-keep public class ${finalMainClass()} {
+        |    public static void main(java.lang.String[]);
+        |}
+        |""".stripMargin
   }
 
   def additionalOptions: T[Seq[String]] = T { Seq[String]() }
