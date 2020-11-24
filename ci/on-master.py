db426bdbc551d233847fdd5f0ffa0c8f87396d0b
Li Haoyi <haoyi.sg@gmail.com>
2020-11-24 15:16:55 +0800

    fix on-master.py

--- ci/on-master.py
+++ ci/on-master.py
@@ -3,7 +3,7 @@
 import os, sys, subprocess
 is_master_commit = (
     os.environ["GITHUB_REPOSITORY"] == "lihaoyi/mill" and
-    (os.environ["GITHUB_REF"].endswith("/master")
+    os.environ["GITHUB_REF"].endswith("/master")
 )
 
 if is_master_commit:
