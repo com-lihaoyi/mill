6882d750cd23c7aea15d78880eb10685024266d3
levi <velocorner.com@gmail.com>
2021-02-13 21:41:11 +0100

    add play 2.8 support

--- contrib/playlib/src/mill/playlib/PlayModule.scala
+++ contrib/playlib/src/mill/playlib/PlayModule.scala
@@ -1,8 +1,8 @@
 package mill
 package playlib
 
+import mill.define.Target
 import mill.scalalib._
-
 import mill.playlib.api.Versions
 
 trait PlayApiModule extends Dependencies with Router with Server{
@@ -14,9 +14,12 @@ trait PlayApiModule extends Dependencies with Router with Server{
           Agg(ivy"org.scalatestplus.play::scalatestplus-play::3.1.2")
         case Versions.PLAY_2_7=>
           Agg(ivy"org.scalatestplus.play::scalatestplus-play::4.0.1")
+        case Versions.PLAY_2_8=>
+          Agg(ivy"org.scalatestplus.play::scalatestplus-play::5.1.0")
       }
     }
     override def sources = T.sources{ millSourcePath }
+
   }
 
   def start(args: String*) = T.command{ run(args:_*) }
