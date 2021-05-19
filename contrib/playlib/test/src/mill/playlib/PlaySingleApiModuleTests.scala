17e45f185f54f66287535ffba04f3cf5fe1fd71d
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-05-19 11:35:06 +0200

    Update utest syntax in PlaySingleApiModuleTests
    - Change symbols to strings in paths

--- contrib/playlib/test/src/mill/playlib/PlaySingleApiModuleTests.scala
+++ contrib/playlib/test/src/mill/playlib/PlaySingleApiModuleTests.scala
@@ -14,66 +14,72 @@ object PlaySingleApiModuleTests extends TestSuite with PlayTestSuite {
     object test extends PlayTests
   }
 
-  val resourcePath: os.Path = os.pwd / 'contrib / 'playlib / 'test / 'resources / "playsingleapi"
+  val resourcePath: os.Path = os.pwd / "contrib" / "playlib" / "test" / "resources" / "playsingleapi"
 
   def tests: Tests = Tests {
-    'playVersion - {
-      'fromBuild - workspaceTest(playsingleapi) { eval =>
-        val Right((result, evalCount)) = eval.apply(playsingleapi.playVersion)
+    test("playVersion") {
+      test("fromBuild") {
+        workspaceTest(playsingleapi) { eval =>
+          val Right((result, evalCount)) = eval.apply(playsingleapi.playVersion)
+          assert(
+            result == "2.7.0",
+            evalCount > 0
+          )
+        }
+      }
+    }
+    test("layout") {
+      test("fromBuild") {
+        workspaceTest(playsingleapi) { eval =>
+          val Right((conf, _)) = eval.apply(playsingleapi.conf)
+          val Right((app, _)) = eval.apply(playsingleapi.app)
+          val Right((sources, _)) = eval.apply(playsingleapi.sources)
+          val Right((resources, _)) = eval.apply(playsingleapi.resources)
+          val Right((testSources, _)) = eval.apply(playsingleapi.test.sources)
+          val Right((testResources, _)) = eval.apply(playsingleapi.test.resources)
+          assert(
+            conf.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("conf"),
+            app.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("app"),
+            sources== app,
+            resources== conf,
+            testSources.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("test"),
+            testResources.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("test/resources")
+          )
+        }
+      }
+    }
+    test("compile") {
+      workspaceTest(playsingleapi) { eval =>
+        val eitherResult = eval.apply(playsingleapi.compile)
+        val Right((result, evalCount)) = eitherResult
+        val outputFiles = os.walk(result.classes.path).filter(os.isFile)
+        val expectedClassfiles = Seq[os.RelPath](
+          os.RelPath("controllers/HomeController.class"),
+          os.RelPath("controllers/ReverseAssets.class"),
+          os.RelPath("controllers/ReverseHomeController.class"),
+          os.RelPath("controllers/routes.class"),
+          os.RelPath("controllers/routes$javascript.class"),
+          os.RelPath("controllers/javascript/ReverseHomeController.class"),
+          os.RelPath("controllers/javascript/ReverseAssets.class"),
+          os.RelPath("router/Routes$$anonfun$routes$1.class"),
+          os.RelPath("router/Routes.class"),
+          os.RelPath("router/RoutesPrefix$.class"),
+          os.RelPath("router/RoutesPrefix.class")
+        ).map(
+          eval.outPath / "compile" / "dest" / "classes" / _
+        )
         assert(
-          result == "2.7.0",
+          result.classes.path == eval.outPath / "compile" / "dest" / "classes",
+          outputFiles.nonEmpty,
+          outputFiles.forall(expectedClassfiles.contains),
+          outputFiles.size == 11,
           evalCount > 0
         )
-      }
-    }
-    'layout - {
-      'fromBuild - workspaceTest(playsingleapi) { eval =>
-        val Right((conf, _)) = eval.apply(playsingleapi.conf)
-        val Right((app, _)) = eval.apply(playsingleapi.app)
-        val Right((sources, _)) = eval.apply(playsingleapi.sources)
-        val Right((resources, _)) = eval.apply(playsingleapi.resources)
-        val Right((testSources, _)) = eval.apply(playsingleapi.test.sources)
-        val Right((testResources, _)) = eval.apply(playsingleapi.test.resources)
-        assert(
-          conf.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("conf"),
-          app.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("app"),
-          sources== app,
-          resources== conf,
-          testSources.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("test"),
-          testResources.map(_.path.relativeTo(playsingleapi.millSourcePath).toString()) == Seq("test/resources")
-        )
-      }
-    }
-    'compile - workspaceTest(playsingleapi) { eval =>
-      val eitherResult = eval.apply(playsingleapi.compile)
-      val Right((result, evalCount)) = eitherResult
-      val outputFiles = os.walk(result.classes.path).filter(os.isFile)
-      val expectedClassfiles = Seq[os.RelPath](
-        os.RelPath("controllers/HomeController.class"),
-        os.RelPath("controllers/ReverseAssets.class"),
-        os.RelPath("controllers/ReverseHomeController.class"),
-        os.RelPath("controllers/routes.class"),
-        os.RelPath("controllers/routes$javascript.class"),
-        os.RelPath("controllers/javascript/ReverseHomeController.class"),
-        os.RelPath("controllers/javascript/ReverseAssets.class"),
-        os.RelPath("router/Routes$$anonfun$routes$1.class"),
-        os.RelPath("router/Routes.class"),
-        os.RelPath("router/RoutesPrefix$.class"),
-        os.RelPath("router/RoutesPrefix.class")
-      ).map(
-        eval.outPath / 'compile / 'dest / 'classes / _
-      )
-      assert(
-        result.classes.path == eval.outPath / 'compile / 'dest / 'classes,
-        outputFiles.nonEmpty,
-        outputFiles.forall(expectedClassfiles.contains),
-        outputFiles.size == 11,
-        evalCount > 0
-      )
 
-      val Right((_, unchangedEvalCount)) = eval.apply(playsingleapi.compile)
+        val Right((_, unchangedEvalCount)) = eval.apply(playsingleapi.compile)
 
-      assert(unchangedEvalCount == 0)
+        assert(unchangedEvalCount == 0)
+      }
     }
   }
 }
