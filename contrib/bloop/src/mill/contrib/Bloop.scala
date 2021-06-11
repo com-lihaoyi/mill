24e843ca033b2739b4375e0380c8490d93577106
Tobias Roeser <le.petit.fou@web.de>
2021-06-11 08:18:24 +0200

    Removed older deprecated API (#1364)
    Replaced deprecated API calls.
    
    Pull request:. https://github.com/com-lihaoyi/mill/pull/1364

--- contrib/bloop/src/mill/contrib/Bloop.scala
+++ contrib/bloop/src/mill/contrib/Bloop.scala
@@ -3,5 +3,5 @@ package mill.contrib
 import mill.eval.Evaluator
 import mill.contrib.bloop.BloopImpl
 
-@deprecated("Use mill.contrib.bloop.Bloop instead", since = "> 0.8.0")
+@deprecated("Use mill.contrib.bloop.Bloop instead", since = "mill after 0.8.0")
 object Bloop extends BloopImpl(Evaluator.currentEvaluator.get, os.pwd)
