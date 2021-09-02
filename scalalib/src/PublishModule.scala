4b177d050d191cf1c4ecd46a1b47262f79043f34
Tobias Roeser <le.petit.fou@web.de>
2021-09-02 11:02:29 +0200

    Add more information in default manifests

--- scalalib/src/PublishModule.scala
+++ scalalib/src/PublishModule.scala
@@ -4,6 +4,7 @@ package scalalib
 import mill.define.{Command, ExternalModule, Target, Task}
 import mill.api.PathRef
 import mill.main.Tasks
+import mill.modules.Jvm
 import mill.scalalib.publish.{Artifact, SonatypePublisher}
 
 /**
@@ -148,6 +149,17 @@ trait PublishModule extends JavaModule { outer =>
       awaitTimeout,
       stagingRelease
     ).publish(artifacts.map { case (a, b) => (a.path, b) }, artifactInfo, release)
+  }
+
+  override def manifest: T[Jvm.JarManifest] = T {
+    val pom = pomSettings()
+    super.manifest().add(
+      "Implementation-Version" -> publishVersion(),
+      "Implementation-Vendor-Id" -> pom.organization,
+      "Description" -> pom.description,
+      "URL" -> pom.url,
+      "Licenses" -> pom.licenses.map(l => s"${l.name} (${l.id})").mkString(",")
+    )
   }
 }
 
