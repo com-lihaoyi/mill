250437b110ea49b4c04b9a178299f820b80cff41
Tobias Roeser <le.petit.fou@web.de>
2021-09-28 08:29:52 +0200

    Fixed bootstrap tests
    We need to use the assembly instead of the script.
    If we use the script, it will read the `.mill-version` file and run with the wrong version.

--- ci/test-mill-bootstrap-0.sh
+++ ci/test-mill-bootstrap-0.sh
@@ -7,14 +7,14 @@ git stash -u
 git stash -a
 
 # First build
-./mill -i all __.publishLocal launcher
-cp out/launcher/dest/mill ~/mill-1
+./mill -i "{__.publishLocal,dev.assembly}"
+cp out/dev/assembly/dest/mill target/mill-1
 
 # Clean up
 git stash -u
 git stash -a
 
-rm -rf ~/.mill
+rm -rf ~/.mill/ammonite
 
 # Differentiate first and second builds
 git config --global user.name "Your Name"
@@ -24,17 +24,17 @@ echo "Build 2" > info.txt && git add info.txt && git commit -m "Add info.txt"
 ci/patch-mill-bootstrap.sh
 
 # Second build
-~/mill-1 -i all __.publishLocal launcher
-cp out/launcher/dest/mill ~/mill-2
+target/mill-1 -i "{__.publishLocal,dev.assembly}"
+cp out/dev/assembly/dest/mill target/mill-2
 
 # Clean up
 git stash -u
 git stash -a
 
-rm -rf ~/.mill
+rm -rf ~/.mill/ammonite
 
 # Patch local build
 ci/patch-mill-bootstrap.sh
 
 # Use second build to run tests using Mill
-~/mill-2 -i all {main,scalalib,scalajslib,scalanativelib,bsp}.__.test
+target/mill-2 -i all {main,scalalib,scalajslib,scalanativelib,bsp}.__.test
