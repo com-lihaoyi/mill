901301ad41425614a534c097781f76cc5b6558eb
Tobias Roeser <le.petit.fou@web.de>
2021-11-11 14:02:38 +0100

    Backported support for more mill-specifics in $ivy-import (#1561)
    Support $MILL_BIN_PLATFORM placeholder and new $ivy.`g::a::v` syntax (borrowed from Scala JS/Native dependency notation)
    
    Also updated Documentation and renamed two pages (Modules => Plugins).
    
    See pull request: https://github.com/com-lihaoyi/mill/pull/1485
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/1561

--- main/src/main/MillIvyHook.scala
+++ main/src/main/MillIvyHook.scala
@@ -3,15 +3,48 @@ import ammonite.runtime.ImportHook.BaseIvy
 import ammonite.runtime.ImportHook
 import java.io.File
 
+import mill.BuildInfo
+
 /**
- * Overrides the ivy hook to interpret $MILL_VERSION as the version of mill
- * the user runs.
+ * Overrides the ivy hook to customize the `$ivy`-import with mill specifics:
  *
- * Can be used to ensure loaded contrib modules keep up to date.
+ * - interpret `$MILL_VERSION` as the mill version
+ *
+ * - interpret `$MILL_BIN_PLATFORM` as the mill binary platform version
+ *
+  * - supports the format `org::name::version` for mill plugins;
+  *   which is equivalent to `org::name_mill$MILL_BIN_PLATFORM:version`
+  *
+  * - supports the format `org:::name::version` for mill plugins;
+  *   which is equivalent to `org:::name_mill$MILL_BIN_PLATFORM:version`
+  *
  */
-object MillIvyHook extends BaseIvy(plugin = false){
-  override def resolve(interp: ImportHook.InterpreterInterface,
-                       signatures: Seq[String])
-      : Either[String,(Seq[coursierapi.Dependency], Seq[File])] =
-    super.resolve(interp, signatures.map(_.replace("$MILL_VERSION", mill.BuildInfo.millVersion)))
+object MillIvyHook extends BaseIvy(plugin = false) {
+  override def resolve(
+      interp: ImportHook.InterpreterInterface,
+      signatures: Seq[String]
+  ): Either[String, (Seq[coursierapi.Dependency], Seq[File])] = {
+
+    // replace platform notation
+    val millSigs: Seq[String] = for (signature <- signatures) yield {
+      signature.split("[:]") match {
+        case Array(org, "", pname, "", version)
+            if org.length > 0 && pname.length > 0 && version.length > 0 =>
+          s"${org}::${pname}_mill$$MILL_BIN_PLATFORM:${version}"
+        case Array(org, "", "", pname, "", version)
+            if org.length > 0 && pname.length > 0 && version.length > 0 =>
+          s"${org}:::${pname}_mill$$MILL_BIN_PLATFORM:${version}"
+        case _ => signature
+      }
+    }
+    // replace variables
+    val replaced = millSigs.map(_
+      .replace("$MILL_VERSION", mill.BuildInfo.millVersion)
+      .replace("${MILL_VERSION}", mill.BuildInfo.millVersion)
+      .replace("$MILL_BIN_PLATFORM", mill.BuildInfo.millBinPlatform)
+      .replace("${MILL_BIN_PLATFORM}", mill.BuildInfo.millBinPlatform))
+
+    super.resolve(interp, replaced)
+  }
 }
+
