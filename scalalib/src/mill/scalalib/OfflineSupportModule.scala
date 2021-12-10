db422f7ee1af3a419328b1e0cee2528337963fea
Tobias Roeser <le.petit.fou@web.de>
2021-12-10 08:36:21 +0100

    Let prepareOffline accept a `all` flag (#1591)
    This should support more control in inheriting modules.
    
    Also, fetch only essential dependencies in Java and Scala modules,
    and keep the full fetch behind an `all` flag, which can be set as parameter.
    
    This is a binary-incompatible change and I have no idea how to preserve compatibility.
    Although I could keep the old signature and just forward to the newer,
    Loading plugins that also inherit older `OfflineSupportModule`s seem to fail nevertheless.
    Also I think Mill isn't checking for multiple commands with the same name, 
    so I fear just because it works on my machine, it may still lead to some failures for others.
    Only fix would be to invent a new name for the command (and deprecate the old).
    As Mill 0.10.x is binary-incompatible anyways, I think this one should be ok.
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/1591

--- scalalib/src/mill/scalalib/OfflineSupportModule.scala
+++ scalalib/src/mill/scalalib/OfflineSupportModule.scala
@@ -1,14 +1,16 @@
 package mill.scalalib
 
+import mainargs.Flag
 import mill.T
 import mill.define.Command
 
 trait OfflineSupportModule extends mill.Module {
 
   /**
-   * Prepare the module for working offline. This should typically fetch (missing) resources like ivy dependencies.
-   */
-  def prepareOffline(): Command[Unit] = T.command {
+    * Prepare the module for working offline. This should typically fetch (missing) resources like ivy dependencies.
+    * @param all If `true`, it also fetches resources not always needed.
+    */
+  def prepareOffline(all: Flag): Command[Unit] = T.command {
     // nothing to do
   }
 
