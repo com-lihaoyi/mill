1fa33ef9eeb52d23749d2e617e94723cd6a81325
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-03-19 15:01:02 +0100

    Fix #1788: Handle symbols in scripts imports

--- main/core/src/mill/internal/AmmoniteUtils.scala
+++ main/core/src/mill/internal/AmmoniteUtils.scala
@@ -3,11 +3,35 @@ package mill.internal
 @mill.api.internal
 private[mill] object AmmoniteUtils {
   def normalizeAmmoniteImportPath(segments: Seq[String]): Seq[String] = {
+    def normalized(segment: String): String = {
+      // Mapping replicated from the Scala compiler
+      // https://github.com/scala/scala/blob/8a2cf63ee5bad8c8c054f76464de0e10226516a0/src/library/scala/reflect/NameTransformer.scala#L45
+      segment.flatMap {
+        case '=' => "$eq"
+        case '<' => "$less"
+        case '-' => "$minus"
+        case '#' => "$hash"
+        case '?' => "$qmark"
+        case '+' => "$plus"
+        case '*' => "$times"
+        case '%' => "$percent"
+        case '&' => "$amp"
+        case '!' => "$bang"
+        case '|' => "$bar"
+        case '→' => "$u2192"
+        case '\\'=> "$bslash"
+        case ':' => "$colon"
+        case '~' => "$tilde"
+        case '/' => "$div"
+        case '>' => "$greater"
+        case c => c.toString
+      }
+    }
     def loop(l: List[String], up: Int): List[String] = l match {
       case ("^" | "$up") :: tail => loop(tail, up + 1)
       case l @ "$file" :: "ammonite" :: Nil => List.fill(up)("$up") ::: l
       case head :: tail if up > 0 => loop(tail, up - 1)
-      case head :: tail => head :: loop(tail, up)
+      case head :: tail => normalized(head) :: loop(tail, up)
       case Nil => Nil
     }
     val reversed = segments.reverse.toList
