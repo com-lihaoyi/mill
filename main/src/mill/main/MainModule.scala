f873f16b85223ad0aa1ee39fa951cda65411c892
Tobias Roeser <le.petit.fou@web.de>
2022-04-07 20:37:25 +0200

    Avoid changing the return type

--- main/src/mill/main/MainModule.scala
+++ main/src/mill/main/MainModule.scala
@@ -81,10 +81,10 @@ object MainModule {
 trait MainModule extends mill.Module {
 
   implicit def millDiscover: mill.define.Discover[_]
-  implicit def millScoptTasksReads[T]: mainargs.TokensReader[Tasks[T]] =
-    new mill.main.Tasks.Scopt[T]()
-  implicit def millScoptEvaluatorReads[T]: TokensReader[Evaluator] =
-    new mill.main.EvaluatorScopt[T]()
+  // TODO: change return type to mainargs.TokensReader[Tasks[T]] when in 0.11 milestone
+  implicit def millScoptTasksReads[T]: Tasks.Scopt[T] = new mill.main.Tasks.Scopt[T]()
+  // TODO: change return type to mainargs.TokensReader[Evaluator] when in 0.11 milestone
+  implicit def millScoptEvaluatorReads[T]: EvaluatorScopt[T] = new mill.main.EvaluatorScopt[T]()
   implicit def taskTokensReader[T](implicit
       tokensReaderOfT: TokensReader[T]
   ): TokensReader[Task[T]] = new TaskScopt[T](tokensReaderOfT)
