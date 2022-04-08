ecc3272ea8e3e919d0a80cf766157d16528247a1
Tobias Roeser <le.petit.fou@web.de>
2022-04-09 00:08:27 +0200

    Updated workspace path

--- integration/local/src/IntegrationTestSuite.scala
+++ integration/local/src/IntegrationTestSuite.scala
@@ -7,7 +7,7 @@ abstract class IntegrationTestSuite(override val workspaceSlug: String, fork: Bo
     extends ScriptTestSuite(fork) {
 
   override def workspacePath: os.Path =
-    os.Path(sys.props.getOrElse("MILL_WORKSPACE_PATH", ???)) / getClass().getName()
+    os.Path(sys.props.getOrElse("MILL_WORKSPACE_PATH", ???)) / workspaceSlug
 
   override def scriptSourcePath: os.Path =
     os.pwd / "integration" / "local" / "resources" / workspaceSlug
--- integration/test/src/IntegrationTestSuite.scala
+++ integration/test/src/IntegrationTestSuite.scala
@@ -6,12 +6,15 @@ import utest._
 abstract class IntegrationTestSuite(repoKey: String, val workspaceSlug: String, fork: Boolean)
     extends ScriptTestSuite(fork) {
   val buildFilePath = os.pwd / "integration" / "test" / "resources" / workspaceSlug
+  override def workspacePath: os.Path =
+    os.Path(sys.props.getOrElse("MILL_WORKSPACE_PATH", ???)) / workspaceSlug
+
   def scriptSourcePath = {
     // The unzipped git repo snapshots we get from github come with a
     // wrapper-folder inside the zip file, so copy the wrapper folder to the
     // destination instead of the folder containing the wrapper.
 
-    val path = sys.props(repoKey)
+    val path = sys.props.getOrElse(repoKey, ???)
     val Seq(wrapper) = os.list(os.Path(path))
     wrapper
   }
