d90e7f23ca991a4220265e7b0e25d4bdb8ed664c
Koji AGAWA <atty303@gmail.com>
2022-04-26 17:10:20 +0900

    added BSP JVM Extension support

--- bsp/src/mill/bsp/MillBuildServer.scala
+++ bsp/src/mill/bsp/MillBuildServer.scala
@@ -216,6 +216,8 @@ class MillBuildServer(
         false
       )
       capabilities.setCanReload(canReload)
+      capabilities.setJvmRunEnvironmentProvider(true)
+      capabilities.setJvmTestEnvironmentProvider(true)
 
       request.getData match {
         case d: JsonObject =>
