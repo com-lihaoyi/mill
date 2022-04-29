340a2c5fab6e62a73614c734a00d4ecca4078321
Tobias Roeser <le.petit.fou@web.de>
2022-04-29 17:25:40 +0200

    .

--- integration/thirdparty/local/src/JawnTests.scala
+++ integration/thirdparty/local/src/JawnTests.scala
@@ -8,21 +8,27 @@ class JawnTests(fork: Boolean) extends IntegrationTestSuite("MILL_JAWN_REPO", "j
     initWorkspace()
 
     def check(scalaVersion: String) = TestUtil.disableInJava9OrAbove("Old tests don't work") {
-      val firstCompile = eval(s"jawn[$scalaVersion].parser.test")
+      val tccl = Thread.currentThread().getContextClassLoader()
+      try {
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader())
+        val firstCompile = eval(s"jawn[$scalaVersion].parser.test")
 
-      assert(
-        firstCompile,
-        os.walk(workspacePath).exists(_.last == "AsyncParser.class"),
-        os.walk(workspacePath).exists(_.last == "CharBuilderSpec.class")
-      )
+        assert(
+          firstCompile,
+          os.walk(workspacePath).exists(_.last == "AsyncParser.class"),
+          os.walk(workspacePath).exists(_.last == "CharBuilderSpec.class")
+        )
 
-      for (scalaFile <- os.walk(workspacePath).filter(_.ext == "scala")) {
-        os.write.append(scalaFile, "\n}")
+        for (scalaFile <- os.walk(workspacePath).filter(_.ext == "scala")) {
+          os.write.append(scalaFile, "\n}")
+        }
+
+        val brokenCompile = eval(s"jawn[$scalaVersion].parser.test")
+
+        assert(!brokenCompile)
+      } finally {
+        Thread.currentThread().setContextClassLoader(tccl)
       }
-
-      val brokenCompile = eval(s"jawn[$scalaVersion].parser.test")
-
-      assert(!brokenCompile)
     }
 
     "scala21111" - check("2.11.11")
