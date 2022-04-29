0534bbfc8644f461d19a41b2a1279f643f336ece
Tobias Roeser <le.petit.fou@web.de>
2022-04-29 16:49:37 +0200

    Set the thread context class loader

--- integration/thirdparty/local/src/AcyclicTests.scala
+++ integration/thirdparty/local/src/AcyclicTests.scala
@@ -8,20 +8,27 @@ class AcyclicTests(fork: Boolean)
     initWorkspace()
 
     def check(scalaVersion: String) = {
-      val firstCompile = eval(s"acyclic[$scalaVersion].compile")
+      val tccl = Thread.currentThread().getContextClassLoader()
+      try {
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader())
 
-      assert(
-        firstCompile,
-        os.walk(workspacePath).exists(_.last == "GraphAnalysis.class"),
-        os.walk(workspacePath).exists(_.last == "PluginPhase.class")
-      )
-      for (scalaFile <- os.walk(workspacePath).filter(_.ext == "scala")) {
-        os.write.append(scalaFile, "\n}")
+        val firstCompile = eval(s"acyclic[$scalaVersion].compile")
+
+        assert(
+          firstCompile,
+          os.walk(workspacePath).exists(_.last == "GraphAnalysis.class"),
+          os.walk(workspacePath).exists(_.last == "PluginPhase.class")
+        )
+        for (scalaFile <- os.walk(workspacePath).filter(_.ext == "scala")) {
+          os.write.append(scalaFile, "\n}")
+        }
+
+        val brokenCompile = eval(s"acyclic[$scalaVersion].compile")
+
+        assert(!brokenCompile)
+      } finally {
+        Thread.currentThread().setContextClassLoader(tccl)
       }
-
-      val brokenCompile = eval(s"acyclic[$scalaVersion].compile")
-
-      assert(!brokenCompile)
     }
 
     "scala2118" - mill.util.TestUtil.disableInJava9OrAbove("Scala 2.11 not supported")(check("2.11.8"))
