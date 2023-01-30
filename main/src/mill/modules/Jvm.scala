8ef23dc64c8557c834410332d45e18e8be345db2
Tobias Roeser <le.petit.fou@web.de>
2023-01-30 21:07:26 +0100

    Added another retry candidate for coursier resolution (Backport of #2287) (#2296)
    Original pull request: https://github.com/com-lihaoyi/mill/pull/2287
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/2296

--- main/src/mill/modules/Jvm.scala
+++ main/src/mill/modules/Jvm.scala
@@ -534,6 +534,12 @@ object Jvm {
           ))
           Thread.sleep(CoursierRetryWait)
           retry(retryCount - 1, ctx, errorMsgExtractor)(f)
+        } else if (errors.exists(e => e.contains("download error"))) {
+          ctx.foreach(_.log.debug(
+            s"Detected a download error by coursier. Attempting a retry (${retryCount} left)"
+          ))
+          Thread.sleep(CoursierRetryWait)
+          retry(retryCount - 1, ctx, errorMsgExtractor)(f)
         } else res
       case r => r.get
     }
