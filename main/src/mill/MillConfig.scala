c03b8b5ab24d144637d3fe820e24aee66b8d1228
Tobias Roeser <le.petit.fou@web.de>
2023-02-14 14:30:09 +0100

    Tried to restore initial order

--- main/src/mill/MillConfig.scala
+++ main/src/mill/MillConfig.scala
@@ -4,42 +4,55 @@ import mainargs.{Flag, Leftover, arg}
 
 case class MillConfig(
     @arg(
+      short = 'h',
+      doc =
+        "(internal) The home directory of internally used Ammonite script engine; " +
+          "where it looks for config and caches."
+    )
+    home: os.Path = mill.api.Ctx.defaultHome,
+    @arg(
+      doc = "Run Mill in interactive mode and start a build REPL. " +
+        "This implies --no-server and no mill server will be used. Must be the first argument."
+    )
+    repl: Flag,
+    @arg(
+      name = "no-server",
+      doc =
+        "Run Mill in single-process mode. In this mode, no mill server will be started or used. " +
+          "Must be the first argument."
+    )
+    noServer: Flag,
+    @arg(doc = """Enable BSP server mode.""")
+    bsp: Flag,
+    @arg(name = "version", short = 'v', doc = "Show mill version and exit.")
+    showVersion: Flag,
+    @arg(
       name = "bell",
       short = 'b',
       doc =
         "Ring the bell once if the run completes successfully, twice if it fails."
     )
     ringBell: Flag,
-    @arg(doc = """Enable BSP server mode.""")
-    bsp: Flag,
-    @arg(
-      name = "define",
-      short = 'D',
-      doc = "Define (or overwrite) a system property"
-    )
-    extraSystemProperties: Map[String, String],
-    @arg(
-      short = 'i',
-      doc =
-        "Run Mill in interactive mode, suitable for opening REPLs and taking user input. " +
-          "This implies --no-server and no mill server will be used. Must be the first argument."
-    )
-    interactive: Flag,
-    @arg(name = "debug", short = 'd', doc = "Show debug output on STDOUT")
-    debugLog: Flag,
     @arg(
       name = "disable-ticker",
       doc =
         "Disable ticker log (e.g. short-lived prints of stages and progress bars)"
     )
     disableTicker: Flag,
-    @arg(doc = "Print this message")
-    help: Flag,
+    @arg(name = "debug", short = 'd', doc = "Show debug output on STDOUT")
+    debugLog: Flag,
     @arg(
-      name = "import",
-      doc = "Additional ivy dependencies to load into mill, e.g. plugins."
+      name = "keep-going",
+      short = 'k',
+      doc = "Continue build, even after build failures."
     )
-    imports: Seq[String],
+    keepGoing: Flag,
+    @arg(
+      name = "define",
+      short = 'D',
+      doc = "Define (or overwrite) a system property"
+    )
+    extraSystemProperties: Map[String, String],
     @arg(
       name = "jobs",
       short = 'j',
@@ -49,46 +62,35 @@ case class MillConfig(
     )
     threadCountRaw: Option[Int],
     @arg(
-      name = "keep-going",
-      short = 'k',
-      doc = "Continue build, even after build failures."
+      name = "import",
+      doc = "Additional ivy dependencies to load into mill, e.g. plugins."
     )
-    keepGoing: Flag,
+    imports: Seq[String],
     @arg(
-      name = "no-server",
+      short = 'i',
       doc =
-        "Run Mill in single-process mode. In this mode, no mill server will be started or used. " +
-          "Must be the first argument."
+        "Run Mill in interactive mode, suitable for opening REPLs and taking user input. " +
+          "This implies --no-server and no mill server will be used. Must be the first argument."
     )
-    noServer: Flag,
-    @arg(
-      doc = "Run Mill in interactive mode and start a build REPL. " +
-        "This implies --no-server and no mill server will be used. Must be the first argument."
-    )
-    repl: Flag,
+    interactive: Flag,
+    @arg(doc = "Print this message")
+    help: Flag,
     @arg(
       short = 'w',
       doc = "Watch and re-run your scripts when they change"
     )
     watch: Flag,
-    @arg(name = "version", short = 'v', doc = "Show mill version and exit.")
-    showVersion: Flag,
-    @arg(
-      short = 'h',
-      doc =
-        "(internal) The home directory of internally used Ammonite script engine; " +
-          "where it looks for config and caches."
-    )
-    home: os.Path = mill.api.Ctx.defaultHome,
     @arg(
       short = 's',
       doc =
         "Make ivy logs during script import resolution go silent instead of printing; " +
-           "though failures will still throw exception")
+          "though failures will still throw exception"
+    )
     silent: Flag,
     @arg(
       name = "no-default-predef",
-      doc = "Disable the default predef and run Mill with the minimal predef possible")
+      doc = "Disable the default predef and run Mill with the minimal predef possible"
+    )
     noDefaultPredef: Flag,
     @arg(
       name = "rest",
