1bf2ecfef282379eb94949b7dc71a8647c15f171
Tobias Roeser <le.petit.fou@web.de>
2023-02-18 11:28:16 +0100

    Make MillConfig a non-case class to allow later bincompat extensions

--- main/src/mill/MillConfig.scala
+++ main/src/mill/MillConfig.scala
@@ -2,21 +2,21 @@ package mill
 
 import mainargs.{Flag, Leftover, arg}
 
-case class MillConfig(
+class MillConfig private (
     @arg(
       short = 'h',
       doc =
         """(internal) The home directory of internally used Ammonite script engine;
            where it looks for config and caches."""
     )
-    home: os.Path = mill.api.Ctx.defaultHome,
+    val home: os.Path,
     @arg(
       doc =
         """Run Mill in interactive mode and start a build REPL.
            This implies --no-server and no mill server will be used.
            Must be the first argument."""
     )
-    repl: Flag,
+    val repl: Flag,
     @arg(
       name = "no-server",
       doc =
@@ -24,37 +24,37 @@ case class MillConfig(
            In this mode, no mill server will be started or used. 
            Must be the first argument."""
     )
-    noServer: Flag,
+    val noServer: Flag,
     @arg(doc = """Enable BSP server mode.""")
-    bsp: Flag,
+    val bsp: Flag,
     @arg(name = "version", short = 'v', doc = "Show mill version and exit.")
-    showVersion: Flag,
+    val showVersion: Flag,
     @arg(
       name = "bell",
       short = 'b',
       doc = """Ring the bell once if the run completes successfully, twice if it fails."""
     )
-    ringBell: Flag,
+    val ringBell: Flag,
     @arg(
       name = "disable-ticker",
       doc =
         """Disable ticker log (e.g. short-lived prints of stages and progress bars)."""
     )
-    disableTicker: Flag,
+    val disableTicker: Flag,
     @arg(name = "debug", short = 'd', doc = "Show debug output on STDOUT")
-    debugLog: Flag,
+    val debugLog: Flag,
     @arg(
       name = "keep-going",
       short = 'k',
       doc = """Continue build, even after build failures."""
     )
-    keepGoing: Flag,
+    val keepGoing: Flag,
     @arg(
       name = "define",
       short = 'D',
       doc = """Define (or overwrite) a system property."""
     )
-    extraSystemProperties: Map[String, String],
+    val extraSystemProperties: Map[String, String],
     @arg(
       name = "jobs",
       short = 'j',
@@ -62,46 +62,88 @@ case class MillConfig(
         """Allow processing N targets in parallel. 
            Use 1 to disable parallel and 0 to use as much threads as available processors."""
     )
-    threadCountRaw: Option[Int],
+    val threadCountRaw: Option[Int],
     @arg(
       name = "import",
       doc = """Additional ivy dependencies to load into mill, e.g. plugins."""
     )
-    imports: Seq[String],
+    val imports: Seq[String],
     @arg(
       short = 'i',
       doc =
         """Run Mill in interactive mode, suitable for opening REPLs and taking user input.
           This implies --no-server and no mill server will be used. Must be the first argument."""
     )
-    interactive: Flag,
+    val interactive: Flag,
     @arg(doc = "Print this help message.")
-    help: Flag,
+    val help: Flag,
     @arg(
       short = 'w',
       doc = """Watch and re-run your scripts when they change."""
     )
-    watch: Flag,
+    val watch: Flag,
     @arg(
       short = 's',
       doc =
         """Make ivy logs during script import resolution go silent instead of printing; 
            though failures will still throw exception."""
     )
-    silent: Flag,
+    val silent: Flag,
     @arg(
       name = "no-default-predef",
       doc = """Disable the default predef and run Mill with the minimal predef possible."""
     )
-    noDefaultPredef: Flag,
+    val noDefaultPredef: Flag,
     @arg(
       name = "rest",
       doc =
         """The name of the targets you want to build, 
            followed by any parameters you wish to pass to those targets."""
     )
-    leftoverArgs: Leftover[String]
+    val leftoverArgs: Leftover[String]
 )
+
+object MillConfig {
+  def apply(
+      home: os.Path = mill.api.Ctx.defaultHome,
+      repl: Flag = Flag(),
+      noServer: Flag = Flag(),
+      bsp: Flag = Flag(),
+      showVersion: Flag = Flag(),
+      ringBell: Flag = Flag(),
+      disableTicker: Flag = Flag(),
+      debugLog: Flag = Flag(),
+      keepGoing: Flag = Flag(),
+      extraSystemProperties: Map[String, String] = Map(),
+      threadCountRaw: Option[Int] = None,
+      imports: Seq[String] = Seq(),
+      interactive: Flag = Flag(),
+      help: Flag = Flag(),
+      watch: Flag = Flag(),
+      silent: Flag = Flag(),
+      noDefaultPredef: Flag = Flag(),
+      leftoverArgs: Leftover[String] = Leftover()
+  ): MillConfig = new MillConfig(
+    home = home,
+    repl = repl,
+    noServer = noServer,
+    bsp = bsp,
+    showVersion = showVersion,
+    ringBell = ringBell,
+    disableTicker = disableTicker,
+    debugLog = debugLog,
+    keepGoing = keepGoing,
+    extraSystemProperties = extraSystemProperties,
+    threadCountRaw = threadCountRaw,
+    imports = imports,
+    interactive = interactive,
+    help = help,
+    watch = watch,
+    silent = silent,
+    noDefaultPredef = noDefaultPredef,
+    leftoverArgs = leftoverArgs
+  )
+}
 
 import mainargs.ParserForClass
 import ammonite.repl.tools.Util.PathRead
