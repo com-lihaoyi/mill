c46a7015d35978768aa40038541a1a970740512e
Tobias Roeser <le.petit.fou@web.de>
2023-03-07 16:44:23 +0100

    Revert SingleModule deprecation

--- contrib/playlib/src/mill/playlib/SingleModule.scala
+++ contrib/playlib/src/mill/playlib/SingleModule.scala
@@ -2,5 +2,6 @@ package mill.playlib
 
 import mill.define.Module
 
-@deprecated("This module doesn't do anything now. You can avoid extending it", since = "mill 0.11.0-M3")
-trait SingleModule extends Module
+trait SingleModule extends Module {
+  override def millSourcePath: os.Path = super.millSourcePath / os.up
+}
