c6240d882efc0d7adf9a5d0ab5813759cbf1c454
Li Haoyi <haoyi.sg@gmail.com>
2023-03-27 18:43:01 +0800

    simplify MultiLevelBuildTests

--- integration/src/mill/integration/MultiLevelBuildTests.scala
+++ integration/src/mill/integration/MultiLevelBuildTests.scala
@@ -4,6 +4,10 @@ import utest._
 
 import scala.util.matching.Regex
 
+// Cause various kinds of changes - valid, parse errors, compile errors,
+// runtime errors - in various levels of build.sc meta-builds, ensuring
+// that the proper messages are reported and the proper files end up
+// getting watched in all cases.
 class MultiLevelBuildTests(fork: Boolean, clientServer: Boolean)
     extends IntegrationTestSuite("multi-level-build", fork, clientServer) {
   val tests = Tests {
@@ -54,247 +58,243 @@ class MultiLevelBuildTests(fork: Boolean, clientServer: Boolean)
 
       assert(expected == millWatchedPaths)
     }
-    // Cause various kinds of changes - valid, parse errors, compile errors,
-    // runtime errors - in various levels of build.sc meta-builds, ensuring
-    // that the proper messages are reported and the proper files end up
-    // getting watched in all cases.
-    test("multiLevelEditing") {
-      def evalCheckErr(expected: String*) = {
-        val res = evalStdout("foo.run")
-        assert(res.isSuccess == false)
-        // Prepend a "\n" to allow callsites to use "\n" to test for start of
-        // line, even though the first line doesn't have a "\n" at the start
-        val err = "\n" + res.err
-        for(e <- expected){
-          assert(err.contains(e))
-        }
+
+    def evalCheckErr(expected: String*) = {
+      val res = evalStdout("foo.run")
+      assert(res.isSuccess == false)
+      // Prepend a "\n" to allow callsites to use "\n" to test for start of
+      // line, even though the first line doesn't have a "\n" at the start
+      val err = "\n" + res.err
+      for (e <- expected) {
+        assert(err.contains(e))
       }
-      
-      test("valid"){
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+    }
 
-        mangleFile(wsRoot / "foo"  / "src" / "Example.scala", _.replace("!", "?"))
+    test("validEdits"){
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(wsRoot / "foo"  / "src" / "Example.scala", _.replace("!", "?"))
 
-        mangleFile(wsRoot / "build.sc", _.replace("hello", "HELLO"))
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>HELLO</h1><p>world</p><p>0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(wsRoot / "build.sc", _.replace("hello", "HELLO"))
 
-        mangleFile(
-          wsRoot / "mill-build" / "build.sc",
-          _.replace("def scalatagsVersion = ", "def scalatagsVersion = \"changed-\" + ")
-        )
+      runAssertSuccess("<h1>HELLO</h1><p>world</p><p>0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(
+        wsRoot / "mill-build" / "build.sc",
+        _.replace("def scalatagsVersion = ", "def scalatagsVersion = \"changed-\" + ")
+      )
 
-        mangleFile(
-          wsRoot / "mill-build" / "mill-build" / "build.sc",
-          _.replace("0.8.2", "0.12.0")
-        )
+      runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.12.0</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(
+        wsRoot / "mill-build" / "mill-build" / "build.sc",
+        _.replace("0.8.2", "0.12.0")
+      )
 
-        mangleFile(
-          wsRoot / "mill-build" / "mill-build" / "build.sc",
-          _.replace("0.12.0", "0.8.2")
-        )
+      runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.12.0</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(
+        wsRoot / "mill-build" / "mill-build" / "build.sc",
+        _.replace("0.12.0", "0.8.2")
+      )
 
-        mangleFile(
-          wsRoot / "mill-build" / "build.sc",
-          _.replace("def scalatagsVersion = \"changed-\" + ", "def scalatagsVersion = ")
-        )
+      runAssertSuccess("<h1>HELLO</h1><p>world</p><p>changed-0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>HELLO</h1><p>world</p><p>0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(
+        wsRoot / "mill-build" / "build.sc",
+        _.replace("def scalatagsVersion = \"changed-\" + ", "def scalatagsVersion = ")
+      )
 
-        mangleFile(wsRoot / "build.sc", _.replace("HELLO", "hello"))
+      runAssertSuccess("<h1>HELLO</h1><p>world</p><p>0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>?")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      mangleFile(wsRoot / "build.sc", _.replace("HELLO", "hello"))
 
-        mangleFile(wsRoot / "foo"  / "src" / "Example.scala", _.replace("?", "!"))
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>?")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
-      }
+      mangleFile(wsRoot / "foo"  / "src" / "Example.scala", _.replace("?", "!"))
 
-      test("parseError") {
-        def causeParseError(p: os.Path) =
-          mangleFile(p, _.replace("extends", "extendx"))
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+    }
 
-        def fixParseError(p: os.Path) =
-          mangleFile(p, _.replace("extendx", "extends"))
+    test("parseErrorEdits") {
+      def causeParseError(p: os.Path) =
+        mangleFile(p, _.replace("extends", "extendx"))
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      def fixParseError(p: os.Path) =
+        mangleFile(p, _.replace("extendx", "extends"))
 
-        causeParseError(wsRoot / "build.sc")
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        evalCheckErr(
-          "\n1 targets failed",
-          "\ngenerateScriptSources build.sc"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      causeParseError(wsRoot / "build.sc")
 
-        causeParseError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "\ngenerateScriptSources build.sc"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
 
-        evalCheckErr(
-          "\n1 targets failed",
-          "\ngenerateScriptSources mill-build/build.sc"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      causeParseError(wsRoot / "mill-build" / "build.sc")
 
-        causeParseError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "\ngenerateScriptSources mill-build/build.sc"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        evalCheckErr(
-          "\n1 targets failed",
-          "\ngenerateScriptSources mill-build/mill-build/build.sc"
-        )
-        checkWatchedFiles(buildPaths3)
+      causeParseError(wsRoot / "mill-build" / "mill-build" / "build.sc")
 
-        fixParseError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "\ngenerateScriptSources mill-build/mill-build/build.sc"
+      )
+      checkWatchedFiles(buildPaths3)
 
-        evalCheckErr(
-          "\n1 targets failed",
-          "\ngenerateScriptSources mill-build/build.sc"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      fixParseError(wsRoot / "mill-build" / "mill-build" / "build.sc")
 
-        fixParseError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "\ngenerateScriptSources mill-build/build.sc"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        evalCheckErr(
-          "\n1 targets failed",
-          "\ngenerateScriptSources build.sc"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      fixParseError(wsRoot / "mill-build" / "build.sc")
 
-        fixParseError(wsRoot / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "\ngenerateScriptSources build.sc"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
-      }
+      fixParseError(wsRoot / "build.sc")
 
-      test("compileError") {
-        def causeCompileError(p: os.Path) =
-          mangleFile(p, _ + "\nimport doesnt.exist")
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+    }
 
-        def fixCompileError(p: os.Path) =
-          mangleFile(p, _.replace("import doesnt.exist", ""))
+    test("compileErrorEdits") {
+      def causeCompileError(p: os.Path) =
+        mangleFile(p, _ + "\nimport doesnt.exist")
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      def fixCompileError(p: os.Path) =
+        mangleFile(p, _.replace("import doesnt.exist", ""))
 
-        causeCompileError(wsRoot / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          // Ensure the file path in the compile error is properly adjusted to point
-          // at the original source file and not the generated file
-          s"$wsRoot/build.sc",
-          "not found: value doesnt"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        causeCompileError(wsRoot / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          s"$wsRoot/mill-build/build.sc",
-          "not found: value doesnt"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      causeCompileError(wsRoot / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        // Ensure the file path in the compile error is properly adjusted to point
+        // at the original source file and not the generated file
+        s"$wsRoot/build.sc",
+        "not found: value doesnt"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
 
-        causeCompileError(wsRoot / "mill-build" / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          s"$wsRoot/mill-build/mill-build/build.sc",
-          "not found: value doesnt"
-        )
-        checkWatchedFiles(buildPaths3)
+      causeCompileError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        s"$wsRoot/mill-build/build.sc",
+        "not found: value doesnt"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        fixCompileError(wsRoot / "mill-build" / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          s"$wsRoot/mill-build/build.sc",
-          "not found: value doesnt"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      causeCompileError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        s"$wsRoot/mill-build/mill-build/build.sc",
+        "not found: value doesnt"
+      )
+      checkWatchedFiles(buildPaths3)
 
-        fixCompileError(wsRoot / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          s"$wsRoot/build.sc",
-          "not found: value doesnt"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      fixCompileError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        s"$wsRoot/mill-build/build.sc",
+        "not found: value doesnt"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        fixCompileError(wsRoot / "build.sc")
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
-      }
+      fixCompileError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        s"$wsRoot/build.sc",
+        "not found: value doesnt"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
 
-      test("runtimeError") {
-        val runErrorSnippet = """{ override def runClasspath = T{ (throw new Exception("boom")): Seq[PathRef] }"""
+      fixCompileError(wsRoot / "build.sc")
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+    }
 
-        def causeRuntimeError(p: os.Path) =
-          mangleFile(p, _.replaceFirst("\\{", runErrorSnippet))
+    test("runtimeErrorEdits") {
+      val runErrorSnippet = """{ override def runClasspath = T{ (throw new Exception("boom")): Seq[PathRef] }"""
 
-        def fixRuntimeError(p: os.Path) =
-          mangleFile(p, _.replaceFirst(Regex.quote(runErrorSnippet), "\\{"))
+      def causeRuntimeError(p: os.Path) =
+        mangleFile(p, _.replaceFirst("\\{", runErrorSnippet))
 
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+      def fixRuntimeError(p: os.Path) =
+        mangleFile(p, _.replaceFirst(Regex.quote(runErrorSnippet), "\\{"))
 
-        causeRuntimeError(wsRoot / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          "foo.runClasspath java.lang.Exception: boom"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
 
-        causeRuntimeError(wsRoot / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          "build.sc",
-          "runClasspath java.lang.Exception: boom"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      causeRuntimeError(wsRoot / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "foo.runClasspath java.lang.Exception: boom"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
 
-        causeRuntimeError(wsRoot / "mill-build" / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          "build.sc",
-          "runClasspath java.lang.Exception: boom"
-        )
-        checkWatchedFiles(buildPaths3)
+      causeRuntimeError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "build.sc",
+        "runClasspath java.lang.Exception: boom"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        fixRuntimeError(wsRoot / "mill-build" / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          "build.sc",
-          "runClasspath java.lang.Exception: boom"
-        )
-        checkWatchedFiles(buildPaths2, buildPaths3)
+      causeRuntimeError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "build.sc",
+        "runClasspath java.lang.Exception: boom"
+      )
+      checkWatchedFiles(buildPaths3)
 
-        fixRuntimeError(wsRoot / "mill-build" / "build.sc")
-        evalCheckErr(
-          "\n1 targets failed",
-          "build.sc",
-          "foo.runClasspath java.lang.Exception: boom"
-        )
-        checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+      fixRuntimeError(wsRoot / "mill-build" / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "build.sc",
+        "runClasspath java.lang.Exception: boom"
+      )
+      checkWatchedFiles(buildPaths2, buildPaths3)
 
-        fixRuntimeError(wsRoot / "build.sc")
-        runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
-        checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
-      }
+      fixRuntimeError(wsRoot / "mill-build" / "build.sc")
+      evalCheckErr(
+        "\n1 targets failed",
+        "build.sc",
+        "foo.runClasspath java.lang.Exception: boom"
+      )
+      checkWatchedFiles(buildPaths, buildPaths2, buildPaths3)
+
+      fixRuntimeError(wsRoot / "build.sc")
+      runAssertSuccess("<h1>hello</h1><p>world</p><p>0.8.2</p>!")
+      checkWatchedFiles(fooPaths, buildPaths, buildPaths2, buildPaths3)
+
     }
   }
 }
