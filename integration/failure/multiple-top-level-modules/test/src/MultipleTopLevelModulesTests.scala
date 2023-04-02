c89d75d4c8128147ebbd04c84af08b18595f2b51
Li Haoyi <haoyi.sg@gmail.com>
2023-04-02 10:42:37 +0800

    fix

--- integration/failure/multiple-top-level-modules/test/src/MultipleTopLevelModulesTests.scala
+++ integration/failure/multiple-top-level-modules/test/src/MultipleTopLevelModulesTests.scala
@@ -11,7 +11,7 @@ object MultipleTopLevelModulesTests extends IntegrationTestSuite {
     test("success"){
       val res = evalStdout("resolve", "_")
       assert(!res.isSuccess)
-      assert(res.err.contains("Only one BaseModule can be defined in a build, not 2: millbuild.build$bar$,millbuild.build$foo$"))
+      assert(res.err.contains("Only one RootModule can be defined in a build, not 2: millbuild.build$bar$,millbuild.build$foo$"))
     }
   }
 }
