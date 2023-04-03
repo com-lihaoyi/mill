f8d7a78d66ac012ed3625cb8eacbcf0a406f5a8b
Li Haoyi <haoyi.sg@gmail.com>
2023-04-03 13:56:03 +0800

    comments

--- scalalib/src/mill/scalalib/CrossScalaModule.scala
+++ scalalib/src/mill/scalalib/CrossScalaModule.scala
@@ -4,7 +4,7 @@ import mill.api.PathRef
 import mill.T
 
 /**
- * A [[ScalaModule]] with is suited to be used with [[mill.define.Cross]].
+ * A [[ScalaModule]] which is suited to be used with [[mill.define.Cross]].
  * It supports additional source directories with the scala version pattern as suffix (`src-{scalaversionprefix}`),
  * e.g.
  * - src
--- scalalib/src/mill/scalalib/PlatformScalaModule.scala
+++ scalalib/src/mill/scalalib/PlatformScalaModule.scala
@@ -1,7 +1,17 @@
 package mill.scalalib
 import mill._
 
-
+/**
+ * A [[ScalaModule]] intended for defining `.jvm`/`.js`/`.native` submodules
+ * It supports additional source directories per platform, e.g. `src-jvm/` or
+ * `src-js/` and can be used inside a [[CrossScalaModule.Wrapper]], to get one
+ * source folder per platform per version e.g. `src-2.12-jvm/`.
+ *
+ * Adjusts the [[millSourcePath]] and [[artifactNameParts]] to ignore the last
+ * path segment, which is assumed to be the name of the platform the module is
+ * built against and not something that should affect the filesystem path or
+ * artifact name
+ */
 trait PlatformScalaModule extends ScalaModule{
   override def millSourcePath = super.millSourcePath / os.up
 
