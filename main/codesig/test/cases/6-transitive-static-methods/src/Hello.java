2f71df05750800f853c2e127c7113c9d450ab28a
Li Haoyi <haoyi.sg@gmail.com>
2023-04-06 11:24:01 +0800

    break up transitive methods test

--- main/codesig/test/cases/6-transitive-static-methods/src/Hello.java
+++ main/codesig/test/cases/6-transitive-static-methods/src/Hello.java
@@ -1,7 +1,7 @@
 package hello;
 public class Hello{
     public static int main(){
-        return usedStatic() + new Hello().used();
+        return usedStatic();
     }
 
     public static int unusedStatic(){ return usedTransitiveStatic(); }
@@ -10,32 +10,12 @@ public class Hello{
 
     public static int usedTransitiveStatic(){ return 2 + Hello.usedDeepestStatic(); }
 
-    public int unused(){ return usedTransitive(); }
-
-    public int used(){ return usedTransitive(); }
-
-    public int usedTransitive(){ return 2 + Hello.usedDeepestStatic(); }
-
     public static int usedDeepestStatic(){ return 3; }
 }
 
 /* EXPECTED TRANSITIVE
 {
-    "hello.Hello#unused()I": [
-        "hello.Hello#usedTransitive()I",
-        "hello.Hello.usedDeepestStatic()I"
-    ],
-    "hello.Hello#used()I": [
-        "hello.Hello#usedTransitive()I",
-        "hello.Hello.usedDeepestStatic()I"
-    ],
-    "hello.Hello#usedTransitive()I": [
-        "hello.Hello.usedDeepestStatic()I"
-    ],
     "hello.Hello.main()I": [
-        "hello.Hello#<init>()V",
-        "hello.Hello#used()I",
-        "hello.Hello#usedTransitive()I",
         "hello.Hello.usedDeepestStatic()I",
         "hello.Hello.usedStatic()I",
         "hello.Hello.usedTransitiveStatic()I"
---
+++ main/codesig/test/cases/7-transitive-virtual-methods/src/Hello.java
@@ -0,0 +1,36 @@
+package hello;
+public class Hello{
+    public static int main(){
+        return new Hello().used();
+    }
+
+    public int unused(){ return usedTransitive(); }
+
+    public int used(){ return usedTransitive(); }
+
+    public int usedTransitive(){ return 2 + Hello.usedDeepestStatic(); }
+
+    public static int usedDeepestStatic(){ return 3; }
+}
+
+/* EXPECTED TRANSITIVE
+{
+    "hello.Hello#unused()I": [
+        "hello.Hello#usedTransitive()I",
+        "hello.Hello.usedDeepestStatic()I"
+    ],
+    "hello.Hello#used()I": [
+        "hello.Hello#usedTransitive()I",
+        "hello.Hello.usedDeepestStatic()I"
+    ],
+    "hello.Hello#usedTransitive()I": [
+        "hello.Hello.usedDeepestStatic()I"
+    ],
+    "hello.Hello.main()I": [
+        "hello.Hello#<init>()V",
+        "hello.Hello#used()I",
+        "hello.Hello#usedTransitive()I",
+        "hello.Hello.usedDeepestStatic()I"
+    ]
+}
+*/
