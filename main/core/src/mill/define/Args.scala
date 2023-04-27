0790389ecf93637ea8ba886108262c444d986a92
Li Haoyi <haoyi.sg@gmail.com>
2023-04-27 21:13:41 +0800

    .

--- main/core/src/mill/define/Args.scala
+++ main/core/src/mill/define/Args.scala
@@ -1,7 +1,13 @@
 package mill.define
 
+/**
+ * Represents an unstructured sequence of command-line arguments that can be
+ * passed to Mill commands; similar to `mainargs.Leftover`.
+ */
 class Args(val value: Seq[String])
 object Args {
-  implicit def createArgs(value: Seq[String]) = new Args(value)
+  /**
+   * Constructs an [[Args]] object from `os.Shellable`s
+   */
   def apply(chunks: os.Shellable*) = new Args(chunks.flatMap(_.value))
 }
