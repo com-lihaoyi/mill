3776ca8de772e32b520c37ae833d683552035440
Your Name <haoyi.sg@gmail.com>
2023-04-30 09:27:46 +0800

    .

--- ci/test-mill-dev.sh
+++ ci/test-mill-dev.sh
@@ -2,21 +2,16 @@
 
 set -eux
 
-# Starting from scratch...
-git stash -u
-git stash -a
-
-# Build Mill
-./mill -i dev.launcher
-
 EXAMPLE=example/scalabuilds/10-scala-realistic
+
+rm -rf $EXAMPLE/out
 
 test ! -d $EXAMPLE/out/foo/3.2.2/compile.dest
 test ! -f $EXAMPLE/out/bar/2.13.8/assembly.dest/out.jar
 
 ./mill -i dev.run $EXAMPLE -i "foo[3.2.2].run"
 
-test -d $EXAMPLE/out/foo/3.2.2/compile.dest/out.jar
+test -d $EXAMPLE/out/foo/3.2.2/compile.dest
 
 ./mill -i dev.run $EXAMPLE show "bar[2.13.8].assembly"
 
