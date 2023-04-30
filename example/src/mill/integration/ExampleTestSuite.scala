68bf2d3246bef448fd55d984498225d5e65cb533
Your Name <haoyi.sg@gmail.com>
2023-04-30 20:57:43 +0800

    .

--- example/src/mill/integration/ExampleTestSuite.scala
+++ example/src/mill/integration/ExampleTestSuite.scala
@@ -51,7 +51,7 @@ object ExampleTestSuite extends IntegrationTestSuite {
         val commandBlocks = ("\n" + usageComment.trim).split("\n> ").filter(_.nonEmpty)
 
         for (commandBlock <- commandBlocks) processCommandBlock(workspaceRoot, commandBlock)
-        
+
         if (integrationTestMode != "fork") evalStdout("shutdown")
       } finally {
         try os.remove.all(workspaceRoot / "out")
