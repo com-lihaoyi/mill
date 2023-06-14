2b19a8cac596323c6ba7a0ac358161c78224d38b
ex0ns <g.tournigand@gmail.com>
2023-06-14 22:56:44 +0200

    Add failing tests

--- scalalib/test/src/mill/scalalib/TestClassLoaderTests.scala
+++ scalalib/test/src/mill/scalalib/TestClassLoaderTests.scala
@@ -5,6 +5,7 @@ import mill.{Agg, T}
 import mill.util.{TestEvaluator, TestUtil}
 import utest._
 import utest.framework.TestPath
+import mill.api.PathRef
 
 object TestClassLoaderTests extends TestSuite {
   object testclassloader extends TestUtil.BaseModule with ScalaModule {
@@ -12,10 +13,26 @@ object TestClassLoaderTests extends TestSuite {
 
     def scalaVersion = sys.props.getOrElse("TEST_SCALA_2_13_VERSION", ???)
 
-    object test extends ScalaTests with TestModule.Utest {
+    object utest extends ScalaTests with TestModule.Utest {
       override def ivyDeps = T {
         super.ivyDeps() ++ Agg(
           ivy"com.lihaoyi::utest:${sys.props.getOrElse("TEST_UTEST_VERSION", ???)}"
+        )
+      }
+    }
+
+    object scalatest extends ScalaTests with TestModule.ScalaTest {
+      override def ivyDeps = T {
+        super.ivyDeps() ++ Agg(
+          ivy"org.scalatest::scalatest:${sys.props.getOrElse("TEST_SCALATEST_VERSION", ???)}"
+        )
+      }
+    }
+
+    object ziotest extends ScalaTests with TestModule.ZioTest {
+      override def ivyDeps = T {
+        super.ivyDeps() ++ Agg(
+          ivy"dev.zio::zio-test:${sys.props.getOrElse("TEST_ZIOTEST_VERSION", ???)}"
         )
       }
     }
@@ -37,11 +54,41 @@ object TestClassLoaderTests extends TestSuite {
     t(eval)
   }
 
+  private val sbtTesting = "org/scala-sbt/test-interface/"
+
+  private val testFramework = List(
+    ("utest", testclassloader.utest),
+    ("ziotest", testclassloader.ziotest),
+    ("scalatest", testclassloader.scalatest)
+  )
+
+  def testSbtInterfaceInClassPath(
+      classpath: TestModule
+  )(implicit path: utest.framework.TestPath) = {
+    workspaceTest(testclassloader) { eval =>
+      val classpathElements = eval.apply(classpath.runClasspath).toOption.get._1
+      assert(classpathElements.exists(_.toString.contains(sbtTesting)))
+    }
+  }
+
   override def tests: Tests = Tests {
+
     test("com.sun classes exist in tests classpath (Java 8 only)") {
       workspaceTest(testclassloader) { eval =>
-        assert(eval.apply(testclassloader.test.test()).isRight)
+        assert(eval.apply(testclassloader.utest.test()).isRight)
       }
+    }
+
+    test("test-interface must be present in utest classpath") {
+      testSbtInterfaceInClassPath(testclassloader.utest)
+    }
+
+    test("test-interface must be present in ziotest classpath") {
+      testSbtInterfaceInClassPath(testclassloader.ziotest)
+    }
+
+    test("test-interface must be present in scalatest classpath") {
+      testSbtInterfaceInClassPath(testclassloader.scalatest)
     }
   }
 }
