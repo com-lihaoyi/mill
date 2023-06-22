9d97efca7d084bf37f25c73372f344a06dadae6e
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-06-22 15:49:39 +0200

    Delete deprecated mill.contrib.Bloop

--- contrib/bloop/src/mill/contrib/Bloop.scala
+++
@@ -1,7 +0,0 @@
-package mill.contrib
-
-import bloop._
-import mill.eval.Evaluator
-
-@deprecated("Use mill.contrib.bloop.Bloop instead", since = "mill 0.11.1")
-object Bloop extends BloopImpl(() => Evaluator.currentEvaluator.value, os.pwd)
