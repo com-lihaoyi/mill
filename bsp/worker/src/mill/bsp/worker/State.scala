2ef5ec8a5958335f2bab5e1d0290d2c793ebb712
Tobias Roeser <le.petit.fou@web.de>
2023-07-04 08:57:14 +0200

    Use RootModuleFinder in BSP

--- bsp/worker/src/mill/bsp/worker/State.scala
+++ bsp/worker/src/mill/bsp/worker/State.scala
@@ -1,7 +1,7 @@
 package mill.bsp.worker
 
 import ch.epfl.scala.bsp4j.BuildTargetIdentifier
-import mill.runner.MillBuildBootstrap
+import mill.runner.{MillBuildBootstrap, RootModuleFinder}
 import mill.scalalib.bsp.BspModule
 import mill.scalalib.internal.{JavaModuleUtils, ModuleUtils}
 import mill.define.Module
@@ -29,39 +29,7 @@ private class State(projectRoot: os.Path, baseLogger: ColorLogger, debug: String
     map
   }
 
-  lazy val rootModules: Seq[mill.main.RootModule] = {
-    val evaluated = new mill.runner.MillBuildBootstrap(
-      projectRoot = projectRoot,
-      home = os.home,
-      keepGoing = false,
-      imports = Nil,
-      env = Map.empty,
-      threadCount = None,
-      targetsAndParams = Seq("resolve", "_"),
-      prevRunnerState = mill.runner.RunnerState.empty,
-      logger = baseLogger
-    ).evaluate()
+  lazy val rootModules: Seq[mill.main.RootModule] = RootModuleFinder.findRootModules(projectRoot, baseLogger)
 
-    val rootModules0 = evaluated.result.frames
-      .flatMap(_.classLoaderOpt)
-      .zipWithIndex
-      .map { case (c, i) =>
-        MillBuildBootstrap
-          .getRootModule(c, i, projectRoot)
-          .fold(sys.error(_), identity(_))
-      }
-
-    val bootstrapModule = evaluated.result.bootstrapModuleOpt.map(m =>
-      MillBuildBootstrap
-        .getChildRootModule(
-          m,
-          evaluated.result.frames.length,
-          projectRoot
-        )
-        .fold(sys.error(_), identity(_))
-    )
-
-    rootModules0 ++ bootstrapModule
-  }
   lazy val bspIdByModule: Map[BspModule, BuildTargetIdentifier] = bspModulesById.map(_.swap)
 }
