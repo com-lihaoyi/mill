com-lihaoyi/mill

commit 3d57f3b2733f6a3ab86b75ee79e42962f81705f0

Cleanup stream handling in PrintLogger to try and avoid over-written lines (#2428)
Right now, if we add a `println` inside `evaluateGroupCached` for
debugging purposes:

```
diff --git a/main/core/src/mill/eval/Evaluator.scala b/main/core/src/mill/eval/Evaluator.scala
index 6805efd41..1fe1e5b02 100644
--- a/main/core/src/mill/eval/Evaluator.scala
+++ b/main/core/src/mill/eval/Evaluator.scala
@@ -306,7 +306,7 @@ class Evaluator private (
       testReporter: TestReporter,
       logger: ColorLogger
   ): Evaluated = {
-
+    println("evaluateGroupCached")
     val externalInputsHash = scala.util.hashing.MurmurHash3.orderedHash(
       group.items.flatMap(_.inputs).filter(!group.contains(_))
         .flatMap(results(_).asSuccess.map(_.value._2))
```

It gets over-written by the ticker in the terminal and not reliably
shown:

```
$ rm -rf example/basic/1-hello-world/out && ./mill -i dev.run example/basic/1-hello-world -i run --text hello
evaluateGroupCached
[35/48] finalMainClassOpt
[36/48] finalMainClass
[37/48] resources
[38/48] localClasspath
[39/48] transitiveLocalClasspath
[40/48] runIvyDeps
[41/48] resolvedRunIvyDeps
[42/48] upstreamAssemblyClasspath
[43/48] runClasspath
[44/48] forkArgs
[45/48] forkEnv
[46/48] forkWorkingDir
[47/48] runUseArgsFile
[48/48] run
<h1>hello</h1>
```

With this PR, the `println` is reliably shown

```
$ rm -rf example/basic/1-hello-world/out && ./mill -i dev.run example/basic/1-hello-world -i run --text hello
evaluateGroupCached
[35/48] finalMainClassOpt
evaluateGroupCached
[36/48] finalMainClass
evaluateGroupCached
[37/48] resources
evaluateGroupCached
[38/48] localClasspath
evaluateGroupCached
[39/48] transitiveLocalClasspath
evaluateGroupCached
[40/48] runIvyDeps
evaluateGroupCached
[41/48] resolvedRunIvyDeps
evaluateGroupCached
[42/48] upstreamAssemblyClasspath
evaluateGroupCached
[43/48] runClasspath
evaluateGroupCached
[44/48] forkArgs
evaluateGroupCached

main/src/mill/main/MainModule.scala
@@ -250,7 +250,7 @@ trait MainModule extends mill.Module {
         // When using `show`, redirect all stdout of the evaluated tasks so the
         // printed JSON is the only thing printed to stdout.
         evaluator.baseLogger match {
-          case p: PrintLogger => p.withOutStream(p.errStream)
+          case p: PrintLogger => p.withOutStream(p.errorStream)
           case l => l
         }
       ),
@@ -278,7 +278,7 @@ trait MainModule extends mill.Module {
         // When using `show`, redirect all stdout of the evaluated tasks so the
         // printed JSON is the only thing printed to stdout.
         evaluator.baseLogger match {
-          case p: PrintLogger => p.withOutStream(outStream = p.errStream)
+          case p: PrintLogger => p.withOutStream(outStream = p.errorStream)
           case l => l
         }
       ),

main/testkit/src/mill/testkit/MillTestkit.scala
@@ -1,15 +1,16 @@
 package mill.testkit
 
 import mill.define._
-import mill.api.Result
+import mill.api.{DummyInputStream, Result, SystemStreams}
 import mill.api.Result.OuterStack
 import mill.api.Strict.Agg
+
 import java.io.{InputStream, PrintStream}
 import mill.define.{Input, Task}
 import mill.eval.Evaluator
-import language.experimental.macros
-import mill.api.{DummyInputStream, Result}
+import mill.util.PrintLogger
 
+import language.experimental.macros
 import scala.collection.mutable
 
 trait MillTestKit {
@@ -81,12 +82,10 @@ trait MillTestKit {
       enableTicker = true,
       mill.util.Colors.Default.info,
       mill.util.Colors.Default.error,
-      outStream,
-      outStream,
-      outStream,
-      inStream,
+      new SystemStreams(outStream, outStream, inStream),
       debugEnabled = debugEnabled,
-      context = ""
+      context = "",
+      new PrintLogger.State()
     ) {
       val prefix = {
         val idx = fullName.value.lastIndexOf(".")

main/util/src/mill/util/ColorLogger.scala
@@ -1,0 +1,10 @@
+package mill.util
+
+import mill.api.Logger
+
+
+trait ColorLogger extends Logger {
+  def infoColor: fansi.Attrs
+  def errorColor: fansi.Attrs
+}
+

main/util/src/mill/util/PrefixLogger.scala
@@ -1,0 +1,43 @@
+package mill.util
+
+import mill.api.SystemStreams
+
+import java.io.PrintStream
+
+
+class PrefixLogger(out: ColorLogger, context: String, tickerContext: String = "")
+  extends ColorLogger {
+  override def colored = out.colored
+
+  def infoColor = out.infoColor
+  def errorColor = out.errorColor
+
+  val systemStreams = new SystemStreams(
+    new PrintStream(new LinePrefixOutputStream(
+      infoColor(context).render,
+      out.systemStreams.err
+    )),
+    new PrintStream(new LinePrefixOutputStream(
+      infoColor(context).render,
+      out.systemStreams.out
+    )),
+    out.systemStreams.in
+  )
+
+  override def info(s: String): Unit = out.info(context + s)
+
+  override def error(s: String): Unit = out.error(context + s)
+
+  override def ticker(s: String): Unit = out.ticker(context + tickerContext + s)
+
+  override def debug(s: String): Unit = out.debug(context + s)
+
+  override def debugEnabled: Boolean = out.debugEnabled
+}
+
+
+
+object PrefixLogger {
+  def apply(out: ColorLogger, context: String, tickerContext: String = ""): PrefixLogger =
+    new PrefixLogger(out, context, tickerContext)
+}

commit 48b28e40ca0ab1d4e99794451d46c7078470c874

Fix redirection of stdout stream to stderr when using `show` (#2689)
Fixes https://github.com/com-lihaoyi/mill/issues/2680

When you use `show`, we want to ensure that nothing goes to `stdout`
except the JSON result of the task shown, so it can easily be piped and
parsed and handled by external programs. This was likely broken in
https://github.com/com-lihaoyi/mill/pull/2428.

The basic problem is that `PrintLogger` and `PrefixLogger` are different
types, with only `PrintLogger` supporting `withOutputStream` but it
quickly gets wrapped in `PrefixLogger` when passed around inside Mill,
meaning `MainModule.show0` sees that it's not an instance of
`PrintLogger` and skips with `withOutputStream`.

This fix in this PR is to move `withOutputStream` to `ColorLogger`, and
implement it in `PrefixLogger` as well.

Tested manually by via the patch and command below. On `main`, `log.txt`
contains the `COMPILING` line in addition to the JSON blob (the bug
above), and most of the Scala compiler output just disappears (which is
also a bug!), and most of stdout goes to stderr (another bug!). On this
PR, the Scala compiler output and `COMPILING` are both properly shown in
the console, while the `log.txt` file contains only the JSON dictionary
`{"analysisFile": "...", "classes": "..."}` and nothing else

```diff
--- a/example/basic/1-simple-scala/build.sc
+++ b/example/basic/1-simple-scala/build.sc
@@ -8,6 +8,11 @@ object foo extends RootModule with ScalaModule {
     ivy"com.lihaoyi::mainargs:0.4.0"
   )

+  def compile = T{
+    println("COMPILING")
+    super.compile()
+  }
+
   object test extends ScalaTests {
     def ivyDeps = Agg(ivy"com.lihaoyi::utest:0.7.11")
     def testFramework = "utest.runner.Framework"
```
```
echo // >> example/basic/1-simple-scala/src/Foo.scala && ./mill -i dev.run example/basic/1-simple-scala -i show compile > log.txt
```

Also updated the unit tests in `mill.main.MainModuleTests` to assert

main/src/mill/main/MainModule.scala
@@ -32,14 +32,12 @@ object MainModule {
       log: Logger,
       watch0: Watchable => Unit
   )(f: Seq[(Any, Option[(RunScript.TaskName, ujson.Value)])] => ujson.Value) = {
+
     RunScript.evaluateTasksNamed(
+      // When using `show`, redirect all stdout of the evaluated tasks so the
+      // printed JSON is the only thing printed to stdout.
       evaluator.withBaseLogger(
-        // When using `show`, redirect all stdout of the evaluated tasks so the
-        // printed JSON is the only thing printed to stdout.
-        evaluator.baseLogger match {
-          case p: PrintLogger => p.withOutStream(p.errorStream)
-          case l => l
-        }
+        evaluator.baseLogger.withOutStream(evaluator.baseLogger.errorStream)
       ),
       targets,
       Separated

main/testkit/src/mill/testkit/MillTestkit.scala
@@ -65,6 +65,7 @@ trait MillTestKit {
       failFast: Boolean = false,
       threads: Option[Int] = Some(1),
       outStream: PrintStream = System.out,
+      errStream: PrintStream = System.err,
       inStream: InputStream = DummyInputStream,
       debugEnabled: Boolean = false,
       extraPathEnd: Seq[String] = Seq.empty,
@@ -78,7 +79,7 @@ trait MillTestKit {
       enableTicker = true,
       mill.util.Colors.Default.info,
       mill.util.Colors.Default.error,
-      new SystemStreams(outStream, outStream, inStream),
+      new SystemStreams(out = outStream, err = errStream, in = inStream),
       debugEnabled = debugEnabled,
       context = "",
       new PrintLogger.State()

main/util/src/mill/util/ColorLogger.scala
@@ -2,7 +2,10 @@ package mill.util
 
 import mill.api.Logger
 
+import java.io.PrintStream
+
 trait ColorLogger extends Logger {
   def infoColor: fansi.Attrs
   def errorColor: fansi.Attrs
+  def withOutStream(outStream: PrintStream): ColorLogger = this
 }

main/util/src/mill/util/PrefixLogger.scala
@@ -4,34 +4,51 @@ import mill.api.SystemStreams
 
 import java.io.PrintStream
 
-class PrefixLogger(out: ColorLogger, context: String, tickerContext: String = "")
-    extends ColorLogger {
-  override def colored = out.colored
+class PrefixLogger(
+    val logger0: ColorLogger,
+    context: String,
+    tickerContext: String = "",
+    outStream0: Option[PrintStream] = None,
+    errStream0: Option[PrintStream] = None
+) extends ColorLogger {
 
-  def infoColor = out.infoColor
-  def errorColor = out.errorColor
+  def this(logger0: ColorLogger, context: String, tickerContext: String) =
+    this(logger0, context, tickerContext, None, None)
+
+  override def colored = logger0.colored
+
+  def infoColor = logger0.infoColor
+  def errorColor = logger0.errorColor
 
   val systemStreams = new SystemStreams(
-    new PrintStream(new LinePrefixOutputStream(
-      infoColor(context).render,
-      out.systemStreams.err
-    )),
-    new PrintStream(new LinePrefixOutputStream(
-      infoColor(context).render,
-      out.systemStreams.out
-    )),
-    out.systemStreams.in
+    out = outStream0.getOrElse(
+      new PrintStream(new LinePrefixOutputStream(
+        infoColor(context).render,
+        logger0.systemStreams.out
+      ))
+    ),
+    err = errStream0.getOrElse(
+      new PrintStream(new LinePrefixOutputStream(
+        infoColor(context).render,
+        logger0.systemStreams.err
+      ))
+    ),
+    logger0.systemStreams.in
   )
 
-  override def info(s: String): Unit = out.info(context + s)
+  override def info(s: String): Unit = logger0.info(context + s)
+  override def error(s: String): Unit = logger0.error(context + s)
+  override def ticker(s: String): Unit = logger0.ticker(context + tickerContext + s)
+  override def debug(s: String): Unit = logger0.debug(context + s)
+  override def debugEnabled: Boolean = logger0.debugEnabled
 
-  override def error(s: String): Unit = out.error(context + s)
-
-  override def ticker(s: String): Unit = out.ticker(context + tickerContext + s)
-
-  override def debug(s: String): Unit = out.debug(context + s)
-
-  override def debugEnabled: Boolean = out.debugEnabled
+  override def withOutStream(outStream: PrintStream): PrefixLogger = new PrefixLogger(
+    logger0.withOutStream(outStream),
+    context,
+    tickerContext,
+    outStream0 = Some(outStream),
+    errStream0 = Some(systemStreams.err)
+  )
 }
 
 object PrefixLogger {

main/util/src/mill/util/PrintLogger.scala
@@ -49,7 +49,7 @@ class PrintLogger(
     }
   }
 
-  def withOutStream(outStream: PrintStream): PrintLogger =
+  override def withOutStream(outStream: PrintStream): PrintLogger =
     copy(systemStreams = new SystemStreams(outStream, systemStreams.err, systemStreams.in))
 
   private def copy(

integration/feature/watch-source-input/test/src/WatchSourceInputTests.scala
@@ -1,6 +1,9 @@
 package mill.integration
 
+import mill.main.client.Util
 import utest._
+
+import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 import scala.concurrent.duration.SECONDS
@@ -31,127 +34,146 @@ object WatchSourceInputTests extends IntegrationTestSuite {
       }
     }
 
-    def testWatchSource(show: Boolean) = {
-      val showArgs = if (show) Seq("show") else Nil
+    def testBase(show: Boolean)(f: (
+        mutable.Buffer[String],
+        mutable.Buffer[String],
+        mutable.Buffer[String]
+    ) => IntegrationTestSuite.EvalResult): Unit = {
+      val expectedOut = mutable.Buffer.empty[String]
+      // Most of these are normal `println`s, so they go to `stdout` by
+      // default unless you use `show` in which case they go to `stderr`.
+      val expectedErr = if (show) mutable.Buffer.empty[String] else expectedOut
+      val expectedShows = mutable.Buffer.empty[String]
+      val res = f(expectedOut, expectedErr, expectedShows)
+      val (shows, out) = res.out.linesIterator.toVector.partition(_.startsWith("\""))
+      val err = res.err.linesIterator.toVector
+        .filter(!_.contains("Watching for changes"))
+        .filter(!_.contains("[info] compiling"))
+        .filter(!_.contains("[info] done compiling"))
 
-      val evalResult = Future { evalTimeoutStdout(maxDuration, "--watch", showArgs, "qux") }
-      val expectedPrints = collection.mutable.Buffer.empty[String]
-      val expectedShows = collection.mutable.Buffer.empty[String]
+      assert(out == expectedOut)
 
-      awaitCompletionMarker("initialized0")
-      awaitCompletionMarker("quxRan0")
-      expectedPrints.append(
-        "Setting up build.sc",
-        "Running qux foo contents initial-foo1 initial-foo2",
-        "Running qux bar contents initial-bar"
-      )
-      expectedShows.append(
-        "Running qux foo contents initial-foo1 initial-foo2 Running qux bar contents initial-bar"
-      )
+      // If show is not enabled, we don't expect any of our custom prints to go to stderr
+      if (show) assert(err == expectedErr)
+      else assert(err.isEmpty)
 
-      os.write.over(wsRoot / "foo1.txt", "edited-foo1")
-      awaitCompletionMarker("quxRan1")
-      expectedPrints.append(
-        "Running qux foo contents edited-foo1 initial-foo2",
-        "Running qux bar contents initial-bar"
-      )
-      expectedShows.append(
-        "Running qux foo contents edited-foo1 initial-foo2 Running qux bar contents initial-bar"
-      )
-
-      os.write.over(wsRoot / "foo2.txt", "edited-foo2")
-      awaitCompletionMarker("quxRan2")
-      expectedPrints.append(
-        "Running qux foo contents edited-foo1 edited-foo2",
-        "Running qux bar contents initial-bar"
-      )
-      expectedShows.append(
-        "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents initial-bar"
-      )
-
-      os.write.over(wsRoot / "bar.txt", "edited-bar")
-      awaitCompletionMarker("quxRan3")
-      expectedPrints.append(
-        "Running qux foo contents edited-foo1 edited-foo2",
-        "Running qux bar contents edited-bar"
-      )
-      expectedShows.append(
-        "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents edited-bar"
-      )
-
-      os.write.append(wsRoot / "build.sc", "\ndef unrelated = true")
-      awaitCompletionMarker("initialized1")
-      expectedPrints.append(
-        "Setting up build.sc"
-        // These targets do not re-evaluate, because the change to the build
-        // file was unrelated to them and does not affect their transitive callgraph
-        //        "Running qux foo contents edited-foo1 edited-foo2",
-        //        "Running qux bar contents edited-bar"
-      )
-      expectedShows.append(
-        "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents edited-bar"
-      )
-
-      os.write.over(wsRoot / "watchValue.txt", "exit")
-      awaitCompletionMarker("initialized2")
-      expectedPrints.append("Setting up build.sc")
-
-      val res = Await.result(evalResult, Duration.apply(maxDuration, SECONDS))
-
-      val (shows, prints) = res.out.linesIterator.toVector.partition(_.startsWith("\""))
-
-      assert(prints == expectedPrints)
       if (show) assert(shows == expectedShows.map('"' + _ + '"'))
     }
+
+    def testWatchSource(show: Boolean) =
+      testBase(show) { (expectedOut, expectedErr, expectedShows) =>
+        val showArgs = if (show) Seq("show") else Nil
+
+        val evalResult = Future { evalTimeoutStdout(maxDuration, "--watch", showArgs, "qux") }
+
+        awaitCompletionMarker("initialized0")
+        awaitCompletionMarker("quxRan0")
+        expectedOut.append(
+          "Setting up build.sc"
+        )
+        expectedErr.append(
+          "Running qux foo contents initial-foo1 initial-foo2",
+          "Running qux bar contents initial-bar"
+        )
+        expectedShows.append(
+          "Running qux foo contents initial-foo1 initial-foo2 Running qux bar contents initial-bar"
+        )
+
+        os.write.over(wsRoot / "foo1.txt", "edited-foo1")
+        awaitCompletionMarker("quxRan1")
+        expectedErr.append(
+          "Running qux foo contents edited-foo1 initial-foo2",
+          "Running qux bar contents initial-bar"
+        )
+        expectedShows.append(
+          "Running qux foo contents edited-foo1 initial-foo2 Running qux bar contents initial-bar"
+        )
+
+        os.write.over(wsRoot / "foo2.txt", "edited-foo2")
+        awaitCompletionMarker("quxRan2")
+        expectedErr.append(
+          "Running qux foo contents edited-foo1 edited-foo2",
+          "Running qux bar contents initial-bar"
+        )
+        expectedShows.append(
+          "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents initial-bar"
+        )
+
+        os.write.over(wsRoot / "bar.txt", "edited-bar")
+        awaitCompletionMarker("quxRan3")
+        expectedErr.append(
+          "Running qux foo contents edited-foo1 edited-foo2",
+          "Running qux bar contents edited-bar"
+        )
+        expectedShows.append(
+          "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents edited-bar"
+        )
+
+        os.write.append(wsRoot / "build.sc", "\ndef unrelated = true")
+        awaitCompletionMarker("initialized1")
+        expectedOut.append(
+          "Setting up build.sc"
+          // These targets do not re-evaluate, because the change to the build
+          // file was unrelated to them and does not affect their transitive callgraph
+          //        "Running qux foo contents edited-foo1 edited-foo2",
+          //        "Running qux bar contents edited-bar"
+        )
+        expectedShows.append(
+          "Running qux foo contents edited-foo1 edited-foo2 Running qux bar contents edited-bar"
+        )
+
+        os.write.over(wsRoot / "watchValue.txt", "exit")
+        awaitCompletionMarker("initialized2")
+        expectedOut.append("Setting up build.sc")
+
+        Await.result(evalResult, Duration.apply(maxDuration, SECONDS))
+      }
 
     test("sources") {
 
       // Make sure we clean up the workspace between retries
-      test("noshow") - retry(3) { initWorkspace(); testWatchSource(false) }
-      test("show") - retry(3) { initWorkspace(); testWatchSource(true) }
+      test("noshow") - retry(3) { if (!Util.isWindows) { initWorkspace(); testWatchSource(false) } }
+      test("show") - retry(3) { if (!Util.isWindows) { initWorkspace(); testWatchSource(true) } }
     }
 
-    def testWatchInput(show: Boolean) = {
-      val showArgs = if (show) Seq("show") else Nil
+    def testWatchInput(show: Boolean) =
+      testBase(show) { (expectedOut, expectedErr, expectedShows) =>
+        val showArgs = if (show) Seq("show") else Nil
 
-      val evalResult = Future { evalTimeoutStdout(maxDuration, "--watch", showArgs, "lol") }
-      val expectedPrints = collection.mutable.Buffer.empty[String]
-      val expectedShows = collection.mutable.Buffer.empty[String]
+        val evalResult = Future { evalTimeoutStdout(maxDuration, "--watch", showArgs, "lol") }
 
-      awaitCompletionMarker("initialized0")
-      awaitCompletionMarker("lolRan0")
-      expectedPrints.append(
-        "Setting up build.sc",
-        "Running lol baz contents initial-baz"
-      )
-      expectedShows.append("Running lol baz contents initial-baz")
+        awaitCompletionMarker("initialized0")
+        awaitCompletionMarker("lolRan0")
+        expectedOut.append(
+          "Setting up build.sc"
+        )
+        expectedErr.append(
+          "Running lol baz contents initial-baz"
+        )
+        expectedShows.append("Running lol baz contents initial-baz")
 
-      os.write.over(wsRoot / "baz.txt", "edited-baz")
-      awaitCompletionMarker("lolRan1")
-      expectedPrints.append("Running lol baz contents edited-baz")
-      expectedShows.append("Running lol baz contents edited-baz")
+        os.write.over(wsRoot / "baz.txt", "edited-baz")
+        awaitCompletionMarker("lolRan1")
+        expectedErr.append("Running lol baz contents edited-baz")
+        expectedShows.append("Running lol baz contents edited-baz")
 
-      os.write.over(wsRoot / "watchValue.txt", "edited-watchValue")
-      awaitCompletionMarker("initialized1")
-      expectedPrints.append("Setting up build.sc")
-      expectedShows.append("Running lol baz contents edited-baz")
+        os.write.over(wsRoot / "watchValue.txt", "edited-watchValue")
+        awaitCompletionMarker("initialized1")
+        expectedOut.append("Setting up build.sc")
+        expectedShows.append("Running lol baz contents edited-baz")
 
-      os.write.over(wsRoot / "watchValue.txt", "exit")
-      awaitCompletionMarker("initialized2")
-      expectedPrints.append("Setting up build.sc")
+        os.write.over(wsRoot / "watchValue.txt", "exit")
+        awaitCompletionMarker("initialized2")
+        expectedOut.append("Setting up build.sc")
 
-      val res = Await.result(evalResult, Duration.apply(maxDuration, SECONDS))
-
-      val (shows, prints) = res.out.linesIterator.toVector.partition(_.startsWith("\""))
-      assert(prints == expectedPrints)
-      if (show) assert(shows == expectedShows.map('"' + _ + '"'))
-    }
+        Await.result(evalResult, Duration.apply(maxDuration, SECONDS))
+      }
 
     test("input") {
 
       // Make sure we clean up the workspace between retries
-      test("noshow") - retry(3) { initWorkspace(); testWatchInput(false) }
-      test("show") - retry(3) { initWorkspace(); testWatchInput(true) }
+      test("noshow") - retry(3) { if (!Util.isWindows) { initWorkspace(); testWatchInput(false) } }
+      test("show") - retry(3) { if (!Util.isWindows) { initWorkspace(); testWatchInput(true) } }
     }
   }
 }

main/test/src/mill/main/MainModuleTests.scala
@@ -6,11 +6,25 @@ import mill.define.{Cross, Discover, Module, Task}
 import mill.util.{TestEvaluator, TestUtil}
 import utest.{TestSuite, Tests, assert, test}
 
+import java.io.{ByteArrayOutputStream, PrintStream}
+
 object MainModuleTests extends TestSuite {
 
   object mainModule extends TestUtil.BaseModule with MainModule {
-    def hello = T { Seq("hello", "world") }
-    def hello2 = T { Map("1" -> "hello", "2" -> "world") }
+    def hello = T {
+      System.out.println("Hello System Stdout")
+      System.err.println("Hello System Stderr")
+      Console.out.println("Hello Console Stdout")
+      Console.err.println("Hello Console Stderr")
+      Seq("hello", "world")
+    }
+    def hello2 = T {
+      System.out.println("Hello2 System Stdout")
+      System.err.println("Hello2 System Stderr")
+      Console.out.println("Hello2 Console Stdout")
+      Console.err.println("Hello2 Console Stderr")
+      Map("1" -> "hello", "2" -> "world")
+    }
     def helloCommand(x: Int, y: Task[String]) = T.command { (x, y(), hello()) }
     override lazy val millDiscover: Discover[this.type] = Discover[this.type]
   }
@@ -81,7 +95,13 @@ object MainModuleTests extends TestSuite {
     }
 
     test("show") {
-      val evaluator = new TestEvaluator(mainModule)
+      val outStream = new ByteArrayOutputStream()
+      val errStream = new ByteArrayOutputStream()
+      val evaluator = new TestEvaluator(
+        mainModule,
+        outStream = new PrintStream(outStream, true),
+        errStream = new PrintStream(errStream, true)
+      )
       test("single") {
         val results =
           evaluator.evaluator.evaluate(Agg(mainModule.show(evaluator.evaluator, "hello")))
@@ -90,7 +110,20 @@ object MainModuleTests extends TestSuite {
 
         val Result.Success(Val(value)) = results.rawValues.head
 
-        assert(value == ujson.Arr.from(Seq("hello", "world")))
+        val shown = ujson.read(outStream.toByteArray)
+        val expected = ujson.Arr.from(Seq("hello", "world"))
+        assert(value == expected)
+        assert(shown == expected)
+
+        // Make sure both stdout and stderr are redirected by `show`
+        // to stderr so that only the JSON file value goes to stdout
+        val strippedErr =
+          fansi.Str(errStream.toString, errorMode = fansi.ErrorMode.Sanitize).plainText
+
+        assert(strippedErr.contains("Hello System Stdout"))
+        assert(strippedErr.contains("Hello System Stderr"))
+        assert(strippedErr.contains("Hello Console Stdout"))
+        assert(strippedErr.contains("Hello Console Stderr"))
       }
       test("multi") {
         val results =
@@ -105,10 +138,24 @@ object MainModuleTests extends TestSuite {
 
         val Result.Success(Val(value)) = results.rawValues.head
 
-        assert(value == ujson.Obj(
+        val shown = ujson.read(outStream.toByteArray)
+
+        val expected = ujson.Obj(
           "hello" -> ujson.Arr("hello", "world"),
           "hello2" -> ujson.Obj("1" -> "hello", "2" -> "world")
-        ))
+        )
+        assert(value == expected)
+        assert(shown == expected)
+
+        // Make sure both stdout and stderr are redirected by `show`
+        // to stderr so that only the JSON file value goes to stdout
+        val strippedErr =
+          fansi.Str(errStream.toString, errorMode = fansi.ErrorMode.Sanitize).plainText
+
+        assert(strippedErr.contains("Hello2 System Stdout"))
+        assert(strippedErr.contains("Hello2 System Stderr"))
+        assert(strippedErr.contains("Hello2 Console Stdout"))
+        assert(strippedErr.contains("Hello2 Console Stderr"))
       }
 
       test("command") {

main/test/src/mill/util/TestEvaluator.scala
@@ -22,6 +22,7 @@ class TestEvaluator(
     failFast: Boolean = false,
     threads: Option[Int] = Some(1),
     outStream: PrintStream = System.out,
+    errStream: PrintStream = System.err,
     inStream: InputStream = DummyInputStream,
     debugEnabled: Boolean = false,
     extraPathEnd: Seq[String] = Seq.empty,
@@ -32,6 +33,7 @@ class TestEvaluator(
       failFast,
       threads,
       outStream,
+      errStream,
       inStream,
       debugEnabled,
       extraPathEnd,