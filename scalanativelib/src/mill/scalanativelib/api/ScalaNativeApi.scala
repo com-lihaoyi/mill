b467fcb9c9d08ad26eba261fb8227390b1c266f1
Tobias Roeser <le.petit.fou@web.de>
2023-09-18 20:07:41 +0200

    scalafmt

--- scalanativelib/src/mill/scalanativelib/api/ScalaNativeApi.scala
+++ scalanativelib/src/mill/scalanativelib/api/ScalaNativeApi.scala
@@ -23,9 +23,11 @@ object ReleaseMode {
   case object Debug extends ReleaseMode("debug")
   case object ReleaseFast extends ReleaseMode("release-fast")
   case object ReleaseFull extends ReleaseMode("release-full")
-  /** Optimizes output binary size and still have relatively fast runtime performance. 
+
+  /**
+   * Optimizes output binary size and still have relatively fast runtime performance.
    *  Equivalent to `-Oz` switch of `clang`.
-   *  Since Scala Native 0.4.10 
+   *  Since Scala Native 0.4.10
    */
   case object ReleaseSize extends ReleaseMode("release-size")
 
