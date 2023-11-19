4bd7c566234667031ea6da7f0629fe9d7b975e08
Your Name <haoyi.sg@gmail.com>
2023-11-19 10:05:43 +0800

    .

--- main/src/mill/main/MainModule.scala
+++ main/src/mill/main/MainModule.scala
@@ -50,8 +50,6 @@ object MainModule {
       case Right((watched, Right(res))) =>
         val output = f(res)
         watched.foreach(watch0)
-        println("log.showOutputStream.println")
-        println("log " + log)
         log.rawOutputStream.println(output.render(indent = 2))
         Result.Success(output)
     }
