ed2c20236870f6e5197f3bcafe470278cf299a45
Tobias Roeser <le.petit.fou@web.de>
2024-01-14 10:30:15 +0100

    Introduce a `BspExtension` trait

---
+++ scalalib/src/mill/scalalib/bsp/BspExtension.scala
@@ -0,0 +1,13 @@
+package mill.scalalib.bsp
+
+import mill.Agg
+import mill.scalalib.Dep
+
+/** A BSP Extension to be loaded by the Mill BSP server.
+ * @param className The extension class to be loaded.
+ * @param ivyDeps Additional dependencies to be loaded into the BSP server classpath.
+ */
+case class BspExtension(
+    className: String,
+    ivyDeps: Agg[Dep]
+)
