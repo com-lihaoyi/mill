00521a0e6437198d6653c734efab9744fbbbcfec
Tobias Roeser <le.petit.fou@web.de>
2024-01-14 10:55:50 +0100

    scalafmt

--- scalalib/src/mill/scalalib/bsp/BspExtension.scala
+++ scalalib/src/mill/scalalib/bsp/BspExtension.scala
@@ -3,7 +3,8 @@ package mill.scalalib.bsp
 import mill.Agg
 import mill.scalalib.Dep
 
-/** A BSP Extension to be loaded by the Mill BSP server.
+/**
+ * A BSP Extension to be loaded by the Mill BSP server.
  * @param className The extension class to be loaded.
  * @param ivyDeps Additional dependencies to be loaded into the BSP server classpath.
  */
