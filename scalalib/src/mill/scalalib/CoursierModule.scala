f556ffd733b65171168e9332e526228c0476888f
Jack C. Viers <jack@writer.com>
2024-02-04 22:57:46 -0600

    WIP: Trying to implement the workers correctly

--- scalalib/src/mill/scalalib/CoursierModule.scala
+++ scalalib/src/mill/scalalib/CoursierModule.scala
@@ -1,17 +1,29 @@
 package mill.scalalib
 
-import coursier.Dependency
-import coursier.Repository
-import coursier.Resolve
-import coursier.cache.FileCache
-import coursier.core.Resolution
+import _root_.{coursier => c}
+import c.Dependency
+import c.Repository
+import c.cache.FileCache
+import c.core.Resolution
+import c.credentials.Credentials
+import c.Resolve
+import c.Repository
+import c.cache.CacheDefaults
+import c.ivy.IvyRepository
+import c.maven.MavenRepository
+
 import mill.Agg
+import mill.api.Ctx
 import mill.T
+import mill.Worker
 import mill.api.PathRef
 import mill.define.Task
-import mill.scalalib.coursier.CoursierWorkerModule
 
 import scala.annotation.nowarn
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 /**
  * This module provides the capability to resolve (transitive) dependencies from (remote) repositories.
@@ -22,6 +34,71 @@ import scala.annotation.nowarn
 trait CoursierModule extends mill.Module {
 
   /**
+   * Sets up the coursier proxy, using coursier's configuration sources.
+   *
+   * @see [[https://scala-cli.virtuslab.org/docs/guides/power/proxy/] Scala-cli proxy]
+   * @see [[https://get-coursier.io/docs/other-proxy] coursier proxy]
+   * @see [[https://github.com/coursier/coursier/blob/f6f82f2e7b55deb461dd041f8f11df428bcf3789/modules/coursier/jvm/src/main/scala/coursier/PlatformResolve.scala#L101] PlatformResolve.scala#L101]
+   */
+  def setupProxy(implicit ctx: Ctx): Worker[Unit] = T.worker {
+    T.input {
+      T.task {
+        Resolve.proxySetup()
+      }.evaluate(ctx)
+    }.t
+  }
+
+  /**
+   * Gets the coursier credentials from coursier's configuration sources.
+   *
+   * @see [[https://github.com/coursier/coursier/blob/f6f82f2e7b55deb461dd041f8f11df428bcf3789/modules/cache/jvm/src/main/scala/coursier/cache/CacheDefaults.scala#L97] CacheDefaults.credentials]
+   * @see [[https://get-coursier.io/docs/other-credentials] Coursier Credentials]
+   * @see [[https://scala-cli.virtuslab.org/docs/guides/power/repositories/#repository-authentication] Scala-cli Repository Authentication]
+   * @return the list of coursier credentials
+   */
+  def coursierCredentials(implicit ctx: Ctx): Worker[Seq[Credentials]] = T.worker {
+    T.input {
+      T.task {
+        CacheDefaults.credentials.flatMap { _.get() }
+      }.evaluate(ctx)
+    }.t
+  }
+
+  /**
+   * Sets up repositories with mirrors, proxies, and credentials from coursier's config sources, without overwriting existing authentication.
+   *
+   * @return a worker containing the result of applying the coursier credentials, proxy setup, and mirrors to the repositories in coursiers configs
+   */
+  def repositoriesWithCredentials(implicit ctx: Ctx): Worker[Seq[Repository]] = T.worker {
+    T.input {
+      T.task {
+        val _ = setupProxy(ctx)
+        val credentials = coursierCredentials(ctx)
+        Await.result(
+          Resolve().finalRepositories.map {
+            _.map {
+              case x: IvyRepository if x.authentication.isEmpty =>
+                x.withAuthentication(
+                  resolvedCredentials.find { c =>
+                    c.matches(x.pattern.string, c.usernameOpt.getOrElse(""))
+                  }.map(_.authentication)
+                )
+              case x: MavenRepository if x.authentication.isEmpty =>
+                x.withAuthentication(
+                  resolvedCredentials.find { c =>
+                    c.matches(x.root, c.usernameOpt.getOrElse(""))
+                  }.map(_.authentication)
+                )
+              case r => r
+            }
+          }.future(),
+          Duration.Inf
+        )
+      }
+    }.t
+  }
+
+  /**
    * Bind a dependency ([[Dep]]) to the actual module contetxt (e.g. the scala version and the platform suffix)
    * @return The [[BoundDep]]
    */
@@ -30,7 +107,7 @@ trait CoursierModule extends mill.Module {
   }
 
   @deprecated("To be replaced by bindDependency", "Mill after 0.11.0-M0")
-  def resolveCoursierDependency: Task[Dep => coursier.Dependency] = T.task {
+  def resolveCoursierDependency: Task[Dep => c.Dependency] = T.task {
     Lib.depToDependencyJava(_: Dep)
   }
 
@@ -64,7 +141,8 @@ trait CoursierModule extends mill.Module {
    * The repositories used to resolve dependencies with [[resolveDeps()]].
    */
   def repositoriesTask: Task[Seq[Repository]] = T.task {
-    CoursierWorkerModule.repositoriesWithCredentials()
+    // what's the idiomatic way to do this @lefou?
+    repositoriesWithCredentials(implicitly)()
   }
 
   /**
@@ -100,10 +178,7 @@ trait CoursierModule extends mill.Module {
    * }}}
    * @return
    */
-  def coursierCacheCustomizer
-      : Task[Option[FileCache[coursier.util.Task] => FileCache[coursier.util.Task]]] =
+  def coursierCacheCustomizer: Task[Option[FileCache[c.util.Task] => FileCache[c.util.Task]]] =
     T.task { None }
 
 }
-
-object Cour
