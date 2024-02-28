47e66b35b7b9b5bf9e04e486cf44bebf501262f1
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-02-28 21:39:33 +0100

    Fix scalalib version for Scala 2

--- scalanativelib/src/mill/scalanativelib/ScalaNativeModule.scala
+++ scalanativelib/src/mill/scalanativelib/ScalaNativeModule.scala
@@ -65,13 +65,15 @@ trait ScalaNativeModule extends ScalaModule { outer =>
   }
 
   def nativeIvyDeps: T[Agg[Dep]] = T {
-    val scalaVersionSpecific =
-      if (ZincWorkerUtil.isScala3(scalaVersion())) {
-        if (scalaNativeVersion().startsWith("0.4"))
-          Agg(ivy"org.scala-native::scala3lib::${scalaNativeVersion()}")
-        else
-          Agg(ivy"org.scala-native::scala3lib::${scalaVersion()}+${scalaNativeVersion()}")
-      } else Agg(ivy"org.scala-native::scalalib::${scalaNativeVersion()}")
+    val scalaVersionSpecific = {
+      val version =
+        if(scalaNativeVersion().startsWith("0.4")) scalaNativeVersion()
+        else s"${scalaVersion()}+${scalaNativeVersion()}"
+
+      if (ZincWorkerUtil.isScala3(scalaVersion()))
+        Agg(ivy"org.scala-native::scala3lib::$version")
+      else Agg(ivy"org.scala-native::scalalib::$version")
+    }
 
     Agg(
       ivy"org.scala-native::nativelib::${scalaNativeVersion()}",
