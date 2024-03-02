d39937d6b3b2321abc7efed03fb0d4db277e11b9
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-03-02 09:34:22 +0100

    Align file names with packages
    Remove unused MillUtils for Scala Native 0.5

--- scalanativelib/worker/0.5/src/mill/scalanativelib/worker/scala/scalanative/Utils.scala
+++
@@ -1,6 +0,0 @@
-package scala.scalanative.build
-
-object MillUtils {
-  def targetsWindows(config: Config): Boolean = config.targetsWindows
-  def targetsMac(config: Config): Boolean = config.targetsMac
-}
