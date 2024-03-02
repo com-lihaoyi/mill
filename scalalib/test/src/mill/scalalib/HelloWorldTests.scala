64757f12d10b30550ce72db11acdfbf78b009305
Tobias Roeser <le.petit.fou@web.de>
2024-03-02 13:21:23 +0100

    formatted code

--- scalalib/test/src/mill/scalalib/HelloWorldTests.scala
+++ scalalib/test/src/mill/scalalib/HelloWorldTests.scala
@@ -695,7 +695,9 @@ object HelloWorldTests extends TestSuite {
       val message = intercept[Exception](
         CrossModuleDeps.cuttingEdge(scala213Version).moduleDeps
       ).getMessage
-      assert(message == s"Unable to find compatible cross version between ${scala213Version} and 2.12.6,3.2.0")
+      assert(
+        message == s"Unable to find compatible cross version between ${scala213Version} and 2.12.6,3.2.0"
+      )
     }
 
     "runMain" - {
