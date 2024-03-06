f9babf39c51909d1bc3524d30fdabfc21a5c3ef0
Tobias Roeser <le.petit.fou@web.de>
2024-03-06 09:41:05 +0100

    docs

--- scalalib/src/mill/scalalib/RunBaseModule.scala
+++ scalalib/src/mill/scalalib/RunBaseModule.scala
@@ -5,10 +5,28 @@ import mill.api.PathRef
 import mill.define.{Module, ModuleRef}
 
 trait RunBaseModule extends Module {
+
+  /**
+   * Any command-line parameters you want to pass to the forked JVM.
+   */
   def forkArgs: T[Seq[String]]
+
+  /**
+   * Any environment variables you want to pass to the forked JVM.
+   */
   def forkEnv: T[Map[String, String]]
+
   def forkWorkingDir: T[os.Path]
+
+  /**
+   * All classfiles and resources including upstream modules and dependencies
+   * necessary to run this module's code.
+   */
   def runClasspath: T[Seq[PathRef]]
+
+  /**
+   * Control whether `run*`-targets should use an args file to pass command line args, if possible.
+   */
   def runUseArgsFile: T[Boolean]
   def zincWorker: ModuleRef[ZincWorkerModule]
 }
