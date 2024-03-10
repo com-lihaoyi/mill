104a212b020a28b0462aa82b9581af17abb57fd9
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-03-10 12:16:28 +0100

    Add test to assert that compilation fails after deleting dependency

--- integration/feature/auxiliary-class-files/test/src/AuxiliaryClassFilesTests.scala
+++ integration/feature/auxiliary-class-files/test/src/AuxiliaryClassFilesTests.scala
@@ -22,6 +22,27 @@ object AuxiliaryClassFilesTests extends IntegrationTestSuite {
       assert(secondRun == Seq.empty)
     }
 
+    test("compilation fails when deleting a class used by other files") {
+      initWorkspace()
+
+      os.write(wd / "app" / "src" / "bar.scala", "object bar { println(foo) }")
+      val firstRunSuccessful = eval("app.jvm.compile")
+      assert(firstRunSuccessful)
+
+      val classes = wd / "out" / "app" / "jvm" / "compile.dest" / "classes"
+      val firstRun = os.list(classes).map(_.last)
+
+      os.remove(wd / "app" / "src" / "foo.scala")
+
+      val secondRunSuccessful = eval("app.jvm.compile")
+      assert(!secondRunSuccessful)
+
+      val secondRun = os.list(classes).map(_.last)
+
+      assert(firstRun == Seq("bar$.class", "bar.class", "bar.tasty", "foo$.class", "foo.class", "foo.tasty"))
+      assert(secondRun == Seq.empty)
+    }
+
     test("nir files are deleted together with companion class files") {
       initWorkspace()
       eval("app.native.compile")
