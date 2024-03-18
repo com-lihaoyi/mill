84310a8e019d509d6c88226c10fbf691af0be192
Tobias Roeser <le.petit.fou@web.de>
2024-03-18 11:23:23 +0100

    Disable ticker in output test

--- scalalib/test/src/mill/scalalib/CrossVersionTests.scala
+++ scalalib/test/src/mill/scalalib/CrossVersionTests.scala
@@ -119,7 +119,7 @@ object CrossVersionTests extends TestSuite {
   }
 
   def init()(implicit tp: TestPath) = {
-    val eval = new TestEvaluator(TestCases)
+    val eval = new TestEvaluator(TestCases, enableTicker = false)
     os.remove.all(eval.outPath)
     os.makeDir.all(TestCases.millSourcePath / os.up)
     eval
@@ -142,8 +142,9 @@ object CrossVersionTests extends TestSuite {
       if (!scala.util.Properties.isWin) {
         // Escape-sequence formatting isn't working under bare Windows
         val expectedDepsTree = tree
-        val depsTree =
-          os.read(eval.evaluator.pathsResolver.resolveDest(mod.ivyDepsTree(IvyDepsTreeArgs())).log)
+        val logFile =
+          eval.evaluator.pathsResolver.resolveDest(mod.ivyDepsTree(IvyDepsTreeArgs())).log
+        val depsTree = os.read(logFile)
         assert(depsTree == expectedDepsTree)
       }
     }
