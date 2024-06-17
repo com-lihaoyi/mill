13ff90dbf9239b0482b0210ceee529854817ad76
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-06-17 18:11:18 +0200

    Add test to check for mill-build

---
+++ integration/feature/bloop/test/src/BloopTests.scala
@@ -0,0 +1,28 @@
+package mill.integration
+
+import utest._
+
+object BloopTests extends IntegrationTestSuite {
+  initWorkspace()
+
+  val installResult = eval("mill.contrib.bloop.Bloop/install")
+
+  val tests: Tests = Tests {
+    test("test") - {
+      assert(installResult)
+
+      "root module bloop config should be created" - {
+        assert(os.exists(wd / ".bloop" / "root-module.json"))
+      }
+      val millBuildJsonFile = wd / ".bloop" / "mill-build-.json"
+      "mill-build module bloop config should be created" - {
+        assert(os.exists(millBuildJsonFile))
+      }
+
+      val config = ujson.read(os.read.stream(millBuildJsonFile))
+      "mill-build config should contain build.sc source" - {
+        assert(config("project")("sources").arr.exists(path => os.Path(path.str).last == "build.sc"))
+      }
+    }
+  }
+}
