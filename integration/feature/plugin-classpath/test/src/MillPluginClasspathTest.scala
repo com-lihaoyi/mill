2598291038e25178cb69ca726cdc88d2b31a02c2
Your Name <haoyi.sg@gmail.com>
2024-07-15 10:52:46 +0800

    .

--- integration/feature/plugin-classpath/test/src/MillPluginClasspathTest.scala
+++ integration/feature/plugin-classpath/test/src/MillPluginClasspathTest.scala
@@ -5,29 +5,6 @@ import utest._
 object MillPluginClasspathTest extends IntegrationTestSuite {
   initWorkspace()
 
-  val embeddedModules: Seq[(String, String)] = Seq(
-    ("com.lihaoyi", "mill-main-client"),
-    ("com.lihaoyi", "mill-main-api_2.13"),
-    ("com.lihaoyi", "mill-main-util_2.13"),
-    ("com.lihaoyi", "mill-main-codesig_2.13"),
-    ("com.lihaoyi", "mill-runner-linenumbers_2.13"),
-    ("com.lihaoyi", "mill-bsp_2.13"),
-    ("com.lihaoyi", "mill-scalanativelib-worker-api_2.13"),
-    ("com.lihaoyi", "mill-testrunner-entrypoint"),
-    ("com.lihaoyi", "mill-scalalib-api_2.13"),
-    ("com.lihaoyi", "mill-testrunner_2.13"),
-    ("com.lihaoyi", "mill-main-define_2.13"),
-    ("com.lihaoyi", "mill-main-resolve_2.13"),
-    ("com.lihaoyi", "mill-main-eval_2.13"),
-    ("com.lihaoyi", "mill-main_2.13"),
-    ("com.lihaoyi", "mill-scalalib_2.13"),
-    ("com.lihaoyi", "mill-scalanativelib_2.13"),
-    ("com.lihaoyi", "mill-scalajslib-worker-api_2.13"),
-    ("com.lihaoyi", "mill-scalajslib_2.13"),
-    ("com.lihaoyi", "mill-runner_2.13"),
-    ("com.lihaoyi", "mill-idea_2.13")
-  )
-
   val tests: Tests = Tests {
     test("runClasspath") - {
       // We expect Mill core transitive dependencies to be filtered out
