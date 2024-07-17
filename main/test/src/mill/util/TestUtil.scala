27ab50a585ee2264937505a36f9194439169ade0
Your Name <haoyi.sg@gmail.com>
2024-07-17 16:17:28 +0800

    TEST_DEST_FOLDER

--- main/test/src/mill/util/TestUtil.scala
+++ main/test/src/mill/util/TestUtil.scala
@@ -12,7 +12,7 @@ import scala.collection.mutable
 
 object TestUtil extends MillTestKit {
 
-  override val targetDir = os.pwd / "target"
+  override val targetDir = os.Path(sys.env("MILL_TEST_DEST_FOLDER"))
 
   def getOutPath()(implicit fullName: sourcecode.FullName, tp: TestPath): os.Path = {
     getOutPath(tp.value)
