eef518d3efbc384d430d366e11e99bfddd081fa1
Your Name <haoyi.sg@gmail.com>
2024-07-19 10:45:47 +0800

    .

---
+++ example/javaweb/1-hello-spring-boot/test/src/HelloSpringBootTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloSpringBootTest {
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void shouldReturnDefaultMessage() {
+    String response = restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+    assertEquals("Hello, World!", response);
+  }
+}
