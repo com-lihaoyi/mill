870acf5b57ffb02bdd0c6f3532a28ddcbdc69508
Pawel Sadlo <psadlo@virtuslab.com>
2024-08-01 13:54:21 +0200

    docs

--- bsp/worker/src/mill/bsp/worker/SyntheticRootBspBuildTargetData.scala
+++ bsp/worker/src/mill/bsp/worker/SyntheticRootBspBuildTargetData.scala
@@ -10,7 +10,7 @@ import scala.jdk.CollectionConverters._
 
 /**
  * Synthesised [[BspBuildTarget]] to handle exclusions.
- * Intellij-Bsp doesn't provide a way to exclude files outside of module,so if there is no module with [[topLevelProjectRoot]] as content root [[SyntheticRootBspBuildTargetData]] will be created
+ * Intellij-Bsp doesn't provide a way to exclude files outside of module,so if there is no module having content root as [[topLevelProjectRoot]], [[SyntheticRootBspBuildTargetData]] will be created
 */
 class SyntheticRootBspBuildTargetData(topLevelProjectRoot:os.Path) {
   val id: BuildTargetIdentifier = new BuildTargetIdentifier(Utils.sanitizeUri(topLevelProjectRoot / s"synth-build-target-${UUID.randomUUID()}"))
