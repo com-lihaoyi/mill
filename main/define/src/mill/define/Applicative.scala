9300acd65875ffd328a84b527a019f98d26251b6
Jamie Thompson <bishbashboshjt@gmail.com>
2024-08-12 10:34:11 +0200

    Part 4 - reimplement Applicative and Target macros

--- main/define/src/mill/define/Applicative.scala
+++ main/define/src/mill/define/Applicative.scala
@@ -4,6 +4,8 @@ import mill.api.internal
 
 import scala.annotation.compileTimeOnly
 
+import scala.quoted.*
+
 /**
  * A generic Applicative-functor macro: translates calls to
  *
@@ -23,7 +25,7 @@ object Applicative {
     def apply[T](t: M[T]): T
   }
   object ApplyHandler {
-    // @compileTimeOnly("Target#apply() can only be used with a Task{...} block")
+    @compileTimeOnly("Target#apply() can only be used with a Task{...} block")
     implicit def defaultApplyHandler[M[+_]]: ApplyHandler[M] = ???
   }
   trait Applyable[M[+_], +T] {
@@ -38,73 +40,110 @@ object Applicative {
     def traverseCtx[I, R](xs: Seq[W[I]])(f: (IndexedSeq[I], Ctx) => Z[R]): T[R]
   }
 
-  def impl[M[_], T: c.WeakTypeTag, Ctx: c.WeakTypeTag](c: Context)(t: c.Expr[T]): c.Expr[M[T]] = {
-    // impl0(c)(t.tree)(implicitly[c.WeakTypeTag[T]], implicitly[c.WeakTypeTag[Ctx]])
-    ???
-  }
-  def impl0[M[_], T: c.WeakTypeTag, Ctx: c.WeakTypeTag](c: Context)(t: c.Tree): c.Expr[M[T]] = {
-    ???
-    // import c.universe._
-    // def rec(t: Tree): Iterator[c.Tree] = Iterator(t) ++ t.children.flatMap(rec(_))
+  def impl[M[_]: Type, W[_]: Type, Z[_]: Type, T: Type, Ctx: Type](using
+      Quotes
+  )(
+      traverseCtx: (Expr[Seq[W[Any]]], Expr[(IndexedSeq[Any], Ctx) => Z[T]]) => Expr[M[T]],
+      t: Expr[Z[T]]
+  ): Expr[M[T]] = {
+    import quotes.reflect.*
 
-    // val exprs = collection.mutable.Buffer.empty[c.Tree]
-    // val targetApplySym = typeOf[Applyable[Nothing, _]].member(TermName("apply"))
+    val targetApplySym = TypeRepr.of[Applyable[Nothing, ?]].typeSymbol.methodMember("apply").head
 
-    // val itemsName = c.freshName(TermName("items"))
-    // val itemsSym = c.internal.newTermSymbol(c.internal.enclosingOwner, itemsName)
-    // c.internal.setFlag(itemsSym, (1L << 44).asInstanceOf[c.universe.FlagSet])
-    // c.internal.setInfo(itemsSym, typeOf[Seq[Any]])
-    // // Derived from @olafurpg's
-    // // https://gist.github.com/olafurpg/596d62f87bf3360a29488b725fbc7608
-    // val defs = rec(t).filter(_.isDef).map(_.symbol).toSet
+    // Derived from @olafurpg's
+    // https://gist.github.com/olafurpg/596d62f87bf3360a29488b725fbc7608
 
-    // val ctxName = TermName(c.freshName("ctx"))
-    // val ctxSym = c.internal.newTermSymbol(c.internal.enclosingOwner, ctxName)
-    // c.internal.setInfo(ctxSym, weakTypeOf[Ctx])
+    def extractDefs(tree: Tree): Set[Symbol] =
+      new TreeAccumulator[Set[Symbol]] {
+        override def foldTree(x: Set[Symbol], tree: Tree)(owner: Symbol): Set[Symbol] = tree match
+          case tree: Definition => foldOverTree(x + tree.symbol, tree)(owner)
+          case tree => foldOverTree(x, tree)(owner)
+      }.foldTree(Set.empty, tree)(Symbol.spliceOwner)
 
-    // val transformed = c.internal.typingTransform(t) {
-    //   case (t @ q"$fun.apply()($handler)", api) if t.symbol == targetApplySym =>
-    //     val localDefs = rec(fun).filter(_.isDef).map(_.symbol).toSet
-    //     val banned = rec(t).filter(x => defs(x.symbol) && !localDefs(x.symbol))
+    def visitAllTrees(tree: Tree)(f: Tree => Unit): Unit =
+      new TreeTraverser {
+        override def traverseTree(tree: Tree)(owner: Symbol): Unit =
+          f(tree)
+          traverseTreeChildren(tree)(owner)
+      }.traverseTree(tree)(Symbol.spliceOwner)
 
-    //     if (banned.hasNext) {
-    //       val banned0 = banned.next()
-    //       c.abort(
-    //         banned0.pos,
-    //         "Target#apply() call cannot use `" + banned0.symbol + "` defined within the Task{...} block"
-    //       )
-    //     }
-    //     val tempName = c.freshName(TermName("tmp"))
-    //     val tempSym = c.internal.newTermSymbol(c.internal.enclosingOwner, tempName)
-    //     c.internal.setInfo(tempSym, t.tpe)
-    //     val tempIdent = Ident(tempSym)
-    //     c.internal.setType(tempIdent, t.tpe)
-    //     c.internal.setFlag(tempSym, (1L << 44).asInstanceOf[c.universe.FlagSet])
-    //     val itemsIdent = Ident(itemsSym)
-    //     exprs.append(q"$fun")
-    //     c.typecheck(q"$itemsIdent(${exprs.size - 1}).asInstanceOf[${t.tpe}]")
-    //   case (t, api)
-    //       if t.symbol != null
-    //         && t.symbol.annotations.exists(_.tree.tpe =:= typeOf[mill.api.Ctx.ImplicitStub]) =>
-    //     val tempIdent = Ident(ctxSym)
-    //     c.internal.setType(tempIdent, t.tpe)
-    //     c.internal.setFlag(ctxSym, (1L << 44).asInstanceOf[c.universe.FlagSet])
-    //     tempIdent
+    val defs = extractDefs(t.asTerm)
 
-    //   case (t, api) => api.default(t)
-    // }
+    var hasErrors = false
 
-    // val ctxBinding = c.internal.valDef(ctxSym)
+    def macroError(msg: String, pos: Position): Unit = {
+      hasErrors = true
+      report.error(msg, pos)
+    }
 
-    // val itemsBinding = c.internal.valDef(itemsSym)
-    // val callback = c.typecheck(q"{(${itemsBinding}, ${ctxBinding}) => $transformed}")
+    def transformed(
+        itemsRef: Expr[IndexedSeq[Any]],
+        ctxRef: Expr[Ctx]
+    ): (Expr[Z[T]], Expr[Seq[W[Any]]]) = {
+      val exprs = collection.mutable.Buffer.empty[Tree]
+      val treeMap = new TreeMap {
 
-    // val res =
-    //   q"${c.prefix}.traverseCtx[_root_.scala.Any, ${weakTypeOf[T]}](${exprs.toList}){ $callback }"
+        override def transformTerm(tree: Term)(owner: Symbol): Term = tree match
+          // case t @ '{$fun.apply()($handler)}
+          case t @ Apply(Apply(sel @ Select(fun, "apply"), Nil), List(handler))
+              if sel.symbol == targetApplySym =>
+            val localDefs = extractDefs(fun)
+            visitAllTrees(t) { x =>
+              val sym = x.symbol
+              if (sym != Symbol.noSymbol && defs(sym) && !localDefs(sym)) {
+                macroError(
+                  "Target#apply() call cannot use `" + x.symbol + "` defined within the Task{...} block",
+                  x.pos
+                )
+              }
+            }
 
-    // c.internal.changeOwner(transformed, c.internal.enclosingOwner, callback.symbol)
+            t.tpe.asType match
+              case '[tt] =>
+                // val tempName = c.freshName(TermName("tmp"))
+                // val tempSym = c.internal.newTermSymbol(c.internal.enclosingOwner, tempName)
+                // c.internal.setInfo(tempSym, t.tpe)
+                // val tempIdent = Ident(tempSym)
+                // c.internal.setType(tempIdent, t.tpe)
+                // c.internal.setFlag(tempSym, (1L << 44).asInstanceOf[c.universe.FlagSet])
+                // val itemsIdent = Ident(itemsSym)
+                // exprs.append(q"$fun")
+                exprs += fun
+                '{ $itemsRef(${ Expr(exprs.size - 1) }).asInstanceOf[tt] }.asTerm
+          case t
+              if t.symbol.exists
+                && t.symbol.annotations.exists(_.tpe =:= TypeRepr.of[mill.api.Ctx.ImplicitStub]) =>
+            // val tempIdent = Ident(ctxSym)
+            // c.internal.setType(tempIdent, t.tpe)
+            // c.internal.setFlag(ctxSym, (1L << 44).asInstanceOf[c.universe.FlagSet])
+            // tempIdent
+            ctxRef.asTerm
 
-    // c.Expr[M[T]](res)
+          case t => super.transformTerm(t)(owner)
+        end transformTerm
+      }
+
+      val newBody = treeMap.transformTree(t.asTerm)(Symbol.spliceOwner).asExprOf[Z[T]]
+      val exprsList = Expr.ofList(exprs.toList.map(_.asExprOf[W[Any]]))
+      (newBody, exprsList)
+    }
+
+    val (callback, exprsList) = {
+      var exprsExpr: Expr[Seq[W[Any]]] | Null = null
+      val cb = '{ (items: IndexedSeq[Any], ctx: Ctx) =>
+        ${
+          val (body, exprs) = transformed('items, 'ctx)
+          exprsExpr = exprs
+          body
+        }
+      }
+      (cb, exprsExpr.nn)
+    }
+
+    if hasErrors then
+      '{ throw new RuntimeException("stub implementation - macro expansion had errors") }
+    else
+      traverseCtx(exprsList, callback)
   }
 
 }
