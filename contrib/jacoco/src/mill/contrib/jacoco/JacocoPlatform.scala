f3796942f55c084740042dbcc3e453f317682289
Your Name <haoyi.sg@gmail.com>
2024-08-20 04:26:25 +0800

    .

--- contrib/jacoco/src/mill/contrib/jacoco/JacocoPlatform.scala
+++
@@ -1,7 +0,0 @@
-package mill.contrib.jacoco
-
-trait JacocoPlatform {
-  import mill.main.TokenReaders._
-//  implicit def taskTokenReader[T] = new mill.main.TaskTokenReader[T]()
-//  implicit def evaluatorTokenReader[T] = new mill.main.EvaluatorTokenReader[T]()
-}
--- contrib/jacoco/test/resources/jacoco/src/test/Main.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Main!");
-    }
-}
