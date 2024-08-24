0e77a149bb9bfd095cc8ae4b0bbe7415a17d1efe
Li Haoyi <haoyi.sg@gmail.com>
2024-08-24 22:37:39 +0800

    .

--- ci/test-mill-release.sh
+++ ci/test-mill-release.sh
@@ -12,10 +12,10 @@ rm -rf $EXAMPLE/out
 test ! -d $EXAMPLE/out/foo/3.3.3/compile.dest
 test ! -f $EXAMPLE/out/bar/2.13.8/assembly.dest/out.jar
 
-(cd $EXAMPLE && ../../../out/dev/assembly.dest/mill -i "foo[3.3.3].run")
+(cd $EXAMPLE && ../../../../out/dev/assembly.dest/mill -i "foo[3.3.3].run")
 
 test -d $EXAMPLE/out/foo/3.3.3/compile.dest
 
-(cd $EXAMPLE && ../../../out/dev/assembly.dest/mill show "bar[2.13.8].assembly")
+(cd $EXAMPLE && ../../../../out/dev/assembly.dest/mill show "bar[2.13.8].assembly")
 
 test -f $EXAMPLE/out/bar/2.13.8/assembly.dest/out.jar
