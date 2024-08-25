e4eb7cb0dbede02d83aafaecaa5d6c3e8e25c56c
Li Haoyi <haoyi.sg@gmail.com>
2024-08-25 08:45:43 +0800

    Update test-mill-release.sh

--- ci/test-mill-release.sh
+++ ci/test-mill-release.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Build Mill
-./mill -i dev.assembly
+./mill -i dist.assembly
 
 EXAMPLE=example/scalalib/builds/9-realistic
 
@@ -12,10 +12,10 @@ rm -rf $EXAMPLE/out
 test ! -d $EXAMPLE/out/foo/3.3.3/compile.dest
 test ! -f $EXAMPLE/out/bar/2.13.8/assembly.dest/out.jar
 
-(cd $EXAMPLE && ../../../../out/dev/assembly.dest/mill -i "foo[3.3.3].run")
+(cd $EXAMPLE && ../../../../out/dist/assembly.dest/mill -i "foo[3.3.3].run")
 
 test -d $EXAMPLE/out/foo/3.3.3/compile.dest
 
-(cd $EXAMPLE && ../../../../out/dev/assembly.dest/mill show "bar[2.13.8].assembly")
+(cd $EXAMPLE && ../../../../out/dist/assembly.dest/mill show "bar[2.13.8].assembly")
 
 test -f $EXAMPLE/out/bar/2.13.8/assembly.dest/out.jar
