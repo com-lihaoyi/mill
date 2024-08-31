6368a0887acffca3dd4076adbdbabdc015d3a7b4
Li Haoyi <haoyi.sg@gmail.com>
2024-08-31 09:47:42 +0800

    .

--- main/client/src/mill/main/client/CodeGenConstants.java
+++ main/client/src/mill/main/client/CodeGenConstants.java
@@ -2,8 +2,9 @@ package mill.main.client;
 
 
 public class CodeGenConstants {
-    final public static String globalPackagePrefix = "millbuild";
+    final public static String globalPackagePrefix = "build_";
     final public static String wrapperObjectName = "package_";
+
     final public static String rootBuildFileName = "build.sc";
     final public static String nestedBuildFileName = "package.sc";
     final public static String rootModuleAlias = "build";
