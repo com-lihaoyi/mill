b193f5cb7549be92ffbfe90b080460ed811fc26a
Li Haoyi <haoyi.sg@gmail.com>
2024-09-02 17:39:48 +0800

    fix for leaking Mill servers in example tests

--- testkit/src/mill/testkit/IntegrationTestSuite.scala
+++ testkit/src/mill/testkit/IntegrationTestSuite.scala
@@ -1,13 +1,6 @@
 package mill.testkit
 
-import os.Path
-import utest._
-
-abstract class IntegrationTestSuite extends TestSuite with IntegrationTester.Impl {
-  def workspaceSourcePath: os.Path = os.Path(sys.env("MILL_INTEGRATION_REPO_ROOT"))
-  val clientServerMode: Boolean = sys.env("MILL_INTEGRATION_SERVER_MODE").toBoolean
-
-  def millExecutable: Path = os.Path(System.getenv("MILL_INTEGRATION_LAUNCHER"), os.pwd)
+abstract class IntegrationTestSuite extends IntegrationTestSuiteBase with IntegrationTester.Impl {
   override def utestAfterEach(path: Seq[String]): Unit = {
     if (clientServerMode) close()
   }
