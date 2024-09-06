3ea823f98b086ec5dfeff8a9ca6add9a8ea9cc99
Li Haoyi <haoyi.sg@gmail.com>
2024-09-06 08:26:52 +0800

    .

--- scalalib/worker/src/mill/scalalib/worker/ZincWorkerImpl.scala
+++ scalalib/worker/src/mill/scalalib/worker/ZincWorkerImpl.scala
@@ -643,9 +643,10 @@ class ZincWorkerImpl(
   override def close(): Unit = {
     val closeableClassloaders = classloaderCache
       .flatMap(_._2.get)
-      .collect{case v: AutoCloseable => v}
+      .collect { case v: AutoCloseable => v }
 
-    val urlClassLoaders = classloaderCache.flatMap(_._2.get).collect{case t: java.net.URLClassLoader => t}
+    val urlClassLoaders =
+      classloaderCache.flatMap(_._2.get).collect { case t: java.net.URLClassLoader => t }
 
     // Make sure we at least pick up all the URLClassLoaders, since we know those are
     // AutoCloseable, although there may be other AutoCloseable classloaders as well
