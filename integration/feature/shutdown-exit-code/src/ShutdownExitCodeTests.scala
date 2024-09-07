d6816f3444b82e981d87b3dcb9f29d2fb39bc381
Li Haoyi <haoyi.sg@gmail.com>
2024-09-07 10:44:40 +0800

    .

--- integration/feature/shutdown-exit-code/src/ShutdownExitCodeTests.scala
+++ integration/feature/shutdown-exit-code/src/ShutdownExitCodeTests.scala
@@ -8,10 +8,15 @@ object ShutdownExitCodeTests extends UtestIntegrationTestSuite {
   // Ensure that `shutdown` succeeds even if the prior command failed
   val tests: Tests = Tests {
     test("test") - integrationTest { tester =>
-      val result1 = tester.eval("doesnt-exit")
-      assert(result1.isSuccess == false)
+      val result1 = tester.eval(("resolve", "_"))
+      assert(result1.isSuccess == true)
       val result2 = tester.eval("shutdown")
       assert(result2.isSuccess == true)
+
+      val result3 = tester.eval("doesnt-exit")
+      assert(result3.isSuccess == false)
+      val result4 = tester.eval("shutdown")
+      assert(result4.isSuccess == true)
     }
   }
 }
