8325d8abd72894f96b9d5c05375cf86ac0341a49
Li Haoyi <haoyi.sg@gmail.com>
2024-09-09 10:16:17 +0800

    .

--- scalalib/src/mill/scalalib/JavaModule.scala
+++ scalalib/src/mill/scalalib/JavaModule.scala
@@ -108,7 +108,8 @@ trait JavaModule
           case mains =>
             Left(
               s"Multiple main classes found (${mains.mkString(",")}) " +
-                "please explicitly specify which one to use by overriding mainClass"
+              "please explicitly specify which one to use by overriding `mainClass` " +
+              "or using `runMain <main-class> <...args>` instead of `run`"
             )
         }
     }
