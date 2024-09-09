775f3721f1f74a3eedfdbb1e7bc09e4e335fbfb9
Ajay Chandran <ajay.chandran.1980@gmail.com>
2024-09-10 02:17:17 +0530

    Improved test data

--- contrib/checkstyle/test/resources/compatible-scala/src/com/etsy/sbt/TestClass.scala
+++ contrib/checkstyle/test/resources/compatible-scala/src/com/etsy/sbt/TestClass.scala
@@ -1,3 +1,7 @@
 package com.etsy.sbt
 
-object TestClass
+object TestClass {
+
+// Checkstyle should ignore a Scala file
+// introduce a compile error to trigger failure in case this file is checked
+//}
