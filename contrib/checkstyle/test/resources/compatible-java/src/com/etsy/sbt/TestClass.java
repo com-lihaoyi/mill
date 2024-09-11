d1f96e95813a1db6ca1a45ec70f7380718c162e7
Ajay Chandran <ajay.chandran.1980@gmail.com>
2024-09-11 17:24:54 +0530

    Added plugins to run Checkstyle and generate reports

---
+++ contrib/checkstyle/test/resources/compatible-java/src/com/etsy/sbt/TestClass.java
@@ -0,0 +1,11 @@
+package com.etsy.sbt;
+
+public class TestClass {
+
+    private TestClass() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
