a02a6f3c4c4649eb61977d02b7e9ffd706aa0f70
Li Haoyi <haoyi.sg@gmail.com>
2024-09-12 09:10:58 +0800

    .

--- example/depth/sandbox/3-breaking/foo/test/src/foo/FooTests.java
+++ example/depth/sandbox/3-breaking/foo/test/src/foo/FooTests.java
@@ -9,7 +9,6 @@ public class FooTests {
     @Test
     public void simple() throws Exception {
         String workspaceRoot = System.getenv("MILL_WORKSPACE_ROOT");
-        if (workspaceRoot == null) workspaceRoot = java.nio.file.Paths.get("").toAbsolutePath
 
         for(Path subpath: Files.list(Paths.get(workspaceRoot)).collect(Collectors.toList())){
             String result = Foo.generateHtml(subpath.getFileName().toString());
