8fb5a805e49a2bfe5fe5073f09096cb59ba8f66c
Li Haoyi <haoyi.sg@gmail.com>
2024-09-12 13:53:13 +0800

    .

--- ci/release-maven.sh
+++ ci/release-maven.sh
@@ -14,10 +14,7 @@ rm gpg_key
 # Publish all artifacts
 ./mill -i \
     mill.scalalib.PublishModule/publishAll \
-    --sonatypeCreds $SONATYPE_USERNAME:$SONATYPE_PASSWORD \
     --gpgArgs --passphrase=$SONATYPE_PGP_PASSWORD,--no-tty,--pinentry-mode,loopback,--batch,--yes,-a,-b \
     --publishArtifacts __.publishArtifacts \
-    --readTimeout  3600000 \
-    --awaitTimeout 3600000 \
     --release true \
     --signed  true
