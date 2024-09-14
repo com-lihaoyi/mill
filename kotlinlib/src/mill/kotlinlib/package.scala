55e5bc6cfc4e112aa7f965ebe106457c96abfc91
0xnm <0xnm@users.noreply.github.com>
2024-09-14 17:52:40 +0200

    Re-export common types in Kotlin package

---
+++ kotlinlib/src/mill/kotlinlib/package.scala
@@ -0,0 +1,23 @@
+package mill
+
+package object kotlinlib {
+  implicit class DepSyntax(ctx: StringContext) {
+    def ivy(args: Any*): Dep = Dep.parse {
+      (
+        ctx.parts.take(args.length).zip(args).flatMap { case (p, a) => Seq(p, a) } ++
+          ctx.parts.drop(args.length)
+      ).mkString
+    }
+  }
+
+  type Dep = mill.scalalib.Dep
+  val Dep = mill.scalalib.Dep
+
+  type MavenModule = mill.scalalib.MavenModule
+
+  type TestModule = mill.scalalib.TestModule
+  val TestModule = mill.scalalib.TestModule
+
+  type PublishModule = mill.scalalib.PublishModule
+  val PublishModule = mill.scalalib.PublishModule
+}
--- kotlinlib/test/src/mill/kotlinlib/HelloWorldTests.scala
+++ kotlinlib/test/src/mill/kotlinlib/HelloWorldTests.scala
@@ -1,7 +1,7 @@
 package mill
 package kotlinlib
 
-import mill.scalalib.{DepSyntax, TestModule}
+import mill.scalalib.TestModule
 import mill.testkit.{TestBaseModule, UnitTester}
 import mill.api.Result
 import utest._
