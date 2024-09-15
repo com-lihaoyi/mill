c0e7665729da9c112cc2641853c7e955686f233f
0xnm <0xnm@users.noreply.github.com>
2024-09-15 23:25:08 +0200

    Add example/kotlinlib/linting

---
+++ example/kotlinlib/linting/2-ktlint/src/example/FooWrong.kt
@@ -0,0 +1,11 @@
+package example;
+
+class foo {
+
+    fun someFun(one: String,
+                two: String) = Unit
+
+    companion object {
+        const val LINE = "veryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyylongline"
+    }
+}
