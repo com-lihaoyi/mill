cb6f9b6df5c5979aabe5a200663a1af223350a80
Li Haoyi <haoyi.sg@gmail.com>
2024-09-19 13:58:52 +0800

    .

--- main/util/src/mill/util/MultilinePromptLogger.scala
+++ main/util/src/mill/util/MultilinePromptLogger.scala
@@ -37,7 +37,7 @@ class MultilinePromptLogger(
 
   val promptUpdaterThread = new Thread(() => {
     while (!stopped) {
-      Thread.sleep(100)
+      Thread.sleep(promptUpdateIntervalMillis)
       if (!paused) {
         synchronized {
           state.refreshPrompt()
@@ -68,7 +68,7 @@ class MultilinePromptLogger(
 
   override def rawOutputStream: PrintStream = systemStreams0.out
 
-  class StateStream(wrapped: PrintStream) extends OutputStream {
+  private class StateStream(wrapped: PrintStream) extends OutputStream {
     override def write(b: Array[Byte], off: Int, len: Int): Unit = synchronized {
       lastIndexOfNewline(b, off, len) match {
         case -1 => wrapped.write(b, off, len)
@@ -97,10 +97,24 @@ class MultilinePromptLogger(
 }
 
 object MultilinePromptLogger {
-  case class Status(startTimeMillis: Long, text: String)
+  /**
+   * How often to update the multiline status prompt on the terminal.
+   * Too frequent is bad because it causes a lot of visual noise,
+   * but too infrequent results in latency. 10 times per second seems reasonable
+   */
+  private val promptUpdateIntervalMillis = 100
+  /**
+   * Add some extra latency delay to the process of removing an entry from the status
+   * prompt entirely, because removing an entry changes the height of the prompt, which
+   * is even more distracting than changing the contents of a line, so we want to minimize
+   * those occurrences even further.
+   */
+  private val statusRemovalDelayMillis = 500
+
+  private case class Status(startTimeMillis: Long, text: String, var removedTimeMillis: Long)
 
   private class State(enableTicker: Boolean, systemStreams0: SystemStreams, startTimeMillis: Long) {
-    private val current = collection.mutable.SortedMap.empty[Int, Seq[Status]]
+    private val statuses = collection.mutable.SortedMap.empty[Int, Status]
 
     private var header = ""
     // Pre-compute the prelude and current prompt as byte arrays so that
@@ -109,18 +123,23 @@ object MultilinePromptLogger {
     private var currentPromptBytes: Array[Byte] = Array[Byte]()
 
     private def updatePromptBytes() = {
+      val now = System.currentTimeMillis()
+      for(k <- statuses.keySet){
+        val removedTime = statuses(k).removedTimeMillis
+        if (removedTime != -1 && now - removedTime > statusRemovalDelayMillis){
+          statuses.remove(k)
+        }
+      }
       // Limit to <120 chars wide
       val maxWidth = 119
-      val now = System.currentTimeMillis()
+
       val totalSecondsStr = renderSeconds(now - startTimeMillis)
       val divider = "=" * (maxWidth - header.length - totalSecondsStr.length - 4)
       val currentPrompt = List(s"  $header $divider $totalSecondsStr") ++
-        current
+        statuses
           .collect {
-            case (threadId, statuses) if statuses.nonEmpty =>
-              val statusesString = statuses
-                .map { status => status.text + " " + renderSeconds(now - status.startTimeMillis) }
-                .mkString(" / ")
+            case (threadId, status) =>
+              val statusesString = status.text + " " + renderSeconds(now - status.startTimeMillis)
 
               if (statusesString.length <= maxWidth) statusesString
               else {
@@ -143,11 +162,10 @@ object MultilinePromptLogger {
     def updateCurrent(sOpt: Option[String]): Unit = synchronized {
       val threadId = Thread.currentThread().getId.toInt
 
+      val now = System.currentTimeMillis()
       sOpt match {
-        case None => current.remove(threadId)
-        case Some(s) =>
-          current(threadId) =
-            current.getOrElse(threadId, Nil) :+ Status(System.currentTimeMillis(), s)
+        case None => statuses.get(threadId).foreach(_.removedTimeMillis = now)
+        case Some(s) => statuses(threadId) = Status(now, s, -1)
       }
       updatePromptBytes()
     }
