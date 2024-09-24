101802ae2e5e24b55a580d125cb75546f82172d9
Li Haoyi <haoyi.sg@gmail.com>
2024-09-24 12:24:20 +0800

    implement test terminal

--- main/util/test/src/mill/util/MultilinePromptLoggerTests.scala
+++ main/util/test/src/mill/util/MultilinePromptLoggerTests.scala
@@ -5,7 +5,6 @@ import mill.main.client.ProxyStream
 import utest._
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintStream}
-
 object MultilinePromptLoggerTests extends TestSuite {
 
   val tests = Tests {
@@ -75,5 +74,136 @@ object MultilinePromptLoggerTests extends TestSuite {
 
       assert(lines == expected)
     }
+    test("testTerminal"){
+      test("wrap") {
+        val t = new TestTerminal(width = 10)
+        t.writeAll("1234567890abcdef")
+        t.grid ==> Seq(
+          "1234567890",
+          "abcdef"
+        )
+      }
+      test("newline") {
+        val t = new TestTerminal(width = 10)
+        t.writeAll("12345\n67890")
+        t.grid ==> Seq(
+          "12345",
+          "67890"
+        )
+      }
+      test("wrapNewline") {
+        val t = new TestTerminal(width = 10)
+        t.writeAll("1234567890\nabcdef")
+        t.grid ==> Seq(
+          "1234567890",
+          "abcdef"
+        )
+      }
+      test("wrapNewline2") {
+        val t = new TestTerminal(width = 10)
+        t.writeAll("1234567890\n\nabcdef")
+        t.grid ==> Seq(
+          "1234567890",
+          "",
+          "abcdef"
+        )
+      }
+      test("up") {
+        val t = new TestTerminal(width = 15)
+        t.writeAll(s"1234567890\nabcdef${AnsiNav.up(1)}X")
+        t.grid ==> Seq(
+          "123456X890",
+          "abcdef",
+        )
+      }
+      test("left") {
+        val t = new TestTerminal(width = 15)
+        t.writeAll(s"1234567890\nabcdef${AnsiNav.left(3)}X")
+        t.grid ==> Seq(
+          "1234567890",
+          "abcXef",
+        )
+      }
+      test("upLeftClearLine") {
+        val t = new TestTerminal(width = 15)
+        t.writeAll(s"1234567890\nabcdef${AnsiNav.up(1)}${AnsiNav.left(3)}X${AnsiNav.clearLine(0)}")
+        t.grid ==> Seq(
+          "123X",
+          "abcdef",
+        )
+      }
+      test("upLeftClearScreen") {
+        val t = new TestTerminal(width = 15)
+        t.writeAll(s"1234567890\nabcdef${AnsiNav.up(1)}${AnsiNav.left(3)}X${AnsiNav.clearScreen(0)}")
+        t.grid ==> Seq("123X")
+      }
+    }
   }
 }
+
+class TestTerminal(width: Int){
+  var grid = collection.mutable.Buffer("")
+  var cursorX = 0
+  var cursorY = 0
+
+  def writeAll(data0: String): Unit = {
+
+    def rec(data: String): Unit = data match{ // pprint.log((grid, cursorX, cursorY, data.head))
+      case "" => // end
+      case s"\u001b[${n}A$rest" => // up
+        cursorY = math.max(cursorY - n.toInt, 0)
+        rec(rest)
+
+      case s"\u001b[${n}B$rest" => // down
+        cursorY = math.min(cursorY + n.toInt, grid.size)
+        rec(rest)
+
+      case s"\u001b[${n}C$rest" => // right
+        cursorX = math.min(cursorX + n.toInt, width)
+        rec(rest)
+
+      case s"\u001b[${n}D$rest" => // left
+        cursorX = math.max(cursorX - n.toInt, 0)
+        rec(rest)
+
+      case s"\u001b[${n}J$rest" => // clearscreen
+        n match{
+          case "0" =>
+            grid(cursorY) = grid(cursorY).take(cursorX)
+            grid = grid.take(cursorY + 1)
+            rec(rest)
+        }
+
+      case s"\u001b[${n}K$rest" => // clearline
+        n match{
+          case "0" =>
+            grid(cursorY) = grid(cursorY).take(cursorX)
+            rec(rest)
+        }
+
+      case normal => // normal text
+        if (normal.head == '\n'){
+          cursorX = 0
+          if (cursorY >= grid.length) grid.append("")
+          cursorY += 1
+        }else {
+          if (cursorX == width){
+            cursorX = 0
+            cursorY += 1
+          }
+          if (cursorY >= grid.length) grid.append("")
+          grid(cursorY) = grid(cursorY).patch(cursorX, Seq(normal.head), 1)
+          if (cursorX < width) {
+            cursorX += 1
+          } else {
+            cursorX = 0
+            cursorY += 1
+          }
+
+        }
+        rec(normal.tail)
+    }
+    rec(data0)
+  }
+}
+
