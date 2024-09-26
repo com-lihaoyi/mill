5131bac56c916c3b82503dd3d28722530bdb8aba
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-09-26 17:04:37 +0200

    Add BSP integration test

---
+++ integration/ide/bsp-server/src/BspServerTests.scala
@@ -0,0 +1,159 @@
+package mill.integration
+
+import ch.epfl.scala.{bsp4j => b}
+import mill.api.BuildInfo
+import mill.bsp.Constants
+import mill.testkit.BspServerUtil._
+import mill.testkit.UtestIntegrationTestSuite
+import utest._
+
+import scala.jdk.CollectionConverters._
+
+object BspServerTests extends UtestIntegrationTestSuite {
+  def fixturePath: os.Path =
+    super.workspaceSourcePath / "fixtures"
+  override protected def workspaceSourcePath: os.Path =
+    super.workspaceSourcePath / "project"
+
+  def tests: Tests = Tests {
+    test("BSP install") - integrationTest { tester =>
+      import tester._
+      eval("mill.bsp.BSP/install", stdout = os.Inherit, stderr = os.Inherit, check = true)
+
+      withBspServer(
+        workspacePath,
+        millTestSuiteEnv
+      ) { (buildServer, initRes) =>
+        val scalaVersion = sys.props.getOrElse("MILL_TEST_SCALA_2_13_VERSION", ???)
+        val scalaTransitiveSubstitutions = {
+          val scalaFetchRes = coursierapi.Fetch.create()
+            .addDependencies(coursierapi.Dependency.of(
+              "org.scala-lang",
+              "scala-compiler",
+              scalaVersion
+            ))
+            .fetchResult()
+          scalaFetchRes.getDependencies.asScala
+            .filter(dep => dep.getModule.getOrganization != "org.scala-lang")
+            .map { dep =>
+              def basePath(version: String) =
+                s"${dep.getModule.getOrganization.split('.').mkString("/")}/${dep.getModule.getName}/$version/${dep.getModule.getName}-$version"
+              basePath(dep.getVersion) -> basePath(s"<${dep.getModule.getName}-version>")
+            }
+        }
+
+        val replaceAll = replaceAllValues(workspacePath) ++ scalaTransitiveSubstitutions ++ Seq(
+          scalaVersion -> "<scala-version>"
+        )
+
+        compareWithGsonFixture(
+          initRes,
+          fixturePath / "initialize-build-result.json",
+          replaceAll = Seq(
+            BuildInfo.millVersion -> "<MILL_VERSION>",
+            Constants.bspProtocolVersion -> "<BSP_VERSION>"
+          )
+        )
+
+        val buildTargets = buildServer.workspaceBuildTargets().get()
+        compareWithGsonFixture(
+          buildTargets,
+          fixturePath / "workspace-build-targets.json",
+          replaceAll = replaceAll
+        )
+
+        val targetIds = buildTargets.getTargets.asScala.map(_.getId).asJava
+
+        val buildTargetSources = buildServer
+          .buildTargetSources(new b.SourcesParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetSources,
+          fixturePath / "build-targets-sources.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetDependencySources = buildServer
+          .buildTargetDependencySources(new b.DependencySourcesParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetDependencySources,
+          fixturePath / "build-targets-dependency-sources.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetDependencyModules = buildServer
+          .buildTargetDependencyModules(new b.DependencyModulesParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetDependencyModules,
+          fixturePath / "build-targets-dependency-modules.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetResources = buildServer
+          .buildTargetResources(new b.ResourcesParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetResources,
+          fixturePath / "build-targets-resources.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetOutputPaths = buildServer
+          .buildTargetOutputPaths(new b.OutputPathsParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetOutputPaths,
+          fixturePath / "build-targets-output-paths.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetJvmRunEnvironments = buildServer
+          .buildTargetJvmRunEnvironment(new b.JvmRunEnvironmentParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetJvmRunEnvironments,
+          fixturePath / "build-targets-jvm-run-environments.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetJvmTestEnvironments = buildServer
+          .buildTargetJvmTestEnvironment(new b.JvmTestEnvironmentParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetJvmTestEnvironments,
+          fixturePath / "build-targets-jvm-test-environments.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetJvmCompileClasspaths = buildServer
+          .buildTargetJvmCompileClasspath(new b.JvmCompileClasspathParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetJvmCompileClasspaths,
+          fixturePath / "build-targets-compile-classpaths.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetJavacOptions = buildServer
+          .buildTargetJavacOptions(new b.JavacOptionsParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetJavacOptions,
+          fixturePath / "build-targets-javac-options.json",
+          replaceAll = replaceAll
+        )
+
+        val buildTargetScalacOptions = buildServer
+          .buildTargetScalacOptions(new b.ScalacOptionsParams(targetIds))
+          .get()
+        compareWithGsonFixture(
+          buildTargetScalacOptions,
+          fixturePath / "build-targets-scalac-options.json",
+          replaceAll = replaceAll
+        )
+      }
+    }
+  }
+}
---
+++ testkit/src/mill/testkit/BspServerUtil.scala
@@ -0,0 +1,184 @@
+package mill.testkit
+
+import ch.epfl.scala.{bsp4j => b}
+import com.google.gson.{Gson, GsonBuilder}
+import coursier.cache.CacheDefaults
+import mill.api.BuildInfo
+import mill.bsp.Constants
+import org.eclipse.{lsp4j => l}
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{ExecutorService, Executors, ThreadFactory}
+
+import scala.jdk.CollectionConverters._
+import scala.reflect.ClassTag
+
+object BspServerUtil {
+
+  val updateFixtures = false
+
+  lazy val bsp4jVersion: String = sys.props.getOrElse("BSP4J_VERSION", ???)
+
+  trait DummyBuildClient extends b.BuildClient {
+    def onBuildLogMessage(params: b.LogMessageParams): Unit = ()
+    def onBuildPublishDiagnostics(params: b.PublishDiagnosticsParams): Unit = ()
+    def onBuildShowMessage(params: b.ShowMessageParams): Unit = ()
+    def onBuildTargetDidChange(params: b.DidChangeBuildTarget): Unit = ()
+    def onBuildTaskFinish(params: b.TaskFinishParams): Unit = ()
+    def onBuildTaskProgress(params: b.TaskProgressParams): Unit = ()
+    def onBuildTaskStart(params: b.TaskStartParams): Unit = ()
+    def onRunPrintStderr(params: b.PrintParams): Unit = ()
+    def onRunPrintStdout(params: b.PrintParams): Unit = ()
+  }
+
+  lazy val dummyBuildClient: b.BuildClient = new DummyBuildClient {}
+
+  val gson: Gson = new GsonBuilder().setPrettyPrinting().create()
+  def compareWithGsonFixture[T: ClassTag](
+      value: T,
+      fixturePath: os.Path,
+      replaceAll: Seq[(String, String)] = Nil
+  ): Unit = {
+
+    def doReplaceAll(input: String, inverse: Boolean = false): String =
+      replaceAll.foldLeft(input) {
+        case (input0, (from0, to0)) =>
+          val (from, to) = if (inverse) (to0, from0) else (from0, to0)
+          input0.replace(from, to)
+      }
+
+    val exists = os.exists(fixturePath)
+    val readOpt = Option.when(exists) {
+      val bytes = os.read.bytes(fixturePath)
+      gson.fromJson(
+        doReplaceAll(new String(bytes), inverse = true),
+        implicitly[ClassTag[T]].runtimeClass
+      )
+    }
+
+    if (!readOpt.contains(value))
+      if (updateFixtures) {
+        System.err.println(if (exists) s"Updating $fixturePath" else s"Writing $fixturePath")
+        val jsonStr = gson.toJson(
+          value,
+          implicitly[ClassTag[T]].runtimeClass
+        )
+        os.write.over(fixturePath, doReplaceAll(jsonStr), createFolders = true)
+      } else
+        Predef.assert(
+          false,
+          if (exists) s"Error: value differs from fixture at $fixturePath"
+          else s"Error: no fixture found at $fixturePath"
+        )
+    else if (updateFixtures) {
+      // Fixture on disk might need to be updated anyway, if replaceAll changed
+      // and new strings should be replaced
+      val jsonStr = doReplaceAll(
+        gson.toJson(
+          value,
+          implicitly[ClassTag[T]].runtimeClass
+        )
+      )
+      val onDiskJsonStr = os.read(fixturePath)
+      if (jsonStr != onDiskJsonStr) {
+        System.err.println(s"Updating $fixturePath")
+        os.write.over(fixturePath, jsonStr)
+      }
+    }
+  }
+
+  val bspJsonrpcPool: ExecutorService = Executors.newCachedThreadPool(
+    new ThreadFactory {
+      val counter = new AtomicInteger
+      def newThread(runnable: Runnable): Thread = {
+        val t = new Thread(runnable, s"mill-bsp-integration-${counter.incrementAndGet()}")
+        t.setDaemon(true)
+        t
+      }
+    }
+  )
+
+  trait MillBuildServer extends b.BuildServer with b.JvmBuildServer
+      with b.JavaBuildServer with b.ScalaBuildServer
+
+  def withBspServer[T](
+      workspacePath: os.Path,
+      millTestSuiteEnv: Map[String, String],
+      client: b.BuildClient = dummyBuildClient
+  )(f: (MillBuildServer, b.InitializeBuildResult) => T): T = {
+
+    val jsonFile = workspacePath / Constants.bspDir / s"${Constants.serverName}.json"
+    assert(os.exists(jsonFile))
+    val contents = os.read(jsonFile)
+    assert(
+      !contents.contains("--debug"),
+      contents.contains(s""""bspVersion":"${bsp4jVersion}"""")
+    )
+
+    val contentsJson = ujson.read(contents)
+    val bspCommand = contentsJson("argv").arr.map(_.str)
+    val proc = os.proc(bspCommand).spawn(
+      cwd = workspacePath,
+      stderr = os.Inherit,
+      env = millTestSuiteEnv
+    )
+
+    try {
+      val launcher = new l.jsonrpc.Launcher.Builder[MillBuildServer]
+        .setExecutorService(bspJsonrpcPool)
+        .setInput(proc.stdout.wrapped)
+        .setOutput(proc.stdin.wrapped)
+        .setRemoteInterface(classOf[MillBuildServer])
+        .setLocalService(client)
+        .setExceptionHandler { t =>
+          System.err.println(s"Error during LSP processing: $t")
+          t.printStackTrace(System.err)
+          l.jsonrpc.RemoteEndpoint.DEFAULT_EXCEPTION_HANDLER.apply(t)
+        }
+        .create()
+
+      launcher.startListening()
+
+      val buildServer = launcher.getRemoteProxy()
+
+      val initRes = buildServer.buildInitialize(
+        new b.InitializeBuildParams(
+          "Mill Integration",
+          BuildInfo.millVersion,
+          b.Bsp4j.PROTOCOL_VERSION,
+          workspacePath.toNIO.toUri.toASCIIString,
+          new b.BuildClientCapabilities(List("java", "scala").asJava)
+        )
+      ).get()
+
+      f(buildServer, initRes)
+    } finally {
+      proc.stdin.close()
+      proc.stdout.close()
+
+      proc.join(2000L)
+    }
+  }
+
+  lazy val millWorkspace: os.Path = {
+    val value = Option(System.getenv("MILL_PROJECT_ROOT")).getOrElse(???)
+    os.Path(value)
+  }
+
+  def replaceAllValues(
+      workspacePath: os.Path,
+      coursierCache: os.Path = os.Path(CacheDefaults.location),
+      javaHome: os.Path = os.Path(sys.props("java.home")),
+      javaVersion: String = sys.props("java.version")
+  ): Seq[(String, String)] =
+    Seq(
+      workspacePath.toNIO.toUri.toASCIIString.stripSuffix("/") -> "file:///workspace",
+      coursierCache.toNIO.toUri.toASCIIString -> "file:///coursier-cache/",
+      millWorkspace.toNIO.toUri.toASCIIString -> "file:///mill-workspace/",
+      javaHome.toNIO.toUri.toASCIIString.stripSuffix("/") -> "file:///java-home",
+      ("\"" + javaVersion + "\"") -> "\"<java-version>\"",
+      workspacePath.toString -> "/workspace",
+      coursierCache.toString -> "/coursier-cache",
+      millWorkspace.toString -> "/mill-workspace"
+    )
+}
