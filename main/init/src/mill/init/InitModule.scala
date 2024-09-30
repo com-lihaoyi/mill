754b2eb97c62beba2f7da3c8b0112d5ae754bef3
Li Haoyi <haoyi.sg@gmail.com>
2024-09-30 20:02:27 +0800

    Tidy up `init` command impl, make it unpack into current folder (#3626)
    Unpacking into the current folder is much more useful due to the fact
    that we recommend people use a bootstrap script.
    
    * With the previous approach, you end up with a bootstrap script in the
    parent dir and a subfolder with another bootstrap script with the real
    build inside.
    * With the new approach, you download a bootstrap script, run init, and
    it fleshes out the example in place
    
    Since we now unpack into an existing folder that may or may not have
    things already in it, we add a new heuristic to fail early if the
    unpacking would over-write any existing files. This is a compromise,
    since we cannot easily assume the folder is 100% empty (what if there's
    a `.DS_STORE` in it???), nor can we remove any previous files we do not
    understand (what if there's already a `.gitignore` or `.git`?), and we
    also do not want to stomp over any existing files (what if there's
    already a `build.mill` and someone accidentally calls `init`?).
    
    Tweak the implementation to avoid using so many nested `Try`s, normal
    exception handling is enough
    
    Tested manually via `./mill installLocalCache`
    
    ---------
    
    Co-authored-by: Pawel Sadlo <psadlo@virtuslab.com>
    Co-authored-by: pawelsadlo <106806258+pawelsadlo@users.noreply.github.com>

--- main/init/src/mill/init/InitModule.scala
+++ main/init/src/mill/init/InitModule.scala
@@ -3,11 +3,8 @@ package mill.init
 import mainargs.{Flag, arg}
 import mill.api.IO
 import mill.define.{Discover, ExternalModule}
-import mill.util.Util.download
 import mill.{Command, Module, T}
 
-import java.io.IOException
-import java.util.UUID
 import scala.util.{Failure, Success, Try, Using}
 
 object InitModule extends ExternalModule with InitModule {
@@ -36,52 +33,60 @@ trait InitModule extends Module {
   ): Command[Seq[String]] =
     T.command {
       usingExamples { examples =>
-        val result: Try[(Seq[String], String)] = exampleId match {
+        exampleId match {
           case None =>
             val exampleIds: Seq[ExampleId] = examples.map { case (exampleId, _) => exampleId }
             def fullMessage(exampleIds: Seq[ExampleId]) =
               msg + "\n\n" + exampleIds.mkString("\n") + "\n\n" + msg
-            if (showAll.value)
-              Success((exampleIds, fullMessage(exampleIds)))
+            if (showAll.value) (exampleIds, fullMessage(exampleIds))
             else {
               val toShow = List("basic", "builds", "web")
-              val filteredIds =
-                exampleIds.filter(_.split('/').lift.apply(1).exists(toShow.contains))
-              Success((filteredIds, fullMessage(filteredIds)))
-            }
-          case Some(value) =>
-            val result: Try[(Seq[String], String)] = for {
-              url <- examples.toMap.get(value).toRight(new Exception(
-                moduleNotExistMsg(value)
-              )).toTry
-              extractedDirName = {
-                val zipName = url.split('/').last
-                if (zipName.toLowerCase.endsWith(".zip")) zipName.dropRight(4) else zipName
-              }
-              downloadDir = T.workspace
-              downloadPath = downloadDir / extractedDirName
-              _ <- if (os.exists(downloadPath)) Failure(new IOException(
-                directoryExistsMsg(downloadPath.toString)
-              ))
-              else Success(())
-              path <-
-                Try({
-                  val tmpName = UUID.randomUUID().toString + ".zip"
-                  val downloaded = download(url, os.rel / tmpName)(downloadDir)
-                  val unpacked = IO.unpackZip(downloaded.path, os.rel)(downloadDir)
-                  Try(os.remove(downloaded.path))
-                  unpacked
-                }).recoverWith(ex =>
-                  Failure(
-                    new IOException(s"Couldn't download example: [$value];\n ${ex.getMessage}")
-                  )
-                )
-            } yield (Seq(path.path.toString()), s"Example downloaded to [$downloadPath]")
+              val filteredIds = exampleIds
+                .filter(_.split('/').lift.apply(1).exists(toShow.contains))
 
-            result
+              (filteredIds, fullMessage(filteredIds))
+            }
+
+          case Some(value) =>
+            val url = examples.toMap.get(value).getOrElse(sys.error(moduleNotExistMsg(value)))
+            println(s"Downloading example from $url...")
+            val zipName = url.split('/').last
+            val extractedDirName = zipName.stripSuffix(".zip")
+            val downloaded = os.temp(requests.get(url))
+            println(s"Unpacking example...")
+            val unpackPath = IO.unpackZip(downloaded, os.rel)
+            val extractedPath = T.dest / extractedDirName
+            val conflicting = for {
+              p <- os.walk(extractedPath)
+              val rel = p.relativeTo(extractedPath)
+              if os.exists(T.workspace / rel)
+            } yield rel
+
+            if (conflicting.nonEmpty) {
+              throw new Exception(
+                "Unable to unpack example because it conflicts with existing file: " +
+                  conflicting.mkString(", ")
+              )
+            }
+
+            for (p <- os.walk(extractedPath)) {
+              println(p.relativeTo(extractedPath))
+            }
+
+            // Remove any existing bootstrap script since the example will come with one
+            os.remove(T.workspace / "mill")
+            os.copy.apply(extractedPath, T.workspace, mergeFolders = true)
+
+            // Make sure the `./mill` launcher is executable
+            os.perms.set(T.workspace / "mill", "rwxrwxrwx")
+
+            (
+              Seq(unpackPath.path.toString()),
+              s"Example download and unpacked to [${T.workspace}]; " +
+                "See `build.mill` for an explanation of this example and instructions on how to use it"
+            )
         }
-        result
-      }.flatten match {
+      } match {
         case Success((ret, msg)) =>
           T.log.outputStream.println(msg)
           ret
