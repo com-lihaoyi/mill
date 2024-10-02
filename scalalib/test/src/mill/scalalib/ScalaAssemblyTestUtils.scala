e68aecb6027b41a9402b7a8a5e131d2c3f0b8a40
Li Haoyi <haoyi.sg@gmail.com>
2024-10-02 21:21:34 +0800

    .

---
+++ scalalib/test/src/mill/scalalib/ScalaAssemblyTestUtils.scala
@@ -0,0 +1,107 @@
+package mill.scalalib
+
+import mill._
+import mill.testkit.{TestBaseModule, UnitTester}
+import utest._
+
+import java.util.jar.JarFile
+import scala.util.Using
+import HelloWorldTests._
+trait ScalaAssemblyTestUtils {
+
+  val akkaHttpDeps = Agg(ivy"com.typesafe.akka::akka-http:10.0.13")
+
+  object HelloWorldAkkaHttpAppend extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq(Assembly.Rule.Append("reference.conf"))
+    }
+  }
+
+  object HelloWorldAkkaHttpExclude extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq(Assembly.Rule.Exclude("reference.conf"))
+    }
+  }
+
+  object HelloWorldAkkaHttpAppendPattern extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq(Assembly.Rule.AppendPattern(".*.conf"))
+    }
+  }
+
+  object HelloWorldAkkaHttpExcludePattern extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq(Assembly.Rule.ExcludePattern(".*.conf"))
+    }
+  }
+
+  object HelloWorldAkkaHttpRelocate extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq(Assembly.Rule.Relocate("akka.**", "shaded.akka.@1"))
+    }
+  }
+
+  object HelloWorldAkkaHttpNoRules extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def ivyDeps = akkaHttpDeps
+      override def assemblyRules = Seq.empty
+    }
+  }
+
+  object HelloWorldMultiAppend extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq(Assembly.Rule.Append("reference.conf"))
+    }
+    object model extends HelloWorldModule
+  }
+
+  object HelloWorldMultiExclude extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq(Assembly.Rule.Exclude("reference.conf"))
+    }
+    object model extends HelloWorldModule
+  }
+
+  object HelloWorldMultiAppendPattern extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq(Assembly.Rule.AppendPattern(".*.conf"))
+    }
+    object model extends HelloWorldModule
+  }
+
+  object HelloWorldMultiAppendByPatternWithSeparator extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq(Assembly.Rule.AppendPattern(".*.conf", "\n"))
+    }
+    object model extends HelloWorldModule
+  }
+
+  object HelloWorldMultiExcludePattern extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq(Assembly.Rule.ExcludePattern(".*.conf"))
+    }
+    object model extends HelloWorldModule
+  }
+
+  object HelloWorldMultiNoRules extends TestBaseModule {
+    object core extends HelloWorldModuleWithMain {
+      override def moduleDeps = Seq(model)
+      override def assemblyRules = Seq.empty
+    }
+    object model extends HelloWorldModule
+  }
+
+  val helloWorldMultiResourcePath =
+    os.Path(sys.env("MILL_TEST_RESOURCE_DIR")) / "hello-world-multi"
+
+}
