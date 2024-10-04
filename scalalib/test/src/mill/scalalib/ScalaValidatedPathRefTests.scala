3b53189c76c1ffd9312b15d8343e2553cceb0263
Li Haoyi <haoyi.sg@gmail.com>
2024-10-04 15:31:27 +0800

    .

--- scalalib/test/src/mill/scalalib/ScalaValidatedPathRefTests.scala
+++ scalalib/test/src/mill/scalalib/ScalaValidatedPathRefTests.scala
@@ -20,7 +20,8 @@ object ScalaValidatedPathRefTests extends TestSuite {
     def checkedPathRef: T[PathRef] = Task { mkDirWithFile().withRevalidateOnce }
     def checkedSeqPathRef: T[Seq[PathRef]] = Task { Seq(mkDirWithFile()).map(_.withRevalidateOnce) }
     def checkedAggPathRef: T[Agg[PathRef]] = Task { Agg(mkDirWithFile()).map(_.withRevalidateOnce) }
-    def checkedTuplePathRef: T[Tuple1[PathRef]] = Task { Tuple1(mkDirWithFile().withRevalidateOnce) }
+    def checkedTuplePathRef: T[Tuple1[PathRef]] =
+      Task { Tuple1(mkDirWithFile().withRevalidateOnce) }
   }
 
   def tests: Tests = Tests {
