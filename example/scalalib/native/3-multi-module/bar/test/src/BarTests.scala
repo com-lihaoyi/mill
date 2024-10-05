4f3a61b25c18bd164658c684edb4c1aa9d918d3f
Jeffrey <jeffwhewhetu@gmail.com>
2024-10-05 14:06:38 +0100

    update scala=native example code

--- example/scalalib/native/3-multi-module/bar/test/src/BarTests.scala
+++ example/scalalib/native/3-multi-module/bar/test/src/BarTests.scala
@@ -5,12 +5,12 @@ import scala.scalanative.unsafe._
 object BarTests extends TestSuite {
   def tests = Tests {
     test("simple") {
-      val result = Bar.generateHtml(c"hello")
+      val result = HelloWorldBar.generateHtml(c"hello")
       assert(result == "<h1>hello</h1>")
       result
     }
     test("escaping") {
-      val result = Bar.generateHtml(c"<hello>")
+      val result = HelloWorldBar.generateHtml(c"<hello>")
       assert(result == "<h1>&lt;hello&gt;</h1>")
       result
     }
