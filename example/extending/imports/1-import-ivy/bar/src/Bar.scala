d43417e1869e82220bd27e343fbff5091a832473
Li Haoyi <haoyi.sg@gmail.com>
2024-10-05 10:32:14 +0200

    Another pass at the docs (#3665)
    * Break out `{language} Library Dependencies` from `{language} Module
    Configuration`
    * Move `Search for dependency updates` to `Library Dependencies in Mill`
    * Remove the now-unnecessary `show` from `visualize` commands
    * `import $ivy` now starts with a Java example
    * `Running Mill with custom JVM options` moved to `Installation and IDE
    support`

--- example/extending/imports/1-import-ivy/bar/src/Bar.scala
+++ example/extending/imports/1-import-ivy/bar/src/Bar.scala
@@ -1,6 +1,6 @@
-package foo
+package bar
 
-object Foo {
+object Bar {
   val value = os.read(os.resource / "snippet.txt")
   def main(args: Array[String]): Unit = {
     println("generated snippet.txt resource: " + value)
