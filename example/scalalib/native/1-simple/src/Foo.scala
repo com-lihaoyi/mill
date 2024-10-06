8dc0853ddd3f2b009c55c5bd5aa96556d680a79e
Jeffrey <jeffwhewhetu@gmail.com>
2024-10-06 13:37:22 +0100

    update 1-simple example code

--- example/scalalib/native/1-simple/src/Foo.scala
+++ example/scalalib/native/1-simple/src/Foo.scala
@@ -1,15 +1,19 @@
 package foo
+
+import scala.scalanative.unsafe._
+import scala.scalanative.libc._
 import scalatags.Text.all._
 import mainargs.{main, ParserForMethods}
 
+
 object Foo {
-  def generateHtml(text: String) = {
-    h1(text).toString
+  def generateHtml(text: CString) = {
+    h1(text).toCString
   }
 
   @main
-  def main(text: String) = {
-    println(generateHtml(text))
+  def main(text: CString) = {
+   stdio.printf(generateHtml(text))
   }
 
   def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
--- example/scalalib/native/1-simple/test/src/FooTests.scala
+++ example/scalalib/native/1-simple/test/src/FooTests.scala
@@ -1,16 +1,17 @@
 package foo
 
+import scala.scalanative.unsafe._
 import utest._
 
 object FooTests extends TestSuite {
   def tests = Tests {
     test("simple") {
-      val result = Foo.generateHtml("hello")
+      val result = Foo.generateHtml(c"hello")
       assert(result == "<h1>hello</h1>")
       result
     }
     test("escaping") {
-      val result = Foo.generateHtml("<hello>")
+      val result = Foo.generateHtml(c"<hello>")
       assert(result == "<h1>&lt;hello&gt;</h1>")
       result
     }
