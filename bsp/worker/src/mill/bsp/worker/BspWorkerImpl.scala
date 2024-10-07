e9583c5db29eeb91b82ca6916b8b424234832e6d
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-10-07 22:35:18 +0200

    fixup debug CI

--- bsp/worker/src/mill/bsp/worker/BspWorkerImpl.scala
+++ bsp/worker/src/mill/bsp/worker/BspWorkerImpl.scala
@@ -94,27 +94,30 @@ private class BspWorkerImpl() extends BspWorker {
         }
       }
 
-      new Thread(() => {
-        listening.get()
-        streams.err.println("Shutting down executor")
-        executor.shutdown()
-        streams.err.println("Executor shut down")
-        import scala.jdk.CollectionConverters._
-        Thread.getAllStackTraces()
-          .asScala
-          .iterator
-          .filter(!_._1.isDaemon())
-          .toVector
-          .sortBy(_._1.getId)
-          .foreach {
-            case (thread, stackTrace) =>
-              thread.getState
-              System.err.println(s"$thread (${thread.getState})")
-              for (elem <- stackTrace)
-                System.err.println(s"  $elem")
-              System.err.println()
-          }
-      }, "bsp-worker-close").start()
+      new Thread(
+        () => {
+          listening.get()
+          streams.err.println("Shutting down executor")
+          executor.shutdown()
+          streams.err.println("Executor shut down")
+          import scala.jdk.CollectionConverters._
+          Thread.getAllStackTraces()
+            .asScala
+            .iterator
+            .filter(!_._1.isDaemon())
+            .toVector
+            .sortBy(_._1.getId)
+            .foreach {
+              case (thread, stackTrace) =>
+                thread.getState
+                System.err.println(s"$thread (${thread.getState})")
+                for (elem <- stackTrace)
+                  System.err.println(s"  $elem")
+                System.err.println()
+            }
+        },
+        "bsp-worker-close"
+      ).start()
 
       Right(bspServerHandle)
     } catch {
