c509447954baad8c80fdb82becc4ccca0b0ae3f6
himanshumahajan138 <himanshumahajan138@gmail.com>
2024-10-12 10:50:26 +0000

    still not resolved

--- kotlinlib/src/mill/kotlinlib/android/AndroidAppKotlinModule.scala
+++ kotlinlib/src/mill/kotlinlib/android/AndroidAppKotlinModule.scala
@@ -27,13 +27,51 @@ trait AndroidAppKotlinModule extends AndroidAppModule with KotlinModule {
     t => t.value, // Convert coursier.Type to a String
     Type(_) // Convert String back to coursier.Type
   )
+
   // Implicit ReadWriter for Set[coursier.Type]
   implicit val coursierTypeSetRW: ReadWriter[Set[Type]] =
     upickle.default.readwriter[Set[String]].bimap(
       set => set.map(_.value), // Serialize Set[coursier.Type] to Set[String]
       set => set.map(Type(_)) // Deserialize Set[String] to Set[coursier.Type]
     )
-  // artifactTypes to add "aar" type to the Set of artifact types
+
+  // Artifact types to add "aar" type to the Set of artifact types
   def artifactTypes: T[Set[Type]] =
-    Task { super.artifactTypes() + coursier.Type("aar") }
+    T { super.artifactTypes() + coursier.Type("aar") }
+
+  // Task to recompile AARs into JARs (without circular dependency)
+  def recompileAARs: T[Seq[PathRef]] = Task {
+    // Get all .aar files from the original compileClasspath
+    val aarFiles = super.compileClasspath().map(_.path).filter(_.ext == "aar").toSeq
+
+    // Convert each .aar file into a .jar file
+    val newJarFiles = aarFiles.map { aarFile =>
+      val extractDir = aarFile / os.up / "ext" // Destination 'ext' folder next to the .aar file
+      os.makeDir.all(extractDir) // Ensure the destination directory exists
+
+      // Unzip the .aar file
+      os.call(Seq("unzip", aarFile.toString, "-d", extractDir.toString))
+
+      // Create a .jar file from the extracted contents
+      val jarFile = aarFile / os.up / s"${aarFile.baseName}.jar" // Target .jar file
+      os.call(Seq("jar", "-cf", jarFile.toString, "-C", extractDir.toString, "."))
+
+      // Clean up the extracted folder
+      os.remove.all(extractDir)
+
+      // Return the PathRef to the newly created .jar file
+      PathRef(jarFile)
+    }
+
+    newJarFiles
+  }
+
+  // Task to update the compile classpath by including the new .jar files
+  def updatedCompileClasspath: T[Agg[PathRef]] = Task {
+    // Return the updated classpath with only .jar files
+    super.compileClasspath().filter(_.path.ext == "jar") ++ Agg.from(recompileAARs())
+  }
+
+  // Override the compile classpath if you want it to include the recompiled AARs by default
+  override def compileClasspath: T[Agg[PathRef]] = updatedCompileClasspath()
 }
