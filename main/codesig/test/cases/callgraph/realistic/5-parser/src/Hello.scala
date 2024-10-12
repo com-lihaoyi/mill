300d692acdf91ee2b5e5cae1c3632403c8e35b81
Li Haoyi <haoyi.sg@gmail.com>
2024-10-12 03:29:58 +0200

    .

--- main/codesig/test/cases/callgraph/realistic/5-parser/src/Hello.scala
+++ main/codesig/test/cases/callgraph/realistic/5-parser/src/Hello.scala
@@ -6,15 +6,15 @@ class Word(s: String) extends Phrase
 class Pair(lhs: Phrase, rhs: Phrase) extends Phrase
 
 object Parser {
-  def prefix[_X: P] = P("hello" | "goodbye").!.map(new Word(_))
+  def prefix[$: P] = P("hello" | "goodbye").!.map(new Word(_))
 
-  def suffix[_X: P] = P("world" | "seattle").!.map(new Word(_))
+  def suffix[$: P] = P("world" | "seattle").!.map(new Word(_))
 
-  def ws[_X: P] = P(" ".rep(1))
+  def ws[$: P] = P(" ".rep(1))
 
-  def parened[_X: P] = P("(" ~ parser ~ ")")
+  def parened[$: P] = P("(" ~ parser ~ ")")
 
-  def parser[_X: P]: P[Phrase] = P((parened | prefix) ~ ws ~ (parened | suffix)).map {
+  def parser[$: P]: P[Phrase] = P((parened | prefix) ~ ws ~ (parened | suffix)).map {
     case (lhs, rhs) => new Pair(lhs, rhs)
   }
 }
@@ -23,21 +23,6 @@ object Parser {
 {
     "hello.Parser$#parened(fastparse.ParsingRun)fastparse.ParsingRun": [
         "hello.Parser$#parser(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
-    "hello.Parser$#parse0$proxy8$1(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
-    "hello.Parser$#parse0$proxy9$1(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
-    "hello.Parser$#parse0$proxy9$2(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
-    "hello.Parser$#parse0$proxy9$3(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
-    "hello.Parser$#parse0$proxy9$4(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
     ],
     "hello.Parser$#parser(fastparse.ParsingRun)fastparse.ParsingRun": [
         "hello.Pair#<init>(hello.Phrase,hello.Phrase)void",
@@ -49,15 +34,8 @@ object Parser {
     "hello.Parser$#prefix(fastparse.ParsingRun)fastparse.ParsingRun": [
         "hello.Word#<init>(java.lang.String)void"
     ],
-    "hello.Parser$#rec$1(fastparse.ParsingRun,int,fastparse.Implicits$Repeater,java.lang.Object,fastparse.ParsingRun,int,int,boolean,boolean,fastparse.internal.Msgs,fastparse.internal.Msgs)fastparse.ParsingRun": [
-        "hello.Parser$#end$1(int,fastparse.ParsingRun,fastparse.Implicits$Repeater,java.lang.Object,int,int,int,boolean)fastparse.ParsingRun",
-        "hello.Parser$#parse0$1$1(fastparse.ParsingRun)fastparse.ParsingRun"
-    ],
     "hello.Parser$#suffix(fastparse.ParsingRun)fastparse.ParsingRun": [
         "hello.Word#<init>(java.lang.String)void"
-    ],
-    "hello.Parser$#ws(fastparse.ParsingRun)fastparse.ParsingRun": [
-        "hello.Parser$#rec$1(fastparse.ParsingRun,int,fastparse.Implicits$Repeater,java.lang.Object,fastparse.ParsingRun,int,int,boolean,boolean,fastparse.internal.Msgs,fastparse.internal.Msgs)fastparse.ParsingRun"
     ],
     "hello.Parser.parened(fastparse.ParsingRun)fastparse.ParsingRun": [
         "hello.Parser$#<init>()void",
