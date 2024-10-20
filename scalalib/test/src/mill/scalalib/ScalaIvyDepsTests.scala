f102c8a96dea1ebdca1de4fec583501b1a60b76d
Li Haoyi <haoyi.sg@gmail.com>
2024-10-20 09:31:15 +0800

    .

--- scalalib/test/src/mill/scalalib/ScalaIvyDepsTests.scala
+++ scalalib/test/src/mill/scalalib/ScalaIvyDepsTests.scala
@@ -17,6 +17,30 @@ object ScalaIvyDepsTests extends TestSuite {
     }
   }
 
+  object TransitiveRunIvyDeps extends TestBaseModule {
+    object upstream extends JavaModule {
+      def ivyDeps = Agg(ivy"org.slf4j:slf4j-api:2.0.16")
+      def runIvyDeps = Agg(ivy"ch.qos.logback:logback-classic:1.5.10")
+    }
+
+    object downstream extends JavaModule {
+      // Make sure runIvyDeps are transitively picked up from normal `moduleDeps`
+      def moduleDeps = Seq(upstream)
+    }
+  }
+
+  object TransitiveRunIvyDeps2 extends TestBaseModule {
+    object upstream extends JavaModule {
+      def ivyDeps = Agg(ivy"org.slf4j:slf4j-api:2.0.16")
+      def runIvyDeps = Agg(ivy"ch.qos.logback:logback-classic:1.5.10")
+    }
+
+    object downstream extends JavaModule {
+      // Make sure both ivyDeps and runIvyDeps are transitively picked up from `runModuleDeps`
+      def runModuleDeps = Seq(upstream)
+    }
+  }
+
   def tests: Tests = Tests {
 
     test("ivyDeps") - UnitTester(HelloWorldIvyDeps, resourcePath).scoped { eval =>
@@ -31,6 +55,24 @@ object ScalaIvyDepsTests extends TestSuite {
         result2.value.exists(_.path.last == "sourcecode_2.12-0.1.4.jar"),
         !result2.value.exists(_.path.last == "sourcecode_2.12-0.1.3.jar")
       )
+    }
+
+    test("transitiveRun") - UnitTester(TransitiveRunIvyDeps, resourcePath).scoped { eval =>
+      val Right(result2) = eval.apply(TransitiveRunIvyDeps.downstream.runClasspath)
+
+      assert(
+        result2.value.exists(_.path.last == "logback-classic-1.5.10.jar")
+      )
+    }
+
+    test("transitiveLocalRuntimeDepsRun") - UnitTester(TransitiveRunIvyDeps2, resourcePath).scoped {
+      eval =>
+        val Right(result2) = eval.apply(TransitiveRunIvyDeps2.downstream.runClasspath)
+
+        assert(
+          result2.value.exists(_.path.last == "logback-classic-1.5.10.jar"),
+          result2.value.exists(_.path.last == "slf4j-api-2.0.16.jar")
+        )
     }
 
   }
