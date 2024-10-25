9cf0bd0540b0b31c8737e83d478c665c3a9523b7
Li Haoyi <haoyi.sg@gmail.com>
2024-10-25 09:05:34 +0800

    .

--- integration/feature/mill-jvm-opts/src/MillJvmOptsTests.scala
+++ integration/feature/mill-jvm-opts/src/MillJvmOptsTests.scala
@@ -13,13 +13,25 @@ object MillJvmOptsTests extends UtestIntegrationTestSuite {
     }
     test("interpolatedEnvVars") - integrationTest { tester =>
       import tester._
-      val res = eval("checkEnvJvmOpts")
-      assert(res.isSuccess)
+      val res = eval(("show", "getEnvJvmOpts"))
+      val out = res.out
+      val expected = "\"value-with-" + tester.workspacePath + "\""
+      assert(out == expected)
+    }
+    test("alternate") - integrationTest { tester =>
+      import tester._
+      val res = eval(("show", "getEnvJvmOpts"), env = Map("MILL_JVM_OPTS_PATH" -> ".mill-jvm-opts-alternate"))
+      assert(res.out == "\"alternate-value-with-" + tester.workspacePath + "\"")
     }
     test("nonJvmOpts") - integrationTest { tester =>
       import tester._
-      val res = eval("checkNonJvmOpts")
-      assert(res.isSuccess)
+      val res = eval(("show", "getNonJvmOpts"))
+      assert(res.out == "17")
+    }
+    test("nonJvmOptsAlternate") - integrationTest { tester =>
+      import tester._
+      val res = eval(("show", "getNonJvmOpts"), env = Map("MILL_OPTS_PATH" -> ".mill-opts-alternate"))
+      assert(res.out == "29")
     }
   }
 }
