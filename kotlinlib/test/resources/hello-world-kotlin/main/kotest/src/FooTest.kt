68269b893d2afe4759d7b8a984342adfc8e9a1c3
Li Haoyi <haoyi.sg@gmail.com>
2024-11-05 09:43:26 +0100

    Filter out anonymous classes during test discovery (#3911)
    Mitigates https://github.com/com-lihaoyi/mill/issues/3910 by hackily
    filtering out all anonymous classes, but non-anonymous non-test classes
    still cause the problem
    
    Added a unit test to check this specific misbehavior

---
+++ kotlinlib/test/resources/hello-world-kotlin/main/kotest/src/FooTest.kt
@@ -0,0 +1,15 @@
+package hello.tests
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import hello.getHelloString
+
+class FooTest : FunSpec({
+    test("testFailure") {
+        getHelloString() shouldBe "Hello, world!"
+    }
+
+    test("testSuccess") {
+        getHelloString() shouldBe "WRONG!"
+    }
+})
