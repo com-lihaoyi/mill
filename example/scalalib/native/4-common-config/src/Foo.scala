845f5c970eed79c3a7b5fe97fd984d2f966b7280
Myyk Seok <2080820+myyk@users.noreply.github.com>
2024-11-07 17:53:01 +0800

    chore: use scala3 formatting for native examples

--- example/scalalib/native/4-common-config/src/Foo.scala
+++ example/scalalib/native/4-common-config/src/Foo.scala
@@ -6,16 +6,15 @@ import fansi._
 
 object Foo {
 
-  def generateHtml(text: String) (using Zone) = {
+  def generateHtml(text: String)(using Zone) = {
     val colored = Console.RED + "<h1>" + text + "</h1>" + Console.RESET
 
     val cResult = toCString(colored)
     cResult
   }
-  
+
   def main(args: Array[String]): Unit = Zone {
     val value = generateHtml("hello")
     stdio.printf(c"Value: %s\n", value)
   }
 }
-
