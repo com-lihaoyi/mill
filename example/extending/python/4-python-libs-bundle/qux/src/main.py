b34552454fdb9ef2b81065b4768aa09328f49e1b
Li Haoyi <haoyi.sg@gmail.com>
2024-11-09 07:27:41 +0100

    Cleanup python example, tweaks docs in response to feedback, fix github actions (#3925)
    * Make pip/pex example multi-module
    * Make bundling PEX actually work
    * Clean up english and code in general
    * Split up python and typescript examples into separate pages
    
    Other miscellaneous doc improvements and fixes to the github actions
    config

--- example/extending/python/4-python-libs-bundle/qux/src/main.py
+++ example/extending/python/4-python-libs-bundle/qux/src/main.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
 import numpy as np
-import pandas as pd # type: ignore
+
+from foo.src.foo import data
+from foo.bar.src.bar import df
+
 def main() -> None:
-    data = np.array([10, 20, 30, 40, 50])
-    df = pd.DataFrame({"Values": data})
     print(f"Numpy : Sum: {np.sum(data)} | Pandas: Mean: {df['Values'].mean()}, Max: {df['Values'].max()}")
+
 if __name__ == "__main__":
     main()
