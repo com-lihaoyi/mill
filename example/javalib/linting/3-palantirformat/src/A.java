956df25ef8335ec6db218da408f9b0ec2801e197
Li Haoyi <haoyi.sg@gmail.com>
2024-11-10 17:28:15 +0800

    .

--- example/javalib/linting/3-palantirformat/src/A.java
+++ example/javalib/linting/3-palantirformat/src/A.java
@@ -1,6 +1,6 @@
 public class A {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     System.out.println("hello"); // indentation should be fixed
-  }
+    }
 }
