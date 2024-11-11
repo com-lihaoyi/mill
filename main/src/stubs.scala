7c5b2d39a9f94356ee5f875cb27a4a3aec1713ca
Li Haoyi <haoyi.sg@gmail.com>
2024-11-12 00:57:17 +0800

    .

---
+++ main/src/stubs.scala
@@ -0,0 +1,14 @@
+/**
+ * Use `import $meta._` to enable the meta-build in the `mill-build/` folder`
+ *
+ * This is just a stub object to provide autocomplete for the import in the IDE
+ */
+object $meta
+
+/**
+ * Use `import $packages._` to allow your Mill build to be broken up into
+ * per-folder `package.mill` files.
+ *
+ * This is just a stub object to provide autocomplete for the import in the IDE
+ */
+object $packages
