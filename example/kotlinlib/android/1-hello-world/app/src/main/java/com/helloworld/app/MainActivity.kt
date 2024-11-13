bc17e75846f8080e5180ee75a8cf1509d6e4060f
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-11-13 23:15:13 +0530

    Fixes:#3868 Added Android Bundle Support (#3935)
    # Pull Request 
    Added Android Bundle Support (with alot of updation in Android App Build
    Process)
    Fixes: #3868
    
    ## Description
    The new `AndroidAppBundle` module adds support for Android app bundles,
    extending `AndroidAppModule`. An example of the bundle process is now
    available in `JavaLib`. `AndroidAppModule` and `AndroidSdkModule` have
    been updated to support Android resources and the `aapt2` tool.
    Additionally, resource support has been added to the `JavaLib` and
    `KotlinLib` examples.
    
    ## Related Issues
    - Link to related issue #3868.
    
    ## Checklist
    - [x] New Module AndroidAppBundle (for adding android bundle support
    extends AndroidAppModule)
    - [x] New Example for Android Bundle Process (Currently under JavaLib)
    - [x] Updated AndroidAppModule(added resource support)
    - [x] Updated AndroidSdkModule(related to Android aapt2 Tool)
    - [x] Updated JavaLib and KotlinLib Android Examples (added resources
    support)
    
    ## Further Addition
    Requires @lihaoyi Review for code satisfaction and then further move to
    Documentation Part(for Build.mill files(updations))

--- example/kotlinlib/android/1-hello-world/app/src/main/java/com/helloworld/app/MainActivity.kt
+++ example/kotlinlib/android/1-hello-world/app/src/main/java/com/helloworld/app/MainActivity.kt
@@ -2,9 +2,9 @@ package com.helloworld.app
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
 import android.view.Gravity
 import android.view.ViewGroup.LayoutParams
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +13,8 @@ class MainActivity : Activity() {
         // Create a new TextView
         val textView = TextView(this)
 
-        // Set the text to "Hello, World!"
-        textView.text = "Hello, World Kotlin!"
+        // Set the text to the string resource
+        textView.text = getString(R.string.hello_world)
 
         // Set text size
         textView.textSize = 32f
@@ -23,10 +23,13 @@ class MainActivity : Activity() {
         textView.gravity = Gravity.CENTER
 
         // Set layout parameters (width and height)
-        textView.layoutParams = LayoutParams(
-            LayoutParams.MATCH_PARENT,
-            LayoutParams.MATCH_PARENT
-        )
+        textView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
+        // Set the text color using a resource
+        textView.setTextColor(getColor(R.color.text_green))
+
+        // Set the background color using a resource
+        textView.setBackgroundColor(getColor(R.color.white))
 
         // Set the content view to display the TextView
         setContentView(textView)
