ea67f9bf9b21353f3dce04c7bd13c3ea66983637
himanshumahajan138 <himanshumahajan138@gmail.com>
2024-11-14 20:32:51 +0530

    TESTING

--- example/kotlinlib/web/7-todo-spring-boot/src/com/example/todoitem/TodoItemNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todoitem
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class TodoItemNotFoundException(itemId: Long) : RuntimeException("TodoItem itemId=$itemId not found")
--- example/kotlinlib/web/7-todo-spring-boot/src/com/example/todoitem/web/TodoItemFormData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todoitem.web
-
-import javax.validation.constraints.NotBlank
-
-data class TodoItemFormData(
-    @field:NotBlank
-    var title: String = ""
-)
