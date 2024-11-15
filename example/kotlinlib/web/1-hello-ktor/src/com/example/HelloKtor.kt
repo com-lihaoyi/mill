4cddacdfcee8a375dd38df8bc8d061eb61b87929
Myyk Seok <2080820+myyk@users.noreply.github.com>
2024-11-16 01:06:33 +0800

    Lint Kotlin examples (#3923)
    * [x] Needs #3919 as this builds on it
    * [x] Update this to use #3961, switching to `KtlintModule`
    * [x] Needs #3966 to disable ktlint for some files
    
    Last change I think needed for #3829
    
    Overview:
    * Changes `KtfmtModule` to set the error code when there's changes like
    is true of the other formatters.
    * Some simplifications were made to the `example/package.mill` based on
    similarities with Java/Kotlin.
    * Files were formatted
    * CI was updated
    
    ---------
    
    Co-authored-by: Li Haoyi <haoyi.sg@gmail.com>

--- example/kotlinlib/web/1-hello-ktor/src/com/example/HelloKtor.kt
+++ example/kotlinlib/web/1-hello-ktor/src/com/example/HelloKtor.kt
@@ -1,10 +1,12 @@
 package com.example
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 fun main() {
     embeddedServer(Netty, port = 8090, host = "0.0.0.0", module = Application::module)
--- kotlinlib/test/resources/contrib/kover/qux/test/src/QuxTest.kt
+++ kotlinlib/test/resources/contrib/kover/qux/test/src/QuxTest.kt
@@ -3,12 +3,13 @@ package qux
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class QuxTest : FunSpec({
-    test("kotlin - success") {
-        action(true, true) shouldBe "one, two"
-    }
+class QuxTest :
+    FunSpec({
+        test("kotlin - success") {
+            action(true, true) shouldBe "one, two"
+        }
 
-    test("java - success") {
-        QuxJava.action(true, true) shouldBe "one, two"
-    }
-})
+        test("java - success") {
+            QuxJava.action(true, true) shouldBe "one, two"
+        }
+    })
