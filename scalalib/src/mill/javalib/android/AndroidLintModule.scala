bd67ec56b4c377cac8d9c50dbf6f82da7db4f2aa
Jeffrey <106184818+c0d33ngr@users.noreply.github.com>
2024-11-17 19:45:34 +0000

    add some changes

--- scalalib/src/mill/javalib/android/AndroidLintModule.scala
+++ scalalib/src/mill/javalib/android/AndroidLintModule.scala
@@ -2,8 +2,8 @@ package mill.javalib.android
 
 import mill._
 import mill.api.PathRef
+import mill.define.ModuleRef
 import mill.scalalib.JavaModule
-import mill.javalib.android.AndroidSdkModule
 
 /**
  * Android Lint Module for integrating the Android Lint tool in a Mill build.
@@ -12,39 +12,38 @@ import mill.javalib.android.AndroidSdkModule
  * specifying lint rules, managing reports, and more.
  */
 @mill.api.experimental
-trait AndroidLintModule extends AndroidSdkModule with JavaModule {
+trait AndroidLintModule extends JavaModule {
 
   /**
-   * Path to the project for which lint should run.
+   * Abstract method to provide access to the Android SDK configuration.
+   *
+   * This method must be implemented by the concrete class to specify the SDK paths.
+   *
+   * @return The Android SDK module that is used across the project.
    */
-  def projectPath: T[PathRef] = Task.Source { millSourcePath }
+  def androidSdkModule: ModuleRef[AndroidSdkModule]
 
   /**
-   * Specifies the file format of lint report.
+   * Specifies the file format of lint report. Available file formats are "html", "xml", "txt" and "sarif".
    */
-  def lintReportFormat: T[String] = Task { "html" }
+  def androidLintReportFmt: T[String] = Task { "html" }
 
   /**
    * Specifies the lint configuration XML file path. This allows setting custom lint rules or modifying existing ones.
    */
-  def lintConfigPath: T[Option[PathRef]] = Task { None }
+  def androidLintConfigPath: T[Option[PathRef]] = Task { None }
 
   /**
-   * Enable or disable warnings in the lint report.
+   * SSpecifies the baseline file for Android Lint.
    */
-  def warningsAsErrors: T[Boolean] = Task { false }
-
-  /**
-   * Additional options for lint (e.g., enabling/disabling specific checks).
-   */
-  def lintOptions: T[Seq[String]] = Task { Seq("--check", "NewApi,InlinedApi") }
+  def androidLintBaselinePath: T[Option[PathRef]] = Task { None }
 
   /**
    * Runs the Android Lint tool to generate a report on code quality issues.
    *
    * This method utilizes Android Lint, a tool provided by the Android SDK,
    * to analyze the source code for potential bugs, performance issues, and
-   * best practices compliance. It generates an HTML report with the analysis
+   * best practices compliance. It generates an HTML report by default with the analysis
    * results for review.
    *
    * The lint tool requires the Android SDK's command-line tools to be installed.
@@ -54,9 +53,9 @@ trait AndroidLintModule extends AndroidSdkModule with JavaModule {
    * [[https://developer.android.com/studio/write/lint]]
    */
 
-  def androidLint: T[PathRef] = Task {
+  def androidLintRun: T[PathRef] = Task {
 
-    val format = lintReportFormat()
+    val format = androidLintReportFmt()
     val lintReport: os.Path = T.dest / s"report.$format"
 
     // Map the report format to the corresponding lint command flag
@@ -64,16 +63,23 @@ trait AndroidLintModule extends AndroidSdkModule with JavaModule {
       case "html" => "--html"
       case "txt" => "--text"
       case "xml" => "--xml"
+      case "sarif" => "--sarif"
       case _ => throw new Exception(s"Unsupported report format: $format")
     }
 
+    // Prepare the baseline argument if the baseline path is set
+    val baselineArg = androidLintBaselinePath().map(baseline => Seq("--baseline", baseline.path.toString)).getOrElse(Seq.empty)
+
+    // Prepare the lint configuration argument if the config path is set
+    val configArg = androidLintConfigPath().map(config => Seq("--config", config.path.toString)).getOrElse(Seq.empty)
+
     os.call(
       Seq(
-        cmdlineToolsPath().path.toString + "/lint",
-        lintReportFlag,
+        androidSdkModule().cmdlineToolsPath().path.toString + "/lint",
+        lintReportFlag.toString,
         lintReport.toString,
-        projectPath.toString
-      )
+        millSourcePath.toString
+      ) ++ configArg ++ baselineArg
     )
 
     PathRef(lintReport)
