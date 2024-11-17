a2ae75efaa6e1441fe48ce3e9b9d81a3822ea357
Li Haoyi <haoyi.sg@gmail.com>
2024-11-17 15:07:03 +0800

    fixes

--- example/depth/javahome/1-custom-jvms/bar/src/bar/Bar.java
+++
@@ -1,9 +0,0 @@
-package bar;
-
-
-public class Bar {
-
-  public static void main(String[] args) throws Exception {
-    System.out.println("Bar running on Java " + System.getProperty("java.version"));
-  }
-}
