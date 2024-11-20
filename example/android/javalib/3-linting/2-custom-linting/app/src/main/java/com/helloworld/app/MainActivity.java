1429e01beb011381172d15d63a7381ef0bcfe16c
Jeffrey <106184818+c0d33ngr@users.noreply.github.com>
2024-11-20 03:12:55 +0000

    udpate code

---
+++ example/android/javalib/3-linting/2-custom-linting/app/src/main/java/com/helloworld/app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.helloworld.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Create a new TextView
+    TextView textView = new TextView(this);
+
+    // Set the text to the string resource
+    textView.setText(getString(R.string.hello_world));
+
+    // Set text size
+    textView.setTextSize(32);
+
+    // Center the text within the view
+    textView.setGravity(Gravity.CENTER);
+
+    // Set the layout parameters (width and height)
+    textView.setLayoutParams(
+        new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+    // Set the text color using a resource
+    textView.setTextColor(getResources().getColor(R.color.text_green));
+
+    // Set the background color using a resource
+    textView.setBackgroundColor(getResources().getColor(R.color.white));
+
+    // Set the content view to display the TextView
+    setContentView(textView);
+  }
+}
+
