72fb07d3bbe688b0bf141704eb81ca0f075692b0
himanshumahajan138 <himanshumahajan138@gmail.com>
2024-11-21 19:56:29 +0530

    FINAL CHANGES: DONE

--- example/pythonlib/basic/3-multi-module/bar/test/src/test.py
+++ example/pythonlib/basic/3-multi-module/bar/test/src/test.py
@@ -4,12 +4,12 @@ from bar import generate_html
 
 class TestScript(unittest.TestCase):
     def test_simple(self) -> None:
-        expected_line = "<h1>bar</h1>"
-        self.assertEqual(generate_html("bar"), expected_line)
+        expected_line = "<h1>world</h1>"
+        self.assertEqual(generate_html("world"), expected_line)
 
     def test_escaping(self) -> None:
-        expected_line = "<h1>&lt;bar&gt;</h1>"
-        self.assertEqual(generate_html("<bar>"), expected_line)
+        expected_line = "<h1>&lt;world&gt;</h1>"
+        self.assertEqual(generate_html("<world>"), expected_line)
 
 if __name__ == "__main__":
     unittest.main()
