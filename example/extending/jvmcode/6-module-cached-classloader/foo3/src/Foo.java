19cc180a04a9639a607984af5b7214d57edb3a83
Li Haoyi <haoyi.sg@gmail.com>
2024-11-22 09:16:20 +0800

    autofmt

---
+++ example/extending/jvmcode/6-module-cached-classloader/foo3/src/Foo.java
@@ -0,0 +1,9 @@
+package foo;
+
+public class Foo {
+  public static final String value = "hello"; // Equivalent to val in Scala
+
+  public static void main(String[] args) {
+    System.out.println("Foo.value: " + Foo.value);
+  }
+}
