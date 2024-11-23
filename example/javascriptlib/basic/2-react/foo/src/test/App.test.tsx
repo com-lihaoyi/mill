f8571ae2fb0821eb46ff4d78fd45e32ee39f5cc1
Monye David <monyedavid@outlook.com>
2024-11-23 16:05:55 +0100

    2-react/: A minimal Typescript module demonstrating

---
+++ example/javascriptlib/basic/2-react/foo/src/test/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from 'app';
+
+test('renders learn react link', () => {
+  render(<App />);
+
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
