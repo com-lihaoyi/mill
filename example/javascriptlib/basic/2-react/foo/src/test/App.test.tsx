9981378345e60afccc74b47a05e9fbed62518b32
Monye David <monyedavid@outlook.com>
2024-11-26 19:57:28 +0100

    fix-tests: 2-react

--- example/javascriptlib/basic/2-react/foo/src/test/App.test.tsx
+++ example/javascriptlib/basic/2-react/foo/src/test/App.test.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from 'app';
 
-test('renders learn react link', () => {
-  render(<App />);
 
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+    jest.spyOn(console, 'warn').mockImplementation(() => {
+    });
+    jest.spyOn(console, 'error').mockImplementation(() => {
+    });
+});
+
+afterAll(() => {
+    console.warn.mockRestore();
+    console.error.mockRestore();
+});
+
+test('renders learn react link', () => {
+    render(<App/>);
+
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
