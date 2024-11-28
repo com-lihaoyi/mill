d6746ddde6fb09d9262c476f6dbca00128142b50
Ajay Chandran <ajay.chandran.1980@gmail.com>
2024-11-29 00:40:15 +0530

    Fix format

--- example/javalib/basic/7-native-image/foo/src/foo/HelloWorld.java
+++ example/javalib/basic/7-native-image/foo/src/foo/HelloWorld.java
@@ -2,7 +2,7 @@ package foo;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
