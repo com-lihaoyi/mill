50deaaa7946be21821fcee4aae22c71c513375f5
Monye David Onoh <monyedavid@outlook.com>
2024-12-05 02:14:36 +0100

    Add first class Javascript/Typescript support to the Mill build tool (#4003)
    ### Related Issues
    
    #3927
    
    ### Checklist
    
    - [x] **example/jslib/basic**
    - [x] 1-simple/: A minimal Typescript module demonstrating
    typechecking/running/testing/bundling of a Node CLI tool
    - [x] 2-react/: A minimal Typescript module demonstrating
    typechecking/running/testing/bundling of a React-based web application,
    served using a static HTML page
    - [x] 3-custom-build-logic/: A Typescript module with custom build logic

---
+++ javascriptlib/src/mill/javascriptlib/JestModule.scala
@@ -0,0 +1,71 @@
+package mill.javascriptlib
+import mill.*
+import os.*
+import mill.define.Target
+import scala.collection.immutable.IndexedSeq
+
+trait JestModule extends TypeScriptModule {
+  override def npmDevDeps: T[Seq[String]] = Task {
+    Seq(
+      "@types/jest@^29.5.14",
+      "@babel/core@^7.26.0",
+      "@babel/preset-env@^7.26.0",
+      "jest@^29.7.0",
+      "ts-jest@^29.2.5",
+      "babel-jest@^29.7.0"
+    )
+  }
+
+  def testSource: Target[PathRef] = Task.Source(millSourcePath / "test")
+
+  def testConfigSource: Target[PathRef] = Task.Source(millSourcePath / os.up / "jest.config.ts")
+
+  override def allSources: Target[IndexedSeq[PathRef]] = Task {
+    (os.walk(sources().path) ++ os.walk(testSource().path) ++ IndexedSeq(testConfigSource().path))
+      .filter(_.ext == "ts")
+      .map(PathRef(_))
+  }
+
+  override def mkENV = Task.Anon {
+    val javascriptOut = compile()._1.path
+    // env
+    // note: ' npmInstall().path / "node_modules" ' required in NODE_PATH for jest to find preset: ts-jest
+    Map("NODE_PATH" -> Seq(
+      ".",
+      javascriptOut,
+      npmInstall().path,
+      npmInstall().path / "node_modules"
+    ).mkString(":"))
+  }
+
+  // specify config file path: --config /path/to/jest/config
+  def getConfigFile: Task[String] =
+    Task { (compile()._1.path / "jest.config.ts").toString }
+
+  // specify test dir path/to/test
+  def getPathToTest: Task[String] =
+    Task { compile()._2.path.toString }
+
+  private def copyJestConfig: Task[Unit] = Task.Anon {
+    os.copy.over(
+      millSourcePath / os.up / "jest.config.ts",
+      compile()._1.path / "jest.config.ts"
+    )
+  }
+
+  def test: Target[CommandResult] = Task {
+    copyJestConfig()
+    os.call(
+      (
+        "node",
+        npmInstall().path / "node_modules/jest/bin/jest.js",
+        "--config",
+        getConfigFile(),
+        getPathToTest()
+      ),
+      stdout = os.Inherit,
+      env = mkENV(),
+      cwd = compile()._1.path
+    )
+  }
+}
