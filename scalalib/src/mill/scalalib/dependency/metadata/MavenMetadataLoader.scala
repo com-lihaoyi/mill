d2fcfbe342939b9885b19cb2f42672ff05c5deb0
Alex Archambault <alexandre.archambault@gmail.com>
2024-12-11 13:44:13 +0100

    Try to address version updates check race condition

--- scalalib/src/mill/scalalib/dependency/metadata/MavenMetadataLoader.scala
+++ scalalib/src/mill/scalalib/dependency/metadata/MavenMetadataLoader.scala
@@ -4,14 +4,17 @@ import coursier.maven.MavenRepository
 import coursier.util.Task
 import mill.scalalib.dependency.versions.Version
 
+import java.time.Clock
+
 private[dependency] final case class MavenMetadataLoader(mavenRepo: MavenRepository)
     extends MetadataLoader {
 
   private val cache = coursier.cache.FileCache[Task]()
 
-  override def getVersions(module: coursier.Module): List[Version] = {
+  override def getVersions(module: coursier.Module, clock: Clock): List[Version] = {
     // TODO fallback to 'versionsFromListing' if 'versions' doesn't work? (needs to be made public in coursier first)
-    val allVersions = mavenRepo.versions(module, cache.fetch).run.unsafeRun()(cache.ec)
+    val allVersions =
+      mavenRepo.versions(module, cache.withClock(clock).fetch).run.unsafeRun()(cache.ec)
     allVersions
       .map(_._1.available.map(Version(_)))
       .getOrElse(List.empty)
