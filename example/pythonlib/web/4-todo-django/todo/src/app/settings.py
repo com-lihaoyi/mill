355523359cf0089e5b2ac486ae878ed90d37be53
himanshumahajan138 <himanshumahajan138@gmail.com>
2024-12-15 19:06:31 +0530

    testing

--- example/pythonlib/web/4-todo-django/todo/src/app/settings.py
+++ example/pythonlib/web/4-todo-django/todo/src/app/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    'TEST': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
     }
 }
 
