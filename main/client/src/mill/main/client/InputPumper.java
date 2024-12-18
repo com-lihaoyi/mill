5196d2a79e9bb786040c8fb7cadc618da11837a1
Li Haoyi <haoyi.sg@gmail.com>
2024-12-18 13:39:12 +0800

    .

--- main/client/src/mill/main/client/InputPumper.java
+++ main/client/src/mill/main/client/InputPumper.java
@@ -38,9 +38,9 @@ public class InputPumper implements Runnable {
       while (running) {
         if (!runningCheck.getAsBoolean()) {
           running = false;
-          // We need to check `.available` and avoid calling `.read`, because if
-          // we call `.read` and there is nothing to read, it can unnecessarily delay
-          // the JVM exit by 350ms https://stackoverflow.com/questions/48951611/blocking-on-stdin-makes-java-process-take-350ms-more-to-exit
+          // We need to check `.available` and avoid calling `.read`, because if we call `.read`
+          // and there is nothing to read, it can unnecessarily delay the JVM exit by 350ms
+          // https://stackoverflow.com/questions/48951611/blocking-on-stdin-makes-java-process-take-350ms-more-to-exit
         } else if (checkAvailable && src.available() == 0) Thread.sleep(1);
         else {
           int n;
