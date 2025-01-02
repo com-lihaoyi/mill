2fdd135345b7f9e02a9044d17263356b0920d66f
Li Haoyi <haoyi.sg@gmail.com>
2025-01-02 18:22:29 +0800

    .

---
+++ integration/failure/fatal-error/src/CompileErrorTests.scala
@@ -0,0 +1,23 @@
+package mill.integration
+
+import mill.testkit.UtestIntegrationTestSuite
+
+import utest._
+
+object CompileErrorTests extends UtestIntegrationTestSuite {
+  val tests: Tests = Tests {
+    test - integrationTest { tester =>
+      val res = tester.eval("fatalTask")
+
+      assert(res.isSuccess == false)
+      assert(res.err.contains("""java.lang.LinkageError: CUSTOM FATAL ERROR IN TASK"""))
+
+      // This worker invalidates re-evaluates every time due to being dependent on
+      // an upstream `Task.Input`. Make sure that a fatal error in the `close()`
+      // call does not hang the Mill process
+      tester.eval("fatalCloseWorker")
+      val res3 = tester.eval("fatalCloseWorker")
+      assert(res3.err.contains("""java.lang.LinkageError: CUSTOM FATAL ERROR"""))
+    }
+  }
+}
