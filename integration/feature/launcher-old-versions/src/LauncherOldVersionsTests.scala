03a03a197f253bae4ddb592cda2f5ff7d09d74e1
Li Haoyi <haoyi.sg@gmail.com>
2025-01-02 16:47:18 +0800

    Rebootstrap on 0.12.5 (#4220)

--- integration/feature/launcher-old-versions/src/LauncherOldVersionsTests.scala
+++ integration/feature/launcher-old-versions/src/LauncherOldVersionsTests.scala
@@ -16,6 +16,9 @@ class LauncherOldVersionsTests(version: String) extends UtestIntegrationTestSuit
       val launcherScript = sys.env(launcherEnv)
       os.write.over(workspacePath / ".mill-version", version)
 
+      // Remove `rt.jar` cache to make sure each old version can download it properly
+      os.remove.all(os.home / ".mill/ammonite")
+
       // Run Mill once beforehand just to make sure it gets initialized and
       // none of the initialization output gets into `outText`
       os.call(cmd = (launcherScript, "version"), cwd = workspacePath, stderr = os.Pipe)
@@ -38,6 +41,6 @@ class LauncherOldVersionsTests(version: String) extends UtestIntegrationTestSuit
 //object LauncherVersionTests_0_6 extends LauncherOldVersionsTests("0.6.3")
 //object LauncherVersionTests_0_7 extends LauncherOldVersionsTests("0.7.4")
 //object LauncherVersionTests_0_8 extends LauncherOldVersionsTests("0.8.0")
-object LauncherVersionTests_0_9 extends LauncherOldVersionsTests("0.9.12")
-object LauncherVersionTests_0_10 extends LauncherOldVersionsTests("0.10.15")
+//object LauncherVersionTests_0_9 extends LauncherOldVersionsTests("0.9.12")
+//object LauncherVersionTests_0_10 extends LauncherOldVersionsTests("0.10.15")
 object LauncherVersionTests_0_11 extends LauncherOldVersionsTests("0.11.13")
