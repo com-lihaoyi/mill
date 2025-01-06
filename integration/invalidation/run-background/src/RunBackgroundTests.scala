2635522468d70f18bf3dadd52b7c83e680a0b525
Li Haoyi <haoyi.sg@gmail.com>
2025-01-07 07:22:40 +0800

    .

--- integration/invalidation/run-background/src/RunBackgroundTests.scala
+++ integration/invalidation/run-background/src/RunBackgroundTests.scala
@@ -32,9 +32,7 @@ object RunBackgroundTests extends UtestIntegrationTestSuite {
       os.remove(stop)
       eval(("foo.runBackground", lock, stop))
       eventually { !probeLockAvailable(lock) }
-      if (tester.clientServerMode) {
-        eval("shutdown")
-      }
+      if (tester.clientServerMode) eval("shutdown")
       continually { !probeLockAvailable(lock) }
       os.write(stop, "")
       eventually { probeLockAvailable(lock) }
