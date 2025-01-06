c4f1ef26df07d1502e14e3a9cdad0ac8253067ca
Li Haoyi <haoyi.sg@gmail.com>
2025-01-07 07:22:34 +0800

    .

--- integration/invalidation/run-background/src/RunBackgroundTests.scala
+++ integration/invalidation/run-background/src/RunBackgroundTests.scala
@@ -6,21 +6,23 @@ import utest.asserts.{RetryInterval, RetryMax}
 import scala.concurrent.duration._
 import utest._
 
+// Make sure that `runBackground` subprocesses properly outlive the execution
+// that created them, and outlive the Mill process whether terminated naturally
+// at the end of `--no-server` or terminated explicitly via `shutdown`
 object RunBackgroundTests extends UtestIntegrationTestSuite {
   implicit val retryMax: RetryMax = RetryMax(5000.millis)
   implicit val retryInterval: RetryInterval = RetryInterval(50.millis)
 
-  def probeLockAvailable(lock: os.Path) = {
+  def probeLockAvailable(lock: os.Path): Boolean = {
     val raf = new RandomAccessFile(lock.toIO, "rw");
     val chan = raf.getChannel();
-    chan.tryLock() match{
+    chan.tryLock() match {
       case null => false
       case locked =>
         locked.release()
         true
     }
   }
-
 
   val tests: Tests = Tests {
     test("simple") - integrationTest { tester =>
@@ -30,6 +32,10 @@ object RunBackgroundTests extends UtestIntegrationTestSuite {
       os.remove(stop)
       eval(("foo.runBackground", lock, stop))
       eventually { !probeLockAvailable(lock) }
+      if (tester.clientServerMode) {
+        eval("shutdown")
+      }
+      continually { !probeLockAvailable(lock) }
       os.write(stop, "")
       eventually { probeLockAvailable(lock) }
     }
