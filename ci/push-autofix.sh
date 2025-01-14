cd47653ab018acd0599befca14f5f4b055349642
Li Haoyi <haoyi.sg@gmail.com>
2025-01-14 23:48:52 +0800

    .

--- ci/push-autofix.sh
+++ ci/push-autofix.sh
@@ -20,4 +20,5 @@ git config user.name "Mill GitHub Bot"
 cat .autofix-repo
 cat .autofix-branch
 # skip git hooks
-git push --no-verify $(cat .autofix-repo) HEAD:$(cat .autofix-branch)
+#git push --no-verify $(cat .autofix-repo) HEAD:$(cat .autofix-branch)
+git push origin gh-pages:gh-pages
