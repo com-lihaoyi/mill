73c31e2bf8699b3f90012a2b18aa5b057db44fed
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-01-17 19:50:08 +0530

    Fix initscript

--- main/gradle/src/mill/main/gradle/BuildGen.scala
+++ main/gradle/src/mill/main/gradle/BuildGen.scala
@@ -90,15 +90,13 @@ object BuildGen {
 
   private def init: os.Path = {
     val file = os.temp.dir() / "init.gradle"
-    val classpath = escape( // for paths on Windows
-      os.Path(
-        classOf[ProjectTreePlugin].getProtectionDomain.getCodeSource.getLocation.toURI
-      ).toString())
+    val classpath =
+      os.Path(classOf[ProjectTreePlugin].getProtectionDomain.getCodeSource.getLocation.toURI)
     val plugin = classOf[ProjectTreePlugin].getName
     val contents =
       s"""initscript {
          |    dependencies {
-         |        classpath files("$classpath")
+         |        classpath files(${escape(classpath.toString()) /* escape for Windows */})
          |    }
          |}
          |
