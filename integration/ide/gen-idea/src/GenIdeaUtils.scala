b876cf3c7fb179859697118daeff074ee37fa443
Li Haoyi <haoyi.sg@gmail.com>
2025-01-19 13:06:11 +0800

    .

--- integration/ide/gen-idea/src/GenIdeaUtils.scala
+++ integration/ide/gen-idea/src/GenIdeaUtils.scala
@@ -35,7 +35,7 @@ object GenIdeaUtils {
 
     val pattern =
       "(?s)^\\Q" + expected.replaceAll(Pattern.quote(ignoreString), "\\\\E.*\\\\Q") + "\\E$"
-    scala.Predef.assert(Pattern.compile(pattern).matcher(found).matches(), found)
+    assert(Pattern.compile(pattern).matcher(found).matches())
   }
 
   private def normaliseLibraryPaths(in: String, workspacePath: os.Path): String = {
