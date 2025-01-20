0da98269918fd5b532ab083b939102143eea52cc
Li Haoyi <haoyi.sg@gmail.com>
2025-01-20 11:44:26 +0800

    .

--- scalajslib/src/mill/scalajslib/ScalaJSModule.scala
+++ scalajslib/src/mill/scalajslib/ScalaJSModule.scala
@@ -26,7 +26,8 @@ trait ScalaJSModule extends scalalib.ScalaModule { outer =>
     override def jsEnvConfig: T[JsEnvConfig] = outer.jsEnvConfig()
     override def scalaJSOptimizer: T[Boolean] = outer.scalaJSOptimizer()
   }
-  @deprecated("use ScalaJSTests", "0.11.0")
+
+      @deprecated("use ScalaJSTests", "0.11.0")
   type ScalaJSModuleTests = ScalaJSTests
   @deprecated("use ScalaJSTests", "0.11.0")
   trait Tests extends ScalaJSTests
