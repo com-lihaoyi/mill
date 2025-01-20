69b4d5ca20900e47b9d6ff0dde2870a4e50e67ab
Li Haoyi <haoyi.sg@gmail.com>
2025-01-20 11:36:26 +0800

    .

--- ci/push-autofix.sh
+++ ci/push-autofix.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-# Build the pages
-./mill -i docs.githubPages
-
 # Prepare ssh-key for git actions
 echo $REPO_DEPLOY_KEY | base64 --decode > deploy_key
 
@@ -18,25 +15,7 @@ git config user.email "haoyi.sg+travis@gmail.com"
 git config user.name "Mill GitHub Bot"
 
 
-PAGES_REPO=gh-pages
-
-# checkout gh-pages
-git worktree add -b gh-pages gh-pages origin/gh-pages
-
-# we want to keep history, so we prepare a new commit
-rm -r ${PAGES_REPO}/*
-cp -r out/docs/githubPages.dest/site/* ${PAGES_REPO}/
-touch ${PAGES_REPO}/.nojekyll
-
-cd $PAGES_REPO
-
-git add .nojekyll
-git add *
-git commit -m "Updated github pages from commit ${GITHUB_SHA}"
-git push origin gh-pages:gh-pages
-
-
-#cat .autofix-repo
-#cat .autofix-branch
-## skip git hooks
-#git push --no-verify $(cat .autofix-repo) HEAD:$(cat .autofix-branch)
+cat .autofix-repo
+cat .autofix-branch
+# skip git hooks
+git push --no-verify $(cat .autofix-repo) HEAD:$(cat .autofix-branch)
