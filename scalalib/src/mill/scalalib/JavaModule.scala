com-lihaoyi/mill

commit 1d12ccb6a56b9840a926f6cba642e0098cafe5db

Overhaul transitive module handling in dependency resolution (#4145)

build.mill
@@ -595,6 +595,10 @@ trait MillStableScalaModule extends MillPublishScalaModule with Mima {
       "mill.main.MainModule.mill$define$BaseModule0$_setter_$evalWatchedValues_="
     ),
 
+    // Overrides added for new methods, ought to be safe
+    ProblemFilter.exclude[ReversedMissingMethodProblem]("mill.scalalib.JavaModule.mill$scalalib$JavaModule$$super$internalRepositories"),
+    ProblemFilter.exclude[ReversedMissingMethodProblem]("mill.scalanativelib.ScalaNativeModule.mill$scalanativelib$ScalaNativeModule$$super$coursierProject"),
+
     // https://github.com/com-lihaoyi/mill/pull/3503
     ProblemFilter.exclude[ReversedMissingMethodProblem](
       "mill.scalalib.ScalaModule#ScalaTests.mill$scalalib$ScalaModule$ScalaTests$$super$mandatoryScalacOptions"

scalanativelib/src/mill/scalanativelib/ScalaNativeModule.scala
@@ -9,16 +9,7 @@ import mill.util.Jvm
 import mill.util.Util.millProjectModule
 import mill.scalalib.api.ZincWorkerUtil
 import mill.scalalib.bsp.{ScalaBuildTarget, ScalaPlatform}
-import mill.scalalib.{
-  BoundDep,
-  CrossVersion,
-  Dep,
-  DepSyntax,
-  Lib,
-  SbtModule,
-  ScalaModule,
-  TestModule
-}
+import mill.scalalib.{CrossVersion, Dep, DepSyntax, Lib, SbtModule, ScalaModule, TestModule}
 import mill.testrunner.{TestResult, TestRunner, TestRunnerUtils}
 import mill.scalanativelib.api._
 import mill.scalanativelib.worker.{
@@ -313,7 +304,11 @@ trait ScalaNativeModule extends ScalaModule { outer =>
     ))
   }
 
-  override def transitiveIvyDeps: T[Agg[BoundDep]] = Task {
+  // override added for binary compatibility
+  override def transitiveIvyDeps: T[Agg[mill.scalalib.BoundDep]] =
+    super.transitiveIvyDeps
+
+  def coursierProject: Task[coursier.core.Project] = Task.Anon {
 
     // Exclude cross published version dependencies leading to conflicts in Scala 3 vs 2.13
     // When using Scala 3 exclude Scala 2.13 standard native libraries,
@@ -330,15 +325,27 @@ trait ScalaNativeModule extends ScalaModule { outer =>
 
     val nativeSuffix = platformSuffix()
 
-    val exclusions = scalaBinaryVersionToExclude.flatMap { scalaBinVersion =>
-      nativeStandardLibraries.map(library =>
-        "org.scala-native" -> s"$library${nativeSuffix}_$scalaBinVersion"
-      )
-    }
+    val exclusions = coursier.core.MinimizedExclusions(
+      scalaBinaryVersionToExclude
+        .flatMap { scalaBinVersion =>
+          nativeStandardLibraries.map { library =>
+            coursier.core.Organization("org.scala-native") ->
+              coursier.core.ModuleName(s"$library${nativeSuffix}_$scalaBinVersion")
+          }
+        }
+        .toSet
+    )
 
-    super.transitiveIvyDeps().map { dep =>
-      dep.exclude(exclusions: _*)
-    }
+    val proj = super.coursierProject()
+
+    proj.withDependencies(
+      proj.dependencies.map {
+        case (config, dep) =>
+          config -> dep.withMinimizedExclusions(
+            dep.minimizedExclusions.join(exclusions)
+          )
+      }
+    )
   }
 
   override def prepareOffline(all: Flag): Command[Unit] = {

example/cli/builtins/1-builtin-commands/build.mill
@@ -248,7 +248,6 @@ foo.unmanagedClasspath
 foo.compileIvyDeps
 ...
 foo.ivyDeps
-foo.transitiveIvyDeps
 foo.compileClasspath
 
 */

example/fundamentals/out-dir/1-out-files/build.mill
@@ -243,7 +243,8 @@ out/mill-server
 
 > cat out/mill-build/methodCodeHashSignatures.dest/current/spanningInvalidationTree.json
 {
-  "call scala.runtime.BoxesRunTime.boxToInteger(int)java.lang.Integer": {
+  "call scala.runtime.BoxesRunTime.boxToInteger(int)java.lang.Integer": {},
+  "call scala.Predef$#println(java.lang.Object)void": {
     "def build_.package_$foo$#<init>(build_.package_)void": {
       "call build_.package_$foo$!<init>(build_.package_)void": {
         "def build_.package_#foo$lzycompute$1()void": {
@@ -253,8 +254,7 @@ out/mill-server
         }
       }
     }
-  },
-  "call scala.Predef$#println(java.lang.Object)void": {}
+  }
 }
 
 */

bsp/worker/src/mill/bsp/worker/MillBuildServer.scala
@@ -296,7 +296,10 @@ private class MillBuildServer(
           Task.Anon {
             (
               m.defaultResolver().resolveDeps(
-                m.transitiveCompileIvyDeps() ++ m.transitiveIvyDeps(),
+                Seq(
+                  m.coursierDependency.withConfiguration(coursier.core.Configuration.provided),
+                  m.coursierDependency
+                ),
                 sources = true
               ),
               m.unmanagedClasspath(),
@@ -332,7 +335,18 @@ private class MillBuildServer(
       hint = "buildTargetDependencyModules",
       targetIds = _ => params.getTargets.asScala.toSeq,
       tasks = { case m: JavaModule =>
-        Task.Anon { (m.transitiveCompileIvyDeps(), m.transitiveIvyDeps(), m.unmanagedClasspath()) }
+        Task.Anon {
+          (
+            // full list of dependencies, including transitive ones
+            m.defaultResolver().allDeps(
+              Seq(
+                m.coursierDependency.withConfiguration(coursier.core.Configuration.provided),
+                m.coursierDependency
+              )
+            ),
+            m.unmanagedClasspath()
+          )
+        }
       }
     ) {
       case (
@@ -340,14 +354,14 @@ private class MillBuildServer(
             state,
             id,
             m: JavaModule,
-            (transitiveCompileIvyDeps, transitiveIvyDeps, unmanagedClasspath)
+            (ivyDeps, unmanagedClasspath)
           ) =>
-        val ivy = transitiveCompileIvyDeps ++ transitiveIvyDeps
-        val deps = ivy.map { dep =>
-          // TODO: add data with "maven" data kind using a ...
+        val deps = ivyDeps.collect {
+          case dep if dep.module.organization != JavaModule.internalOrg =>
+            // TODO: add data with "maven" data kind using a ...
 //          MavenDependencyModule
 
-          new DependencyModule(dep.dep.module.repr, dep.dep.version)
+            new DependencyModule(dep.module.repr, dep.version)
         }
         val unmanaged = unmanagedClasspath.map { dep =>
           new DependencyModule(s"unmanaged-${dep.path.last}", "")

contrib/bloop/src/mill/contrib/bloop/BloopImpl.scala
@@ -392,8 +392,10 @@ class BloopImpl(evs: () => Seq[Evaluator], wd: os.Path) extends ExternalModule {
     val bloopResolution: Task[BloopConfig.Resolution] = Task.Anon {
       val repos = module.repositoriesTask()
       // same as input of resolvedIvyDeps
-      val allIvyDeps = module.transitiveIvyDeps() ++ module.transitiveCompileIvyDeps()
-      val coursierDeps = allIvyDeps.map(_.dep).toList
+      val coursierDeps = Seq(
+        module.coursierDependency.withConfiguration(coursier.core.Configuration.provided),
+        module.coursierDependency
+      )
       BloopConfig.Resolution(artifacts(repos, coursierDeps))
     }
 

contrib/playlib/src/mill/playlib/Static.scala
@@ -44,10 +44,12 @@ trait Static extends ScalaModule {
   }
 
   /**
-   * webjar dependencies - created from transitive ivy deps
+   * webjar dependencies - created from ivy deps
    */
   def webJarDeps = Task {
-    transitiveIvyDeps().filter(_.dep.module.organization.value == "org.webjars")
+    ivyDeps()
+      .filter(_.dep.module.organization.value == "org.webjars")
+      .map(bindDependency())
   }
 
   /**

commit 45c655b108eb89a94d18b502b322dbfde0fcf799

Address bin compat issues introduced in #4145

build.mill
@@ -596,14 +596,6 @@ trait MillStableScalaModule extends MillPublishScalaModule with Mima {
       "mill.main.MainModule.mill$define$BaseModule0$_setter_$evalWatchedValues_="
     ),
 
-    // Overrides added for new methods, ought to be safe
-    ProblemFilter.exclude[ReversedMissingMethodProblem](
-      "mill.scalalib.JavaModule.mill$scalalib$JavaModule$$super$internalRepositories"
-    ),
-    ProblemFilter.exclude[ReversedMissingMethodProblem](
-      "mill.scalanativelib.ScalaNativeModule.mill$scalanativelib$ScalaNativeModule$$super$coursierProject"
-    ),
-
     // https://github.com/com-lihaoyi/mill/pull/3503
     ProblemFilter.exclude[ReversedMissingMethodProblem](
       "mill.scalalib.ScalaModule#ScalaTests.mill$scalalib$ScalaModule$ScalaTests$$super$mandatoryScalacOptions"

scalalib/src/mill/scalalib/JavaModule.scala
@@ -438,15 +438,7 @@ trait JavaModule
       JavaModule.internalVersion
     ).withConfiguration(cs.Configuration.compile)
 
-  /**
-   * The `coursier.Project` corresponding to this `JavaModule`.
-   *
-   * This provides details about this module to the coursier resolver (details such as
-   * dependencies, BOM dependencies, dependency management, etc.). Beyond more general
-   * resolution parameters (such as artifact types, etc.), this should be the only way
-   * we provide details about this module to coursier.
-   */
-  def coursierProject: Task[cs.Project] = Task {
+  private[mill] def javaModuleCoursierProject: Task[cs.Project] = Task {
 
     // Tells coursier that if something depends on a given scope of ours, we should also
     // pull other scopes of our own dependencies.
@@ -568,6 +560,17 @@ trait JavaModule
   }
 
   /**
+   * The `coursier.Project` corresponding to this `JavaModule`.
+   *
+   * This provides details about this module to the coursier resolver (details such as
+   * dependencies, BOM dependencies, dependency management, etc.). Beyond more general
+   * resolution parameters (such as artifact types, etc.), this should be the only way
+   * we provide details about this module to coursier.
+   */
+  def coursierProject: Task[cs.Project] =
+    javaModuleCoursierProject
+
+  /**
    * Coursier project of this module and those of all its transitive module dependencies
    */
   def transitiveCoursierProjects: Task[Seq[cs.Project]] = Task {
@@ -677,7 +680,7 @@ trait JavaModule
    * doing.
    */
   def internalRepositories: Task[Seq[cs.Repository]] = Task.Anon {
-    super.internalRepositories() ++ Seq(internalDependenciesRepository())
+    Seq(internalDependenciesRepository())
   }
 
   /**

scalanativelib/src/mill/scalanativelib/ScalaNativeModule.scala
@@ -336,7 +336,9 @@ trait ScalaNativeModule extends ScalaModule { outer =>
         .toSet
     )
 
-    val proj = super.coursierProject()
+    // could be a problem if
+    //   javaModuleCoursierProject ne super.coursierProject
+    val proj = javaModuleCoursierProject()
 
     proj.withDependencies(
       proj.dependencies.map {