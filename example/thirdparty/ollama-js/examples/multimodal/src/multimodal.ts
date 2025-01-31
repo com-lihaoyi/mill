0630ab5df74715cf0f52177b55e850da05db2fdb
Monye David <monyedavid@outlook.com>
2025-01-31 09:18:44 +0100

    [wip]

---
+++ example/thirdparty/ollama-js/examples/multimodal/src/multimodal.ts
@@ -0,0 +1,12 @@
+import ollama from 'ollama'
+
+const imagePath = './cat.jpg'
+const response = await ollama.generate({
+  model: 'llava',
+  prompt: 'describe this image:',
+  images: [imagePath],
+  stream: true,
+})
+for await (const part of response) {
+  process.stdout.write(part.response)
+}
