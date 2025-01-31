c0730367ab7d41666f0de05c46a4050a028abe61
Jakob Odersky <jakob@odersky.com>
2025-01-31 11:07:03 +0100

    wip

--- pythonlib/src/mill/pythonlib/CoverageModule.scala
+++ pythonlib/src/mill/pythonlib/CoverageModule.scala
@@ -3,15 +3,34 @@ package mill.pythonlib
 import mill._
 
 /**
- * Code coverage
+ * Code coverage via Python's [coverage](https://coverage.readthedocs.io/)
+ * package.
+ *
+ * Mix this in to any module to run them
  */
 trait CoverageModule extends PythonModule {
+
+  override def pythonToolDeps = Task {
+    super.pythonToolDeps() ++ Seq("coverage>=7.6.10")
+  }
+
+  // override def pythonOptions = Task {
+  //   Seq("-m", "coverage", "run") ++ super.pythonOptions()
+  // }
+
+  def report = Task {
+
+    pythonExe()
+
+  }
+
+
+
 
   trait CoverageTests extends PythonTests {
     override def pythonToolDeps = Task {
       super.pythonToolDeps() ++ Seq("coverage>=7.6.9")
     }
-
     override def pythonOptions = Task {
       Seq("-m", "coverage", "run") ++ super.pythonOptions()
     }
