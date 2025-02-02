d93c7192f65d14225716e467b68ad8ce7d9a8134
Jeffrey <106184818+c0d33ngr@users.noreply.github.com>
2025-02-02 08:00:04 +0000

    initial commit

---
+++ example/scalalib/spark/4-hello-pyspark/bar/src/bar.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+logFile = "README.md"
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+spark.stop()
