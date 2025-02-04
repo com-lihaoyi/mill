ffb2b8875ba6a5afd45347de4ef56eedb4cdbb8e
Monye David <monyedavid@outlook.com>
2025-02-04 22:27:00 +0100

    [wip]

---
+++ example/large/multi/14-multi-language/server/src/com/example/Server.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+}
+
