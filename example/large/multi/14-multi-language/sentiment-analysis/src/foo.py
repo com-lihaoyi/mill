0e22e19721cf1af3b363d402eadb03a4a4b9c68a
Monye David <monyedavid@outlook.com>
2025-02-04 23:47:15 +0100

    example/large/multi/14-multi-language

--- example/large/multi/14-multi-language/sentiment-analysis/src/foo.py
+++ example/large/multi/14-multi-language/sentiment-analysis/src/foo.py
@@ -1,25 +1,3 @@
-# import argparse
-# from argparse import Namespace
-# from jinja2 import Template
-#
-#
-# def generate_html(text: str) -> str:
-#     template = Template("<h1>{{ text }}</h1>")
-#     return template.render(text=text)
-#
-#
-# def main() -> str:
-#     parser = argparse.ArgumentParser(description="Inserts text into an HTML template")
-#     parser.add_argument("-t", "--text", required=True, help="Text to insert")
-#
-#     args: Namespace = parser.parse_args()
-#     html: str = generate_html(args.text)
-#     return html
-#
-#
-# if __name__ == "__main__":
-#     print(main())
-
 import sys
 from textblob import TextBlob
 
