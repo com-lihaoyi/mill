e41623f4a55930685f886a75d1d094e0d449330a
Li Haoyi <haoyi.sg@gmail.com>
2025-02-06 09:20:20 +0800

    .

--- kotlinlib/src/mill/kotlinlib/KotlinMavenModule.scala
+++ kotlinlib/src/mill/kotlinlib/KotlinMavenModule.scala
@@ -8,22 +8,22 @@ import mill.javalib.MavenModule
  */
 trait KotlinMavenModule extends KotlinModule with MavenModule {
   override def sources = Task.Sources(
-    millSourcePath / "src/main/java",
-    millSourcePath / "src/main/kotlin"
+    "src/main/java",
+    "src/main/kotlin"
   )
   override def resources = Task.Sources {
-    millSourcePath / "src/main/resources"
+    "src/main/resources"
   }
 
   trait KotlinMavenTests extends KotlinTests with MavenTests {
     override def intellijModulePath: os.Path = millSourcePath / "src/test"
 
     override def sources = Task.Sources(
-      millSourcePath / "src/test/java",
-      millSourcePath / "src/test/kotlin"
+      "src/test/java",
+      "src/test/kotlin"
     )
     override def resources = Task.Sources {
-      millSourcePath / "src/test/resources"
+      "src/test/resources"
     }
   }
 }
