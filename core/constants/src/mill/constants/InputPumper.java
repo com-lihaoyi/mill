8d3c9439fb37c0d62dd38e73ca2298731b1dba52
Tobias Roeser <le.petit.fou@web.de>
2025-02-07 11:04:15 +0100

    Mark overridden method

--- core/constants/src/mill/constants/InputPumper.java
+++ core/constants/src/mill/constants/InputPumper.java
@@ -29,6 +29,7 @@ public class InputPumper implements Runnable {
 
   boolean running = true;
 
+  @Override
   public void run() {
     InputStream src = src0.get();
     OutputStream dest = dest0.get();
