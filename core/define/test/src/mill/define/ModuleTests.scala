6f6e22fe58886dd304efc17c52985a934fab6cb8
Li Haoyi <haoyi.sg@gmail.com>
2025-02-09 21:03:04 +0800

    failing unit test for Module#toString

---
+++ core/define/test/src/mill/define/ModuleTests.scala
@@ -0,0 +1,30 @@
+package mill.define
+
+import mill.define.{Discover, DynamicModule, ModuleRef, NamedTask, TaskModule}
+import mill.testkit.TestBaseModule
+import mill.util.TestGraphs
+import mill.util.TestGraphs.*
+import mill.{Cross, Module, Task}
+import utest.*
+
+object ModuleTests extends TestSuite {
+
+  val tests = Tests {
+
+    object graphs extends TestGraphs()
+    import graphs._
+    import TestGraphs._
+
+    test("test"){
+      singleton.toString ==> ""
+      singleton.single.toString ==> "single"
+    }
+    test("cross"){
+      nestedCrosses.toString ==> ""
+      nestedCrosses.cross.toString ==> "cross"
+      nestedCrosses.cross("210").toString ==> "cross[210]"
+      nestedCrosses.cross("210").cross2.toString ==> "cross[210].cross2"
+      nestedCrosses.cross("210").cross2("js").toString ==> "cross[210].cross2[js]"
+    }
+  }
+}
--- core/resolve/src/mill/resolve/ResolveCore.scala
+++ core/resolve/src/mill/resolve/ResolveCore.scala
@@ -98,6 +98,11 @@ private object ResolveCore {
       seenModules: Set[Class[?]],
       cache: Cache
   ): Result = {
+    assert(
+      querySoFar == current.segments,
+      "" + pprint.apply(querySoFar) + " != " + pprint.apply(current.segments)
+    )
+
     def moduleClasses(resolved: Iterable[Resolved]): Set[Class[?]] = {
       resolved.collect { case Resolved.Module(_, cls) => cls }.toSet
     }
@@ -106,6 +111,8 @@ private object ResolveCore {
       case Nil => Success(Seq(current))
       case head :: tail =>
         def recurse(searchModules: Seq[Resolved]): Result = {
+          pprint.log(current)
+          pprint.log(searchModules)
           val (failures, successesLists) = searchModules
             .map { r =>
               val rClasses = moduleClasses(Set(r))
