f564ad1f637cdc2cd1cbb4f3e3952450d46040b8
Li Haoyi <haoyi.sg@gmail.com>
2025-02-14 10:21:51 +0800

    .

--- main/src/mill/main/VisualizeModule.scala
+++ main/src/mill/main/VisualizeModule.scala
@@ -13,6 +13,8 @@ import guru.nidi.graphviz.attribute.Rank.RankDir
 import guru.nidi.graphviz.attribute.{Rank, Shape, Style}
 import mill.exec
 import mill.exec.Plan
+import mill.eval.Evaluator
+import mill.define.SelectMode
 
 object VisualizeModule extends ExternalModule with VisualizeModule {
   def repositories: Seq[Repository] = Seq(
@@ -22,7 +24,42 @@ object VisualizeModule extends ExternalModule with VisualizeModule {
   )
 
   lazy val millDiscover = Discover[this.type]
+
+  private type VizWorker = (
+      LinkedBlockingQueue[(scala.Seq[NamedTask[Any]], scala.Seq[NamedTask[Any]], os.Path)],
+      LinkedBlockingQueue[Result[scala.Seq[PathRef]]]
+  )
+
+  private[mill] def visualize0(
+      evaluator: Evaluator,
+      targets: Seq[String],
+      ctx: mill.api.Ctx,
+      vizWorker: VizWorker,
+      planTasks: Option[List[NamedTask[?]]] = None
+  ): Result[Seq[PathRef]] = {
+    def callVisualizeModule(
+        tasks: List[NamedTask[Any]],
+        transitiveTasks: List[NamedTask[Any]]
+    ): Result[Seq[PathRef]] = {
+      val (in, out) = vizWorker
+      in.put((tasks, transitiveTasks, ctx.dest))
+      val res = out.take()
+      res.map { v =>
+        println(upickle.default.write(v.map(_.path.toString()), indent = 2))
+        v
+      }
+    }
+
+    evaluator.resolveTasks(targets, SelectMode.Multi).flatMap {
+      rs =>
+        planTasks match {
+          case Some(allRs) => callVisualizeModule(rs, allRs)
+          case None => callVisualizeModule(rs, rs)
+        }
+    }
+  }
 }
+
 trait VisualizeModule extends mill.define.TaskModule {
   def repositories: Seq[Repository]
   def defaultCommandName() = "run"
