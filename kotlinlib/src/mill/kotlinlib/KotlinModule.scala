3cd69df97c4b231afc2e2958a13e24fcd5647fc4
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-02-14 18:27:32 +0200

    Add unit test and inherit kotlin compiler embeddable to kotlin tests

--- kotlinlib/src/mill/kotlinlib/KotlinModule.scala
+++ kotlinlib/src/mill/kotlinlib/KotlinModule.scala
@@ -412,6 +412,8 @@ trait KotlinModule extends JavaModule { outer =>
       outer.kotlincOptions().filterNot(_.startsWith("-Xcommon-sources")) ++
         Seq(s"-Xfriend-paths=${outer.compile().classes.path.toString()}")
     }
+
+    override def kotlinCompilerEmbeddable: Task[Boolean] = outer.kotlinCompilerEmbeddable
   }
 
 }
