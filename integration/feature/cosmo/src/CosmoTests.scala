d866f1ce8c86af79f4f5bec3cafb95f9fd8acd7e
Kien Dang <kiend@proton.me>
2025-02-15 00:51:25 +0800

    Cosmo everything

--- integration/feature/cosmo/src/CosmoTests.scala
+++ integration/feature/cosmo/src/CosmoTests.scala
@@ -4,29 +4,29 @@ import mill.testkit.UtestIntegrationTestSuite
 
 import utest._
 
-object CosmopolitanTests extends UtestIntegrationTestSuite {
+object CosmoTests extends UtestIntegrationTestSuite {
   val tests: Tests = Tests {
-    test("test APE assemblies") - integrationTest {
+    test("test Cosmopolitan APE assemblies") - integrationTest {
       tester =>
         import tester._
 
         // test running a simple hello world APE assembly
-        val res0 = eval("helloworld.apeAssembly")
+        val res0 = eval("helloworld.cosmoAssembly")
         assert(res0.isSuccess)
-        val assembly0 = workspacePath / "out/helloworld/apeAssembly.dest/out.jar.exe"
+        val assembly0 = workspacePath / "out/helloworld/cosmoAssembly.dest/out.jar.exe"
         assert(os.call(assembly0).out.text().trim == "Hello World")
 
         // test running an APE assembly with arguments
-        val res1 = eval("hello.apeAssembly")
+        val res1 = eval("hello.cosmoAssembly")
         assert(res1.isSuccess)
-        val assembly1 = workspacePath / "out/hello/apeAssembly.dest/out.jar.exe"
+        val assembly1 = workspacePath / "out/hello/cosmoAssembly.dest/out.jar.exe"
         val args = "scala".toSeq
         assert(os.call((assembly1, args)).out.text().trim == s"Hello ${args.mkString(" ")}")
 
         // test running an APE assembly with forkArgs
-        val res2 = eval("javaopts.apeAssembly")
+        val res2 = eval("javaopts.cosmoAssembly")
         assert(res2.isSuccess)
-        val assembly2 = workspacePath / "out/javaopts/apeAssembly.dest/out.jar.exe"
+        val assembly2 = workspacePath / "out/javaopts/cosmoAssembly.dest/out.jar.exe"
 
         val forkArgs = "my.java.property" -> "hello"
         val forkArgsArgv0 = "my.argv0" -> assembly2.toString
