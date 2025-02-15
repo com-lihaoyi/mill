8b141aa11153c8987ba655244a57327d89d8c805
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-02-15 21:08:14 +0800

    Remove the unnecessary code marked with TODOs to be removed in the previous commit, adjust the format of some code, and inline an sbt task

--- main/init/sbt/models/src/mill/main/sbt/Models.scala
+++ main/init/sbt/models/src/mill/main/sbt/Models.scala
@@ -7,32 +7,6 @@ import upickle.default.{macroRW, ReadWriter => RW}
 
 object Models {
   type URL = String
-
-  // TODO Remove. This seems not supported.
-
-  type Id[X] = X // not working with uPickle
-  /*object Id {
-    //implicit def rw[T](tRw : RW[T]) : RW[Id[T]] = tRw
-    implicit def rw[T: RW]: RW[Id[T]] = macroRW
-  }*/
-}
-
-// TODO Remove. Not used.
-/**
- * A wrapper added because [[Models.Id]] does not work with uPickle.
- * @param wrappedValue named this way in order not to conflict with [[sbt.std.MacroValue.value]]
- */
-case class Wrapper[+A](wrappedValue: A) extends AnyVal {
-  def map[B](f: A => B): Wrapper[B] = Wrapper(f(wrappedValue))
-}
-object Wrapper {
-  import scala.language.implicitConversions
-  implicit def to[A](value: A): Wrapper[A] =
-    Wrapper(value)
-  implicit def from[A](wrapper: Wrapper[A]): A =
-    wrapper.wrappedValue
-
-  implicit def rw[T: RW]: RW[Wrapper[T]] = macroRW
 }
 
 case class BuildExport(
@@ -42,136 +16,6 @@ case class BuildExport(
 )
 object BuildExport {
   implicit val rw: RW[BuildExport] = macroRW
-}
-
-// TODO remove these plain ones
-
-case class PlainBuildInfo(
-    buildPublicationInfo: PlainBuildPublicationInfo,
-    /** @see [[Keys.javacOptions]] */
-    javacOptions: Seq[String],
-    /** @see [[Keys.resolvers]] */
-    resolvers: Seq[Resolver]
-)
-object PlainBuildInfo {
-  implicit val rw: RW[PlainBuildInfo] = macroRW
-}
-
-case class PlainBuildPublicationInfo(
-    /** @see [[Keys.description]] */
-    description: String,
-    /**
-     * corresponds to `url` in POM
-     *
-     * @see [[Keys.homepage]]
-     */
-    homepage: Option[String],
-    /** @see [[Keys.licenses]] */
-    licenses: Seq[License],
-    // organization: Option[String],
-    /*
-                                      /**
-     * corresponds to Maven's `organization` in POM
-     *
-     * @see [[Keys.organization]]
-     */
-                                      organizationName: String,
-     */
-    /**
-     * corresponds to `organizationUrl` in POM
-     *
-     * @see [[Keys.organizationHomepage]]
-     */
-    organizationHomepage: Option[String],
-    /** @see [[Keys.developers]] */
-    developers: Seq[Developer],
-    /** @see [[Keys.scmInfo]] */
-    scmInfo: Option[ScmInfo]
-)
-object PlainBuildPublicationInfo {
-  implicit val rw: RW[PlainBuildPublicationInfo] = macroRW
-
-  /** @see [[sbt.librarymanagement.License]] */
-  type License = (String, URL)
-}
-
-/*
-TODO I used higher-kinded types here along with some higher-rank types (`GetValueFn` and `MapFn`) in the sbt plugin,
- because I first thought that the default build info's members don't need to be wrapped with `Option` while the project build info's members do,
- and we can reuse some more code this way,
- but it turns out that both kinds have to be wrapped with `Option` and the abstraction doesn't work with the sbt `value` macros.
- Most likely we can just replace `F` with `Option` in them and remove the `TC` suffixes,
- resulting in `mill.main.sbt.BuildInfo` below,
- unless they can possibly provide more extensibility, which is unlikely as I think now.
- See other related definitions such as `Id`, `Wrapper`,
- `getBuildInfo[F[_]](ref: Reference, getValue: GetValueFn[F], map: MapFn[F]): BuildInfoTC[F]`, `GetValueFn`, and `MapFn`.
- */
-
-case class BuildInfoTC[F[_]](
-    buildPublicationInfo: BuildPublicationInfoTC[F],
-    /** @see [[Keys.javacOptions]] */
-    javacOptions: F[Seq[String]],
-    /** @see [[Keys.scalacOptions]] */
-    scalacOptions: F[Seq[String]],
-    /** @see [[Keys.resolvers]] */
-    resolvers: F[Seq[Resolver]]
-)
-object BuildInfoTC {
-  type BuildInfo = BuildInfoTC[Wrapper]
-  implicit val wrapperRw: RW[BuildInfo] = macroRW
-  type OptionBuildInfo = BuildInfoTC[Option]
-  implicit val optionRw: RW[OptionBuildInfo] = macroRW
-}
-
-case class BuildPublicationInfoTC[F[_]](
-    /** @see [[Keys.description]] */
-    description: F[String],
-    /**
-     * corresponds to `url` in POM
-     *
-     * @see [[Keys.homepage]]
-     */
-    homepage: F[Option[String]],
-    /** @see [[Keys.licenses]] */
-    licenses: F[Seq[PlainBuildPublicationInfo.License]],
-    // organization: Option[String],
-    /*
-    /**
-     * corresponds to Maven's `organization` in POM
-     *
-     * @see [[Keys.organization]]
-     */
-    organizationName: F[String],
-     */
-    /**
-     * corresponds to `organizationUrl` in POM
-     *
-     * @see [[Keys.organizationHomepage]]
-     */
-    organizationHomepage: F[Option[String]],
-    /** @see [[Keys.developers]] */
-    developers: F[Seq[Developer]],
-    /** @see [[Keys.scmInfo]] */
-    scmInfo: F[Option[ScmInfo]]
-)
-object BuildPublicationInfoTC {
-
-  /** @see [[sbt.librarymanagement.License]] */
-  type License = (String, URL)
-
-  /*
-  // This does not work: "could not find implicit value for parameter e: upickle.default.Reader[F[String]]".
-  implicit def rw[F[_]]: RW[BuildPublicationInfoTC[F]] = macroRW
-   */
-  /*
-  // This does not work: "could not find implicit value for parameter e: upickle.default.Reader[mill.main.sbt.Models.Id[Seq[mill.main.sbt.BuildPublicationInfo.License]]]".
-  type BuildPublicationInfo = BuildPublicationInfoTC[Id]
-  implicit val IdRw: RW[BuildPublicationInfo] = macroRW
-   */
-  type BuildPublicationInfo = BuildPublicationInfoTC[Wrapper]
-  implicit val wrapperRw: RW[BuildPublicationInfo] = macroRW
-  type OptionBuildPublicationInfo = BuildPublicationInfoTC[Option]
-  implicit val optionRw: RW[OptionBuildPublicationInfo] = macroRW
 }
 
 case class BuildInfo(
@@ -200,7 +44,7 @@ case class BuildPublicationInfo(
      */
     homepage: Option[Option[String]],
     /** @see [[Keys.licenses]] */
-    licenses: Option[Seq[PlainBuildPublicationInfo.License]],
+    licenses: Option[Seq[License]],
     /**
      * corresponds to `groupId` in POM and Mill's `PomSettings.organization`
      *
@@ -273,24 +117,10 @@ case class Project(
     // dirs: ProjectDirs, // relative
     projectDirectory: String,
     buildInfo: BuildInfo,
-    // dependencyConfigurations: Seq[DependencyConfiguration] // TODO remove
     allDependencies: Seq[Dependency]
 )
 object Project {
   implicit val rw: RW[Project] = macroRW
-
-  // TODO Remove. Not used.
-  type ProjectDirs = Seq[String]
-}
-
-// TODO Remove. For an old implementation of retrieving `Configuration / allDependencies` that doesn't work.
-case class DependencyConfiguration(
-    id: String,
-    allDependencies: Seq[Dependency]
-    /*, projectDependencies: Seq[ProjectDirs], libraryDependencies: Seq[LibraryDependency]*/
-)
-object DependencyConfiguration {
-  implicit val rw: RW[DependencyConfiguration] = macroRW
 }
 
 case class Dependency(
--- main/init/sbt/sbt-mill-init-export-build/src/main/scala/mill/main/sbt/ExportBuildPlugin.scala
+++ main/init/sbt/sbt-mill-init-export-build/src/main/scala/mill/main/sbt/ExportBuildPlugin.scala
@@ -1,7 +1,6 @@
 package mill.main.sbt
 
 import sbt.Keys.*
-import sbt.Project.extract
 import sbt.io.IO
 import sbt.librarymanagement.Disabled
 import sbt.{Def, Developer as _, Project as _, Resolver as _, ScmInfo as _, *}
@@ -18,54 +17,11 @@ object ExportBuildPlugin extends AutoPlugin {
     val millInitBuildInfo = taskKey[BuildInfo](
       "get the `mill.main.sbt.BuildInfo` model of this build or this project"
     )
-    // TODO Remove. No longer used.
-    val millInitDependencyConfiguration = taskKey[DependencyConfiguration](
-      "get the `mill.main.sbt.DependencyConfiguration` model of this project and configuration"
-    )
-    val millInitAllDependencies = taskKey[Seq[Dependency]](
-      "get the all the `mill.main.sbt.Dependency`s of this project"
-    )
+    // not used anymore
+    val millInitAllDependencies = taskKey[Seq[Dependency]]("get the all the `mill.main.sbt.Dependency`s of this project")
     val millInitProject = taskKey[Project]("get the `mill.main.sbt.Project` model of this project")
     val millInitExportBuild = taskKey[File]("export the build in a JSON file for `mill init`")
   }
-
-  // TODO `getBuildInfo` along with `MapFn` and `GetValueFn` does not work with the `value` macro. Remove.
-
-  trait MapFn[F[_]] {
-    // def apply[A, B](fa : F[A], f: A => B): F[B] // Type inference doesn't work well with this.
-    def apply[A, B](fa: F[A])(f: A => B): F[B]
-  }
-
-  trait GetValueFn[F[_]] {
-    def apply[T](key: SettingKey[T]): F[T]
-
-    def apply[T](key: TaskKey[T]): F[T]
-  }
-
-  def getBuildInfo[F[_]](ref: Reference, getValue: GetValueFn[F], map: MapFn[F]): BuildInfoTC[F] =
-    BuildInfoTC(
-      BuildPublicationInfoTC(
-        getValue(ref / description),
-        map(getValue(ref / homepage))(_.map(_.toExternalForm)),
-        map(getValue(ref / licenses))(_.map { case (name, url) => (name, url.toExternalForm) }),
-        //getValue(ref / organizationName), // not needed
-        map(getValue(ref / organizationHomepage))(_.map(_.toExternalForm)),
-        map(getValue(ref / developers))(_.map(developer =>
-          Developer(developer.id, developer.name, developer.email, developer.url.toExternalForm)
-        )),
-        map(getValue(ref / scmInfo))(_.map(scmInfo =>
-          ScmInfo(scmInfo.browseUrl.toExternalForm, scmInfo.connection, scmInfo.devConnection)
-        ))
-      ),
-      getValue(ref / javacOptions),
-      getValue(ref / scalacOptions),
-      map(getValue(ref / resolvers))(_.flatMap {
-        case mavenRepository: MavenRepository => Some(Resolver(mavenRepository.root))
-        case resolver =>
-          println(s"A `Resolver` which is not a `MavenRepository` is skipped: $resolver")
-          None
-      })
-    )
 
   import autoImport.*
 
@@ -101,38 +57,41 @@ object ExportBuildPlugin extends AutoPlugin {
     buildInfoSetting
   )
 
-  // TODO Remove. For an old implementation that depends on retrieving `configuration / allDependencies` which doesn't work.
-  private val neededConfigurations = Seq(Compile, Test, Runtime, Default, Provided, Optional)
-
-  // TODO Remove. No longer used. Replaced by `millInitAllDependencies`.
-  val millInitDependencyConfigurationSetting =
-    millInitDependencyConfiguration := DependencyConfiguration(
-      configuration.value.id,
-
-      /** See the TODO in [[sbt.Defaults]] above `allDependencies :=` for more details (v1.10.7, Lines 3210 - 3212). */
-      /*allDependencies.value*/ (projectDependencies.value ++ libraryDependencies.value).map { moduleID =>
-        Dependency(
-          moduleID.organization,
-          moduleID.name,
-          moduleID.crossVersion match {
-            case Disabled => false
-            case _: Binary => true
-            case crossVersion =>
-              println(s"Unsupported `CrossVersion`: $crossVersion")
-              false
-          },
-          moduleID.revision,
-          moduleID.configurations
-        )
-      }
-    )
-
   override lazy val projectSettings: Seq[Setting[?]] = Seq(
-    Seq(buildInfoSetting),
-    // TODO Remove. For an old implementation that depends on `configuration / allDependencies` which doesn't work.
-    neededConfigurations.flatMap(configuration => inConfig(configuration)(Seq(millInitDependencyConfigurationSetting))),
-    Seq(
-      millInitAllDependencies := {
+    buildInfoSetting,
+    millInitProject :=
+      Project(
+        //organization.value,
+        name.value,
+        //version.value,
+        //baseDirectory.value.relativeTo((ThisBuild / baseDirectory).value).get.getPath.split(File.separator),
+        baseDirectory.value.getPath,
+        {
+          // keep the project `BuildInfo` members only when they are different
+          val defaultBi = (ThisBuild / millInitBuildInfo).value
+          val projectBi = millInitBuildInfo.value
+          import projectBi.*
+          BuildInfo({
+            val defaultBpi = defaultBi.buildPublicationInfo
+            import projectBi.buildPublicationInfo.*
+            BuildPublicationInfo(
+              if (description != defaultBpi.description) description else None,
+              if (homepage != defaultBpi.homepage) homepage else None,
+              if (licenses != defaultBpi.licenses) licenses else None,
+              if (organization != defaultBpi.organization) organization else None,
+              //if (organizationName != defaultBpi.organizationName) organizationName else None, // not needed
+              if (organizationHomepage != defaultBpi.organizationHomepage) organizationHomepage else None,
+              if (developers != defaultBpi.developers) developers else None,
+              if (scmInfo != defaultBpi.scmInfo) scmInfo else None,
+              if (version != defaultBpi.version) version else None
+            )
+          },
+            if (javacOptions != defaultBi.javacOptions) javacOptions else None,
+            if (scalacOptions != defaultBi.scalacOptions) scalacOptions else None,
+            if (resolvers != defaultBi.resolvers) resolvers else None
+          )
+        },
+
         /** See the TODO in [[sbt.Defaults]] above `allDependencies :=` for more details (v1.10.7, Lines 3210 - 3212). */
         /*allDependencies.value*/ (projectDependencies.value ++ libraryDependencies.value).map { moduleID =>
           Dependency(
@@ -149,99 +108,16 @@ object ExportBuildPlugin extends AutoPlugin {
             moduleID.configurations
           )
         }
-      },
-      millInitProject := {
-        // TODO remove
-        val extracted = extract(state.value) // TODO move to a separate task if used
-        Project(
-          //organization.value,
-          name.value,
-          //version.value,
-          //baseDirectory.value.relativeTo((ThisBuild / baseDirectory).value).get.getPath.split(File.separator),
-          baseDirectory.value.getPath,
-          // TODO remove old implementation
-          /*
-          getBuildInfo[Option](
-            ThisProject,
-            new GetValueFn[Option] {
-              override def apply[T](key: SettingKey[T]): Option[T] = extracted.getOpt(key)
+      ),
+    // `target.value` doesn't work in `globalSettings` and `buildSettings`, so this is added to `projectSettings.
+    millInitExportBuild := {
+      val defaultBuildInfo = (ThisBuild / millInitBuildInfo).value
+      val projects = millInitProject.all(ScopeFilter(inAnyProject)).value
+      val buildExport = BuildExport(defaultBuildInfo, projects)
 
-              override def apply[T](key: TaskKey[T]): Option[T] =
-                ??? // extracted.getOpt(key).map(_.value)
-            },
-            new MapFn[Option] {
-              override def apply[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
-            }
-          )
-           */
-          {
-            // keep the project `BuildInfo` members only when they are different
-            val defaultBi = (ThisBuild / millInitBuildInfo).value
-            val projectBi = millInitBuildInfo.value
-            import projectBi.*
-            BuildInfo({
-              val defaultBpi = defaultBi.buildPublicationInfo
-              import projectBi.buildPublicationInfo.*
-              BuildPublicationInfo(
-                if (description != defaultBpi.description) description else None,
-                if (homepage != defaultBpi.homepage) homepage else None,
-                if (licenses != defaultBpi.licenses) licenses else None,
-                if (organization != defaultBpi.organization) organization else None,
-                //if (organizationName != defaultBpi.organizationName) organizationName else None, // not needed
-                if (organizationHomepage != defaultBpi.organizationHomepage) organizationHomepage else None,
-                if (developers != defaultBpi.developers) developers else None,
-                if (scmInfo != defaultBpi.scmInfo) scmInfo else None,
-                if (version != defaultBpi.version) version else None
-              )
-            },
-              if (javacOptions != defaultBi.javacOptions) javacOptions else None,
-              if (scalacOptions != defaultBi.scalacOptions) scalacOptions else None,
-              if (resolvers != defaultBi.resolvers) resolvers else None
-            )
-          },
-          {
-            // TODO Remove. For an old implementation that depends on retrieving `configuration / allDependencies` which doesn't work.
-            millInitDependencyConfiguration.all(
-              ScopeFilter(configurations = inConfigurations(neededConfigurations *))
-            ).value
-            millInitAllDependencies.value
-          }
-        )
-      },
-      // `target.value` doesn't work in `globalSettings` and `buildSettings`, so this is added to `projectSettings.
-      millInitExportBuild := {
-        // TODO This does not work with the `value` macro. Remove.
-        /*
-        val defaultBuildInfo = getBuildInfo[Wrapper](
-          ThisBuild,
-          new GetValueFn[Wrapper] {
-            override def apply[T](key: SettingKey[T]): Wrapper[T] =
-              ??? // Wrapper(key.value) // Could not find proxy
-            override def apply[T](key: TaskKey[T]): Wrapper[T] =
-              ??? // Wrapper(key.value) // Could not find proxy
-          },
-          new MapFn[Wrapper] {
-            override def apply[A, B](fa: Wrapper[A])(f: A => B): Wrapper[B] = fa.map(f)
-          }
-        )
-         */
-
-        val defaultBuildInfo = (ThisBuild / millInitBuildInfo).value
-
-        // TODO remove the old implementation code
-        // `(projectRef / ...)` can't be used directly here due to macro limitations: "java.lang.IllegalArgumentException: Could not find proxy for projectRef: sbt.ProjectRef in ..."
-        /*
-        val allProjectPairs = buildStructure.value.allProjectPairs
-        val projects = allProjectPairs.map { case (resolvedProject, projectRef) => ??? }
-         */
-
-        val projects = millInitProject.all(ScopeFilter(inAnyProject)).value
-
-        val buildExport = BuildExport(defaultBuildInfo, projects)
-
-        val outputFile = target.value / "mill-init-build-export.json"
-        IO.write(outputFile, write(buildExport))
-        outputFile
-      }
-    )).flatten
+      val outputFile = target.value / "mill-init-build-export.json"
+      IO.write(outputFile, write(buildExport))
+      outputFile
+    }
+  )
 }
