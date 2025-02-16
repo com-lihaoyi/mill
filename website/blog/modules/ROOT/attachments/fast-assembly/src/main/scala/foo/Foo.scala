88863665121c1d90bbf9997dd07991f00581d739
Li Haoyi <haoyi.sg@gmail.com>
2025-02-16 20:58:49 +0800

    Blog Post: Fast JVM Assembly Jars Creation with the Mill Build Tool (#4558)
    Co-authored-by: Tobias Roeser <le.petit.fou@web.de>

---
+++ website/blog/modules/ROOT/attachments/fast-assembly/src/main/scala/foo/Foo.scala
@@ -0,0 +1,44 @@
+package foo
+
+import org.apache.spark.sql.{SparkSession, Dataset, DataFrame}
+import org.apache.spark.sql.functions._
+
+object Foo {
+
+  case class Transaction(id: Int, category: String, amount: Double)
+
+  def computeSummary(transactions: Dataset[Transaction]): DataFrame = {
+    transactions.groupBy("category")
+      .agg(
+        sum("amount").alias("total_amount"),
+        avg("amount").alias("average_amount"),
+        count("amount").alias("transaction_count")
+      )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("SparkExample")
+      .master("local[*]")
+      .getOrCreate()
+
+    val resourcePath: String = args(0)
+
+    import spark.implicits._
+
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(resourcePath)
+
+    val transactionsDS: Dataset[Transaction] = df.as[Transaction]
+    val summaryDF = computeSummary(transactionsDS)
+
+    println("Summary Statistics by Category:")
+    summaryDF.show()
+
+    spark.stop()
+  }
+}
+
+object dummy2
