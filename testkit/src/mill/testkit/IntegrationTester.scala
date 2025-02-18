f2dbb98ce6eb8f038d4cb6d641095100f7516ef5
Li Haoyi <haoyi.sg@gmail.com>
2025-02-18 15:33:32 +0800

    .

--- testkit/src/mill/testkit/IntegrationTester.scala
+++ testkit/src/mill/testkit/IntegrationTester.scala
@@ -88,7 +88,7 @@ object IntegrationTester {
         timeout = timeout,
         check = check,
         propagateEnv = propagateEnv,
-        shutdownGracePeriod = timeoutGracePeriod,
+        shutdownGracePeriod = timeoutGracePeriod
       )
 
       IntegrationTester.EvalResult(
