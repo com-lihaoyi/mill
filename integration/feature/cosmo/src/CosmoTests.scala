5cefa9e4871ba588d1c24e6b08a7be79bd120cbf
Kien Dang <kiend@proton.me>
2025-02-19 14:38:54 +0800

    Add test for JAVA_OPTS

--- integration/feature/cosmo/src/CosmoTests.scala
+++ integration/feature/cosmo/src/CosmoTests.scala
@@ -4,7 +4,15 @@ import mill.testkit.UtestIntegrationTestSuite
 
 import utest._
 
+import scala.jdk.CollectionConverters._
+
 object CosmoTests extends UtestIntegrationTestSuite {
+  def updateEnv(env: Map[String, String], k: String, v: String): Map[String, String] = {
+    env.get(k).fold(env.updated(k, v)) { v0 =>
+      env.updated(k, s"$v0 $v")
+    }
+  }
+
   val tests: Tests = Tests {
     test("test Cosmopolitan APE assemblies") - integrationTest {
       tester =>
@@ -39,6 +47,24 @@ object CosmoTests extends UtestIntegrationTestSuite {
 
         assert(props.contains(forkArgs))
         assert(props.contains(forkArgsArgv0))
+
+        val vars = Map(
+          "JAVA_OPTS" -> "-Dmy.jvm.property=0",
+          "JDK_JAVA_OPTIONS" -> "-Dmy.jvm.property=1",
+          "JAVA_TOOL_OPTIONS" -> "-Dmy.jvm.property=2"
+        )
+
+        val updatedEnv = vars.foldRight(System.getenv().asScala.toMap) { case ((k, v), env) =>
+          updateEnv(env, k, v)
+        }
+
+        val props0 = os.call(assembly2, env = updatedEnv, propagateEnv = false).out.lines()
+          .map(_.split('='))
+          .collect {
+            case Array(k, v) => k -> v
+          }
+
+        assert(props0.contains("my.jvm.property" -> "0"))
     }
   }
 }
