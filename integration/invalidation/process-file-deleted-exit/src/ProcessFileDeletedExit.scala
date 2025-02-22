6b65476198016f7ed3d40e1ff81865ff23639391
Li Haoyi <haoyi.sg@gmail.com>
2025-02-22 10:26:30 +0800

    .

--- integration/invalidation/process-file-deleted-exit/src/ProcessFileDeletedExit.scala
+++ integration/invalidation/process-file-deleted-exit/src/ProcessFileDeletedExit.scala
@@ -16,34 +16,37 @@ object ProcessFileDeletedExit extends UtestIntegrationTestSuite {
   implicit val retryMax: RetryMax = RetryMax(30.seconds)
   implicit val retryInterval: RetryInterval = RetryInterval(1.seconds)
   val tests: Tests = Tests {
-    integrationTest { tester =>
-      import tester._
-
-      assert(!os.exists(workspacePath / "out/mill-server"))
-      assert(!os.exists(workspacePath / "out/mill-no-server"))
-
-      @volatile var watchTerminated = false
-      Future {
-        eval(
-          ("--watch", "foo"),
-          stdout = os.ProcessOutput.Readlines { println(_) },
-          stderr = os.ProcessOutput.Readlines { println(_) }
-        )
-        watchTerminated = true
+    retry(3) {
+      integrationTest { tester =>
+        import tester._
+  
+        assert(!os.exists(workspacePath / "out/mill-server"))
+        assert(!os.exists(workspacePath / "out/mill-no-server"))
+  
+        @volatile var watchTerminated = false
+        Future {
+          eval(
+            ("--watch", "foo"),
+            stdout = os.ProcessOutput.Readlines { println(_) },
+            stderr = os.ProcessOutput.Readlines { println(_) }
+          )
+          watchTerminated = true
+        }
+  
+        if (tester.clientServerMode) eventually { os.exists(workspacePath / "out/mill-server") }
+        else eventually { os.exists(workspacePath / "out/mill-no-server") }
+  
+        assert(watchTerminated == false)
+  
+        val processRoot =
+          if (tester.clientServerMode) workspacePath / "out/mill-server"
+          else workspacePath / "out/mill-no-server"
+  
+        os.list(processRoot).map(p => os.remove(p / "processId"))
+  
+        eventually { watchTerminated == true }
       }
-
-      if (tester.clientServerMode) eventually { os.exists(workspacePath / "out/mill-server") }
-      else eventually { os.exists(workspacePath / "out/mill-no-server") }
-
-      assert(watchTerminated == false)
-
-      val processRoot =
-        if (tester.clientServerMode) workspacePath / "out/mill-server"
-        else workspacePath / "out/mill-no-server"
-
-      os.list(processRoot).map(p => os.remove(p / "processId"))
-
-      eventually { watchTerminated == true }
     }
   }
+    
 }
