cbb0491804afba4c571862b00f163e89f2ccf2d0
Li Haoyi <haoyi.sg@gmail.com>
2025-02-24 16:56:03 +0800

    debug

--- integration/feature/no-java-bootstrap/src/NoJavaBootstrapTests.scala
+++ integration/feature/no-java-bootstrap/src/NoJavaBootstrapTests.scala
@@ -5,26 +5,34 @@ import mill.testkit.UtestIntegrationTestSuite
 import utest._
 
 object NoJavaBootstrapTests extends UtestIntegrationTestSuite {
+  println("A")
   // Don't propagate `JAVA_HOME` to this test suite, because we want to exercise
   // the code path where `JAVA_HOME` is not present during bootstrapping
   override def propagateJavaHome = false
   val tests: Tests = Tests {
+    println("B")
     test - integrationTest { tester =>
+      println("C")
       import tester._
+      println("D")
       os.remove(tester.workspacePath / ".mill-jvm-version")
-
+      println("E")
       // The Mill server process should use the default Mill Java version,
       // even without the `.mill-jvm-version` present
       //
       // Force Mill client to ignore any system `java` installation, to make sure
       // this tests works reliably regardless of what is installed on the system
       val res1 = eval("foo", env = Map("MILL_TEST_SUITE_IGNORE_SYSTEM_JAVA" -> "true"), stderr = os.Inherit)
+      println("F")
       assert(res1.out == System.getProperty("java.version"))
 
       // Any `JavaModule`s run from the Mill server should also inherit
       // the default Mill Java version from it
+      println("G")
       val res2 = eval("bar.run", env = Map("MILL_TEST_SUITE_IGNORE_SYSTEM_JAVA" -> "true"), stderr = os.Inherit)
+      println("H")
       assert(res2.out == s"Hello World! ${System.getProperty("java.version")}")
+      println("I")
     }
   }
 }
