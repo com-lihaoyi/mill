5a677fb0fadb29e53ebec709326c94b1f28e1b9a
Li Haoyi <haoyi.sg@gmail.com>
2025-02-24 12:54:29 +0800

    .

--- integration/feature/no-java-bootstrap/src/NoJavaBootstrapTests.scala
+++ integration/feature/no-java-bootstrap/src/NoJavaBootstrapTests.scala
@@ -10,8 +10,15 @@ object NoJavaBootstrapTests extends UtestIntegrationTestSuite {
       import tester._
       os.remove(tester.workspacePath / ".mill-jvm-version")
 
-      val out = eval("foo")
-      assert(out.out == "17.0.14")
+      // The Mill server process should use the default Mill Java version,
+      // evenw without the `.mill-jvm-version` present
+      val res1 = eval("foo")
+      assert(res1.out == "17.0.14")
+
+      // Any `JavaModule`s run from the Mill server should also inherit
+      // the default Mill Java version from it
+      val res2 = eval("bar.run")
+      assert(res2.out == "Hello World! 17.0.14")
     }
   }
 }
