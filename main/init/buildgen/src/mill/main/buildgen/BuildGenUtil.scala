com-lihaoyi/mill

commit 22033e909dd461689b374815ca4602b087b8830f

Support comparing a module's `def`'s with and extending super base module's `def`s in converted builds as required in https://github.com/com-lihaoyi/mill/pull/4586#issuecomment-2674530292
- Refactor some types to use Scala 3 explicit nulls BTW.
- Fix a bug that `pomSettings` in modules different from that in the base module is lost in builds converted from Maven, BTW.
- Update unit test code to test the changes and the expected snapshots in test resources correspondingly.

main/init/buildgen/src/mill/main/buildgen/BuildGenUtil.scala
@@ -51,10 +51,7 @@ object BuildGenUtil {
 
   }
 
-  def takeIrPomIfNeeded(baseInfo: IrBaseInfo, irPom: IrPom): IrPom =
-    if (baseInfo.noPom || irPom != baseInfo.moduleTypedef.pomSettings) irPom else null
-
-  def renderIrPom(value: IrPom): String = {
+  def renderIrPom(value: IrPom | Null): String = {
     if (value == null) ""
     else {
       import value.*
@@ -64,8 +61,12 @@ object BuildGenUtil {
     }
   }
 
-  def renderIrBuild(value: IrBuild): String = {
-    import value.*
+  /**
+   * @param baseInfo to compare with [[build]] and render the values only if they are different.
+   */
+  def renderIrBuild(build: IrBuild, baseInfo: IrBaseInfo): String = {
+    val baseTrait = baseInfo.moduleTypedef
+    import build.*
     val testModuleTypedef =
       if (!hasTest) ""
       else {
@@ -90,13 +91,22 @@ object BuildGenUtil {
 
     s"""${renderArtifactName(projectName, dirs)}
        |
-       |${renderJavacOptions(javacOptions)}
+       |${renderJavacOptions(
+        javacOptions,
+        if (baseTrait != null) baseTrait.javacOptions else Seq.empty
+      )}
        |
-       |${renderScalaVersion(scalaVersion)}
+       |${renderScalaVersion(scalaVersion, if (baseTrait != null) baseTrait.scalaVersion else None)}
        |
-       |${renderScalacOptions(scalacOptions)}
+       |${renderScalacOptions(
+        scalacOptions,
+        if (baseTrait != null) baseTrait.scalacOptions else None
+      )}
        |
-       |${renderRepositories(repositories)}
+       |${renderRepositories(
+        repositories,
+        if (baseTrait != null) baseTrait.repositories else Seq.empty
+      )}
        |
        |${renderBomIvyDeps(scopedDeps.mainBomIvyDeps)}
        |
@@ -112,9 +122,16 @@ object BuildGenUtil {
        |
        |${renderRunModuleDeps(scopedDeps.mainRunModuleDeps)}
        |
-       |${if (pomSettings == null) "" else renderPomSettings(renderIrPom(pomSettings))}
+       |${
+        if (pomSettings != (if (baseTrait != null) baseTrait.pomSettings else null))
+          renderPomSettings(renderIrPom(pomSettings))
+        else ""
+      }
        |
-       |${renderPublishVersion(publishVersion)}
+       |${renderPublishVersion(
+        publishVersion,
+        if (baseTrait != null) baseTrait.publishVersion else null
+      )}
        |
        |${renderPomPackaging(packaging)}
        |
@@ -303,7 +320,7 @@ object BuildGenUtil {
   def isBom(groupArtifactVersion: (String, String, String)): Boolean =
     groupArtifactVersion._2.endsWith("-bom")
 
-  def isNullOrEmpty(value: String): Boolean =
+  def isNullOrEmpty(value: String | Null): Boolean =
     null == value || value.isEmpty
 
   val linebreak: String =
@@ -347,6 +364,7 @@ object BuildGenUtil {
        |  def jvmId = "$jvmId"
        |}""".stripMargin
 
+  // TODO consider renaming to `renderOptionalDef` or `renderIfArgsNonEmpty`?
   def optional(construct: String, args: IterableOnce[String]): String =
     optional(construct + "(", args, ",", ")")
 
@@ -355,6 +373,43 @@ object BuildGenUtil {
     if (itr.isEmpty) ""
     else itr.mkString(start, sep, end)
   }
+
+  def renderStringSeqWithSuper(
+      defName: String,
+      args: Seq[String],
+      superArgs: Seq[String] = Seq.empty,
+      transform: String => String
+  ): Option[String] =
+    if (args.startsWith(superArgs)) {
+      val superLength = superArgs.length
+      if (args.length == superLength) None
+      else
+        // Note that the super def is called even when it's empty.
+        // Some super functions can be called without parentheses, but we just add it here for simplicity.
+        Some(optional(
+          s"super.$defName() ++ Seq",
+          args.iterator.drop(superLength).map(transform)
+        ))
+    } else
+      Some(optional(s"Seq", args.iterator.map(transform)))
+
+  def renderStringSeqTargetDefWithSuper(
+      defName: String,
+      args: Seq[String],
+      superArgs: Seq[String] = Seq.empty,
+      transform: String => String
+  ) =
+    renderStringSeqWithSuper(defName, args, superArgs, transform).map(s"def $defName = " + _)
+
+  def renderStringSeqTaskDefWithSuper(
+      defName: String,
+      args: Seq[String],
+      superArgs: Seq[String] = Seq.empty,
+      transform: String => String
+  ) =
+    renderStringSeqWithSuper(defName, args, superArgs, transform).map(s =>
+      s"def $defName = Task.Anon { $s }"
+    )
 
   def scalafmtConfigFile: os.Path =
     os.temp(
@@ -394,30 +449,26 @@ object BuildGenUtil {
   def renderRunModuleDeps(args: IterableOnce[String]): String =
     optional("def runModuleDeps = super.runModuleDeps ++ Seq", args)
 
-  def renderJavacOptions(args: IterableOnce[String]): String =
-    optional(
-      "def javacOptions = super.javacOptions() ++ Seq",
-      args.iterator.map(escape)
-    )
+  def renderJavacOptions(args: Seq[String], superArgs: Seq[String] = Seq.empty): String =
+    renderStringSeqTargetDefWithSuper("javacOptions", args, superArgs, escape).getOrElse("")
 
-  def renderScalaVersion(scalaVersion: Option[String]): String =
-    scalaVersion.fold("")(scalaVersion => s"def scalaVersion = ${escape(scalaVersion)}")
+  def renderScalaVersion(arg: Option[String], superArg: Option[String] = None): String =
+    if (arg != superArg) arg.fold("")(scalaVersion => s"def scalaVersion = ${escape(scalaVersion)}")
+    else ""
 
-  def renderScalacOptions(args: Option[IterableOnce[String]]): String =
-    args.fold("")(args =>
-      optional(
-        "def scalacOptions = super.scalacOptions() ++ Seq",
-        args.iterator.map(escape)
-      )
-    )
+  def renderScalacOptions(
+      args: Option[Seq[String]],
+      superArgs: Option[Seq[String]] = None
+  ): String =
+    renderStringSeqTargetDefWithSuper(
+      "scalacOptions",
+      args.getOrElse(Seq.empty),
+      superArgs.getOrElse(Seq.empty),
+      escape
+    ).getOrElse("")
 
-  def renderRepositories(args: IterableOnce[String]): String =
-    optional(
-      "def repositoriesTask = Task.Anon { super.repositoriesTask() ++ Seq(",
-      args,
-      ", ",
-      ") }"
-    )
+  def renderRepositories(args: Seq[String], superArgs: Seq[String] = Seq.empty): String =
+    renderStringSeqTaskDefWithSuper("repositoriesTask", args, superArgs, identity).getOrElse("")
 
   def renderResources(args: IterableOnce[os.SubPath]): String =
     optional(
@@ -438,15 +489,20 @@ object BuildGenUtil {
     if (isNullOrEmpty(artifact)) ""
     else s"def pomParentProject = Some($artifact)"
 
-  def renderPomSettings(arg: String): String =
+  def renderPomSettings(arg: String | Null, superArg: String | Null = null): String =
     if (isNullOrEmpty(arg)) ""
     else s"def pomSettings = $arg"
 
-  def renderPublishVersion(arg: String): String =
-    if (isNullOrEmpty(arg)) ""
-    else s"def publishVersion = ${escape(arg)}"
+  def renderPublishVersion(arg: String | Null, superArg: String | Null = null): String =
+    if (arg != superArg)
+      if (isNullOrEmpty(arg)) ""
+      else s"def publishVersion = ${escape(arg)}"
+    else ""
 
-  def renderPublishProperties(args: IterableOnce[(String, String)]): String = {
+  def renderPublishProperties(
+      args: Seq[(String, String)],
+      superArgs: Seq[(String, String)] = Seq.empty
+  ): String = {
     val tuples = args.iterator.map { case (k, v) => s"(${escape(k)}, ${escape(v)})" }
     optional("def publishProperties = super.publishProperties() ++ Map", tuples)
   }

main/init/buildgen/src/mill/main/buildgen/BuildGenBase.scala
@@ -49,7 +49,7 @@ trait BuildGenBase[M, D, I] {
           println(s"converting module $name")
 
           val build = optionalBuild.copy(value = moduleModel)
-          val inner = extractIrBuild(cfg, baseInfo, build, packages)
+          val inner = extractIrBuild(cfg, build, packages)
 
           val isNested = optionalBuild.dirs.nonEmpty
           BuildObject(
@@ -60,7 +60,7 @@ trait BuildGenBase[M, D, I] {
                 SortedMap((name, SortedMap(inner.scopedDeps.namedIvyDeps.toSeq*)))
               ),
             supertypes = getSupertypes(cfg, baseInfo, build),
-            inner = BuildGenUtil.renderIrBuild(inner),
+            inner = BuildGenUtil.renderIrBuild(inner, baseInfo),
             outer =
               if (isNested || baseInfo.moduleTypedef == null) ""
               else BuildGenUtil.renderIrTrait(baseInfo.moduleTypedef)
@@ -87,7 +87,7 @@ trait BuildGenBase[M, D, I] {
 
   def extractIrBuild(
       cfg: C,
-      baseInfo: IrBaseInfo,
+      // baseInfo: IrBaseInfo, // `baseInfo` is no longer needed as we compare the `IrBuild` with `IrBaseInfo`/`IrTrait` in common code now.
       build: Node[M],
       packages: Map[(String, String, String), String]
   ): IrBuild

main/init/buildgen/src/mill/main/buildgen/ir.scala
@@ -43,8 +43,8 @@ case class IrTrait(
     javacOptions: Seq[String],
     scalaVersion: Option[String],
     scalacOptions: Option[Seq[String]],
-    pomSettings: IrPom,
-    publishVersion: String,
+    pomSettings: IrPom | Null,
+    publishVersion: String | Null,
     publishProperties: Seq[(String, String)],
     repositories: Seq[String]
 )
@@ -78,6 +78,7 @@ case class IrLicense(
 )
 
 // TODO Consider renaming to `IrModule(Build)` to disambiguate? sbt, for example, uses `ThisBuild` and `buildSettings` to refer to the whole build.
+// TODO reuse the members in `IrTrait`?
 case class IrBuild(
     scopedDeps: IrScopedDeps,
     testModule: String,
@@ -89,10 +90,10 @@ case class IrBuild(
     scalaVersion: Option[String],
     scalacOptions: Option[Seq[String]],
     projectName: String,
-    pomSettings: IrPom,
-    publishVersion: String,
-    packaging: String,
-    pomParentArtifact: IrArtifact,
+    pomSettings: IrPom | Null,
+    publishVersion: String | Null,
+    packaging: String | Null,
+    pomParentArtifact: IrArtifact | Null,
     resources: Seq[os.SubPath],
     testResources: Seq[os.SubPath],
     publishProperties: Seq[(String, String)]
@@ -139,7 +140,9 @@ case class IrScopedDeps(
     testCompileModuleDeps: SortedSet[String] = SortedSet()
 )
 
+// TODO remove `IrBaseInfo` and just use `IrTrait` directly?
 case class IrBaseInfo(
+    /*
     javacOptions: Seq[String] = Nil,
     scalaVersion: Option[String] = None,
     scalacOptions: Option[Seq[String]] = None,
@@ -147,7 +150,9 @@ case class IrBaseInfo(
     noPom: Boolean = true,
     publishVersion: String = "",
     publishProperties: Seq[(String, String)] = Nil,
-    moduleTypedef: IrTrait = null
+     */
+    // TODO consider renaming directly to `trait` or `baseTrait`?
+    moduleTypedef: IrTrait | Null = null
 )
 
 sealed class IrDependencyType

main/init/gradle/src/mill/main/gradle/GradleBuildGenMain.scala
@@ -150,21 +150,12 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Java
       repos
     )
 
-    IrBaseInfo(
-      javacOptions,
-      scalaVersion,
-      scalacOptions,
-      repos,
-      pomSettings == null,
-      publishVersion,
-      Seq.empty,
-      typedef
-    )
+    IrBaseInfo(typedef)
   }
 
   override def extractIrBuild(
       cfg: Config,
-      baseInfo: IrBaseInfo,
+      // baseInfo: IrBaseInfo,
       build: Node[ProjectModel],
       packages: Map[(String, String, String), String]
   ): IrBuild = {
@@ -177,13 +168,13 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Java
       testModuleMainType = "MavenTests",
       hasTest = os.exists(getMillSourcePath(project) / "src/test"),
       dirs = build.dirs,
-      repositories = getRepositories(project).diff(baseInfo.repositories),
-      javacOptions = getJavacOptions(project).diff(baseInfo.javacOptions),
+      repositories = getRepositories(project),
+      javacOptions = getJavacOptions(project),
       scalaVersion = None,
       scalacOptions = None,
       projectName = getArtifactId(project),
-      pomSettings = if (baseInfo.noPom) extractPomSettings(project) else null,
-      publishVersion = if (version == baseInfo.publishVersion) null else version,
+      pomSettings = extractPomSettings(project),
+      publishVersion = version,
       packaging = getPomPackaging(project),
       // not available
       pomParentArtifact = null,
@@ -209,7 +200,10 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Java
 
   def getSupertypes(cfg: Config, baseInfo: IrBaseInfo, build: Node[ProjectModel]): Seq[String] =
     Seq("RootModule") ++
-      Option.when(null != build.value.maven().pom() && baseInfo.noPom) { "PublishModule" } ++
+      Option.when(null != build.value.maven().pom() && {
+        val baseTrait = baseInfo.moduleTypedef
+        baseTrait == null || !baseTrait.moduleSupertypes.contains("PublishModule")
+      }) { "PublishModule" } ++
       Option.when(build.dirs.nonEmpty || os.exists(getMillSourcePath(build.value) / "src")) {
         getModuleSupertypes(cfg)
       }.toSeq.flatten
@@ -242,7 +236,7 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Java
         .toSeq
     } else Seq.empty
 
-  def getPublishVersion(project: ProjectModel): String =
+  def getPublishVersion(project: ProjectModel): String | Null =
     project.version() match {
       case "" | "unspecified" => null
       case version => version
@@ -252,7 +246,7 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Java
     BuildGenUtil.renderIvyString(dep.group(), dep.name(), dep.version())
   }
 
-  def extractPomSettings(project: ProjectModel): IrPom = {
+  def extractPomSettings(project: ProjectModel): IrPom | Null = {
     val pom = project.maven.pom()
     if (null == pom) null
     else {

main/init/maven/src/mill/main/maven/MavenBuildGenMain.scala
@@ -89,21 +89,12 @@ object MavenBuildGenMain extends BuildGenBase.MavenAndGradle[Model, Dependency] 
       getRepositories(model)
     )
 
-    IrBaseInfo(
-      javacOptions,
-      scalaVersion,
-      scalacOptions,
-      repositories,
-      noPom = false,
-      publishVersion,
-      publishProperties,
-      typedef
-    )
+    IrBaseInfo(typedef)
   }
 
   override def extractIrBuild(
       cfg: Config,
-      baseInfo: IrBaseInfo,
+      // baseInfo: IrBaseInfo,
       build: Node[Model],
       packages: Map[(String, String, String), String]
   ): IrBuild = {
@@ -117,12 +108,12 @@ object MavenBuildGenMain extends BuildGenBase.MavenAndGradle[Model, Dependency] 
       hasTest = os.exists(getMillSourcePath(model) / "src/test"),
       dirs = build.dirs,
       repositories = getRepositories(model),
-      javacOptions = Plugins.MavenCompilerPlugin.javacOptions(model).diff(baseInfo.javacOptions),
+      javacOptions = Plugins.MavenCompilerPlugin.javacOptions(model),
       scalaVersion = None,
       scalacOptions = None,
       projectName = getArtifactId(model),
-      pomSettings = if (baseInfo.noPom) extractPomSettings(model) else null,
-      publishVersion = if (version == baseInfo.publishVersion) null else version,
+      pomSettings = extractPomSettings(model),
+      publishVersion = version,
       packaging = model.getPackaging,
       pomParentArtifact = mkPomParent(model.getParent),
       resources =
@@ -131,7 +122,7 @@ object MavenBuildGenMain extends BuildGenBase.MavenAndGradle[Model, Dependency] 
       testResources =
         processResources(model.getBuild.getTestResources, getMillSourcePath(model))
           .filterNot(_ == mavenTestResourceDir),
-      publishProperties = getPublishProperties(model, cfg.shared).diff(baseInfo.publishProperties)
+      publishProperties = getPublishProperties(model, cfg.shared)
     )
   }
 

main/init/maven/test/resources/expected/config/build.mill
@@ -27,6 +27,20 @@ object `package` extends RootModule with MyModule {
     def javacOptions = super.javacOptions() ++
       Seq("-source", "1.6", "-target", "1.6")
 
+    def pomSettings = PomSettings(
+      "Sample multi module Maven project with a working, deployable site.",
+      "com.example.maven-samples",
+      "http://www.example.com",
+      Seq(),
+      VersionControl(
+        Some("http://github.com/gabrielf/maven-samples"),
+        Some("scm:git:git@github.com:gabrielf/maven-samples.git"),
+        Some("scm:git:git@github.com:gabrielf/maven-samples.git"),
+        Some("HEAD")
+      ),
+      Seq()
+    )
+
     def pomPackagingType = PackagingType.Pom
 
     def publishProperties = super.publishProperties() ++ Map(
@@ -38,6 +52,20 @@ object `package` extends RootModule with MyModule {
 
       def javacOptions = super.javacOptions() ++
         Seq("-source", "1.6", "-target", "1.6")
+
+      def pomSettings = PomSettings(
+        "Logic.",
+        "com.example.maven-samples",
+        "http://www.example.com/server",
+        Seq(),
+        VersionControl(
+          Some("http://github.com/gabrielf/maven-samples/server"),
+          Some("scm:git:git@github.com:gabrielf/maven-samples.git/server"),
+          Some("scm:git:git@github.com:gabrielf/maven-samples.git/server"),
+          Some("HEAD")
+        ),
+        Seq()
+      )
 
       def pomParentProject = Some(Artifact(
         "com.example.maven-samples",
@@ -72,6 +100,20 @@ object `package` extends RootModule with MyModule {
       def compileIvyDeps = super.compileIvyDeps() ++
         Seq(Deps.`javax.servlet.jsp:jsp-api`, Deps.`javax.servlet:servlet-api`)
 
+      def pomSettings = PomSettings(
+        "Webapp.",
+        "com.example.maven-samples",
+        "http://www.example.com/webapp",
+        Seq(),
+        VersionControl(
+          Some("http://github.com/gabrielf/maven-samples/webapp"),
+          Some("scm:git:git@github.com:gabrielf/maven-samples.git/webapp"),
+          Some("scm:git:git@github.com:gabrielf/maven-samples.git/webapp"),
+          Some("HEAD")
+        ),
+        Seq()
+      )
+
       def pomPackagingType = "war"
 
       def pomParentProject = Some(Artifact(
@@ -97,6 +139,20 @@ object `package` extends RootModule with MyModule {
 
     def ivyDeps = super.ivyDeps() ++
       Seq(Deps.`javax.servlet.jsp:jsp-api`, Deps.`javax.servlet:servlet-api`)
+
+    def pomSettings = PomSettings(
+      "Sample single module Maven project with a working, deployable site.",
+      "com.example.maven-samples",
+      "http://www.example.com",
+      Seq(),
+      VersionControl(
+        Some("http://github.com/gabrielf/maven-samples"),
+        Some("scm:git:git@github.com:gabrielf/maven-samples.git"),
+        Some("scm:git:git@github.com:gabrielf/maven-samples.git"),
+        Some("HEAD")
+      ),
+      Seq()
+    )
 
     def publishProperties = super.publishProperties() ++ Map(
       ("project.build.sourceEncoding", "utf-8"),

main/init/sbt/src/mill/main/sbt/SbtBuildGenMain.scala
@@ -229,21 +229,12 @@ object SbtBuildGenMain
       repositories
     )
 
-    IrBaseInfo(
-      javacOptions,
-      scalaVersion,
-      scalacOptions,
-      repositories,
-      noPom = false, // always publish
-      publishVersion,
-      Seq.empty,
-      typedef
-    )
+    IrBaseInfo(typedef)
   }
 
   override def extractIrBuild(
       cfg: Config,
-      baseInfo: IrBaseInfo,
+      // baseInfo: IrBaseInfo,
       build: Node[Project],
       packages: Map[(String, String, String), String]
   ): IrBuild = {
@@ -257,18 +248,13 @@ object SbtBuildGenMain
       testModuleMainType = "SbtTests",
       hasTest = os.exists(getMillSourcePath(project) / "src/test"),
       dirs = build.dirs,
-      repositories = getRepositories(buildInfo).diff(baseInfo.repositories),
-      javacOptions = getJavacOptions(buildInfo).diff(baseInfo.javacOptions),
-      scalaVersion =
-        if (buildInfo.scalaVersion != baseInfo.scalaVersion) buildInfo.scalaVersion else None,
-      scalacOptions = buildInfo.scalacOptions.map(scalacOptions =>
-        baseInfo.scalacOptions.fold(scalacOptions)(baseScalacOptions =>
-          scalacOptions.diff(baseScalacOptions)
-        )
-      ),
+      repositories = getRepositories(buildInfo),
+      javacOptions = getJavacOptions(buildInfo),
+      scalaVersion = buildInfo.scalaVersion,
+      scalacOptions = buildInfo.scalacOptions,
       projectName = project.name,
-      pomSettings = takeIrPomIfNeeded(baseInfo, extractPomSettings(buildInfo.buildPublicationInfo)),
-      publishVersion = if (version == baseInfo.publishVersion) null else version,
+      pomSettings = extractPomSettings(buildInfo.buildPublicationInfo),
+      publishVersion = version,
       packaging = null, // not available in sbt as it seems
       pomParentArtifact = null, // not available
       resources = Nil,
@@ -305,7 +291,7 @@ object SbtBuildGenMain
       s"coursier.maven.MavenRepository(${escape(resolver.root)})"
     )
 
-  def getPublishVersion(buildInfo: BuildInfo): String =
+  def getPublishVersion(buildInfo: BuildInfo): String | Null =
     buildInfo.buildPublicationInfo.version.orNull
 
   // originally named `ivyInterp` in the Maven and module

main/init/sbt/test/resources/expected/config/all/sbt-multi-project-example/build.mill
@@ -79,6 +79,8 @@ object `package` extends RootModule with BaseModule {
 
   object multi1 extends BaseModule {
 
+    def scalacOptions = super.scalacOptions() ++ Seq("-V")
+
     def ivyDeps = super.ivyDeps() ++ Seq(
       Deps.`ch.qos.logback:logback-classic`,
       Deps.`com.github.julien-truffaut:monocle-core`,
@@ -124,6 +126,16 @@ object `package` extends RootModule with BaseModule {
   }
 
   object multi2 extends BaseModule {
+
+    def scalacOptions = Seq(
+      "-unchecked",
+      "-feature",
+      "-language:existentials",
+      "-language:higherKinds",
+      "-language:implicitConversions",
+      "-language:postfixOps",
+      "-deprecation"
+    )
 
     def ivyDeps = super.ivyDeps() ++ Seq(
       Deps.`ch.qos.logback:logback-classic`,

commit 729fc9d619ec4a9d30be6bf2347fb13d310544de

Fix bugs introduced in commit 22033e909dd461689b374815ca4602b087b8830f revealed by `MillInitMavenNettyTests` in the integration tests

main/init/buildgen/src/mill/main/buildgen/BuildGenUtil.scala
@@ -386,12 +386,13 @@ object BuildGenUtil {
       else
         // Note that the super def is called even when it's empty.
         // Some super functions can be called without parentheses, but we just add it here for simplicity.
-        Some(optional(
-          s"super.$defName() ++ Seq",
-          args.iterator.drop(superLength).map(transform)
-        ))
+        Some(args.iterator.drop(superLength).map(transform)
+          .mkString(s"super.$defName() ++ Seq(", ",", ")"))
     } else
-      Some(optional(s"Seq", args.iterator.map(transform)))
+      Some(
+        if (args.isEmpty) "Seq.empty[String]" // The inference type is `Seq[Nothing]` otherwise.
+        else args.iterator.map(transform).mkString("Seq(", ",", ")")
+      )
 
   def renderStringSeqTargetDefWithSuper(
       defName: String,