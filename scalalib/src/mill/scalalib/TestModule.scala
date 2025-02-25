c0ca70bb3be9bd74959a300dbe9645083c501dc0
Li Haoyi <haoyi.sg@gmail.com>
2025-02-25 10:10:39 +0800

    Move `TestModule.*` dependencies to `mandatoryIvyDeps` (#3279)
    . This better fits what they are, since `mandatoryIvyDeps` is defined as
    *Mandatory ivy dependencies that are typically always required and
    shouldn't be removed by overriding*, and it makes it less likely people
    will accidentally override `ivyDeps` without `super` and stomp over them

--- scalalib/src/mill/scalalib/TestModule.scala
+++ scalalib/src/mill/scalalib/TestModule.scala
@@ -242,8 +242,8 @@ object TestModule {
    */
   trait TestNg extends TestModule {
     override def testFramework: T[String] = "mill.testng.TestNGFramework"
-    override def ivyDeps: T[Seq[Dep]] = Task {
-      super.ivyDeps() ++ Seq(
+    override def mandatoryIvyDeps: T[Seq[Dep]] = Task {
+      super.mandatoryIvyDeps() ++ Seq(
         ivy"com.lihaoyi:mill-contrib-testng:${mill.api.BuildInfo.millVersion}"
       )
     }
@@ -255,8 +255,8 @@ object TestModule {
    */
   trait Junit4 extends TestModule {
     override def testFramework: T[String] = "com.novocode.junit.JUnitFramework"
-    override def ivyDeps: T[Seq[Dep]] = Task {
-      super.ivyDeps() ++ Seq(ivy"${mill.scalalib.api.Versions.sbtTestInterface}")
+    override def mandatoryIvyDeps: T[Seq[Dep]] = Task {
+      super.mandatoryIvyDeps() ++ Seq(ivy"${mill.scalalib.api.Versions.sbtTestInterface}")
     }
   }
 
@@ -266,8 +266,8 @@ object TestModule {
    */
   trait Junit5 extends TestModule {
     override def testFramework: T[String] = "com.github.sbt.junit.jupiter.api.JupiterFramework"
-    override def ivyDeps: T[Seq[Dep]] = Task {
-      super.ivyDeps() ++ Seq(ivy"${mill.scalalib.api.Versions.jupiterInterface}")
+    override def mandatoryIvyDeps: T[Seq[Dep]] = Task {
+      super.mandatoryIvyDeps() ++ Seq(ivy"${mill.scalalib.api.Versions.jupiterInterface}")
     }
 
     /**
@@ -393,6 +393,7 @@ object TestModule {
 
   trait JavaModuleBase extends BspModule {
     def ivyDeps: T[Seq[Dep]] = Seq.empty[Dep]
+    def mandatoryIvyDeps: T[Seq[Dep]] = Seq.empty[Dep]
     def resources: T[Seq[PathRef]] = Task { Seq.empty[PathRef] }
   }
 
