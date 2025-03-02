cb289f86ea6554d58175a6afbd2227b387b37c26
Li Haoyi <haoyi.sg@gmail.com>
2025-03-02 14:51:00 +0800

    fix-tests

--- integration/failure/os-checker/src/OsCheckerTests.scala
+++ integration/failure/os-checker/src/OsCheckerTests.scala
@@ -10,16 +10,17 @@ object OsCheckerTests extends UtestIntegrationTestSuite {
       import tester._
       val res = tester.eval("foo.bar")
 
+      val sep = if (mill.constants.Util.isWindows) "\\" else "/"
       assert(res.isSuccess == false)
       assert(res.err.contains(
-        s"Writing to $workspacePath/foo not allowed during resolution phase"
+        s"Writing to $workspacePath${sep}foo not allowed during resolution phase"
       ))
 
       val res2 = tester.eval("qux")
 
       assert(res2.isSuccess == false)
       assert(res2.err.contains(
-        s"Writing to $workspacePath/file.txt not allowed during execution phase"
+        s"Writing to $workspacePath${sep}file.txt not allowed during execution phase"
       ))
 
       tester.modifyFile(workspacePath / "build.mill", _.replace("if (false)", "if (true)"))
