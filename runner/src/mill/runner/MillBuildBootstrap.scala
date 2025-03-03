f940e2f011d81a1e3ada5527ef4aa63d74ae5e02
Li Haoyi <haoyi.sg@gmail.com>
2025-03-03 12:28:00 +0800

    Update MillBuildBootstrap.scala

--- runner/src/mill/runner/MillBuildBootstrap.scala
+++ runner/src/mill/runner/MillBuildBootstrap.scala
@@ -164,7 +164,6 @@ class MillBuildBootstrap(
       } else {
 
         def renderFailure(e: Throwable): String = {
-          mill.constants.DebugLog.println("renderFailure " + e)
           e match {
             case e: ExceptionInInitializerError if e.getCause != null => renderFailure(e.getCause)
             case e: NoClassDefFoundError if e.getCause != null => renderFailure(e.getCause)
