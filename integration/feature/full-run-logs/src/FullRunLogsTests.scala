110d3a840847204aad69f6811023a774b1bcde7d
Li Haoyi <haoyi.sg@gmail.com>
2025-03-06 10:48:59 +0800

    fix keep going error logging

--- integration/feature/full-run-logs/src/FullRunLogsTests.scala
+++ integration/feature/full-run-logs/src/FullRunLogsTests.scala
@@ -53,6 +53,37 @@ object FullRunLogsTests extends UtestIntegrationTestSuite {
       val normErr = res.err.replace('\\', '/').replaceAll("(\r\n)|\r", "\n")
       assert(expectedErrorRegex.r.matches(normErr))
     }
+    test("keepGoingFailure") - integrationTest { tester =>
+      import tester._
+
+      modifyFile(workspacePath / "src/foo/Foo.java", _ + "}")
+      val res = eval(("--ticker", "true", "--keep-going", "jar"))
+      res.isSuccess ==> false
+
+      val expectedErrorRegex = java.util.regex.Pattern
+        .quote(
+          s"""<dashes> jar <dashes>
+             |[build.mill-<digits>/<digits>] compile
+             |[build.mill-<digits>] [info] compiling 1 Scala source to ${tester.workspacePath}/out/mill-build/compile.dest/classes ...
+             |[build.mill-<digits>] [info] done compiling
+             |[<digits>/<digits>] compile
+             |[<digits>] [info] compiling 1 Java source to ${tester.workspacePath}/out/compile.dest/classes ...
+             |[<digits>] [error] ${tester.workspacePath}/src/foo/Foo.java:36:1: class, interface, enum, or record expected
+             |[<digits>] compile failed
+             |[<digits>/<digits>, 1 failed] <dashes> jar <dashes> <digits>s
+             |1 tasks failed
+             |compile javac returned non-zero exit code"""
+            .stripMargin
+            .replaceAll("(\r\n)|\r", "\n")
+            .replace('\\', '/')
+        )
+        .replace("<digits>", "\\E\\d+\\Q")
+        .replace("<dashes>", "\\E=+\\Q")
+
+
+      val normErr = res.err.replace('\\', '/').replaceAll("(\r\n)|\r", "\n")
+      assert(expectedErrorRegex.r.matches(normErr))
+    }
     test("show") - integrationTest { tester =>
       import tester._
       // Make sure when we have nested evaluations, e.g. due to usage of evaluator commands
@@ -74,59 +105,6 @@ object FullRunLogsTests extends UtestIntegrationTestSuite {
       // Profile logs for show itself
       assert(millProfile.exists(_.obj("label").str == "show"))
       assert(millChromeProfile.exists(_.obj("name").str == "show"))
-    }
-    test("failedTasksCounter") - integrationTest { tester =>
-      import tester._
-
-      val javaSource = os.Path(workspacePath, os.pwd)
-      val fooJava = javaSource / "src" / "foo" / "Foo.java"
-      val originalContent = os.read(fooJava)
-
-      // Introduce a compilation error by adding an unclosed brace
-      modifyFile(fooJava, _.replace("class Foo", "class Foo {"))
-
-      // Run with ticker to see the failed tasks count for compile
-      val compileRes = eval(("--ticker", "true", "compile"))
-      compileRes.isSuccess ==> false
-
-      // Verify the output shows failed tasks count in the progress indicator
-      val failedPattern = "\\[(\\d+)/(\\d+),\\s*(\\d+)\\s*failed\\]".r // Matches [X/Y, N failed]
-      val failedMatch = failedPattern.findFirstIn(compileRes.err)
-      failedMatch.isDefined ==> true
-
-      // Extract and verify the number of failed tasks
-      val failedCount =
-        failedPattern.findFirstMatchIn(compileRes.err).map(_.group(3).toInt).getOrElse(0)
-      failedCount ==> 1 // Expecting 1 failed task for compile
-
-      // Run jar task with --keep-going to see upstream failures
-      val jarKeepGoingRes = eval(("--ticker", "true", "--keep-going", "jar"))
-      jarKeepGoingRes.isSuccess ==> false
-
-      // Verify the output shows failed tasks count in the progress indicator
-      val jarKeepGoingFailedMatch = failedPattern.findFirstIn(jarKeepGoingRes.err)
-      jarKeepGoingFailedMatch.isDefined ==> true
-
-      // Extract and verify the number of failed tasks
-      val jarKeepGoingFailedCount =
-        failedPattern.findFirstMatchIn(jarKeepGoingRes.err).map(_.group(3).toInt).getOrElse(0)
-      jarKeepGoingFailedCount ==> 1 // Expecting 1 failed task for jar with --keep-going
-
-      // Run jar task without --keep-going to see it fail immediately
-      val jarRes = eval(("--ticker", "true", "jar"))
-      jarRes.isSuccess ==> false
-
-      // Verify the output shows failed tasks count in the progress indicator
-      val jarFailedMatch = failedPattern.findFirstIn(jarRes.err)
-      jarFailedMatch.isDefined ==> true
-
-      // Extract and verify the number of failed tasks
-      val jarFailedCount =
-        failedPattern.findFirstMatchIn(jarRes.err).map(_.group(3).toInt).getOrElse(0)
-      jarFailedCount ==> 1 // Expecting 1 failed task for jar without --keep-going
-
-      // Restore the original content
-      modifyFile(fooJava, _ => originalContent)
     }
   }
 }
