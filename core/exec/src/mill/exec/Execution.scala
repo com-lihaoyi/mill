87e29a918ab7b6549114753282cd7c535c893fb4
Li Haoyi <haoyi.sg@gmail.com>
2025-03-06 09:25:39 +0800

    .

--- core/exec/src/mill/exec/Execution.scala
+++ core/exec/src/mill/exec/Execution.scala
@@ -169,12 +169,11 @@ private[mill] case class Execution(
                 } yield upstreamResults(item).map(_._1)
                 val logRun = inputResults.forall(_.isInstanceOf[ExecResult.Success[?]])
 
-                val tickerPrefix =
-                  if (logRun && logger.prompt.enableTicker) terminal.toString else ""
+                val tickerPrefix = if (logRun) terminal.toString else ""
 
                 val contextLogger = new PrefixLogger(
                   logger0 = logger,
-                  key0 = if (!logger.prompt.enableTicker) Nil else Seq(countMsg),
+                  key0 = Seq(countMsg),
                   keySuffix = keySuffix,
                   message = tickerPrefix,
                   noPrefix = exclusive
