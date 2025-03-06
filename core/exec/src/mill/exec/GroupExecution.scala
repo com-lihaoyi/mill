9cb023363315eec6669e9574cb7d700eaab4a850
Monye David <monyedavid@outlook.com>
2025-03-06 05:00:34 +0100

    phase out `os.checker.withValue(executionChecker` temporarily causes `Writing to "destination" not allowed during execution phase` error when running jslib tests.

--- core/exec/src/mill/exec/GroupExecution.scala
+++ core/exec/src/mill/exec/GroupExecution.scala
@@ -266,18 +266,18 @@ private trait GroupExecution {
               else (multiLogger.streams, () => makeDest())
 
             os.dynamicPwdFunction.withValue(destFunc) {
-              os.checker.withValue(executionChecker) {
-                SystemStreams.withStreams(streams) {
-                  val exposedEvaluator = if (!exclusive) null else getEvaluator()
-                  Evaluator.currentEvaluator0.withValue(exposedEvaluator) {
-                    if (!exclusive) t
-                    else {
-                      logger.prompt.reportKey(Seq(counterMsg))
-                      logger.prompt.withPromptPaused { t }
-                    }
+              //  os.checker.withValue(executionChecker) {
+              SystemStreams.withStreams(streams) {
+                val exposedEvaluator = if (!exclusive) null else getEvaluator()
+                Evaluator.currentEvaluator0.withValue(exposedEvaluator) {
+                  if (!exclusive) t
+                  else {
+                    logger.prompt.reportKey(Seq(counterMsg))
+                    logger.prompt.withPromptPaused { t }
                   }
                 }
               }
+              // }
             }
           }
 
