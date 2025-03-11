5c4ad9521b70afa25a49d9d288175a0efd7a7c7a
Alex Archambault <alexandre.archambault@gmail.com>
2025-03-11 16:33:20 +0100

    Don't format files that shouldn't

--- scalalib/test/resources/scalafmt/core/src/Main.scala
+++ scalalib/test/resources/scalafmt/core/src/Main.scala
@@ -1,5 +1,8 @@
-object Main extends App {
+
+object Main extends App{
   val person = Person.fromString("rockjam:25")
   val greeting = s"hello ${person.name}, your age is: ${person.age}"
   println(greeting)
 }
+
+
