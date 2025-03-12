99fcbc8570e5c8565d83d9b73430a1bfba1e2c16
Denis Rosca <denisrosca@users.noreply.github.com>
2025-03-12 12:06:51 +0200

    Make source path configurable in TestBaseModule

--- testkit/src/mill/testkit/TestBaseModule.scala
+++ testkit/src/mill/testkit/TestBaseModule.scala
@@ -5,20 +5,26 @@ import mill.define.{Caller, Discover}
 /**
  * A wrapper of [[mill.define.BaseModule]] meant for easy instantiation in test suites.
  */
-class TestBaseModule(implicit
+abstract class TestBaseModule(
+    baseModuleSourcePath: os.Path
+)(implicit
     millModuleEnclosing0: sourcecode.Enclosing,
     millModuleLine0: sourcecode.Line,
     millModuleFile0: sourcecode.File
-) extends mill.define.BaseModule(
-      {
-        os.makeDir.all(os.pwd / "out/mill-test-base-module")
-        os.temp.dir(os.pwd / "out/mill-test-base-module", deleteOnExit = false)
-      }
-    )(
+) extends mill.define.BaseModule(millSourcePath0 = baseModuleSourcePath)(
       millModuleEnclosing0,
       millModuleLine0,
       millModuleFile0,
       Caller(null)
     ) {
+  def this()(implicit
+      millModuleEnclosing0: sourcecode.Enclosing,
+      millModuleLine0: sourcecode.Line,
+      millModuleFile0: sourcecode.File
+  ) = this({
+    os.makeDir.all(os.pwd / "out/mill-test-base-module")
+    os.temp.dir(os.pwd / "out/mill-test-base-module", deleteOnExit = false)
+  })
+
   lazy val millDiscover: Discover = Discover[this.type]
 }
