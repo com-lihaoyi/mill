11bba7cf834f33ef3ab4906882e93e09270af974
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-03-12 10:26:42 +0200

    Ksp/hilt and compiling for android
    # Conflicts:
    #	kotlinlib/src/mill/kotlinlib/android/AndroidAppKotlinModule.scala
    #	kotlinlib/src/mill/kotlinlib/android/AndroidKotlinModule.scala
    #	kotlinlib/src/mill/kotlinlib/ksp/KspModule.scala

---
+++ example/android/kotlinlib/4-todo-architecture-sample/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/LocalTask.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Internal model used to represent a task stored locally in a Room database. This is used inside
+ * the data layer only.
+ *
+ * See ModelMappingExt.kt for mapping functions used to convert this model to other
+ * models.
+ */
+@Entity(
+    tableName = "task"
+)
+data class LocalTask(
+    @PrimaryKey val id: String,
+    var title: String,
+    var description: String,
+    var isCompleted: Boolean,
+)
