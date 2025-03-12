621b4fd8ccd0082b24b16183cbe7f79506fcea85
Alex Archambault <alexandre.archambault@gmail.com>
2025-03-12 11:24:39 +0100

    fix bin compat

--- scalalib/src/mill/scalalib/PublishModule.scala
+++ scalalib/src/mill/scalalib/PublishModule.scala
@@ -366,6 +366,12 @@ trait PublishModule extends JavaModule { outer =>
     Result.Success(())
   }
 
+  // bin-compat shim
+  def publishLocal(
+      localIvyRepo: String
+  ): define.Command[Unit] =
+    publishLocal(localIvyRepo, sources = true, doc = true, transitive = false)
+
   /**
    * Publish artifacts the local ivy repository.
    */
