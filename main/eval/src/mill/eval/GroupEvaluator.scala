334cdd0963a31b646a33e6e5ee23a04956065873
Li Haoyi <haoyi.sg@gmail.com>
2025-03-22 10:25:32 +0800

    .

--- main/eval/src/mill/eval/GroupEvaluator.scala
+++ main/eval/src/mill/eval/GroupEvaluator.scala
@@ -289,9 +289,9 @@ private[mill] trait GroupEvaluator {
     val (newResults, newEvaluated) = computeAll()
 
     if (!failFast) maybeTargetLabel.foreach { targetLabel =>
-      val taskFailed = newResults.exists(task => !task._2.isInstanceOf[Success[_]])
+      val taskFailed = newResults.exists(task => task._2.isInstanceOf[Result.Failing[?]])
       if (taskFailed) {
-        logger.error(s"[$counterMsg] $targetLabel failed")
+        logger.error(s"$targetLabel failed")
       }
     }
 
