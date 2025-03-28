6c7ddf293530ea06f8a7e3a584729c47c2ea5dd1
Alex Archambault <alexandre.archambault@gmail.com>
2025-03-28 19:57:51 +0100

    Don't require BSP/install any more

--- bsp/src/mill/bsp/BspClasspathWorker.scala
+++ bsp/src/mill/bsp/BspClasspathWorker.scala
@@ -1,6 +1,7 @@
 package mill.bsp
 
-import mill.api.{Logger, SystemStreams, Result}
+import mill.api.{Ctx, DummyTestReporter, Logger, SystemStreams, Result}
+import mill.scalalib.{CoursierModule, Dep}
 
 import java.io.PrintStream
 import java.net.URL
@@ -23,45 +24,52 @@ object BspClasspathWorker {
       workspace: os.Path,
       log: Logger,
       workerLibs: Option[Seq[URL]] = None
-  ): Result[BspClasspathWorker] = {
-    worker match {
-      case Some(x) => Result.Success(x)
-      case None =>
-        val urlsOpt = workerLibs
-          .map { urls =>
-            log.debug("Using direct submitted worker libs")
-            urls.map(url => os.Path(url.getPath))
-          }
-          .orElse {
-            // load extra classpath entries from file
-            val resources = s"${Constants.serverName}-${mill.main.BuildInfo.millVersion}.resources"
-            val cpFile = workspace / Constants.bspDir / resources
-            if (os.exists(cpFile)) {
-              // TODO: if outdated, we could regenerate the resource file and re-load the worker
+  ): BspClasspathWorker = {
+    worker.getOrElse {
+      val jars: Seq[os.Path] = workerLibs
+        .map { urls =>
+          log.debug("Using direct submitted worker libs")
+          urls.map(url => os.Path(url.getPath))
+        }
+        .orElse {
+          // load extra classpath entries from file
+          val resources = s"${Constants.serverName}-${mill.main.BuildInfo.millVersion}.resources"
+          val cpFile = workspace / Constants.bspDir / resources
+          if (os.exists(cpFile)) {
+            // TODO: if outdated, we could regenerate the resource file and re-load the worker
 
-              // read the classpath from resource file
-              log.debug(s"Reading worker classpath from file: ${cpFile}")
-              Some(os.read(cpFile).linesIterator.map(u => os.Path(new URL(u).getPath)).toSeq)
-            } else
-              None
-          }
+            // read the classpath from resource file
+            log.debug(s"Reading worker classpath from file: ${cpFile}")
+            Some(os.read(cpFile).linesIterator.map(u => os.Path(new URL(u).getPath)).toSeq)
+          } else
+            None
+        }
+        .getOrElse {
+          implicit val ctxForResolution: Ctx = new mill.api.Ctx.Impl(
+            args = Vector(),
+            dest0 = () => null,
+            log = log,
+            env = Map(),
+            reporter = _ => None,
+            testReporter = DummyTestReporter,
+            workspace = mill.api.WorkspaceRoot.workspaceRoot,
+            systemExit = _ => ???,
+            fork = null,
+            jobs = 1
+          )
 
-        urlsOpt match {
-          case Some(urls) =>
-            // create classloader with bsp.worker and deps
-            val cl = mill.util.Jvm.createClassLoader(urls, getClass().getClassLoader())
-
-            val workerCls = cl.loadClass(Constants.bspWorkerImplClass)
-            val ctr = workerCls.getConstructor()
-            val workerImpl = ctr.newInstance().asInstanceOf[BspClasspathWorker]
-            worker = Some(workerImpl)
-            Result.Success(workerImpl)
-          case None =>
-            Result.Failure(
-              "You need to run `mill mill.bsp.BSP/install` before you can use the BSP server"
-            )
+          CoursierModule.defaultResolver.classpath(Seq(Dep.millProjectModule("mill-bsp-worker")))
+            .map(_.path)
         }
 
+      // create classloader with bsp.worker and deps
+      val cl = mill.util.Jvm.createClassLoader(jars, getClass().getClassLoader())
+
+      val workerCls = cl.loadClass(Constants.bspWorkerImplClass)
+      val ctr = workerCls.getConstructor()
+      val workerImpl = ctr.newInstance().asInstanceOf[BspClasspathWorker]
+      worker = Some(workerImpl)
+      workerImpl
     }
   }
 
