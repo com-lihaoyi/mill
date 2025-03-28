0740b197bb3fee0fe5ce98a1038942e1d8f730c8
Alex Archambault <alexandre.archambault@gmail.com>
2025-03-28 17:58:06 +0100

    Remove boundary stuff

--- bsp/src/mill/bsp/BspClasspathWorker.scala
+++ bsp/src/mill/bsp/BspClasspathWorker.scala
@@ -4,7 +4,6 @@ import mill.api.{Logger, SystemStreams, Result}
 
 import java.io.PrintStream
 import java.net.URL
-import scala.util.boundary
 
 private trait BspClasspathWorker {
   def startBspServer(
@@ -24,36 +23,45 @@ object BspClasspathWorker {
       workspace: os.Path,
       log: Logger,
       workerLibs: Option[Seq[URL]] = None
-  ): Result[BspClasspathWorker] = boundary {
+  ): Result[BspClasspathWorker] = {
     worker match {
       case Some(x) => Result.Success(x)
       case None =>
-        val urls = workerLibs.map { urls =>
-          log.debug("Using direct submitted worker libs")
-          urls.map(url => os.Path(url.getPath))
-        }.getOrElse {
-          // load extra classpath entries from file
-          val resources = s"${Constants.serverName}-${mill.main.BuildInfo.millVersion}.resources"
-          val cpFile = workspace / Constants.bspDir / resources
-          if (!os.exists(cpFile)) boundary.break(Result.Failure(
-            "You need to run `mill mill.bsp.BSP/install` before you can use the BSP server"
-          ))
+        val urlsOpt = workerLibs
+          .map { urls =>
+            log.debug("Using direct submitted worker libs")
+            urls.map(url => os.Path(url.getPath))
+          }
+          .orElse {
+            // load extra classpath entries from file
+            val resources = s"${Constants.serverName}-${mill.main.BuildInfo.millVersion}.resources"
+            val cpFile = workspace / Constants.bspDir / resources
+            if (os.exists(cpFile)) {
+              // TODO: if outdated, we could regenerate the resource file and re-load the worker
 
-          // TODO: if outdated, we could regenerate the resource file and re-load the worker
+              // read the classpath from resource file
+              log.debug(s"Reading worker classpath from file: ${cpFile}")
+              Some(os.read(cpFile).linesIterator.map(u => os.Path(new URL(u).getPath)).toSeq)
+            } else
+              None
+          }
 
-          // read the classpath from resource file
-          log.debug(s"Reading worker classpath from file: ${cpFile}")
-          os.read(cpFile).linesIterator.map(u => os.Path(new URL(u).getPath)).toSeq
+        urlsOpt match {
+          case Some(urls) =>
+            // create classloader with bsp.worker and deps
+            val cl = mill.util.Jvm.createClassLoader(urls, getClass().getClassLoader())
+
+            val workerCls = cl.loadClass(Constants.bspWorkerImplClass)
+            val ctr = workerCls.getConstructor()
+            val workerImpl = ctr.newInstance().asInstanceOf[BspClasspathWorker]
+            worker = Some(workerImpl)
+            Result.Success(workerImpl)
+          case None =>
+            Result.Failure(
+              "You need to run `mill mill.bsp.BSP/install` before you can use the BSP server"
+            )
         }
 
-        // create classloader with bsp.worker and deps
-        val cl = mill.util.Jvm.createClassLoader(urls, getClass().getClassLoader())
-
-        val workerCls = cl.loadClass(Constants.bspWorkerImplClass)
-        val ctr = workerCls.getConstructor()
-        val workerImpl = ctr.newInstance().asInstanceOf[BspClasspathWorker]
-        worker = Some(workerImpl)
-        Result.Success(workerImpl)
     }
   }
 
