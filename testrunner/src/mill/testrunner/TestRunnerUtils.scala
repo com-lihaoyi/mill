df5b8b51dc97b555b8eb10c0482278fa43f398bd
Li Haoyi <haoyi.sg@gmail.com>
2025-03-29 13:38:48 +0100

    .

--- testrunner/src/mill/testrunner/TestRunnerUtils.scala
+++ testrunner/src/mill/testrunner/TestRunnerUtils.scala
@@ -133,7 +133,7 @@ import java.util.concurrent.atomic.AtomicBoolean
       .groupBy(_.taskDef().fullyQualifiedName())
       .values
       .toArray
-      .sortBy(_(0).taskDef().fullyQualifiedName())
+      .sortBy(_.headOption.map(_.taskDef().fullyQualifiedName()))
 
     (runner, groupedTasks)
   }
