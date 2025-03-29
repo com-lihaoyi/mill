1817da2ae9de85768b1659c31f0c78f1ea151712
Li Haoyi <haoyi.sg@gmail.com>
2025-03-29 14:14:50 +0100

    .

--- scalalib/test/resources/testrunner/doneMessageFailure/src/DoneMessageFailureFramework.scala
+++ scalalib/test/resources/testrunner/doneMessageFailure/src/DoneMessageFailureFramework.scala
@@ -13,30 +13,26 @@ class DoneMessageFailureFramework extends Framework {
     def args() = Array.empty
     def done() = "test failure done message"
     def remoteArgs() = Array.empty
-    def tasks(taskDefs: Array[TaskDef]) = Array(new Task {
-      def taskDef(): TaskDef = taskDefs.head
-      def execute(
-          eventHandler: EventHandler,
-          loggers: Array[Logger]
-      ): Array[Task] = {
-        eventHandler.handle(new Event {
+    def tasks(taskDefs: Array[TaskDef]) =
+      if (taskDefs.isEmpty) Array.empty[Task]
+      else Array(new Task {
+        def taskDef(): TaskDef = taskDefs.head
+        def execute(
+            eventHandler: EventHandler,
+            loggers: Array[Logger]
+        ): Array[Task] = {
+          eventHandler.handle(new Event {
+            override def fullyQualifiedName(): String = "foo.bar"
+            override def fingerprint(): Fingerprint = new Fingerprint {}
+            override def selector(): Selector = new TestSelector("foo.bar")
+            override def status(): Status = Status.Failure
+            override def throwable(): OptionalThrowable = new OptionalThrowable()
+            override def duration(): Long = 0L
 
-          override def fullyQualifiedName(): String = "foo.bar"
-
-          override def fingerprint(): Fingerprint = new Fingerprint {}
-
-          override def selector(): Selector = new TestSelector("foo.bar")
-
-          override def status(): Status = Status.Failure
-
-          override def throwable(): OptionalThrowable = new OptionalThrowable()
-
-          override def duration(): Long = 0L
-
-        })
-        Array.empty
-      }
-      def tags = Array.empty
-    })
+          })
+          Array.empty
+        }
+        def tags = Array.empty
+      })
   }
 }
