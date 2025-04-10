3abff6671b66d33894e615fa83455f608589de16
Li Haoyi <haoyi.sg@gmail.com>
2025-04-10 08:34:00 -0400

    test compile classpath

--- integration/feature/build-classpath-contents/src/BuildClasspathContentsTests.scala
+++ integration/feature/build-classpath-contents/src/BuildClasspathContentsTests.scala
@@ -8,7 +8,7 @@ object BuildClasspathContentsTests extends UtestIntegrationTestSuite {
 
   val tests: Tests = Tests {
     test("test") - integrationTest { tester =>
-      val result1 = tester.eval(("--meta-level", "1", "show", "runClasspath"))
+      val result1 = tester.eval(("--meta-level", "1", "show", "compileClasspath"), stderr = os.Inherit)
       val deserialized = upickle.default.read[Seq[mill.define.PathRef]](result1.out)
       val simplified = deserialized
         .map(_.path)
@@ -18,18 +18,17 @@ object BuildClasspathContentsTests extends UtestIntegrationTestSuite {
         .sortBy(_.toString)
       pprint.log(simplified)
 
+      // Make sure we don't include `core. exec`, `core.resolve`, `core`, `runner`, `runner.server`,
+      // etc. since users should not need to write code that compiles against those interfaces
       val expected: Seq[os.SubPath] = List(
         "out/bsp/buildInfoResources.dest",
         "out/bsp/compile.dest/classes",
         "out/core/api/buildInfoResources.dest",
         "out/core/api/compile.dest/classes",
-        "out/core/compile.dest/classes",
         "out/core/constants/buildInfoResources.dest",
         "out/core/constants/compile.dest/classes",
         "out/core/define/compile.dest/classes",
-        "out/core/exec/compile.dest/classes",
         "out/core/internal/compile.dest/classes",
-        "out/core/resolve/compile.dest/classes",
         "out/dist/localTestOverridesClasspath.dest",
         "out/idea/compile.dest/classes",
         "out/javascriptlib/compile.dest/classes",
