97ced8cc25fd43372e73fa20232530ca70a0b1b6
Li Haoyi <haoyi.sg@gmail.com>
2025-04-10 13:28:59 -0400

    .

--- main/util/src/mill/util/Jvm.scala
+++ main/util/src/mill/util/Jvm.scala
@@ -62,9 +62,9 @@ object Jvm {
    */
   def callProcess(
       mainClass: String,
-      mainArgs: Iterable[String] = Seq.empty,
+      mainArgs: os.Shellable = Seq.empty,
       javaHome: Option[os.Path] = None,
-      jvmArgs: Iterable[String] = Seq.empty,
+      jvmArgs: os.Shellable = Seq.empty,
       classPath: Iterable[os.Path],
       cpPassingJarPath: Option[os.Path] = None,
       env: Map[String, String] = Map.empty,
@@ -86,14 +86,14 @@ object Jvm {
       case _ => classPath
     }
 
-    val commandArgs = (Vector(javaExe(javaHome)) ++
-      jvmArgs ++
+    val commandArgs = Vector(javaExe(javaHome)) ++
+      jvmArgs.value ++
       Option.when(cp.nonEmpty)(Vector(
         "-cp",
         cp.mkString(java.io.File.pathSeparator)
       )).getOrElse(Vector.empty) ++
       Vector(mainClass) ++
-      mainArgs).filterNot(_.isBlank)
+      mainArgs.value
 
     if (cwd != null) os.makeDir.all(cwd)
 
@@ -147,9 +147,9 @@ object Jvm {
    */
   def spawnProcess(
       mainClass: String,
-      mainArgs: Iterable[String] = Seq.empty,
+      mainArgs: os.Shellable = Seq.empty,
       javaHome: Option[os.Path] = None,
-      jvmArgs: Iterable[String] = Seq.empty,
+      jvmArgs: os.Shellable = Seq.empty,
       classPath: Iterable[os.Path],
       cpPassingJarPath: Option[os.Path] = None,
       env: Map[String, String] = Map.empty,
@@ -169,13 +169,13 @@ object Jvm {
       case _ => classPath
     }
 
-    val commandArgs = (Vector(javaExe(javaHome)) ++
-      jvmArgs ++
+    val commandArgs = Vector(javaExe(javaHome)) ++
+      jvmArgs.value ++
       Option.when(cp.nonEmpty)(
         Vector("-cp", cp.mkString(java.io.File.pathSeparator))
       ).getOrElse(Vector.empty) ++
       Vector(mainClass) ++
-      mainArgs).filterNot(_.isBlank)
+      mainArgs.value
 
     if (cwd != null) os.makeDir.all(cwd)
 
@@ -383,15 +383,15 @@ object Jvm {
       mainClass: String,
       shellClassPath: Seq[String],
       cmdClassPath: Seq[String],
-      jvmArgs: Seq[String],
+      jvmArgs: os.Shellable,
       shebang: Boolean = false,
-      shellJvmArgs: Seq[String] = Nil,
-      cmdJvmArgs: Seq[String] = Nil
+      shellJvmArgs: os.Shellable = Nil,
+      cmdJvmArgs: os.Shellable = Nil
   ): String = {
 
     universalScript(
       shellCommands = {
-        val jvmArgsStr = (jvmArgs ++ shellJvmArgs).mkString(" ")
+        val jvmArgsStr = (jvmArgs.value ++ shellJvmArgs.value).mkString(" ")
         val classpathStr = shellClassPath.mkString(":")
 
         s"""if [ -z "$$JAVA_HOME" ] ; then
@@ -404,7 +404,7 @@ object Jvm {
            |""".stripMargin
       },
       cmdCommands = {
-        val jvmArgsStr = (jvmArgs ++ cmdJvmArgs).mkString(" ")
+        val jvmArgsStr = (jvmArgs.value ++ cmdJvmArgs.value).mkString(" ")
         val classpathStr = cmdClassPath.mkString(";")
         s"""setlocal EnableDelayedExpansion
            |set "JAVACMD=java.exe"
@@ -419,13 +419,11 @@ object Jvm {
     )
   }
 
-  def createLauncher(mainClass: String, classPath: Seq[os.Path], jvmArgs: Seq[String])(implicit
-      ctx: TaskCtx.Dest
-  ): PathRef = {
+  def createLauncher(mainClass: String, classPath: Seq[os.Path], jvmArgs: os.Shellable, destFolder: os.Path): PathRef = {
     val isWin = scala.util.Properties.isWin
     val isBatch =
       isWin && !(org.jline.utils.OSUtils.IS_CYGWIN || org.jline.utils.OSUtils.IS_MSYSTEM)
-    val outputPath = ctx.dest / (if (isBatch) "run.bat" else "run")
+    val outputPath = destFolder / (if (isBatch) "run.bat" else "run")
     val classPathStrs = classPath.map(_.toString)
 
     os.write(outputPath, launcherUniversalScript(mainClass, classPathStrs, classPathStrs, jvmArgs))
