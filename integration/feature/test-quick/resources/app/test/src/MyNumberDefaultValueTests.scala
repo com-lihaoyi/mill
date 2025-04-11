e63d0c4bbb07827c4b705a51bd8e97a06a458be5
HollandDM <hoangdinh@anduintransact.com>
2025-04-11 21:16:10 +0700

    add integration tests

---
+++ integration/feature/test-quick/resources/app/test/src/MyNumberDefaultValueTests.scala
@@ -0,0 +1,13 @@
+package app
+
+import utest.*
+import app.MyNumber
+
+object MyNumberDefaultValueTests extends TestSuite {
+  def tests = Tests { 
+    test("simple") {
+      val result = MyNumber.defaultValue
+      assert(result == MyNumber(0))
+    }
+  }
+}
