a3db4d1c9a33b9a92899099746e58cc98435e0b4
Chaitanya Waikar <chaitanyawaikar1993@gmail.com>
2025-04-12 19:09:22 +0200

    Add examples of mill + cask + scalasql

---
+++ example/scalalib/web/10-todo-webapp-cask-scalasql/src/WebApp.scala
@@ -0,0 +1,206 @@
+package webapp
+
+import scalasql.H2Dialect._
+import scalasql.core.DbClient
+import scalasql.{Sc, Table}
+import scalatags.Text.all._
+import scalatags.Text.tags2
+import webapp.WebApp.DB.Todos
+
+object WebApp extends cask.MainRoutes {
+  case class Todo(checked: Boolean, text: String)
+
+  object Todo {
+    implicit def todoRW: upickle.default.ReadWriter[Todo] = upickle.default.macroRW[Todo]
+  }
+
+  object DB {
+
+    //  Table Definition and its corresponding ORM mapping
+    case class Todos[T[_]](
+                            id: T[Int],
+                            checked: T[Boolean],
+                            text: T[String]
+                          )
+
+    object Todos extends Table[Todos]
+
+    def getDatabaseClient: DbClient.DataSource = {
+      // The example H2 database comes from the library `com.h2database:h2:2.2.224`
+      val dataSource = new org.h2.jdbcx.JdbcDataSource
+      dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+
+      new scalasql.DbClient.DataSource(
+        dataSource,
+        config = new scalasql.Config {}
+      )
+    }
+
+    def initializeSchema(h2Client: scalasql.DbClient.DataSource): Unit = {
+      h2Client.transaction { db =>
+        db.updateRaw(
+          """
+          CREATE TABLE IF NOT EXISTS todos (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            checked BOOLEAN NOT NULL,
+            text VARCHAR NOT NULL
+        );
+        """)
+        println("Schema initialized (if not exists).") // Optional logging
+      }
+    }
+
+  }
+
+  lazy val dbClient: DbClient.DataSource = DB.getDatabaseClient
+  DB.initializeSchema(dbClient)
+
+  @cask.post("/list/:state")
+  def list(state: String) = {
+    val todos: Seq[Todo] = dbClient.transaction { db =>
+      db.run(Todos.select).sortBy(_.id)
+    }.map(r => Todo(r.checked, r.text))
+
+    renderBody(state, todos)
+  }
+
+  @cask.post("/add/:state")
+  def add(state: String, request: cask.Request) = {
+    dbClient.transaction { db =>
+      db.run(
+        Todos.insert.batched(_.checked, _.text)(
+          (false, state)
+        )
+      )
+    }
+    list(state)
+  }
+
+  @cask.post("/delete/:state/:index")
+  def delete(state: String, index: Int) = {
+    dbClient.transaction { db =>
+      db.run(Todos.delete(_.id === index))
+    }
+    list(state)
+  }
+
+  // check this
+  @cask.post("/toggle/:state/:index")
+  def toggle(state: String, index: Int) = {
+    val value1: Sc[Boolean] = dbClient.transaction { db =>
+      db.run(Todos.select.filter(_.id === index)).map(_.checked).head
+    }
+    list(state)
+  }
+
+  @cask.post("/clear-completed/:state")
+  def clearCompleted(state: String) = {
+    dbClient.transaction { db =>
+      db.run(Todos.delete(_.checked === true))
+    }
+    list(state)
+  }
+
+  @cask.post("/toggle-all/:state")
+  def toggleAll(state: String) = {
+    dbClient.transaction { db =>
+      db.updateRaw(
+        """
+          UPDATE todos SET checked = CASE
+              WHEN (SELECT COUNT(*) FROM todos WHERE checked = TRUE) > 0 THEN FALSE
+              ELSE TRUE
+          END;
+          |
+          |""".stripMargin)
+    }
+    list(state)
+  }
+
+  def renderBody(state: String, todos: Seq[Todo]) /*: scalatags.Text.TypedTag[String] */ = {
+
+    val filteredTodos = state match {
+      case "all" => todos.zipWithIndex
+      case "active" => todos.zipWithIndex.filter(!_._1.checked)
+      case "completed" => todos.zipWithIndex.filter(_._1.checked)
+    }
+    div(
+      header(
+        cls := "header",
+        h1("todos"),
+        input(cls := "new-todo", placeholder := "What needs to be done?", autofocus := "")
+      ),
+      tags2.section(
+        cls := "main",
+        input(
+          id := "toggle-all",
+          cls := "toggle-all",
+          `type` := "checkbox",
+          if (todos.filter(_.checked).size != 0) checked else ()
+        ),
+        label(`for` := "toggle-all", "Mark all as complete"),
+        ul(
+          cls := "todo-list",
+          for ((todo, index) <- filteredTodos) yield li(
+            if (todo.checked) cls := "completed" else (),
+            div(
+              cls := "view",
+              input(
+                cls := "toggle",
+                `type` := "checkbox",
+                if (todo.checked) checked else (),
+                data("todo-index") := index
+              ),
+              label(todo.text),
+              button(cls := "destroy", data("todo-index") := index)
+            ),
+            input(cls := "edit", value := todo.text)
+          )
+        )
+      ),
+      footer(
+        cls := "footer",
+        span(cls := "todo-count", strong(todos.filter(!_.checked).size), " items left"),
+        ul(
+          cls := "filters",
+          li(cls := "todo-all", a(if (state == "all") cls := "selected" else (), "All")),
+          li(cls := "todo-active", a(if (state == "active") cls := "selected" else (), "Active")),
+          li(
+            cls := "todo-completed",
+            a(if (state == "completed") cls := "selected" else (), "Completed")
+          )
+        ),
+        button(cls := "clear-completed", "Clear completed")
+      )
+    )
+  }
+
+  @cask.get("/")
+  def index() = {
+    doctype("html")(
+      html(
+        lang := "en",
+        head(
+          meta(charset := "utf-8"),
+          meta(name := "viewport", content := "width=device-width, initial-scale=1"),
+          tags2.title("Template • TodoMVC"),
+          link(rel := "stylesheet", href := "/static/index.css")
+        ),
+        body(
+          tags2.section(cls := "todoapp", list("all")),
+          footer(
+            cls := "info",
+            p("Double-click to edit a todo"),
+            p("Created by ", a(href := "http://todomvc.com", "Li Haoyi")),
+            p("Part of ", a(href := "http://todomvc.com", "TodoMVC"))
+          ),
+          script(src := "/static/main.js")
+        )
+      )
+    )
+  }
+
+  @cask.staticResources("/static")
+  def static() = "webapp"
+
+  initialize()
+}
