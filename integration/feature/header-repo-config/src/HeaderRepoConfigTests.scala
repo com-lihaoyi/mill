3b0933c50f780384d5f71171159d50e20d1216b4
Li Haoyi <haoyi.sg@gmail.com>
2025-04-22 15:06:13 -0700

    reenable actions

--- integration/feature/header-repo-config/src/HeaderRepoConfigTests.scala
+++ integration/feature/header-repo-config/src/HeaderRepoConfigTests.scala
@@ -8,12 +8,12 @@ object HeaderRepoConfigTests extends UtestIntegrationTestSuite {
   val tests: Tests = Tests {
     test("test") - integrationTest { tester =>
       import tester._
-      val result = eval(("parseJson", "--json-string", """{"a": 1, "b": 2, "c": 3}"""))
+      val result = eval(("parseJson", "--json-string", """[1, 2, 3]"""))
       assert(result.isSuccess)
       assert(result.out ==
-        """Key: a, Value: 1
-          |Key: b, Value: 2
-          |Key: c, Value: 3""".stripMargin)
+        """Value: 1
+          |Value: 2
+          |Value: 3""".stripMargin)
     }
   }
 }
