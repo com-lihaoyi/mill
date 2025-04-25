ac6da26b83449d538cba3966bb9ef3f318fcb081
Alex Archambault <alexandre.archambault@gmail.com>
2025-04-25 15:18:16 +0200

    Revert "debug CI issues"
    This reverts commit 088bafc05e1b4fd3eb1928d523e5a838c460c8db.

--- core/util/src/mill/util/Jvm.scala
+++ core/util/src/mill/util/Jvm.scala
@@ -27,7 +27,6 @@ import mill.define.{PathRef, TaskCtx}
 import scala.collection.mutable
 import scala.util.Properties.isWin
 import scala.util.chaining.scalaUtilChainingOps
-import coursier.cache.loggers.RefreshLogger
 
 object Jvm {
 
@@ -488,9 +487,11 @@ object Jvm {
       coursierCacheCustomizer: Option[FileCache[Task] => FileCache[Task]]
   ) =
     FileCache[Task]()
-      .withLogger(RefreshLogger.create(RefreshLogger.defaultDisplay(fallbackMode = true)))
       .pipe { cache =>
         coursierCacheCustomizer.fold(cache)(c => c.apply(cache))
+      }
+      .pipe { cache =>
+        ctx.fold(cache)(c => cache.withLogger(new CoursierTickerResolutionLogger(c)))
       }
       .pipe { cache =>
         if (ctx.fold(false)(_.offline)) cache.withCachePolicies(Seq(CachePolicy.LocalOnly))
