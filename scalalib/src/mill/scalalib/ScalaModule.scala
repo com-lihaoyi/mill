d81805a71ec59592ffb8e22578bc066e3f71e07e
Alex Archambault <alexandre.archambault@gmail.com>
2025-04-25 23:40:20 +0200

    Stop using mapDependencies

--- scalalib/src/mill/scalalib/ScalaModule.scala
+++ scalalib/src/mill/scalalib/ScalaModule.scala
@@ -58,24 +58,27 @@ trait ScalaModule extends JavaModule with TestModule.ScalaModuleBase { outer =>
    */
   def scalaVersion: T[String]
 
-  override def mapDependencies: Task[coursier.Dependency => coursier.Dependency] = Task.Anon {
-    super.mapDependencies().andThen { (d: coursier.Dependency) =>
-      val artifacts =
-        if (JvmWorkerUtil.isDotty(scalaVersion()))
-          Set("dotty-library", "dotty-compiler")
-        else if (JvmWorkerUtil.isScala3(scalaVersion()))
-          Set("scala3-library", "scala3-compiler")
-        else
-          Set("scala-library", "scala-compiler", "scala-reflect")
-      if (!artifacts(d.module.name.value)) d
+  override def resolutionParams: Task[coursier.params.ResolutionParams] = Task.Anon {
+    val isTypelevelScala = scalaOrganization() == "org.typelevel"
+    val scalaModuleNames =
+      if (JvmWorkerUtil.isDotty(scalaVersion()))
+        Seq("dotty-library", "dotty-compiler")
+      else if (JvmWorkerUtil.isScala3(scalaVersion()))
+        Seq("scala3-library", "scala3-compiler")
       else
-        d.withModule(
-          d.module.withOrganization(
-            coursier.Organization(scalaOrganization())
-          )
-        )
-          .withVersion(scalaVersion())
+        Seq("scala-library", "scala-compiler", "scala-reflect")
+    val constraint = coursier.version.VersionConstraint(scalaVersion())
+    val forced = scalaModuleNames.map { name =>
+      val mod = coursier.core.Module(
+        coursier.core.Organization(scalaOrganization()),
+        coursier.core.ModuleName(name),
+        Map.empty
+      )
+      (mod, constraint)
     }
+    super.resolutionParams()
+      .withTypelevel(isTypelevelScala)
+      .addForceVersion0(forced *)
   }
 
   override def resolveCoursierDependency: Task[Dep => coursier.Dependency] =
