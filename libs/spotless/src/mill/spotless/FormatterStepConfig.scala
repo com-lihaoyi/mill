914ff2ce5038cc434fd05fa88db75fb8b17506b7
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-04-29 16:12:12 +0530

    Removed intermediate traits

--- libs/spotless/src/mill/spotless/FormatterStepConfig.scala
+++ libs/spotless/src/mill/spotless/FormatterStepConfig.scala
@@ -22,133 +22,118 @@ sealed trait FormatterStepConfig derives Reader {
 
 object FormatterStepConfig {
 
-  sealed trait Generic extends FormatterStepConfig derives Reader
-
-  object Generic {
-
-    case class LicenseHeader(
-        delimiter: String,
-        header: Option[String] = None,
-        headerFile: Option[os.RelPath] = None,
-        name: Option[String] = None,
-        contentPattern: Option[String] = None,
-        yearSeparator: String = LicenseHeaderStep.defaultYearDelimiter(),
-        yearMode: LicenseHeaderStep.YearMode = LicenseHeaderStep.YearMode.PRESERVE,
-        skipLinesMatching: Option[String] = None
-    ) extends Generic derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep = {
-        val _header = header.getOrElse(headerFile
-          .flatMap(bytes(_)).fold(throw new Exception(s"$headerFile not found"))(
-            new String(_, ctx.encoding)
-          ))
-        contentPattern.foldLeft(
-          name.foldLeft(
-            LicenseHeaderStep.headerDelimiter(_header, delimiter)
-          )(_.withName(_))
-        )(_.withContentPattern(_)
-          .withYearSeparator(yearSeparator))
-          .withYearMode(yearMode)
-          .withSkipLinesMatching(skipLinesMatching.orNull)
-          .build()
-      }
+  case class LicenseHeader(
+      delimiter: String,
+      header: Option[String] = None,
+      headerFile: Option[os.RelPath] = None,
+      name: Option[String] = None,
+      contentPattern: Option[String] = None,
+      yearSeparator: String = LicenseHeaderStep.defaultYearDelimiter(),
+      yearMode: LicenseHeaderStep.YearMode = LicenseHeaderStep.YearMode.PRESERVE,
+      skipLinesMatching: Option[String] = None
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep = {
+      val _header = header.getOrElse(headerFile
+        .flatMap(bytes(_)).fold(throw new Exception(s"$headerFile not found"))(
+          new String(_, ctx.encoding)
+        ))
+      contentPattern.foldLeft(
+        name.foldLeft(
+          LicenseHeaderStep.headerDelimiter(_header, delimiter)
+        )(_.withName(_))
+      )(_.withContentPattern(_)
+        .withYearSeparator(yearSeparator))
+        .withYearMode(yearMode)
+        .withSkipLinesMatching(skipLinesMatching.orNull)
+        .build()
     }
   }
 
-  sealed trait Java extends FormatterStepConfig derives Reader
+  case class FormatAnnotations(
+      addedTypeAnnotations: Seq[String] = Nil,
+      removedTypeAnnotations: Seq[String] = Nil
+  ) extends FormatterStepConfig derives Reader {
+    def build(using SpotlessContext): FormatterStep =
+      FormatAnnotationsStep.create(addedTypeAnnotations.asJava, removedTypeAnnotations.asJava)
+  }
 
-  object Java {
+  case class GoogleJavaFormat(
+      groupArtifact: String = GoogleJavaFormatStep.defaultGroupArtifact(),
+      version: String = GoogleJavaFormatStep.defaultVersion(),
+      style: String = GoogleJavaFormatStep.defaultStyle(),
+      reflowLongStrings: Boolean = GoogleJavaFormatStep.defaultReflowLongStrings(),
+      reorderImports: Boolean = GoogleJavaFormatStep.defaultReorderImports(),
+      formatJavadoc: Boolean = GoogleJavaFormatStep.defaultFormatJavadoc()
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep =
+      GoogleJavaFormatStep.create(
+        groupArtifact,
+        version,
+        style,
+        ctx,
+        reflowLongStrings,
+        reorderImports,
+        formatJavadoc
+      )
+  }
 
-    case class FormatAnnotations(
-        addedTypeAnnotations: Seq[String] = Nil,
-        removedTypeAnnotations: Seq[String] = Nil
-    ) extends Java derives Reader {
-      def build(using SpotlessContext): FormatterStep =
-        FormatAnnotationsStep.create(addedTypeAnnotations.asJava, removedTypeAnnotations.asJava)
-    }
+  case class PalantirJavaFormat(
+      version: String = PalantirJavaFormatStep.defaultVersion(),
+      style: String = PalantirJavaFormatStep.defaultStyle(),
+      formatJavadoc: Boolean = PalantirJavaFormatStep.defaultFormatJavadoc()
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep =
+      PalantirJavaFormatStep.create(version, style, formatJavadoc, ctx)
+  }
 
-    case class GoogleJavaFormat(
-        groupArtifact: String = GoogleJavaFormatStep.defaultGroupArtifact(),
-        version: String = GoogleJavaFormatStep.defaultVersion(),
-        style: String = GoogleJavaFormatStep.defaultStyle(),
-        reflowLongStrings: Boolean = GoogleJavaFormatStep.defaultReflowLongStrings(),
-        reorderImports: Boolean = GoogleJavaFormatStep.defaultReorderImports(),
-        formatJavadoc: Boolean = GoogleJavaFormatStep.defaultFormatJavadoc()
-    ) extends Java derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep =
-        GoogleJavaFormatStep.create(
-          groupArtifact,
-          version,
-          style,
-          ctx,
-          reflowLongStrings,
-          reorderImports,
-          formatJavadoc
-        )
-    }
+  case class Diktat(
+      version: String = DiktatStep.defaultVersionDiktat(),
+      isScript: Boolean = false,
+      configFile: os.RelPath = "diktat-analysis.yml"
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep =
+      DiktatStep.create(
+        version,
+        ctx,
+        isScript,
+        signature(configFile)
+      )
+  }
 
-    case class PalantirJavaFormat(
-        version: String = PalantirJavaFormatStep.defaultVersion(),
-        style: String = PalantirJavaFormatStep.defaultStyle(),
-        formatJavadoc: Boolean = PalantirJavaFormatStep.defaultFormatJavadoc()
-    ) extends Java derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep =
-        PalantirJavaFormatStep.create(version, style, formatJavadoc, ctx)
+  case class Ktfmt(
+      version: String = KtfmtStep.defaultVersion(),
+      style: Option[KtfmtStep.Style] = None,
+      maxWidth: Option[Int] = None,
+      blockIndent: Option[Int] = None,
+      continuationIndent: Option[Int] = None,
+      removeUnusedImports: Option[Boolean] = None,
+      manageTrailingCommas: Option[Boolean] = None
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep = {
+      val options = new KtfmtStep.KtfmtFormattingOptions(
+        maxWidth.fold(null)(Int.box),
+        blockIndent.fold(null)(Int.box),
+        continuationIndent.fold(null)(Int.box),
+        removeUnusedImports.fold(null)(Boolean.box),
+        manageTrailingCommas.fold(null)(Boolean.box)
+      )
+      KtfmtStep.create(version, ctx, style.orNull, options)
     }
   }
 
-  sealed trait Kotlin extends FormatterStepConfig derives Reader
-
-  object Kotlin {
-
-    case class Diktat(
-        version: String = DiktatStep.defaultVersionDiktat(),
-        isScript: Boolean = false,
-        configFile: os.RelPath = "diktat-analysis.yml"
-    ) extends Kotlin derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep =
-        DiktatStep.create(
-          version,
-          ctx,
-          isScript,
-          signature(configFile)
-        )
-    }
-
-    case class Ktfmt(
-        version: String = KtfmtStep.defaultVersion(),
-        style: Option[KtfmtStep.Style] = None,
-        maxWidth: Option[Int] = None,
-        blockIndent: Option[Int] = None,
-        continuationIndent: Option[Int] = None,
-        removeUnusedImports: Option[Boolean] = None,
-        manageTrailingCommas: Option[Boolean] = None
-    ) extends Kotlin derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep = {
-        val options = new KtfmtStep.KtfmtFormattingOptions(
-          maxWidth.fold(null)(Int.box),
-          blockIndent.fold(null)(Int.box),
-          continuationIndent.fold(null)(Int.box),
-          removeUnusedImports.fold(null)(Boolean.box),
-          manageTrailingCommas.fold(null)(Boolean.box)
-        )
-        KtfmtStep.create(version, ctx, style.orNull, options)
-      }
-    }
-
-    case class KtLint(
-        version: String = KtLintStep.defaultVersion(),
-        configFile: os.RelPath = ".editorconfig",
-        customRuleSets: Seq[String] = Nil
-    ) extends Kotlin derives Reader {
-      def build(using ctx: SpotlessContext): FormatterStep =
-        KtLintStep.create(
-          version,
-          ctx,
-          signature(configFile),
-          Collections.emptyMap(),
-          customRuleSets.asJava
-        )
-    }
+  case class KtLint(
+      version: String = KtLintStep.defaultVersion(),
+      configFile: os.RelPath = ".editorconfig",
+      customRuleSets: Seq[String] = Nil
+  ) extends FormatterStepConfig derives Reader {
+    def build(using ctx: SpotlessContext): FormatterStep =
+      KtLintStep.create(
+        version,
+        ctx,
+        signature(configFile),
+        Collections.emptyMap(),
+        customRuleSets.asJava
+      )
   }
 
   private def bytes(rel: os.RelPath)(using res: PathResolver) =
