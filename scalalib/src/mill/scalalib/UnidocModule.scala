1a7c2309ffe26134922926295127f08cd87c8627
Artūras Šlajus <x11@arturaz.net>
2025-05-01 11:33:42 +0300

    https://github.com/com-lihaoyi/mill/issues/5034

--- scalalib/src/mill/scalalib/UnidocModule.scala
+++ scalalib/src/mill/scalalib/UnidocModule.scala
@@ -1,5 +1,5 @@
 package mill.scalalib
-import mill._
+import mill.*
 
 /**
  * Mix this in to any [[ScalaModule]] to provide a [[unidocSite]] target that
@@ -19,36 +19,45 @@ trait UnidocModule extends ScalaModule {
     allSourceFiles() ++ Task.traverse(moduleDeps)(_.allSourceFiles)().flatten
   }
 
+  /** The title of the scaladoc site. */
+  def unidocDocumentTitle: T[String] = Task { "Mill" }
+
+  /** Extra options passed to scaladoc. */
+  def unidocOptions: T[Seq[String]] = Task { Seq.empty[String] }
+
+  /**
+   * @param local whether to use 'file://' as the `-doc-source-url`.
+   */
   def unidocCommon(local: Boolean) = Task.Anon {
 
     val unidocSourceFiles0 = unidocSourceFiles()
 
     Task.log.info(s"Staging scaladoc for ${unidocSourceFiles0.length} files")
 
-    // the details of the options and zincWorker call are significantly
-    // different between scala-2 scaladoc and scala-3 scaladoc
-    // below is for scala-2 variant
-    val options: Seq[String] = Seq(
-      "-doc-title",
-      "Mill",
-      "-d",
-      Task.dest.toString,
-      "-classpath",
-      unidocCompileClasspath().map(_.path).mkString(sys.props("path.separator"))
-    ) ++
-      unidocVersion().toSeq.flatMap(Seq("-doc-version", _)) ++
-      unidocSourceUrl().toSeq.flatMap { url =>
-        if (local) Seq(
-          "-doc-source-url",
-          "file://€{FILE_PATH}.scala"
-        )
-        else Seq(
-          "-doc-source-url",
-          url + "€{FILE_PATH}.scala",
-          "-sourcepath",
-          Task.workspace.toString
-        )
-      }
+    val options: Seq[String] = // the details of the options and zincWorker call are significantly
+      // different between scala-2 scaladoc and scala-3 scaladoc
+      // below is for scala-2 variant
+      Seq(
+        "-doc-title",
+        unidocDocumentTitle(),
+        "-d",
+        Task.dest.toString,
+        "-classpath",
+        unidocCompileClasspath().map(_.path).mkString(sys.props("path.separator"))
+      ) ++
+        unidocVersion().toSeq.flatMap(Seq("-doc-version", _)) ++
+        unidocSourceUrl().toSeq.flatMap { url =>
+          if (local) Seq(
+            "-doc-source-url",
+            "file://€{FILE_PATH}.scala"
+          )
+          else Seq(
+            "-doc-source-url",
+            url + "€{FILE_PATH}.scala",
+            "-sourcepath",
+            Task.workspace.toString
+          )
+        } ++ unidocOptions()
 
     jvmWorker().worker().docJar(
       scalaVersion(),
