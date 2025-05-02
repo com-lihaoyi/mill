d01410e0f0ffed78d34553e1f1de5f5ca9a7afb3
Li Haoyi <haoyi.sg@gmail.com>
2025-05-02 17:20:48 +0800

    naive check

--- core/define/src/mill/define/internal/ResolveChecker.scala
+++ core/define/src/mill/define/internal/ResolveChecker.scala
@@ -1,7 +1,10 @@
 package mill.define.internal
 
 class ResolveChecker(workspace: os.Path) extends os.Checker {
-  def onRead(path: os.ReadablePath): Unit = ()
+  def onRead(path: os.ReadablePath): Unit = path match {
+    case path: os.Path => sys.error(s"Reading from ${path.relativeTo(workspace)} not allowed during resolution phase")
+    case _ =>
+  }
 
   def onWrite(path: os.Path): Unit = {
     sys.error(s"Writing to ${path.relativeTo(workspace)} not allowed during resolution phase")
