93de06e7b6c1c747f4ca3f536b7b8621419acdfc
Li Haoyi <haoyi.sg@gmail.com>
2025-05-02 17:31:23 +0800

    restrict filesystem reads during resolution phase

--- core/define/src/mill/define/internal/ResolveChecker.scala
+++ core/define/src/mill/define/internal/ResolveChecker.scala
@@ -1,9 +1,11 @@
 package mill.define.internal
 
 class ResolveChecker(workspace: os.Path) extends os.Checker {
-  def onRead(path: os.ReadablePath): Unit = path match {
-    case path: os.Path => sys.error(s"Reading from ${path.relativeTo(workspace)} not allowed during resolution phase")
-    case _ =>
+  def onRead(path: os.ReadablePath): Unit = {
+    path match {
+      case path: os.Path => sys.error(s"Reading from ${path.relativeTo(workspace)} not allowed during resolution phase")
+      case _ =>
+    }
   }
 
   def onWrite(path: os.Path): Unit = {
