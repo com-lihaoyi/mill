41b1904c3d1930439f1d6b66ce28125d603b4034
Tobias Roeser <le.petit.fou@web.de>
2025-05-08 21:43:35 +0200

    Build: move shared traits into separate Scala files (#5084)
    Move shared `trait`s and `object`s into dedicated `.scala` files under
    `mill-build/src`.
    
    This makes reading, editing and code navigation easier. It should also
    speed up compilation after a local change a bit.
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/5084

---
+++ mill-build/src/millbuild/Settings.scala
@@ -0,0 +1,28 @@
+package millbuild
+
+object Settings {
+  val pomOrg = "com.lihaoyi"
+  val githubOrg = "com-lihaoyi"
+  val githubRepo = "mill"
+  val projectUrl = s"https://github.com/${githubOrg}/${githubRepo}"
+  val changelogUrl = s"${projectUrl}#changelog"
+  val newIssueUrl = s"${projectUrl}/issues/new/choose"
+  val docUrl = "https://mill-build.org"
+  val mavenRepoUrl = "https://repo1.maven.org/maven2"
+  val bestMillVersion = "0.12.10"
+  // the exact branches containing a doc root
+  val docBranches = Seq()
+  // the exact tags containing a doc root. Publish docs for
+  // the last point version in each minor release series
+  val legacyDocTags: Seq[String] = Seq(
+    "0.9.12",
+    "0.10.15"
+  )
+  val docTags: Seq[String] = Seq(
+    "0.11.13",
+    "0.12.10"
+  )
+  val mimaBaseVersions: Seq[String] = Seq("0.13.0-M0")
+
+  val graalvmJvmId = "graalvm-community:23.0.1"
+}
