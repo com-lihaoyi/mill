b5344b762affaaab30acc6583afe0bd873d1b5cd
Li Haoyi <haoyi.sg@gmail.com>
2025-05-14 07:29:26 +0200

    remove echos

--- dist/scripts/src/mill.bat
+++ dist/scripts/src/mill.bat
@@ -220,14 +220,12 @@ if [!DOWNLOAD_FROM_MAVEN!]==[1] (
     set DOWNLOAD_URL=!GITHUB_RELEASE_CDN!%MILL_REPO_URL%/releases/download/!MILL_VERSION_TAG!/!MILL_VERSION!!DOWNLOAD_SUFFIX!
 )
 
-echo "mill.bat if defined MILL_TEST_DRY_RUN_LAUNCHER_SCRIPT"
 if defined MILL_TEST_DRY_RUN_LAUNCHER_SCRIPT (
     echo %MILL%
     echo %DOWNLOAD_URL%
     exit /b 0
 )
 
-echo "mill.bat if not exist MILL"
 if not exist "%MILL%" (
 
     rem there seems to be no way to generate a unique temporary file path (on native Windows)
