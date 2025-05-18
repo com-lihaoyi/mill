abf665369d86ac9684a94207c5c7a3990bd82df7
Li Haoyi <haoyi.sg@gmail.com>
2025-05-18 17:19:04 +0200

    fixes

--- runner/meta/src/mill/meta/CodeGen.scala
+++ runner/meta/src/mill/meta/CodeGen.scala
@@ -106,18 +106,13 @@ object CodeGen {
           millTopLevelProjectRoot = millTopLevelProjectRoot,
           output = output
         )
-        os.write(
-          supportDestDir / "MillMiscInfo.scala",
-          miscInfo,
-          createFolders = true
-        )
 
-        val wrapperObjectGetter = generateWrapperObjectGetter(pkg)
-        os.write(
-          supportDestDir / "BuildFileImpl.scala",
-          wrapperObjectGetter,
-          createFolders = true
-        )
+        os.write(supportDestDir / "MillMiscInfo.scala", miscInfo, createFolders = true)
+
+        if (scriptFolderPath == projectRoot){
+          val buildFileImplCode = generateBuildFileImpl(pkg)
+          os.write(supportDestDir / "BuildFileImpl.scala", buildFileImplCode, createFolders = true)
+        }
       }
 
       val parts =
@@ -183,7 +178,7 @@ object CodeGen {
         |""".stripMargin
   }
 
-  def generateWrapperObjectGetter(pkg: String) = {
+  def generateBuildFileImpl(pkg: String) = {
     s"""|$generatedFileHeader
         |package $pkg
         |
