889d9d8c21356bf682baa01266b1369f37aac09c
Li Haoyi <haoyi.sg@gmail.com>
2025-05-18 19:21:33 +0200

    fix

--- core/define/src/mill/define/internal/BuildFileCls.scala
+++ core/define/src/mill/define/internal/BuildFileCls.scala
@@ -3,7 +3,7 @@ package mill.define.internal
 class BuildFileCls(rootModule0: => mill.define.RootModule0) extends mill.api.internal.BuildFileApi {
   def value = this
   def checker = mill.define.internal.ResolveChecker(mill.define.BuildCtx.workspaceRoot)
-  def rootModule = os.checker.withValue(checker) { rootModule0 }
+  val rootModule = os.checker.withValue(checker) { rootModule0 }
 
   def moduleWatchedValues = mill.define.BuildCtx.watchedValues.toSeq
   def evalWatchedValues = mill.define.BuildCtx.evalWatchedValues
