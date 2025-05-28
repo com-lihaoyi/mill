fcec64f87344979d62506408b36a3bbf4c9df24e
Li Haoyi <haoyi.sg@gmail.com>
2025-05-28 12:36:27 +0200

    fmt

--- integration/feature/mill-jvm-property/src/MillJvmPropertyTests.scala
+++ integration/feature/mill-jvm-property/src/MillJvmPropertyTests.scala
@@ -9,21 +9,19 @@ object MillJvmPropertyTests extends UtestIntegrationTestSuite {
     test("simple") - integrationTest { tester =>
       import tester._
       // Property not set
-      val res1 = eval(("printSystemProperty", "--propName", "foo-property"))
+      val res1 = eval(("printSysProp", "--propName", "foo-property"))
       assert(res1.out == "null")
 
       // Property newly set
-      val res2 =
-        eval(("-Dfoo-property=hello-world", "printSystemProperty", "--propName", "foo-property"))
+      val res2 = eval(("-Dfoo-property=hello-world", "printSysProp", "--propName", "foo-property"))
       assert(res2.out == "hello-world")
 
       // Existing property modified
-      val res3 =
-        eval(("-Dfoo-property=i-am-cow", "printSystemProperty", "--propName", "foo-property"))
+      val res3 = eval(("-Dfoo-property=i-am-cow", "printSysProp", "--propName", "foo-property"))
       assert(res3.out == "i-am-cow")
 
       // Existing property removed
-      val res4 = eval(("printSystemProperty", "--propName", "foo-property"))
+      val res4 = eval(("printSysProp", "--propName", "foo-property"))
       assert(res4.out == "null")
     }
   }
