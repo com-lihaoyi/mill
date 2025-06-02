33066573647eb4548af693453bb30a5c67e21bab
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-06-02 10:20:16 +0530

    Cleanup

--- libs/scalalib/src/mill/scalalib/spotless/package.scala
+++ libs/scalalib/src/mill/scalalib/spotless/package.scala
@@ -3,5 +3,5 @@ package mill.scalalib
 import mill.define.ExternalModule
 
 // A Spotless CLI is under development at https://github.com/diffplug/spotless-cli.
-// Once a stable release is available, the worker implementation used here can be replaced.
+// Once a stable release is available, this implementation can be replaced.
 package object spotless extends ExternalModule.Alias(SpotlessModule)
