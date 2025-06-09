e4f44399907810ef63aa7d38a32dcb3de0665e8a
Alex Archambault <alexandre.archambault@gmail.com>
2025-06-09 12:17:59 +0200

    fixup

--- integration/failure/daemon-early-crash/src/DaemonEarlyCrashTests.scala
+++ integration/failure/daemon-early-crash/src/DaemonEarlyCrashTests.scala
@@ -7,9 +7,8 @@ import utest._
 object DaemonEarlyCrashTests extends UtestIntegrationTestSuite {
   val tests: Tests = Tests {
     test("check") - integrationTest { tester =>
-      import tester._
       if (daemonMode) {
-        val res = eval("version", env = Map("MILL_DAEMON_CRASH" -> "true"), timeout = 10000L)
+        val res = tester.eval("version", env = Map("MILL_DAEMON_CRASH" -> "true"), timeout = 10000L)
         assert(res.exitCode == 1)
         assert(res.err.contains("Mill daemon exited!"))
         assert(res.err.contains("Mill daemon early crash requested"))
