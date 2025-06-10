4fd20cef27e23de4ad8851a1be275de9792cf8b9
Li Haoyi <haoyi.sg@gmail.com>
2025-06-10 19:44:12 +0800

    .

--- core/resolve/src/mill/resolve/ResolveCore.scala
+++ core/resolve/src/mill/resolve/ResolveCore.scala
@@ -27,9 +27,24 @@ private object ResolveCore {
   }
 
   object Resolved {
+    implicit object resolvedOrder extends Ordering[Resolved] {
+      def orderingKey(r: Resolved) = r match {
+        case _: Module => 1
+        case _: Command => 2
+        case _: NamedTask => 3
+      }
+
+      override def compare(x: Resolved, y: Resolved): Int = {
+        val keyX = orderingKey(x)
+        val keyY = orderingKey(y)
+        if (keyX == keyY) Segments.ordering.compare(x.segments, y.segments)
+        else Ordering.Int.compare(keyX, keyY)
+      }
+    }
+
     case class Module(segments: Segments, cls: Class[?]) extends Resolved
-    case class NamedTask(segments: Segments) extends Resolved
     case class Command(segments: Segments) extends Resolved
+    case class NamedTask(segments: Segments) extends Resolved
   }
 
   sealed trait Result
