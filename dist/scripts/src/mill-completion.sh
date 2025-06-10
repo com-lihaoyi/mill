f4920125e23060ebafab140165d80826b9f4e0c5
Li Haoyi <haoyi.sg@gmail.com>
2025-06-10 21:22:11 +0800

    wip

--- dist/scripts/src/mill-completion.sh
+++ dist/scripts/src/mill-completion.sh
@@ -1,15 +1,12 @@
 _mill_bash() {
-  local completions=$(${COMP_WORDS[0]} --tabcomplete "$COMP_CWORD" $COMP_WORDS)
-  COMPREPLY=( $(compgen -W "$completions" -- "$cur") )
+  COMPREPLY=( $(${COMP_WORDS[0]} --tab-complete "$COMP_CWORD" $COMP_WORDS) )
   compopt -o nospace 2>/dev/null
 }
 
 _mill_zsh() {
-  local completions=$($words[1] --tabcomplete "$CURRENT" $words)
-  local -a suggestions=(${(f)completions})
   # `-S` to avoid the trailing space after a completion, since it is
   # common that the user will want to put a `.` and continue typing
-  compadd -S '' -- $suggestions
+  compadd -S '' -- $($words[1] --tab-complete "$CURRENT" $words)
 }
 
 if [ -n "${ZSH_VERSION:-}" ]; then
