5c1854e5d2edad048fbc3b292fffacb204d99be8
Li Haoyi <haoyi.sg@gmail.com>
2025-06-14 23:39:06 +0800

    fixtest

--- libs/init/gradle/src/mill/main/gradle/GradleBuildGenMain.scala
+++ libs/init/gradle/src/mill/main/gradle/GradleBuildGenMain.scala
@@ -202,7 +202,7 @@ object GradleBuildGenMain extends BuildGenBase.MavenAndGradle[ProjectModel, Dep]
       Option.when(null != build.value.maven().pom() && {
         val baseTrait = baseInfo.moduleTypedef
         baseTrait == null || !baseTrait.moduleSupertypes.contains("PublishModule")
-      }) { "PublishModule" } ++
+      }) { "PublishModule" }.toSeq ++
       Option.when(build.dirs.nonEmpty || os.exists(getMillSourcePath(build.value) / "src")) {
         getModuleSupertypes(cfg)
       }.toSeq.flatten
