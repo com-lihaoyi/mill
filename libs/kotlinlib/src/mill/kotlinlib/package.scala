6b9218e3be525ea6619c13228322095b0987588c
Tobias Roeser <le.petit.fou@web.de>
2025-06-18 08:44:24 +0200

    .

--- libs/kotlinlib/src/mill/kotlinlib/package.scala
+++ libs/kotlinlib/src/mill/kotlinlib/package.scala
@@ -3,6 +3,9 @@ package mill
 package object kotlinlib {
   implicit class DepSyntax(ctx: StringContext) extends AnyVal {
     def mvn(args: Any*): Dep = mill.scalalib.DepSyntax(ctx).mvn(args*)
+
+    @deprecated("Use `mvn` string interpolator instead.", "Mill 0.12.11")
+    def ivy(args: Any*): Dep = mill.scalalib.DepSyntax(ctx).mvn(args*)
   }
 
   type Dep = mill.scalalib.Dep
