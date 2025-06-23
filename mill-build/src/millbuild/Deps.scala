77d76312f5871db03c90deba0897eec13fd28565
Tobias Roeser <le.petit.fou@web.de>
2025-06-23 22:17:45 +0200

    Update classgraph to 4.8.180

--- mill-build/src/millbuild/Deps.scala
+++ mill-build/src/millbuild/Deps.scala
@@ -84,7 +84,7 @@ object Deps {
   val asmTree = mvn"org.ow2.asm:asm-tree:9.8"
   val bloopConfig = mvn"ch.epfl.scala::bloop-config:1.5.5".withDottyCompat(scalaVersion)
 
-  val classgraph = mvn"io.github.classgraph:classgraph:4.8.179"
+  val classgraph = mvn"io.github.classgraph:classgraph:4.8.180"
   val coursierVersion = "2.1.25-M13"
   val coursier = mvn"io.get-coursier::coursier:$coursierVersion".withDottyCompat(scalaVersion)
   val coursierCore =
