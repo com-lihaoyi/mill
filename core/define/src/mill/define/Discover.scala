e9314168db486c484bcea8dc2269cc2bd7121762
Li Haoyi <haoyi.sg@gmail.com>
2025-06-26 13:15:00 +0800

    .

--- core/define/src/mill/define/Discover.scala
+++ core/define/src/mill/define/Discover.scala
@@ -23,6 +23,10 @@ class Discover(val classInfo: Map[Class[?], Discover.ClassInfo]) {
       if ep.mainName.getOrElse(ep.defaultName) == name
     } yield ep
 
+    // When there are multiple entrypoints matching the class and name,
+    // return the one with the longest argument lists, since users may
+    // add additional arguments to a command (with default values) to 
+    // preserve binary compatibility while evolving the method signature
     res.maxByOption(_.argSigs0.length)
   }
 }
