3cf6ff33c2ec0cf32014cf5e75e03ff4e716bddb
Li Haoyi <haoyi.sg@gmail.com>
2025-06-27 10:01:51 +0800

    wip

--- libs/vcs/src/mill/vcs/VcsVersion.scala
+++ libs/vcs/src/mill/vcs/VcsVersion.scala
@@ -37,7 +37,7 @@ object VcsVersion extends ExternalModule with VcsVersion {
         countSep: String = "-",
         commitCountPad: Byte = 0,
         revSep: String = "-",
-        revHashDigits: Int = 6,
+        revHashDigits: Int = -1,
         dirtySep: String = "-DIRTY",
         dirtyHashDigits: Int = 8,
         tagModifier: String => String = stripV,
@@ -57,7 +57,16 @@ object VcsVersion extends ExternalModule with VcsVersion {
       } else ""
 
       val revisionPart = if (isUntagged) {
-        s"$revSep${currentRevision.take(revHashDigits)}"
+        val actualRevHashDigits =
+          if (revHashDigits != -1) currentRevision.take(revHashDigits)
+          else {
+            os.call(("git", "rev-parse", "--short", currentRevision),
+              cwd = vcsBasePath,
+              stderr = os.Pipe
+            ).out.trim()
+          }
+
+        s"$revSep$actualRevHashDigits"
       } else ""
 
       val dirtyPart = dirtyHash match {
