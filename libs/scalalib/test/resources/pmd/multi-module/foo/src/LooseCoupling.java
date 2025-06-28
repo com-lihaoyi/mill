00cfed4aa624daeec2845f8d670c421a20174c6f
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-06-28 17:39:26 +0530

    Added support for PMD (Programming Mistake Detector)

---
+++ libs/scalalib/test/resources/pmd/multi-module/foo/src/LooseCoupling.java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+
+public class LooseCoupling {
+
+    public HashSet<String> getFoo() {
+        return new HashSet<String>();
+    }
+}
---
+++ libs/scalalib/test/resources/pmd/no-violations/src/LooseCoupling.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LooseCoupling {
+
+    public Set<String> getFoo() {
+        return new HashSet<String>();
+    }
+}
