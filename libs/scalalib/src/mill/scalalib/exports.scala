6b1abda2f256e13422fbb1638434c0f54d968dc8
Li Haoyi <haoyi.sg@gmail.com>
2025-07-01 15:10:03 +0800

    .

--- libs/scalalib/src/mill/scalalib/exports.scala
+++ libs/scalalib/src/mill/scalalib/exports.scala
@@ -1,81 +1,81 @@
 package mill.scalalib
 
-val Assembly = mill.javalib.Assembly
+lazy val Assembly = mill.javalib.Assembly
 type Assembly = mill.javalib.Assembly
 
-val AssemblyModule = mill.javalib.AssemblyModule
+lazy val AssemblyModule = mill.javalib.AssemblyModule
 type AssemblyModule = mill.javalib.AssemblyModule
 
 type BomModule = mill.javalib.BomModule
 
-val BoundDep = mill.javalib.BoundDep
+lazy val BoundDep = mill.javalib.BoundDep
 type BoundDep = mill.javalib.BoundDep
 
-val CoursierModule = mill.javalib.CoursierModule
+lazy val CoursierModule = mill.javalib.CoursierModule
 type CoursierModule = mill.javalib.CoursierModule
 
-val Dep = mill.javalib.Dep
+lazy val Dep = mill.javalib.Dep
 type Dep = mill.javalib.Dep
 
-val Dependency = mill.javalib.Dependency
+lazy val Dependency = mill.javalib.Dependency
 // type Dependency = mill.javalib.Dependency
 
-// val GenIdeaModule = mill.javalib.GenIdeaModule
+// lazy val GenIdeaModule = mill.javalib.GenIdeaModule
 type GenIdeaModule = mill.javalib.GenIdeaModule
 
-// val JavaHomeModule = mill.javalib.JavaHomeModule
+// lazy val JavaHomeModule = mill.javalib.JavaHomeModule
 type JavaHomeModule = mill.javalib.JavaHomeModule
 
-val JavaModule = mill.javalib.JavaModule
+lazy val JavaModule = mill.javalib.JavaModule
 type JavaModule = mill.javalib.JavaModule
 
-// val JlinkModule = mill.javalib.JlinkModule
+// lazy val JlinkModule = mill.javalib.JlinkModule
 type JlinkModule = mill.javalib.JlinkModule
 
-// val JpackageModule = mill.javalib.JpackageModule
+// lazy val JpackageModule = mill.javalib.JpackageModule
 type JpackageModule = mill.javalib.JpackageModule
 
-val JsonFormatters = mill.javalib.JsonFormatters
+lazy val JsonFormatters = mill.javalib.JsonFormatters
 type JsonFormatters = mill.javalib.JsonFormatters
 
-val JvmWorkerModule = mill.javalib.JvmWorkerModule
+lazy val JvmWorkerModule = mill.javalib.JvmWorkerModule
 type JvmWorkerModule = mill.javalib.JvmWorkerModule
 
-val Lib = mill.javalib.Lib
+lazy val Lib = mill.javalib.Lib
 // type Lib = mill.javalib.Lib
 
-// val MavenModule = mill.javalib.MavenModule
+// lazy val MavenModule = mill.javalib.MavenModule
 type MavenModule = mill.javalib.MavenModule
 
-val MvnDepsTreeArgs = mill.javalib.MvnDepsTreeArgs
+lazy val MvnDepsTreeArgs = mill.javalib.MvnDepsTreeArgs
 type MvnDepsTreeArgs = mill.javalib.MvnDepsTreeArgs
 
-// val NativeImageModule = mill.javalib.NativeImageModule
+// lazy val NativeImageModule = mill.javalib.NativeImageModule
 type NativeImageModule = mill.javalib.NativeImageModule
 
-val OfflineSupport = mill.javalib.OfflineSupport
+lazy val OfflineSupport = mill.javalib.OfflineSupport
 // type OfflineSupport = mill.javalib.OfflineSupport
 
-// val OfflineSupportModule = mill.javalib.OfflineSupportModule
+// lazy val OfflineSupportModule = mill.javalib.OfflineSupportModule
 type OfflineSupportModule = mill.javalib.OfflineSupportModule
 
-val PublishModule = mill.javalib.PublishModule
+lazy val PublishModule = mill.javalib.PublishModule
 type PublishModule = mill.javalib.PublishModule
 
-val RunModule = mill.javalib.RunModule
+lazy val RunModule = mill.javalib.RunModule
 type RunModule = mill.javalib.RunModule
 
-// val SonatypeCentralPublisher = mill.javalib.SonatypeCentralPublisher.self
+// lazy val SonatypeCentralPublisher = mill.javalib.SonatypeCentralPublisher.self
 type SonatypeCentralPublisher = mill.javalib.SonatypeCentralPublisher
 
-val SonatypeCentralPublishModule = mill.javalib.SonatypeCentralPublishModule
+lazy val SonatypeCentralPublishModule = mill.javalib.SonatypeCentralPublishModule
 type SonatypeCentralPublishModule = mill.javalib.SonatypeCentralPublishModule
 
-val TestModule = mill.javalib.TestModule
+lazy val TestModule = mill.javalib.TestModule
 type TestModule = mill.javalib.TestModule
 
-val TestModuleUtil = mill.javalib.TestModuleUtil
+lazy val TestModuleUtil = mill.javalib.TestModuleUtil
 // type TestModuleUtil = mill.javalib.TestModuleUtil
 
-// val WithJvmWorkerModule = mill.javalib.WithJvmWorkerModule
+// lazy val WithJvmWorkerModule = mill.javalib.WithJvmWorkerModule
 type WithJvmWorkerModule = mill.javalib.WithJvmWorkerModule
