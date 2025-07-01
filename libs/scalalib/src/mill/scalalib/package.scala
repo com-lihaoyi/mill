3dc98145172e760aa4de6cedf60cfc9dbe6ea15f
Li Haoyi <haoyi.sg@gmail.com>
2025-07-01 14:00:04 +0800

    move

--- libs/scalalib/src/mill/scalalib/package.scala
+++ libs/scalalib/src/mill/scalalib/package.scala
@@ -14,7 +14,26 @@ package object scalalib extends mill.scalalib.JsonFormatters {
     def ivy(args: Any*): Dep = mvn(args*)
   }
 
-  val CrossVersion = mill.api.CrossVersion
-  type CrossVersion = mill.api.CrossVersion
 
+  export mill.javalib.Assembly
+  export mill.javalib.AssemblyModule
+  export mill.javalib.CoursierModule
+  export mill.javalib.Dep
+  export mill.javalib.Dependency
+  export mill.javalib.JavaHomeModule
+  export mill.javalib.JavaModule
+  export mill.javalib.JlinkModule
+  export mill.javalib.JpackageModule
+  export mill.javalib.JvmWorkerModule
+  export mill.javalib.Lib
+  export mill.javalib.MavenModule
+  export mill.javalib.MvnDepsTreeArgs
+  export mill.javalib.NativeImageModule
+  export mill.javalib.PublishModule
+  export mill.javalib.RunModule
+  export mill.javalib.SonatypeCentralPublisher
+  export mill.javalib.SonatypeCentralPublishModule
+  export mill.javalib.TestModule
+  export mill.javalib.TestModuleUtil
+  export mill.javalib.WithJvmWorkerModule
 }
