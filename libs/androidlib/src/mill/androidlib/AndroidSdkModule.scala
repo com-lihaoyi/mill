1b6e98aee7a50ee43f80b2951578010bf0c62482
Stylianos Anastasiou <stylianos@vaslabs.io>
2025-07-03 14:27:57 +0300

    fix

--- libs/androidlib/src/mill/androidlib/AndroidSdkModule.scala
+++ libs/androidlib/src/mill/androidlib/AndroidSdkModule.scala
@@ -285,7 +285,7 @@ trait AndroidSdkModule extends Module {
       s"build-tools;${buildToolsVersion()}",
       s"platforms;${platformsVersion()}",
       "cmdline-tools;latest",
-      "tools;latest"
+      "tools"
     )
     // sdkmanager executable and state of the installed package is a shared resource, which can be accessed
     // from the different Android SDK modules.
