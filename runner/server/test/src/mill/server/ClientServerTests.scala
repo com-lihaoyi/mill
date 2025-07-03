com-lihaoyi/mill

commit 51ca8349390acd5a46e06355c176205cb4a71401

Use a single Mill server process (#5066)
Fixes https://github.com/com-lihaoyi/mill/issues/5031

* We now only have a single server process running at a time, and no
longer keep up to 5 separate `serverDir-1` to `serverDir-5` folders.
Essentially we are migrating from the style of concurrency used by
`gradle` and `mvnd` to the style of concurrency used by `bazel`

* We now only hold the `clientLock` until the server process is
initialized (and has taken it's `processLock`) to avoid redundant
initialization, and after that we release it to allow multiple clients
to connect

* Since there is only one server and one `serverDir`, we have to remove
several per-server files since there is no way to ensure they are unique
in the presence of multiple clients
* `runArgs` can no longer be a file, and instead we send its contents
over the socket before starting the `InputPumper`

* We can no longer rely on `clientLock` probing to check if the client
has disconnected, and so we instead have the server send
`ProxyStream.HEARTBEAT` bytes (127) and look for errors

* `Server#run` can no longer accept socket connections and process them
in a single-threaded loop, and instead needs to spawn off threads to
handle each connected socket

* We cannot perform the mutex at the server/client/connection level
because there are scenarios we want multiple clients connected at once,
e.g. when one client is in `--watch` mode and idle we want to allow
another client to proceed with execution. So locking needs to be done at
a finer-grained granularity within the server

* The original `millLock` file (renamed `millOutLock`) has been combined
with a `MemoryLock` via `DoubleLock`, which lets us check the
memory-lock first to avoid taking the file-lock twice on the same JVM
and causing a `OverlappingFileLockException`. We still need to keep the
file-lock to provide mutex between server and no-server processes, or
between multiple no-server processes

runner/server/src/mill/server/Server.scala
@@ -2,7 +2,7 @@ package mill.server
 
 import mill.api.SystemStreams
 import mill.constants.ProxyStream.Output
-import mill.client.lock.{Lock, Locks}
+import mill.client.lock.{DoubleLock, Lock, Locks}
 import mill.client.*
 import mill.constants.ServerFiles
 import mill.constants.InputPumper
@@ -47,7 +47,7 @@ abstract class Server[T](
     val initialSystemProperties = sys.props.toMap
 
     try {
-      Server.tryLockBlock(locks.processLock) {
+      Server.tryLockBlock(locks.serverLock) {
         serverLog("server file locked")
         Server.watchProcessIdFile(
           serverDir / ServerFiles.processId,
@@ -67,11 +67,13 @@ abstract class Server[T](
               case None => false
               case Some(sock) =>
                 serverLog("handling run")
-                try handleRun(sock, initialSystemProperties)
-                catch {
-                  case e: Throwable =>
-                    serverLog(e.toString + "\n" + e.getStackTrace.mkString("\n"))
-                } finally sock.close();
+                new Thread(() =>
+                  try handleRun(sock, initialSystemProperties)
+                  catch {
+                    case e: Throwable =>
+                      serverLog(e.toString + "\n" + e.getStackTrace.mkString("\n"))
+                  } finally sock.close();
+                ).start()
                 true
             }
           }
@@ -136,19 +138,39 @@ abstract class Server[T](
   def handleRun(clientSocket: Socket, initialSystemProperties: Map[String, String]): Unit = {
 
     val currentOutErr = clientSocket.getOutputStream
+    var clientDisappeared = false
+    // We cannot use Socket#{isConnected, isClosed, isBound} because none of these
+    // detect client-side connection closing, so instead we send a no-op heartbeat
+    // message to see if the socket can receive data.
+    def checkClientAlive() = {
+      try {
+        ProxyStream.sendHeartbeat(currentOutErr)
+        true
+      } catch {
+        case e: Throwable =>
+          clientDisappeared = true
+          false
+      }
+    }
     try {
       val stdout = new PrintStream(new Output(currentOutErr, ProxyStream.OUT), true)
       val stderr = new PrintStream(new Output(currentOutErr, ProxyStream.ERR), true)
 
+      val socketIn = clientSocket.getInputStream
+
+      val interactive = socketIn.read() != 0
+      val clientMillVersion = ClientUtil.readString(socketIn)
+      val args = ClientUtil.parseArgs(socketIn)
+      val env = ClientUtil.parseMap(socketIn)
+      serverLog("args " + upickle.default.write(args))
+      serverLog("env " + upickle.default.write(env.asScala))
+//      val userSpecifiedProperties = ClientUtil.parseMap(socketIn)
       // Proxy the input stream through a pair of Piped**putStream via a pumper,
       // as the `UnixDomainSocketInputStream` we get directly from the socket does
       // not properly implement `available(): Int` and thus messes up polling logic
       // that relies on that method
-      val proxiedSocketInput = proxyInputStreamThroughPumper(clientSocket.getInputStream)
+      val proxiedSocketInput = proxyInputStreamThroughPumper(socketIn)
 
-      val argStream = os.read.inputStream(serverDir / ServerFiles.runArgs)
-      val interactive = argStream.read() != 0
-      val clientMillVersion = ClientUtil.readString(argStream)
       val serverMillVersion = BuildInfo.millVersion
       if (clientMillVersion != serverMillVersion) {
         stderr.println(
@@ -160,12 +182,6 @@ abstract class Server[T](
         )
         System.exit(ClientUtil.ExitServerCodeWhenVersionMismatch())
       }
-      val args = ClientUtil.parseArgs(argStream)
-      val env = ClientUtil.parseMap(argStream)
-      serverLog("args " + upickle.default.write(args))
-      serverLog("env " + upickle.default.write(env.asScala))
-      val userSpecifiedProperties = ClientUtil.parseMap(argStream)
-      argStream.close()
 
       @volatile var done = false
       @volatile var idle = false
@@ -179,7 +195,7 @@ abstract class Server[T](
               new SystemStreams(stdout, stderr, proxiedSocketInput),
               env.asScala.toMap,
               idle = _,
-              userSpecifiedProperties.asScala.toMap,
+              Map(),
               initialSystemProperties,
               systemExit = exitCode => {
                 os.write.over(serverDir / ServerFiles.exitCode, exitCode.toString)
@@ -202,7 +218,7 @@ abstract class Server[T](
       // We cannot simply use Lock#await here, because the filesystem doesn't
       // realize the clientLock/serverLock are held by different threads in the
       // two processes and gives a spurious deadlock error
-      while (!done && !locks.clientLock.probe()) Thread.sleep(1)
+      while (!done && checkClientAlive()) Thread.sleep(1)
 
       if (!idle) {
         serverLog("client interrupted while server was executing command")
@@ -224,7 +240,9 @@ abstract class Server[T](
       System.out.flush()
       System.err.flush()
 
-    } finally ProxyStream.sendEnd(currentOutErr) // Send a termination
+    } finally {
+      if (!clientDisappeared) ProxyStream.sendEnd(currentOutErr) // Send a termination
+    }
   }
 
   def main0(
@@ -299,12 +317,11 @@ object Server {
       noWaitForBuildLock: Boolean,
       out: os.Path,
       targetsAndParams: Seq[String],
-      streams: SystemStreams
+      streams: SystemStreams,
+      outLock: Lock
   )(t: => T): T = {
     if (noBuildLock) t
     else {
-      val outLock = Lock.file((out / OutFiles.millLock).toString)
-
       def activeTaskString =
         try os.read(out / OutFiles.millActiveCommand)
         catch {

runner/server/test/src/mill/server/ClientServerTests.scala
@@ -76,7 +76,7 @@ object ClientServerTests extends TestSuite {
     os.makeDir.all(dest)
     val outDir = os.temp.dir(dest, deleteOnExit = false)
 
-    val memoryLocks = Array.fill(10)(Locks.memory());
+    val memoryLock = Locks.memory()
 
     def apply(
         env: Map[String, String] = Map(),
@@ -92,7 +92,7 @@ object ClientServerTests extends TestSuite {
         new PrintStream(err),
         env.asJava,
         args,
-        memoryLocks,
+        memoryLock,
         forceFailureForTestingMillisDelay
       ) {
         def preRun(serverDir: Path) = { /*do nothing*/ }
@@ -135,7 +135,7 @@ object ClientServerTests extends TestSuite {
 
   def tests = Tests {
 
-    test("hello") - retry(3) {
+    test("hello") - {
       // Continue logging when out folder is deleted so we can see the logs
       // and ensure the correct code path is taken as the server exits
       val tester = new Tester(testLogEvenWhenServerIdWrong = true)
@@ -214,8 +214,8 @@ object ClientServerTests extends TestSuite {
       assert(resF1.outDir == resF2.outDir)
       assert(resF2.outDir == resF3.outDir)
       // but the serverDir is placed in different subfolders
-      assert(resF1.serverDir != resF2.serverDir)
-      assert(resF2.serverDir != resF3.serverDir)
+      assert(resF1.serverDir == resF2.serverDir)
+      assert(resF2.serverDir == resF3.serverDir)
 
       assert(resF1.out == s"hello World$ENDL")
       assert(resF2.out == s"hello WORLD$ENDL")
@@ -238,12 +238,9 @@ object ClientServerTests extends TestSuite {
       val logLines = os.read.lines(os.Path(pathStr, os.pwd) / "server.log")
 
       assert(
-        logLines.takeRight(5) ==
+        logLines.takeRight(2) ==
           Seq(
             "server-0 client interrupted while server was executing command",
-            "server-0 exiting server",
-            "server-0 server loop ended",
-            "server-0 finally exitServer",
             "server-0 exiting server"
           )
       )

commit 750cf0c77c028d197e1432e1bc067f10400220bc

Prevent Mill daemon from timing out while launcher is still connected (#5438)
Fixes https://github.com/com-lihaoyi/mill/issues/5436.

That bug was introduced in https://github.com/com-lihaoyi/mill/pull/5066
migrating to a single-server architecture.

* Before, each server's `serverSocket.accept()` loop would block on each
launcher connection completing before the subsequent
`serverSocket.accept()` starts, with the inactivity timer only starting
from then when the launcher disconnected

* After the single-server architecture, the server loop would
immediately start the next `serverSocket.accept()` call since we needed
to support multiple connected launchers (e.g. 1 launcher idle in
`--watch` and 1 launcher actively running a command), thus starting the
inactivity timer even while the launcher was still running

This means that long-lived commands like `--watch`, or even just a long
`__.compile`, may be killed halfway due to the server shutting down out
of inactivity.

This PR moves the timeout handling out of the `serverSocket.accept()`
loop. Instead, we keep track of an `activeConnections` count when each
launcher command starts and finishes, and the `timeoutThread` waits
until `activeConnections` reaches and remains at 0 for the timeout
duration before stopping the server.

I also simplified `class Server`  slightly:

- Removed the `var running: Boolean` field. Instead, we rely on and
`ServerSocket#{close,isClosed}` both to interrupt the listening socket
and to mark the server as "stopped".
- Remove `def exitServer`, and moved the `System.exit` call out of the
`exitServer` override to instead just run when the method returns

This simplifies the state machine and removes some early exits, and
should hopefully make it a bit easier to reason about

Added a unit test to cover this case, and tested it manually by setting
the idle server timeout to 10s: on `main` branch `./mill-assembly.jar
__.compile` dies after 10s halfway through compiling things, while on

runner/client/src/mill/client/FileToStreamTailer.java
@@ -20,7 +20,7 @@ public class FileToStreamTailer extends Thread implements AutoCloseable {
   private volatile boolean flush = false;
 
   public FileToStreamTailer(File file, PrintStream stream, int intervalMsec) {
-    super("Tail");
+    super("FileToStreamTailerThread");
     this.intervalMsec = intervalMsec;
     setDaemon(true);
     this.file = file;

runner/daemon/src/mill/daemon/MillDaemonMain.scala
@@ -30,13 +30,15 @@ object MillDaemonMain {
       )
 
       val acceptTimeoutMillis =
-        Try(System.getProperty("mill.server_timeout").toInt).getOrElse(30 * 60 * 1000) // 30 minutes
+        Try(System.getProperty("mill.server_timeout").toInt).getOrElse(30 * 1000) // 30 minutes
 
       new MillDaemonMain(
         daemonDir = os.Path(args0(0)),
         acceptTimeoutMillis = acceptTimeoutMillis,
         Locks.files(args0(0))
       ).run()
+
+      System.exit(ClientUtil.ExitServerCodeWhenIdle())
     }
   }
 }
@@ -50,9 +52,6 @@ class MillDaemonMain(
       locks
     ) {
 
-  override def exitServer(): Unit = {
-    super.exitServer(); System.exit(ClientUtil.ExitServerCodeWhenIdle())
-  }
   def stateCache0 = RunnerState.empty
 
   val out = os.Path(OutFiles.out, BuildCtx.workspaceRoot)

runner/server/src/mill/server/Server.scala
@@ -32,8 +32,6 @@ abstract class Server[T](
   def outLock: mill.client.lock.Lock
   def out: os.Path
 
-  @volatile var running = true
-  def exitServer(): Unit = running = false
   var stateCache = stateCache0
   def stateCache0: T
 
@@ -55,16 +53,48 @@ abstract class Server[T](
     try {
       Server.tryLockBlock(locks.daemonLock) { locked =>
         serverLog("server file locked")
+        val serverSocket = new java.net.ServerSocket(0, 0, InetAddress.getByName(null))
         Server.watchProcessIdFile(
           daemonDir / DaemonFiles.processId,
           processId,
-          running = () => running,
+          running = () => !serverSocket.isClosed,
           exit = msg => {
             serverLog(msg)
-            exitServer()
+            serverSocket.close()
           }
         )
-        val serverSocket = new java.net.ServerSocket(0, 0, InetAddress.getByName(null))
+
+        // Wrapper object to encapsulate `activeConnections` and `inactiveTimestampOpt`,
+        // ensuring they get incremented and decremented together across multiple threads
+        // and never get out of sync
+        object ConnectionTracker {
+          private var activeConnections = 0
+          private var inactiveTimestampOpt: Option[Long] = None
+          def wrap(t: => Unit) = synchronized { if (!serverSocket.isClosed) { t } }
+          def increment() = wrap {
+            activeConnections += 1
+            serverLog(s"$activeConnections active connections")
+            inactiveTimestampOpt = None
+          }
+
+          def decrement() = wrap {
+            activeConnections -= 1
+            serverLog(s"$activeConnections active connections")
+            if (activeConnections == 0) {
+              inactiveTimestampOpt = Some(System.currentTimeMillis())
+            }
+          }
+
+          def closeIfTimedOut() = wrap {
+            inactiveTimestampOpt.foreach { inactiveTimestamp =>
+              if (System.currentTimeMillis() - inactiveTimestamp > acceptTimeoutMillis) {
+                serverLog(s"shutting down due inactivity")
+                serverSocket.close()
+              }
+            }
+          }
+        }
+
         try {
           os.write.over(daemonDir / DaemonFiles.socketPort, serverSocket.getLocalPort.toString)
           serverLog("listening on port " + serverSocket.getLocalPort)
@@ -81,27 +111,49 @@ abstract class Server[T](
             sys.exit(exitCode)
           }
 
-          while (
-            running && {
-              interruptWithTimeout(() => serverSocket.close(), () => serverSocket.accept()) match {
-                case None => false
-                case Some(sock) =>
-                  serverLog("handling run")
-                  new Thread(
-                    () =>
-                      try handleRun(systemExit, sock, initialSystemProperties)
-                      catch {
-                        case e: Throwable =>
-                          serverLog(e.toString + "\n" + e.getStackTrace.mkString("\n"))
-                      } finally sock.close();,
-                    "HandleRunThread"
-                  ).start()
-                  true
+          val timeoutThread = new Thread(
+            () => {
+              while (!serverSocket.isClosed) {
+                Thread.sleep(1)
+                ConnectionTracker.closeIfTimedOut()
               }
+            },
+            "MillServerTimeoutThread"
+          )
+          timeoutThread.start()
+
+          while (!serverSocket.isClosed) {
+            val socketOpt =
+              try Some(serverSocket.accept())
+              catch { case e: java.net.SocketException => None }
+
+            socketOpt match {
+              case Some(sock) =>
+                serverLog("handling run")
+                new Thread(
+                  () =>
+                    try {
+                      ConnectionTracker.increment()
+                      handleRun(
+                        systemExit,
+                        sock,
+                        initialSystemProperties,
+                        () => serverSocket.close()
+                      )
+                    } catch {
+                      case e: Throwable =>
+                        serverLog(e.toString + "\n" + e.getStackTrace.mkString("\n"))
+                    } finally {
+                      ConnectionTracker.decrement()
+                      sock.close()
+                    },
+                  "HandleRunThread"
+                ).start()
+              case None =>
             }
-          ) ()
+          }
+
         } finally serverSocket.close()
-        serverLog("server loop ended")
       }.getOrElse(throw new Exception("Mill server process already present"))
     } catch {
       case e: Throwable =>
@@ -109,8 +161,7 @@ abstract class Server[T](
         serverLog("server loop stack trace: " + e.getStackTrace.mkString("\n"))
         throw e
     } finally {
-      serverLog("finally exitServer")
-      exitServer()
+      serverLog("exiting server")
     }
   }
 
@@ -125,43 +176,11 @@ abstract class Server[T](
     pipedInput
   }
 
-  def interruptWithTimeout[T](close: () => Unit, t: () => T): Option[T] = {
-    @volatile var interrupt = true
-    @volatile var interrupted = false
-    val thread = new Thread(
-      () => {
-        try Thread.sleep(acceptTimeoutMillis)
-        catch {
-          case t: InterruptedException => /* Do Nothing */
-        }
-        if (interrupt) {
-          interrupted = true
-          serverLog(s"Interrupting after ${acceptTimeoutMillis}ms")
-          close()
-        }
-      },
-      "MillSocketTimeoutInterruptThread"
-    )
-
-    thread.start()
-    try {
-      val res =
-        try Some(t())
-        catch { case e: Throwable => None }
-
-      if (interrupted) None
-      else res
-
-    } finally {
-      interrupt = false
-      thread.interrupt()
-    }
-  }
-
   def handleRun(
       systemExit: Int => Nothing,
       clientSocket: Socket,
-      initialSystemProperties: Map[String, String]
+      initialSystemProperties: Map[String, String],
+      serverSocketClose: () => Unit
   ): Unit = {
     val currentOutErr = clientSocket.getOutputStream
     val writtenExitCode = AtomicBoolean()
@@ -277,7 +296,7 @@ abstract class Server[T](
 
       if (!idle) {
         serverLog("client interrupted while server was executing command")
-        exitServer()
+        serverSocketClose()
       }
 
       t.interrupt()

integration/feature/leak-hygiene/src/LeakHygieneTests.scala
@@ -23,7 +23,8 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
     assert(read == expected)
   }
 
-  def checkThreads(tester: IntegrationTester)(expected: String*) = {
+  def checkThreads(tester: IntegrationTester)(expected0: String*) = {
+    val expected = expected0.sorted
     val out = tester.eval(("show", "countThreads")).out
     val read = upickle.default.read[Seq[String]](out)
     // Filter out threads from the thread pool that runs tasks
@@ -31,6 +32,7 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
     val taskPoolPrefixOpt = read
       .find(_.startsWith("!execution-contexts-threadpool-"))
       .map(_.stripPrefix("!").split("-thread-").apply(0) + "-thread-")
+
     val filtered = read
       .filter {
         case s"coursier-pool-$_" => false
@@ -47,10 +49,11 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
         case s"Timer-$n" => "Timer"
         case s => s
       }
+      .sorted
 
     if (filtered != expected) {
-      pprint.log(expected.sorted)
-      pprint.log(filtered.sorted)
+      pprint.log(expected)
+      pprint.log(filtered)
     }
     assert(filtered == expected)
   }
@@ -68,10 +71,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
         checkThreads(tester)(
           "HandleRunThread",
           "MillServerActionRunner",
-          "MillSocketTimeoutInterruptThread",
+          "MillServerTimeoutThread",
           "Process ID Checker Thread",
-          "Tail",
-          "Tail",
+          "FileToStreamTailerThread",
+          "FileToStreamTailerThread",
           "main",
           "prompt-logger-stream-pumper-thread",
           "proxyInputStreamThroughPumper"
@@ -91,10 +94,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
           checkThreads(tester)(
             "HandleRunThread",
             "MillServerActionRunner",
-            "MillSocketTimeoutInterruptThread",
+            "MillServerTimeoutThread",
             "Process ID Checker Thread",
-            "Tail",
-            "Tail",
+            "FileToStreamTailerThread",
+            "FileToStreamTailerThread",
             "Timer",
             "main",
             "prompt-logger-stream-pumper-thread",
@@ -116,10 +119,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
           checkThreads(tester)(
             "HandleRunThread",
             "MillServerActionRunner",
-            "MillSocketTimeoutInterruptThread",
+            "MillServerTimeoutThread",
             "Process ID Checker Thread",
-            "Tail",
-            "Tail",
+            "FileToStreamTailerThread",
+            "FileToStreamTailerThread",
             "Timer",
             "main",
             "prompt-logger-stream-pumper-thread",
@@ -138,10 +141,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
         checkThreads(tester)(
           "HandleRunThread",
           "MillServerActionRunner",
-          "MillSocketTimeoutInterruptThread",
+          "MillServerTimeoutThread",
           "Process ID Checker Thread",
-          "Tail",
-          "Tail",
+          "FileToStreamTailerThread",
+          "FileToStreamTailerThread",
           "main",
           "prompt-logger-stream-pumper-thread",
           "proxyInputStreamThroughPumper"
@@ -160,10 +163,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
           checkThreads(tester)(
             "HandleRunThread",
             "MillServerActionRunner",
-            "MillSocketTimeoutInterruptThread",
+            "MillServerTimeoutThread",
             "Process ID Checker Thread",
-            "Tail",
-            "Tail",
+            "FileToStreamTailerThread",
+            "FileToStreamTailerThread",
             "Timer",
             "main",
             "prompt-logger-stream-pumper-thread",
@@ -185,10 +188,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
           checkThreads(tester)(
             "HandleRunThread",
             "MillServerActionRunner",
-            "MillSocketTimeoutInterruptThread",
+            "MillServerTimeoutThread",
             "Process ID Checker Thread",
-            "Tail",
-            "Tail",
+            "FileToStreamTailerThread",
+            "FileToStreamTailerThread",
             "Timer",
             "main",
             "prompt-logger-stream-pumper-thread",
@@ -212,10 +215,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
           checkThreads(tester)(
             "HandleRunThread",
             "MillServerActionRunner",
-            "MillSocketTimeoutInterruptThread",
+            "MillServerTimeoutThread",
             "Process ID Checker Thread",
-            "Tail",
-            "Tail",
+            "FileToStreamTailerThread",
+            "FileToStreamTailerThread",
             "Timer",
             "main",
             "prompt-logger-stream-pumper-thread",
@@ -236,10 +239,10 @@ object LeakHygieneTests extends UtestIntegrationTestSuite {
         checkThreads(tester)(
           "HandleRunThread",
           "MillServerActionRunner",
-          "MillSocketTimeoutInterruptThread",
+          "MillServerTimeoutThread",
           "Process ID Checker Thread",
-          "Tail",
-          "Tail",
+          "FileToStreamTailerThread",
+          "FileToStreamTailerThread",
           "Timer",
           "leaked thread",
           "main",

runner/server/test/src/mill/server/ClientServerTests.scala
@@ -21,7 +21,8 @@ object ClientServerTests extends TestSuite {
       override val processId: String,
       daemonDir: os.Path,
       locks: Locks,
-      testLogEvenWhenServerIdWrong: Boolean
+      testLogEvenWhenServerIdWrong: Boolean,
+      commandSleepMillis: Int = 0
   ) extends Server[Option[Int]](daemonDir, 1000, locks, testLogEvenWhenServerIdWrong)
       with Runnable {
 
@@ -29,10 +30,6 @@ object ClientServerTests extends TestSuite {
 
     override def out = os.temp.dir()
 
-    override def exitServer() = {
-      serverLog("exiting server")
-      super.exitServer()
-    }
     def stateCache0 = None
 
     override def serverLog0(s: String) = {
@@ -40,6 +37,11 @@ object ClientServerTests extends TestSuite {
       super.serverLog0(s)
     }
 
+    @volatile var runCompleted = false
+    override def run() = {
+      super.run()
+      runCompleted = true
+    }
     def main0(
         args: Array[String],
         stateCache: Option[Int],
@@ -51,29 +53,31 @@ object ClientServerTests extends TestSuite {
         initialSystemProperties: Map[String, String],
         systemExit: Int => Nothing
     ) = {
-
-      val reader = new BufferedReader(new InputStreamReader(streams.in))
-      val str = reader.readLine()
-      Thread.sleep(200)
-      if (args.nonEmpty) {
-        streams.out.println(str + args(0))
+      Thread.sleep(commandSleepMillis)
+      if (!runCompleted) {
+        val reader = new BufferedReader(new InputStreamReader(streams.in))
+        val str = reader.readLine()
+        Thread.sleep(200)
+        if (args.nonEmpty) {
+          streams.out.println(str + args(0))
+        }
+        env.toSeq.sortBy(_._1).foreach {
+          case (key, value) => streams.out.println(s"$key=$value")
+        }
+        systemProperties.toSeq.sortBy(_._1).foreach {
+          case (key, value) => streams.out.println(s"$key=$value")
+        }
+        if (args.nonEmpty) {
+          streams.err.println(str.toUpperCase + args(0))
+        }
+        streams.out.flush()
+        streams.err.flush()
       }
-      env.toSeq.sortBy(_._1).foreach {
-        case (key, value) => streams.out.println(s"$key=$value")
-      }
-      systemProperties.toSeq.sortBy(_._1).foreach {
-        case (key, value) => streams.out.println(s"$key=$value")
-      }
-      if (args.nonEmpty) {
-        streams.err.println(str.toUpperCase + args(0))
-      }
-      streams.out.flush()
-      streams.err.flush()
       (true, None)
     }
   }
 
-  class Tester(testLogEvenWhenServerIdWrong: Boolean) {
+  class Tester(testLogEvenWhenServerIdWrong: Boolean, commandSleepMillis: Int = 0) {
 
     var nextServerId: Int = 0
     val terminatedServers = collection.mutable.Set.empty[String]
@@ -108,7 +112,8 @@ object ClientServerTests extends TestSuite {
             processId,
             os.Path(daemonDir, os.pwd),
             locks,
-            testLogEvenWhenServerIdWrong
+            testLogEvenWhenServerIdWrong,
+            commandSleepMillis = commandSleepMillis
           )).start()
           null
         }
@@ -164,7 +169,7 @@ object ClientServerTests extends TestSuite {
         // Make sure the server times out of not used for a while
         Thread.sleep(2000)
 
-        assert(res2.logsFor("Interrupting after 1000ms") == Seq("server-0"))
+        assert(res2.logsFor("shutting down due inactivity") == Seq("server-0"))
         assert(res2.logsFor("exiting server") == Seq("server-0"))
 
         // Have a third client spawn/connect-to a new server at the same path
@@ -180,7 +185,7 @@ object ClientServerTests extends TestSuite {
         Thread.sleep(1000)
 
         assert(res3.logsFor("processId file missing") == Seq("server-1"))
-        assert(res3.logsFor("exiting server") == Seq("server-1", "server-1"))
+        assert(res3.logsFor("exiting server") == Seq("server-1"))
       }
     }
     test("dontLogWhenOutFolderDeleted") - retry(3) {
@@ -249,6 +254,17 @@ object ClientServerTests extends TestSuite {
             "server-0 client interrupted while server was executing command",
             "server-0 exiting server"
           )
+      )
+    }
+    test("longCommandNotInterrupted") {
+      // Make sure that when the command at 3000ms takes longer than the server
+      // timeout at 1000ms, the command still finishes running and the server doesn't
+      // shut down half way through
+      val tester = new Tester(testLogEvenWhenServerIdWrong = true, commandSleepMillis = 3000)
+      val res1 = tester(args = Array("world"))
+      assert(
+        res1.out == s"helloworld$ENDL",
+        res1.err == s"HELLOworld$ENDL"
       )
     }
 