73e4d8dd8bf415626590398250337e74e97cbeb3
Li Haoyi <haoyi.sg@gmail.com>
2025-07-04 11:47:57 +0800

    wip

--- mill-build/src/millbuild/MillStableScalaModule.scala
+++ mill-build/src/millbuild/MillStableScalaModule.scala
@@ -1,40 +1,40 @@
 package millbuild
+import com.github.lolgab.mill.mima._
+import mill._, scalalib._
 
 /** Publishable module which contains strictly handled API. */
-trait MillStableScalaModule extends MillPublishScalaModule /*with Mima*/ {
-  /*
-  import com.github.lolgab.mill.mima._
-//  override def mimaBinaryIssueFilters: T[Seq[ProblemFilter]] = Seq()
+trait MillStableScalaModule extends MillPublishScalaModule with Mima {
+
+
+  override def mimaBinaryIssueFilters: T[Seq[ProblemFilter]] = Seq.empty[ProblemFilter]
 
   def mimaPreviousVersions: T[Seq[String]] = Settings.mimaBaseVersions
 
   def mimaPreviousArtifacts: T[Seq[Dep]] = Task {
-    Agg.from(
-      Settings.mimaBaseVersions
-        .filter(v => !skipPreviousVersions().contains(v))
-        .map({ version =>
-          val patchedSuffix = {
-            val base = artifactSuffix()
-            version match {
-              case s"0.$minor.$_" if minor.toIntOption.exists(_ < 12) =>
-                base match {
-                  case "_3" => "_2.13"
-                  case s"_3_$suffix" => s"_2.13_$suffix"
-                  case _ => base
-                }
-              case _ => base
-            }
+    Settings.mimaBaseVersions
+      .filter(v => !skipPreviousVersions().contains(v))
+      .map({ version =>
+        val patchedSuffix = {
+          val base = artifactSuffix()
+          version match {
+            case s"0.$minor.$_" if minor.toIntOption.exists(_ < 12) =>
+              base match {
+                case "_3" => "_2.13"
+                case s"_3_$suffix" => s"_2.13_$suffix"
+                case _ => base
+              }
+            case _ => base
           }
-          val patchedId = artifactName() + patchedSuffix
-          mvn"${pomSettings().organization}:${patchedId}:${version}"
-        })
-    )
+        }
+        val patchedId = artifactName() + patchedSuffix
+        mvn"${pomSettings().organization}:${patchedId}:${version}"
+      })
   }
 
-  def mimaExcludeAnnotations = Seq("mill.api.internal"shared., "mill.api.experimental")
+  def mimaExcludeAnnotations = Seq("mill.api", "mill.api.experimental")
 //  def mimaCheckDirection = CheckDirection.Backward
   def skipPreviousVersions: T[Seq[String]] = Task {
     Task.log.info("Skipping mima for previous versions (!!1000s of errors due to Scala 3)")
     mimaPreviousVersions() // Task {Seq.empty[String]}
-  }*/
+  }
 }
