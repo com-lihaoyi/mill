aa1a84f3af28e2171f7887391086655a4ed2f0f8
Li Haoyi <haoyi.sg@gmail.com>
2025-07-07 19:14:34 +0800

    fixes

--- mill-build/src/millbuild/MillStableScalaModule.scala
+++ mill-build/src/millbuild/MillStableScalaModule.scala
@@ -10,23 +10,24 @@ trait MillStableScalaModule extends MillPublishScalaModule with Mima {
   def mimaPreviousVersions: T[Seq[String]] = Settings.mimaBaseVersions
 
   def mimaPreviousArtifacts: T[Seq[Dep]] = Task {
-    Settings.mimaBaseVersions
-      .map({ version =>
-        val patchedSuffix = {
-          val base = artifactSuffix()
-          version match {
-            case s"0.$minor.$_" if minor.toIntOption.exists(_ < 12) =>
-              base match {
-                case "_3" => "_2.13"
-                case s"_3_$suffix" => s"_2.13_$suffix"
-                case _ => base
-              }
-            case _ => base
-          }
-        }
-        val patchedId = artifactName() + patchedSuffix
-        mvn"${pomSettings().organization}:${patchedId}:${version}"
-      })
+//    Settings.mimaBaseVersions
+//      .map({ version =>
+//        val patchedSuffix = {
+//          val base = artifactSuffix()
+//          version match {
+//            case s"0.$minor.$_" if minor.toIntOption.exists(_ < 12) =>
+//              base match {
+//                case "_3" => "_2.13"
+//                case s"_3_$suffix" => s"_2.13_$suffix"
+//                case _ => base
+//              }
+//            case _ => base
+//          }
+//        }
+//        val patchedId = artifactName() + patchedSuffix
+//        mvn"${pomSettings().organization}:${patchedId}:${version}"
+//      })
+    Seq.empty[Dep]
   }
 
   def mimaExcludeAnnotations = Seq("mill.api.internal.internal", "mill.api.experimental")
