952115588dcaf88610c9c9e283adcba106a3f7a2
Li Haoyi <haoyi.sg@gmail.com>
2025-07-08 12:06:37 +0800

    fmt

---
+++ libs/util/src/mill/util/package.scala
@@ -0,0 +1,8 @@
+package mill
+
+/**
+ * Non-language-specific utility APIs provided by Mill for use in your
+ * `build.mill` and `package.mill`, in addition to the core APIs provided in
+ * the `mill.api` package
+ */
+package object util
