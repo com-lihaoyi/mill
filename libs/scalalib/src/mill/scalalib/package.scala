bd420875340a5840c758db13dd7b09c85dadbdda
Li Haoyi <haoyi.sg@gmail.com>
2025-07-08 21:09:16 +0800

    .

--- libs/scalalib/src/mill/scalalib/package.scala
+++ libs/scalalib/src/mill/scalalib/package.scala
@@ -5,13 +5,6 @@ package mill
  * Scala projects on the JVM. Scala.js and Scala-Native toolchains are in `mill.scalajslib`
  * and `mill.scalanativelib` respectively.
  */
-package object scalalib extends mill.scalalib.JsonFormatters {
+package object scalalib
 
-  implicit class DepSyntax(ctx: StringContext) extends AnyVal {
-    def mvn(args: Any*): Dep = mill.javalib.DepSyntax(ctx).mvn(args*)
 
-    @deprecated("Use `mvn` string interpolator instead.", "Mill 0.12.11")
-    def ivy(args: Any*): Dep = mill.javalib.DepSyntax(ctx).mvn(args*)
-  }
-
-}
