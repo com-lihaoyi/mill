653537f15d8e63d66429fd9313aa561c3acf97fc
Li Haoyi <haoyi.sg@gmail.com>
2025-07-08 10:35:46 +0800

    .

--- libs/init/src/mill/init/Giter8Module.scala
+++ libs/init/src/mill/init/Giter8Module.scala
@@ -8,11 +8,11 @@ import mill.scalalib._
 import mill.util.BuildInfo
 import mill.api.BuildCtx
 
-private[mill] object Giter8Module extends ExternalModule with Giter8Module {
+object Giter8Module extends ExternalModule with Giter8Module {
   lazy val millDiscover = Discover[this.type]
 }
 
-private[mill] trait Giter8Module extends CoursierModule {
+trait Giter8Module extends CoursierModule {
 
   def init(args: String*): Task.Command[Unit] = Task.Command {
     Task.log.info("Creating a new project...")
