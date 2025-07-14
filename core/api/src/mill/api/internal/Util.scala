aa9ad0fd8479dd89d563d8f31b34c18e2e203dd4
Li Haoyi <haoyi.sg@gmail.com>
2025-07-14 22:40:00 +0800

    .

--- core/api/src/mill/api/internal/Util.scala
+++ core/api/src/mill/api/internal/Util.scala
@@ -6,7 +6,7 @@ object Util {
     char.toString * (targetLength - s.length) + s
   }
 
-  def renderSecondsSuffix(millis: Long) = (millis / 1000).toInt match {
+  def renderSecondsSuffix(millis: Long, @com.lihaoyi.unroll dummy: Int = 0) = (millis / 1000).toInt match {
     case 0 => ""
     case n => s" ${n}s"
   }
