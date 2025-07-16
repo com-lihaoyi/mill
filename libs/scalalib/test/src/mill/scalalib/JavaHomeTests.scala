247c12c19caf59e49f831488c5cb4677757c20d7
Artūras Šlajus <x11@arturaz.net>
2025-07-16 14:34:16 +0300

    Refactor tests.

--- libs/scalalib/test/src/mill/scalalib/JavaHomeTests.scala
+++ libs/scalalib/test/src/mill/scalalib/JavaHomeTests.scala
@@ -1,5 +1,6 @@
 package mill.javalib
 
+import mill.api.daemon.ExecResult
 import mill.api.{Discover, Task}
 import mill.testkit.{TestRootModule, UnitTester}
 import mill.util.TokenReaders.*
@@ -36,19 +37,25 @@ object JavaHomeTests extends TestSuite {
   val resourcePath = os.Path(sys.env("MILL_TEST_RESOURCE_DIR")) / "hello-java"
 
   def tests: Tests = Tests {
-
     test("compileApis") {
       val resourcePathCompile = os.Path(sys.env("MILL_TEST_RESOURCE_DIR")) / "java-scala-11"
+
+      def captureOutput[A, R](module: TestRootModule, task: Task[A])(f:
+        (Either[ExecResult.Failing[A], UnitTester.Result[A]], String) => R
+      ): R = {
+        val errStream = new ByteArrayOutputStream()
+        UnitTester(module, resourcePathCompile, errStream = new PrintStream(errStream)).scoped { eval =>
+          val result = eval.apply(task)
+          val stderr = errStream.toString()
+          f(result, stderr)
+        }
+      }
+
       test("jdk11java") {
-        val baos = new ByteArrayOutputStream()
-        UnitTester(
-          JavaJdk11DoesntCompile,
-          resourcePathCompile,
-          errStream = new PrintStream(baos)
-        ).scoped { eval =>
-          val Left(_) = eval.apply(JavaJdk11DoesntCompile.javamodule.compile): @unchecked
-          assert(baos.toString.contains("cannot find symbol"))
-          assert(baos.toString.contains("method indent"))
+        captureOutput(JavaJdk11DoesntCompile, JavaJdk11DoesntCompile.javamodule.compile) { (result, stderr) =>
+          val Left(_) = result: @unchecked
+          assert(stderr.contains("cannot find symbol"))
+          assert(stderr.contains("method indent"))
         }
       }
 
@@ -58,13 +65,13 @@ object JavaHomeTests extends TestSuite {
         }
       }
 
-      // This doesn't work because Zinc doesn't apply the javaHome config to
-      // the Scala compiler JVM, which always runs in-memory https://github.com/sbt/zinc/discussions/1498
-//      test("jdk11scala"){
-//        val baos = new ByteArrayOutputStream()
-//        val eval = UnitTester(JavaJdk11DoesntCompile, resourcePathCompile)
-//        val Left(result) = eval.apply(JavaJdk11DoesntCompile.scalamodule.compile)
-//      }
+      test("jdk11scala") {
+        captureOutput(JavaJdk11DoesntCompile, JavaJdk11DoesntCompile.scalamodule.compile) { (result, stderr) =>
+          val Left(_) = result: @unchecked
+          assert(stderr.contains("cannot find symbol"))
+          assert(stderr.contains("method indent"))
+        }
+      }
 
       test("jdk17scala") {
         UnitTester(JavaJdk17Compiles, resourcePathCompile).scoped { eval =>
