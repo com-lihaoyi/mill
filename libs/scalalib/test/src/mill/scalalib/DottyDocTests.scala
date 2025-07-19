c8a2ab6ca96b87980efc45e96b96e085424faeb6
Tobias Roeser <le.petit.fou@web.de>
2025-07-19 09:01:30 +0200

    Split up `docJar` into javadoc/scaladoc generation and jar creation (#5565)
    `JavaModule`, `ScalaModule` and `KoltinModule` currently implement the
    `docJar` task to produce an Javadoc-compatible JAR ready for publishing
    and consumption by IDEs. Unfortunately, this JAR is rather unsuited for
    direct consumption by a browser, e.g. to read the API documentation
    directly. It either needs to be extracted (Mill doesn't provide any
    convenience task for that) or users can use the implementation detail
    like the `javadoc` directory in the `javaDoc.dest` directory, which is
    not documented and can change at any time.
    
    This PR splits the generation of the API documentation from the `docJar`
    packaging. As a result, users can use the result of `javadocGenerated`,
    `scalaDocGenerated` and `dokkaGenerated` tasks to directly access the
    exploded API documentation. The names were choosed to have the same
    prefix as the tools and other already existing configuration tasks, like
    `javadocOptions`, `scalaDocOptions` and `dokkaOptions`.
    
    As a neat side effect, in Kotlin or Scala modules, users can generate
    the pure Java API part with `javadocGenerated`.
    
    Since some documentation processors support advanced features like
    static site generation, the `scalaDocGenerated` task can later split up
    further, to improve the incremental build speed.
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/5565

--- libs/scalalib/test/src/mill/scalalib/DottyDocTests.scala
+++ libs/scalalib/test/src/mill/scalalib/DottyDocTests.scala
@@ -40,35 +40,39 @@ object DottyDocTests extends TestSuite {
   def tests: Tests = Tests {
     test("static") - UnitTester(StaticDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(StaticDocsModule.static.docJar): @unchecked
-      val dest = eval.outPath / "static/docJar.dest"
+      val scaladoc = eval.outPath / "static/scalaDocGenerated.dest"
+      val docJar = eval.outPath / "static/docJar.dest"
       assert(
-        os.exists(dest / "out.jar"), // final jar should exist
         // check if extra markdown files have been included and translated to html
-        os.exists(dest / "javadoc/_site/index.html"),
-        os.exists(dest / "javadoc/_site/nested/extra.html"),
+        os.exists(scaladoc / "javadoc/_site/index.html"),
+        os.exists(scaladoc / "javadoc/_site/nested/extra.html"),
         // also check that API docs have been generated
-        os.exists(dest / "javadoc/_site/api/pkg/SomeClass.html")
+        os.exists(scaladoc / "javadoc/_site/api/pkg/SomeClass.html"),
+        // final jar should exist
+        os.exists(docJar / "out.jar")
       )
     }
     test("empty") - UnitTester(EmptyDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(EmptyDocsModule.empty.docJar): @unchecked
-      val dest = eval.outPath / "empty/docJar.dest"
+      val scaladoc = eval.outPath / "empty/scalaDocGenerated.dest"
+      val docJar = eval.outPath / "empty/docJar.dest"
       assert(
-        os.exists(dest / "out.jar"),
-        os.exists(dest / "javadoc/_site/api/pkg/SomeClass.html")
+        os.exists(scaladoc / "javadoc/_site/api/pkg/SomeClass.html"),
+        os.exists(docJar / "out.jar")
       )
     }
     test("multiple") - UnitTester(MultiDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(MultiDocsModule.multidocs.docJar): @unchecked
-      val dest = eval.outPath / "multidocs/docJar.dest"
+      val scaladoc = eval.outPath / "multidocs/scalaDocGenerated.dest"
+      val docJar = eval.outPath / "multidocs/docJar.dest"
       assert(
-        os.exists(dest / "out.jar"), // final jar should exist
-        os.exists(dest / "javadoc/_site/api/pkg/SomeClass.html"),
-        os.exists(dest / "javadoc/_site/index.html"),
-        os.exists(dest / "javadoc/_site/nested/original.html"),
-        os.exists(dest / "javadoc/_site/nested/extra.html"),
+        os.exists(docJar / "out.jar"), // final jar should exist
+        os.exists(scaladoc / "javadoc/_site/api/pkg/SomeClass.html"),
+        os.exists(scaladoc / "javadoc/_site/index.html"),
+        os.exists(scaladoc / "javadoc/_site/nested/original.html"),
+        os.exists(scaladoc / "javadoc/_site/nested/extra.html"),
         // check that later doc sources overwrite earlier ones
-        os.read(dest / "javadoc/_site/index.html").contains("overwritten")
+        os.read(scaladoc / "javadoc/_site/index.html").contains("overwritten")
       )
     }
   }
--- libs/scalalib/test/src/mill/scalalib/ScalaDoc3Tests.scala
+++ libs/scalalib/test/src/mill/scalalib/ScalaDoc3Tests.scala
@@ -41,35 +41,38 @@ object ScalaDoc3Tests extends TestSuite {
   def tests: Tests = Tests {
     test("static") - UnitTester(StaticDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(StaticDocsModule.static.docJar): @unchecked
-      val dest = eval.outPath / "static/docJar.dest"
+      val docjar = eval.outPath / "static/docJar.dest"
+      val scaladoc = eval.outPath / "static/scalaDocGenerated.dest"
       assert(
-        os.exists(dest / "out.jar"), // final jar should exist
+        os.exists(docjar / "out.jar"), // final jar should exist
         // check if extra markdown files have been included and translated to html
-        os.exists(dest / "javadoc/index.html"),
-        os.exists(dest / "javadoc/nested/extra.html"),
+        os.exists(scaladoc / "javadoc/index.html"),
+        os.exists(scaladoc / "javadoc/nested/extra.html"),
         // also check that API docs have been generated
-        os.exists(dest / "javadoc/api/pkg/SomeClass.html")
+        os.exists(scaladoc / "javadoc/api/pkg/SomeClass.html")
       )
     }
     test("empty") - UnitTester(EmptyDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(EmptyDocsModule.empty.docJar): @unchecked
-      val dest = eval.outPath / "empty/docJar.dest"
+      val scaladoc = eval.outPath / "empty/scalaDocGenerated.dest"
+      val docJar = eval.outPath / "empty/docJar.dest"
       assert(
-        os.exists(dest / "out.jar"),
-        os.exists(dest / "javadoc/api/pkg/SomeClass.html")
+        os.exists(docJar / "out.jar"),
+        os.exists(scaladoc / "javadoc/api/pkg/SomeClass.html")
       )
     }
     test("multiple") - UnitTester(MultiDocsModule, resourcePath).scoped { eval =>
       val Right(_) = eval.apply(MultiDocsModule.multidocs.docJar): @unchecked
-      val dest = eval.outPath / "multidocs/docJar.dest"
+      val docJar = eval.outPath / "multidocs/docJar.dest"
+      val scaladoc = eval.outPath / "multidocs/scalaDocGenerated.dest"
       assert(
-        os.exists(dest / "out.jar"), // final jar should exist
-        os.exists(dest / "javadoc/api/pkg/SomeClass.html"),
-        os.exists(dest / "javadoc/index.html"),
-        os.exists(dest / "javadoc/docs/nested/original.html"),
-        os.exists(dest / "javadoc/docs/nested/extra.html"),
+        os.exists(docJar / "out.jar"), // final jar should exist
+        os.exists(scaladoc / "javadoc/api/pkg/SomeClass.html"),
+        os.exists(scaladoc / "javadoc/index.html"),
+        os.exists(scaladoc / "javadoc/docs/nested/original.html"),
+        os.exists(scaladoc / "javadoc/docs/nested/extra.html"),
         // check that later doc sources overwrite earlier ones
-        os.read(dest / "javadoc/index.html").contains("overwritten")
+        os.read(scaladoc / "javadoc/index.html").contains("overwritten")
       )
     }
   }
