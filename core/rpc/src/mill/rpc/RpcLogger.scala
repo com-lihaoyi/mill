f36c7627c3c497e22d5dc549ce05f955333541d5
Artūras Šlajus <x11@arturaz.net>
2025-07-21 16:31:34 +0300

    WIP

--- core/rpc/src/mill/rpc/RpcLogger.scala
+++ core/rpc/src/mill/rpc/RpcLogger.scala
@@ -4,13 +4,13 @@ import mill.api.daemon.Logger
 
 /** Logs messages by sending them from the server to the client via an RPC. */
 object RpcLogger {
-  sealed trait Message extends MillRpcMessage derives upickle.default.ReadWriter
+  sealed trait Message derives upickle.default.ReadWriter
   object Message {
-    case class Info(s: String) extends Message with MillRpcMessage.NoResponse
-    case class Debug(s: String) extends Message with MillRpcMessage.NoResponse
-    case class Warn(s: String) extends Message with MillRpcMessage.NoResponse
-    case class Error(s: String) extends Message with MillRpcMessage.NoResponse
-    case class Ticker(s: String) extends Message with MillRpcMessage.NoResponse
+    case class Info(s: String) extends Message
+    case class Debug(s: String) extends Message
+    case class Warn(s: String) extends Message
+    case class Error(s: String) extends Message
+    case class Ticker(s: String) extends Message
   }
 
   def create(send: Message => Unit): Logger.Actions = new {
