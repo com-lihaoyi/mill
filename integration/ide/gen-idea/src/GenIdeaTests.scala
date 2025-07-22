c22dc910f9202822aefad7aff2940167defd7e95
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2025-07-22 16:47:08 +0200

    Use system calls to get terminal size on Linux / Mac (0.12.x) (#4497)
    This makes Mill use my
    [native-terminal](https://github.com/alexarchambault/native-terminal)
    library (formerly windows-ansi) to get the terminal size. That way, Mill
    doesn't run `tput` commands every ~100 ms to query the terminal size.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Li Haoyi <haoyi.sg@gmail.com>

--- integration/ide/gen-idea/src/GenIdeaTests.scala
+++ integration/ide/gen-idea/src/GenIdeaTests.scala
@@ -2,7 +2,6 @@ package mill.integration
 
 import utest.{Tests, assert, _}
 
-import scala.util.Try
 import mill.testkit.UtestIntegrationTestSuite
 import GenIdeaUtils._
 import os.Path
@@ -12,7 +11,7 @@ object GenIdeaTests extends UtestIntegrationTestSuite {
   override def workspaceSourcePath: Path = super.workspaceSourcePath / "hello-idea"
 
   def tests: Tests = Tests {
-    test("helper assertPartialContentMatches works") - integrationTest { tester =>
+    test("helper partialContentMatches works") - integrationTest { tester =>
       val testContent =
         s"""line 1
            |line 2
@@ -20,32 +19,40 @@ object GenIdeaTests extends UtestIntegrationTestSuite {
            |line 4
            |""".stripMargin
 
-      assertPartialContentMatches(testContent, testContent)
+      assert(partialContentMatches(testContent, testContent))
       intercept[utest.AssertionError] {
-        assertPartialContentMatches(testContent, "line 1")
+        assert(partialContentMatches(testContent, "line 1"))
       }
-      assertPartialContentMatches(
-        found = testContent,
-        expected =
-          s"""line 1${ignoreString}line 4
-             |""".stripMargin
-      )
-      intercept[utest.AssertionError] {
-        assertPartialContentMatches(
+      assert(
+        partialContentMatches(
           found = testContent,
           expected =
-            s"""line 1${ignoreString}line 2${ignoreString}line 2${ignoreString}line 4
+            s"""line 1${ignoreString}line 4
                |""".stripMargin
         )
-      }
-      assertPartialContentMatches(
-        found = testContent,
-        expected = s"line 1${ignoreString}line 2$ignoreString"
       )
       intercept[utest.AssertionError] {
-        assertPartialContentMatches(
+        assert(
+          partialContentMatches(
+            found = testContent,
+            expected =
+              s"""line 1${ignoreString}line 2${ignoreString}line 2${ignoreString}line 4
+                 |""".stripMargin
+          )
+        )
+      }
+      assert(
+        partialContentMatches(
           found = testContent,
-          expected = s"line 1${ignoreString}line 2${ignoreString}line 2$ignoreString"
+          expected = s"line 1${ignoreString}line 2$ignoreString"
+        )
+      )
+      intercept[utest.AssertionError] {
+        assert(
+          partialContentMatches(
+            found = testContent,
+            expected = s"line 1${ignoreString}line 2${ignoreString}line 2$ignoreString"
+          )
         )
       }
       ()
@@ -53,20 +60,13 @@ object GenIdeaTests extends UtestIntegrationTestSuite {
 
     test("genIdeaTests") - integrationTest { tester =>
       import tester._
-      val expectedBase = workspacePath / "idea"
+      val expectedBase = tester.workspaceSourcePath / "idea"
       val resources = os.walk(expectedBase).filter(os.isFile).map(_.subRelativeTo(expectedBase))
 
       eval("mill.idea/")
 
-      val checks = resources.map { resource =>
-        Try {
-          assertIdeaXmlResourceMatchesFile(
-            workspacePath,
-            resource
-          )
-        }
-      }
-      assert(checks.forall(_.isSuccess))
+      for (resource <- resources)
+        assertIdeaXmlResourceMatchesFile(tester.workspaceSourcePath, workspacePath, resource)
     }
   }
 
