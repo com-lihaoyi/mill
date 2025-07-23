8d407ed261383dc8b688cffebb74818d9f6a9526
Li Haoyi <haoyi.sg@gmail.com>
2025-07-23 12:57:29 +0800

    .

--- runner/bsp/worker/src/mill/bsp/worker/MillScalaBuildServer.scala
+++ runner/bsp/worker/src/mill/bsp/worker/MillScalaBuildServer.scala
@@ -56,9 +56,26 @@ private trait MillScalaBuildServer extends ScalaBuildServer { this: MillBuildSer
 
   override def buildTargetScalaMainClasses(p: ScalaMainClassesParams)
       : CompletableFuture[ScalaMainClassesResult] =
-    CompletableFuture.completedFuture(
-      new ScalaMainClassesResult(new java.util.ArrayList())
-    )
+    handlerTasks(
+      targetIds = _ => p.getTargets.asScala.toSeq,
+      tasks = { case m: ScalaModuleApi => m.bspJavaModule().bspBuildTargetScalaMainClasses },
+      requestDescription = "Getting main classes of {}",
+      originId = p.getOriginId
+    ) {
+      case (_, _, id, _, res) =>
+        // We find all main classes, although we could also find only the configured one
+        val mainClasses = res.classes
+        // val mainMain = m.mainClass().orElse(if(mainClasses.size == 1) mainClasses.headOption else None)
+        val items = mainClasses.map { mc =>
+          val scalaMc = new ScalaMainClass(mc, Seq().asJava, res.forkArgs.asJava)
+          scalaMc.setEnvironmentVariables(res.forkEnv.map(e => s"${e._1}=${e._2}").toSeq.asJava)
+          scalaMc
+        }
+        new ScalaMainClassesItem(id, items.asJava)
+
+    } {
+      new ScalaMainClassesResult(_)
+    }
 
   override def buildTargetScalaTestClasses(p: ScalaTestClassesParams)
       : CompletableFuture[ScalaTestClassesResult] =
