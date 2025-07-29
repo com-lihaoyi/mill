e42a4005b978329d5950bdfee623cb0ec57f34d9
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-07-29 14:38:43 +0300

    Fix android install (#5623)
    Fixes caches not being invalidated for androidDex when source code
    changes
    
    ref: https://github.com/com-lihaoyi/mill/issues/5616
    
    ## Explanation
    
    - androidD8Dex was just returning the cli arguments with the paths as
    strings, thus while it was executed, androidDex was not, because the
    result from `androidD8Dex` was identical even after source changes.
    After adding the list of PathRef of the compiled classes the androidDex
    was executed on altering source files as in turn, compiled files were
    changed and detection is propagated by path ref hashing.
    - androidDex was forwarding the PathRef of `androidD8Dex` which was
    being created on an empty directory, thus the PathRef signature was
    identical. Recreating it gives a fresh hash.
    
    ## Fix demo
    
    
    https://github.com/user-attachments/assets/76a85669-c62b-43a7-85e4-5931f2f53b6d

--- libs/androidlib/src/mill/androidlib/AndroidR8AppModule.scala
+++ libs/androidlib/src/mill/androidlib/AndroidR8AppModule.scala
@@ -24,7 +24,7 @@ trait AndroidR8AppModule extends AndroidAppModule {
 
     os.call(dex.dexCliArgs)
 
-    dex.outPath
+    PathRef(dex.outPath.path)
 
   }
 
@@ -129,7 +129,8 @@ trait AndroidR8AppModule extends AndroidAppModule {
    * Prepares the R8 cli command to build this android app!
    * @return
    */
-  def androidR8Dex: T[(outPath: PathRef, dexCliArgs: Seq[String])] = Task {
+  private def androidR8Dex
+      : Task[(outPath: PathRef, dexCliArgs: Seq[String], appCompiledFiles: Seq[PathRef])] = Task {
     val destDir = Task.dest / "minify"
     os.makeDir.all(destDir)
 
@@ -154,16 +155,16 @@ trait AndroidR8AppModule extends AndroidAppModule {
          |""".stripMargin.trim
     os.write.over(extraRulesFile, extraRulesContent)
 
-    val classpathClassFiles: Seq[String] = androidPackagedClassfiles()
+    val classpathClassFiles: Seq[PathRef] = androidPackagedClassfiles()
       .filter(_.path.ext == "class")
-      .map(_.path.toString)
 
-    val appCompiledFiles: Seq[String] = androidPackagedCompiledClasses()
+    val appCompiledFiles: Seq[PathRef] = androidPackagedCompiledClasses()
       .filter(_.path.ext == "class")
-      .map(_.path.toString)
 
-    val allClassFiles =
-      classpathClassFiles ++ appCompiledFiles ++ androidPackagedDeps().map(_.path.toString)
+    val allClassFilesPathRefs =
+      classpathClassFiles ++ appCompiledFiles ++ androidPackagedDeps()
+
+    val allClassFiles = allClassFilesPathRefs.map(_.path.toString)
 
     val r8ArgsBuilder = Seq.newBuilder[String]
 
@@ -224,7 +225,7 @@ trait AndroidR8AppModule extends AndroidAppModule {
 
     val r8Args = r8ArgsBuilder.result()
 
-    PathRef(outputPath) -> r8Args
+    (PathRef(outputPath), r8Args, allClassFilesPathRefs)
   }
 
 }
