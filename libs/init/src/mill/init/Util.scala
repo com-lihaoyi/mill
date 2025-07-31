d34a25f55b37019a751438312d8cdfab392d84ca
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-08-01 00:44:39 +0530

    Updated failing example

--- libs/init/src/mill/init/Util.scala
+++ libs/init/src/mill/init/Util.scala
@@ -3,18 +3,19 @@ import mill.constants.OutFiles
 import mill.constants.CodeGenConstants.{nestedBuildFileNames, rootBuildFileNames}
 object Util {
 
+  def scalafmtConfigContent: String =
+    """version = "3.8.4"
+      |runner.dialect = scala213
+      |newlines.source=fold
+      |newlines.topLevelStatementBlankLines = [
+      |  {
+      |    blanks { before = 1 }
+      |  }
+      |]
+      |""".stripMargin
+
   def scalafmtConfigFile: os.Path =
-    os.temp(
-      """version = "3.8.4"
-        |runner.dialect = scala213
-        |newlines.source=fold
-        |newlines.topLevelStatementBlankLines = [
-        |  {
-        |    blanks { before = 1 }
-        |  }
-        |]
-        |""".stripMargin
-    )
+    os.temp(scalafmtConfigContent)
 
   def buildFiles(workspace: os.Path): geny.Generator[os.Path] =
     os.walk.stream(workspace, skip = (workspace / OutFiles.out).equals)
