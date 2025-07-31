a3bf48c5ee378cc216cf323ab2e6e6abd7288ebd
Li Haoyi <haoyi.sg@gmail.com>
2025-07-31 14:40:44 +0800

    .

--- libs/javalib/src/mill/javalib/TestModuleUtil.scala
+++ libs/javalib/src/mill/javalib/TestModuleUtil.scala
@@ -292,6 +292,8 @@ final class TestModuleUtil(
     }) {
       val allResultsOpt =
         try {
+          // Don't crash if the result.log file is malformed, just try again
+          // since that might happen transiently during a write
           Some(subprocessFutures.map(_._1 / "result.log").map(p =>
             upickle.default.read[(Long, Long)](os.read.stream(p))
           ))
