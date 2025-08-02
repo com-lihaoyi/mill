0d11d84ee32efbb7756ec33a4c19ee29d7c3b30b
Li Haoyi <haoyi.sg@gmail.com>
2025-08-02 22:38:55 +0800

    .

--- libs/tabcomplete/test/src/mill/tabcomplete/TabCompleteTests.scala
+++ libs/tabcomplete/test/src/mill/tabcomplete/TabCompleteTests.scala
@@ -39,189 +39,198 @@ object TabCompleteTests extends TestSuite {
         os.write(tester.evaluator.workspace / "file2.txt", "")
         tester.evaluator.evaluate(Seq("mill.tabcomplete.TabCompleteModule/complete") ++ s).get
       }
-      outStream.toString.linesIterator.toSeq
+      outStream.toString.linesIterator.toSet
     }
 
     test("empty-bash") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", ""),
-        List("bar", "foo", "qux", "task1")
+        Set("bar", "foo", "qux", "task1")
       )
     }
     test("empty-zsh") {
       assertGoldenLiteral(
         evalComplete("1", "./mill"),
-        List("bar", "foo", "qux", "task1")
+        Set("bar", "foo", "qux", "task1")
       )
     }
     test("task") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "t"),
-        List("task1")
+        Set("task1")
       )
     }
     test("firstTask") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "t", "bar.task2"),
-        List("task1")
+        Set("task1")
       )
     }
 
     test("secondNonTask") {
       assertGoldenLiteral(
         evalComplete("2", "./mill", "bar.task2", "f"),
-        List("file2.txt", "file1.txt")
+        Set("file2.txt", "file1.txt")
       )
     }
     test("secondNonTaskEmpty") {
       assertGoldenLiteral(
         evalComplete("2", "./mill", "bar.task2", ""),
-        List("file2.txt", "file1.txt", "out")
+        Set("file2.txt", "file1.txt", "out")
       )
     }
 
     test("module") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "fo"),
-        List("foo")
+        Set("foo")
       )
     }
 
     test("exactModule") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "bar"),
-        List("bar", "bar.task2")
+        Set("bar", "bar.task2")
       )
     }
 
     test("nested") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "bar."),
-        List("bar.task2")
+        Set("bar.task2")
       )
     }
 
     test("cross") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux["),
-        List("qux[12]", "qux[34]", "qux[56]")
+        Set("qux[12]", "qux[34]", "qux[56]")
       )
     }
 
     test("cross2") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux"),
-        List("qux", "qux[12]", "qux[34]", "qux[56]")
+        Set("qux", "qux[12]", "qux[34]", "qux[56]")
       )
     }
 
     test("crossPartial") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux[1"),
-        List("qux[12]")
+        Set("qux[12]")
       )
     }
 
     test("crossNested") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux[12]"),
-        List("qux[12].task3")
+        Set("qux[12].task3")
       )
     }
 
     test("crossNestedSlashed") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux\\[12\\]"),
-        List("qux[12].task3")
+        Set("qux[12].task3")
       )
     }
     test("crossNestedSingleQuoted") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "'qux[12]"),
-        List("qux[12].task3")
+        Set("qux[12].task3")
       )
     }
     test("crossNestedDoubleQuoted") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "\"qux[12]"),
-        List("qux[12].task3")
+        Set("qux[12].task3")
       )
     }
 
     test("crossComplete") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "qux[12].task3"),
-        List("qux[12].task3")
+        Set("qux[12].task3")
       )
 
     }
     test("shortflags") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "-"),
-        List("-v", "-b", "-d", "-k", "-D", "-j", "-i", "-w", "-h", "-s")
+        Set("-v", "-b", "-d", "-k", "-D", "-j", "-i", "-w", "-h", "-s")
       )
     }
     test("emptyAfterFlag") {
       assertGoldenLiteral(
         evalComplete("2", "./mill", "-v"),
-        List("bar", "foo", "qux", "task1")
+        Set("bar", "foo", "qux", "task1")
       )
 
     }
     test("filterAfterFlag") {
       assertGoldenLiteral(
         evalComplete("2", "./mill", "-v", "f"),
-        List("foo")
+        Set("foo")
       )
     }
     test("filterAfterFlagAfterTask") {
       assertGoldenLiteral(
         evalComplete("3", "./mill", "-v", "task1", "f"),
-        Seq("file2.txt", "file1.txt")
+        Set("file2.txt", "file1.txt")
       )
     }
     test("longflags") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "--"),
-        List(
-          "--no-daemon",
-          "--ticker",
-          "--keep-going",
-          "--interactive",
-          "--help",
-          "--help-advanced",
+        Set(
+          "--jobs",
           "--watch",
-          "--color",
-          "--meta-level",
-          "--allow-positional",
-          "--bsp",
-          "--bsp-install",
-          "--bsp-watch",
           "--no-build-lock",
-          "--no-wait-for-build-lock",
-          "--offline",
-          "--no-filesystem-checker",
-          "--tab-complete",
-          "--home",
+          "--interactive",
           "--repl",
-          "--no-server",
-          "--silent",
+          "--home",
+          "--no-wait-for-build-lock",
+          "--help",
+          "--color",
+          "--help-advanced",
+          "--tab-complete",
+          "--disable-ticker",
+          "--offline",
+          "--allow-positional",
+          "--ticker",
           "--disable-prompt",
+          "--no-server",
           "--enable-ticker",
-          "--disable-ticker"
+          "--no-filesystem-checker",
+          "--disable-callgraph",
+          "--keep-going",
+          "--bsp",
+          "--meta-level",
+          "--version",
+          "--no-daemon",
+          "--import",
+          "--bsp-install",
+          "--task",
+          "--notify-watch",
+          "--bsp-watch",
+          "--define",
+          "--bell",
+          "--silent",
+          "--debug"
         )
       )
     }
     test("longflagsfiltered") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "--h"),
-        List("--help", "--help-advanced", "--home")
+        Set("--help", "--help-advanced", "--home")
       )
     }
     test("longFlagBrokenEarlier") {
       assertGoldenLiteral(
         evalComplete("1", "./mill", "--jo", "1", "task1"),
-        List("--jobs")
+        Set("--jobs")
       )
     }
   }
