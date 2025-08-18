9491f1a36f90a750414821a71881de44590d5197
Artūras Šlajus <as@arturaz.net>
2025-08-18 13:31:34 +0300

    Fix Zinc worker performance regression (#5710)
    Fixes the delay when using Mill RPC infrastructure and sending large
    messages over it.
    
    The cause was PipedInput/OutputStream waiting for one second when it's
    internal buffer fills up. Because `ProxyStream` protocol used a very
    small (126 byte) buffer, things got slow very fast once a large message
    was sent.
    
    This PR removes the dependence on the piped streams and uses socket
    streams directly. It also ditches ProxyStream protocol for Mill RPC and
    instead sends heartbeats as empty lines.
    
    Other changes:
    - `Server` now is basic server, `ProxyStream` specific logic was moved
    to `ProxyStreamServer`.
    - A lot more logging is now done. You can find these logs in
    `out/mill/javalib/JvmWorkerModule/internalWorker.dest/jvm-worker.log`
    and
    `out/mill/javalib/JvmWorkerModule/internalWorker.dest/zinc-worker/DAEMON_HASH/daemon/server.log`
    respectively.
    - To aid debugging `DebuggingInputStream` and `DebuggingOutputStream`
    was created and left in the codebase if we'll need to debug something in
    the future.
    - `mill.util.Timed` was introduced as a utility to time code blocks.
    
    This has been tested manually:
    ```
    arturas@tuxedo-ibp14-gen9:~/work/mill-javalib-1-simple$ rm -rf out* in_* && time ../mill/mill-assembly.jar foo.compile
    [build.mill-59/64] compile
    [build.mill-59] [info] compiling 3 Scala sources to /home/arturas/work/mill-javalib-1-simple/out/mill-build/compile.dest/classes ...
    [build.mill-59] [info] done compiling
    [35/35] foo.compile
    [35] [info] compiling 1 Java source to /home/arturas/work/mill-javalib-1-simple/out/foo/compile.dest/classes ...
    [35] [info] done compiling
    [35/35] ============================== foo.compile ============================== 8s
    
    real    0m10,160s
    user    0m5,591s
    sys     0m0,614s
    arturas@tuxedo-ibp14-gen9:~/work/mill-javalib-1-simple$ time ../mill/mill-assembly.jar clean
    [1/1] clean
    [1/1] ============================== clean ==============================
    
    real    0m0,266s
    user    0m0,288s
    sys     0m0,071s
    arturas@tuxedo-ibp14-gen9:~/work/mill-javalib-1-simple$ time ../mill/mill-assembly.jar foo.compile
    [35/35] foo.compile
    [35] [info] compiling 1 Java source to /home/arturas/work/mill-javalib-1-simple/out/foo/compile.dest/classes ...
    [35] [info] done compiling
    [35/35] ============================== foo.compile ============================== 1s
    
    real    0m1,470s
    user    0m0,387s
    sys     0m0,109s
    ```
    
    The first run has to spawn a Zinc subprocess, so it takes 10s, but then
    the subsequent compilations are fast, as we expect them to be.
    
    ---
    
    Fixes https://github.com/com-lihaoyi/mill/issues/5693
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- libs/javalib/worker/src/mill/javalib/zinc/ZincWorkerMain.scala
+++ libs/javalib/worker/src/mill/javalib/zinc/ZincWorkerMain.scala
@@ -1,15 +1,14 @@
 package mill.javalib.zinc
 
-import mill.api.daemon.{DummyInputStream, SystemStreams}
 import mill.api.SystemStreamsUtils
+import mill.api.daemon.{DummyInputStream, SystemStreams}
 import mill.client.lock.Locks
 import mill.rpc.MillRpcWireTransport
 import mill.server.Server
 import pprint.{TPrint, TPrintColors}
 
-import java.io.{BufferedReader, InputStream, InputStreamReader, PrintStream}
+import java.io.{BufferedReader, InputStreamReader, PrintStream}
 import scala.util.Using
-import scala.util.control.NonFatal
 
 /** Entry point for the Zinc worker subprocess. */
 object ZincWorkerMain {
@@ -30,58 +29,84 @@ object ZincWorkerMain {
     }
   }
 
-  private class ZincWorkerTcpServer(daemonDir: os.Path) extends Server(
+  private class ZincWorkerTcpServer(daemonDir: os.Path) extends Server(Server.Args(
         daemonDir,
         // The worker kills the process when it needs to.
         acceptTimeout = None,
-        Locks.files(daemonDir.toString)
-      ) {
+        Locks.files(daemonDir.toString),
+        bufferSize = 4 * 1024
+      )) {
     private val className = summon[TPrint[ZincWorkerTcpServer]].render(using TPrintColors.Colors)
 
-    override protected type PreHandleConnectionData = Unit
+    protected class WriteSynchronizer
+
+    override protected type PreHandleConnectionData = WriteSynchronizer
 
     override protected def preHandleConnection(
-        socketInfo: Server.SocketInfo,
-        stdin: InputStream,
-        stdout: PrintStream,
-        stderr: PrintStream,
-        stopServer: Server.StopServer,
-        initialSystemProperties: Map[String, String]
-    ): PreHandleConnectionData = ()
+        connectionData: ConnectionData,
+        stopServer: Server.StopServer
+    ): WriteSynchronizer = new WriteSynchronizer
 
     override protected def handleConnection(
-        socketInfo: Server.SocketInfo,
-        stdin: InputStream,
-        stdout: PrintStream,
-        stderr: PrintStream,
+        connectionData: ConnectionData,
         stopServer: Server.StopServer,
         setIdle: Server.SetIdle,
-        initialSystemProperties: Map[String, String],
-        data: PreHandleConnectionData
-    ): Int = {
-      val serverName = s"$className{${socketInfo.remote} -> ${socketInfo.local}}"
-      Using.resource(BufferedReader(InputStreamReader(stdin))) { stdin =>
-        val transport = MillRpcWireTransport.ViaStreams(serverName, stdin, stdout)
-        try {
-          val server = ZincWorkerRpcServer(serverName, transport, setIdle, serverLog)
+        writeSynchronizer: WriteSynchronizer
+    ) = {
+      import connectionData.socketInfo
 
-          // Make sure stdout and stderr is sent to the client
-          SystemStreamsUtils.withStreams(SystemStreams(
-            out = PrintStream(server.clientStdout.asStream),
-            err = PrintStream(server.clientStderr.asStream),
-            in = DummyInputStream
-          )) {
-            serverLog("server.run() starting")
-            server.run()
-            serverLog("server.run() finished")
-            0
-          }
-        } catch {
-          case NonFatal(err) =>
-            serverLog(s"$socketInfo failed: $err")
-            1
+      val serverName = s"$className{${socketInfo.remote} -> ${socketInfo.local}}"
+      Using.Manager { use =>
+        val stdin = use(BufferedReader(InputStreamReader(connectionData.clientToServer)))
+        val stdout = use(PrintStream(connectionData.serverToClient))
+        val transport =
+          MillRpcWireTransport.ViaStreams(serverName, stdin, stdout, writeSynchronizer)
+        val server = ZincWorkerRpcServer(serverName, transport, setIdle, serverLog)
+
+        // Make sure stdout and stderr is sent to the client
+        SystemStreamsUtils.withStreams(SystemStreams(
+          out = PrintStream(server.clientStdout.asStream),
+          err = PrintStream(server.clientStderr.asStream),
+          in = DummyInputStream
+        )) {
+          serverLog("server.run() starting")
+          server.run()
+          serverLog("server.run() finished")
         }
+      }.get
+    }
+
+    override protected def onExceptionInHandleConnection(
+        connectionData: ConnectionData,
+        stopServer: Server.StopServer,
+        writeSynchronizer: WriteSynchronizer,
+        exception: Throwable
+    ): Unit = {}
+
+    override protected def beforeSocketClose(
+        connectionData: ConnectionData,
+        stopServer: Server.StopServer,
+        writeSynchronizer: WriteSynchronizer
+    ): Unit = {}
+
+    override protected def checkIfClientAlive(
+        connectionData: ConnectionData,
+        stopServer: Server.StopServer,
+        writeSynchronizer: WriteSynchronizer
+    ): Boolean = {
+      writeSynchronizer.synchronized {
+        connectionData.serverToClient.write('\n'.toInt)
+        connectionData.serverToClient.flush()
+        true
       }
     }
+
+    override protected def onStopServer(
+        from: String,
+        reason: String,
+        exitCode: Int,
+        connectionData: ConnectionData,
+        data: Option[PreHandleConnectionData]
+    ): Unit = {}
   }
 }
