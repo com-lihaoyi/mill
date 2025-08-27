56bae4b96dd2af9733733fcc33ea2d29ba58a72c
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-08-27 13:40:50 +0300

    KSP 2 support (#5727)
    ## Motivation: 
    
    https://github.com/com-lihaoyi/mill/issues/5676
    
    Also, language version 1.9 is deprecated since Kotlin 2.2.0. 
    
    It turns out, that integration wise, it's much easier to run the symbol
    processor with KSP 2 via a CLI wrapper that it is for KSP 1 as a kotlin
    compiler plugin
    
    
    ## Details
    
    The documentation for the CLI wrapper is
    [here](https://github.com/google/ksp/blob/main/docs/ksp2cmdline.md)
    albeit a bit outdated. The key component that is missing, is a reference
    to the `-libraries` argument, which (as our trial and error shows)
    expects the module classpath.
    
    Micronaut had also a tricky bit, failing to resolve java sdk sources.
    Passing the `-jdk-home` resolves that.
    
    From there on, Dagger, Hilt and micronaut work without any major
    adaptations from the user's api perspective as we made an effort to
    consolidate `KSPModule` and `KSP2Module` input parameters
    
    We have also retested the pokemon module outside of this repo which also
    works, surprisingly without needing the `-friends` parameter
    
    ## Summary
    
    This PR gives mill the capability to build KSP 2 with language version
    and api version >= 2.0 , bringing a solution to the deprecated language
    version <=1.9 needed by KSP 1.
    
    Also, added an equivalent Micronaut example as the hello-micronaut in
    javalib/web, but with full KSP, no annotation processors needed
    
    ## Credits
    
    This PR was co-authored by @souvlakias
    
    ---------
    
    Co-authored-by: souvlakias <stylianos@vaslabs.io>

---
+++ libs/kotlinlib/src/mill/kotlinlib/ksp/Ksp2Module.scala
@@ -0,0 +1,151 @@
+package mill.kotlinlib.ksp
+
+import mill.*
+import mill.api.Result
+import mill.api.{PathRef, Task}
+import mill.kotlinlib.{Dep, DepSyntax}
+import mill.util.Jvm
+
+import java.io.File
+
+/**
+ * Sets up the kotlin compiler for using KSP 2.x (Kotlin Symbol Processing)
+ * This module is based on KSP 2.x which supports language version 2.0 and later.
+ * For KSP 1.x, use [[KspModule]] instead.
+ *
+ *  Documentation: https://github.com/google/ksp/blob/main/docs/ksp2cmdline.md
+ */
+@mill.api.experimental
+trait Ksp2Module extends KspBaseModule { outer =>
+
+  /**
+   * The JVM target version for the KSP compilation step.
+   */
+  def kspJvmTarget: T[String]
+
+  /**
+   * The kotlin language version used for the KSP compilation stage.
+   * [[kspApiVersion]] must be less than or equal to this version.
+   * @return
+   */
+  def kspLanguageVersion: T[String] = kotlinLanguageVersion()
+
+  /**
+   * The kotlin api version used for the KSP compilation stage.
+   * @return
+   */
+  def kspApiVersion: T[String] = kotlinApiVersion()
+
+  /**
+   * The jars needed to run KSP 2 via `com.google.devtools.ksp.cmdline.KSPJvmMain` .
+   *
+   * The versions are computed from [[kotlinVersion]]-[[kspVersion]]
+   *
+   * For finding the right versions, also see [[https://github.com/google/ksp/releases]]
+   * For more info go to [[https://github.com/google/ksp/blob/main/docs/ksp2cmdline.md]]
+   */
+  def kspDeps: T[Seq[Dep]] = Task {
+    Seq(
+      mvn"com.google.devtools.ksp:symbol-processing-aa-embeddable:${kotlinVersion()}-${kspVersion()}",
+      mvn"com.google.devtools.ksp:symbol-processing-api:${kotlinVersion()}-${kspVersion()}",
+      mvn"com.google.devtools.ksp:symbol-processing-common-deps:${kotlinVersion()}-${kspVersion()}",
+      mvn"org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.10.2"
+    )
+  }
+
+  def kspDepsResolved: T[Seq[PathRef]] = Task {
+    defaultResolver().classpath(kspDeps(), resolutionParamsMapOpt = Some(addJvmVariantAttributes))
+  }
+
+  /**
+   * Any extra args passed to the KSP
+   * com.google.devtools.ksp.cmdline.KSPJvmMain
+   *
+   * For more info go to [[https://github.com/google/ksp/blob/main/docs/ksp2cmdline.md]]
+   * @return
+   */
+  def kspArgs: T[Seq[String]] = Task { Seq.empty[String] }
+
+  /**
+   * The Kotlin compile task with KSP.
+   * This task should run as part of the [[generatedSources]] task to
+   * so that the generated  sources are in the [[compileClasspath]]
+   * for the main compile task.
+   */
+  def generatedSourcesWithKSP: T[GeneratedKSPSources] = Task {
+
+    val processorResolvedClasspath = kotlinSymbolProcessorsResolved().map(_.path)
+    val processorClasspath = processorResolvedClasspath.mkString(File.pathSeparator)
+
+    val kspOutputDir = Task.dest / "generated"
+    val java = kspOutputDir / "java"
+    val kotlin = kspOutputDir / "kotlin"
+    val resources = kspOutputDir / "resources"
+    val classes = kspOutputDir / "classes"
+    val kspCachesDir = Task.dest / "caches"
+
+    val processorOptionsValue =
+      kspProcessorOptions().map((key, value) => s"$key=$value").toSeq.mkString(File.pathSeparator)
+
+    val processorOptions = if (processorOptionsValue.isEmpty)
+      ""
+    else
+      s"-processor-options=${processorOptionsValue}"
+    val args = Seq(
+      s"-module-name=${kspModuleName}",
+      "-jvm-target",
+      kspJvmTarget(),
+      s"-jdk-home=${System.getProperty("java.home")}",
+      s"-source-roots=${sources().map(_.path).mkString(File.pathSeparator)}",
+      s"-project-base-dir=${moduleDir.toString}",
+      s"-output-base-dir=${kspOutputDir}",
+      s"-caches-dir=${kspCachesDir}",
+      s"-libraries=${kspClasspath().map(_.path).mkString(File.pathSeparator)}",
+      s"-class-output-dir=${classes}",
+      s"-kotlin-output-dir=${kotlin}",
+      s"-java-output-dir=${java}",
+      s"-resource-output-dir=${resources}",
+      s"-language-version=${kspLanguageVersion()}",
+      s"-incremental=true",
+      s"-incremental-log=true",
+      s"-api-version=${kspApiVersion()}",
+      processorOptions,
+      s"-map-annotation-arguments-in-java=false"
+    ) ++ kspArgs() :+ processorClasspath
+
+    val kspJvmMainClasspath = kspDepsResolved().map(_.path)
+    val mainClass = "com.google.devtools.ksp.cmdline.KSPJvmMain"
+    Task.log.debug(
+      s"Running Kotlin Symbol Processing with java -cp ${kspJvmMainClasspath.mkString(File.pathSeparator)} ${mainClass} ${args.mkString(" ")}"
+    )
+
+    val jvmCall = Jvm.callProcess(
+      mainClass = mainClass,
+      classPath = kspJvmMainClasspath,
+      mainArgs = args
+    )
+
+    Task.log.info(
+      s"KSP finished with exit code: ${jvmCall.exitCode}"
+    )
+
+    Task.log.info(
+      s"KSP output: ${jvmCall.out.text()}"
+    )
+
+    GeneratedKSPSources(PathRef(java), PathRef(kotlin), PathRef(resources), PathRef(classes))
+
+  }
+
+  /**
+   * A test sub-module linked to its parent module best suited for unit-tests.
+   */
+  trait Ksp2Tests extends Ksp2Module with KotlinTests {
+    override def kspVersion: T[String] = outer.kspVersion()
+    override def kspJvmTarget: T[String] = outer.kspJvmTarget()
+    override def kotlinVersion: T[String] = outer.kotlinVersion()
+    override def kspLanguageVersion: T[String] = outer.kspLanguageVersion()
+
+    override def kspApiVersion: T[String] = outer.kspApiVersion()
+  }
+}
