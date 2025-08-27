e4ef3587dee278dacd924072c3a0962f9be20012
Tobias Roeser <le.petit.fou@web.de>
2025-08-27 11:48:27 +0200

    Implement support for Scala 3.8 (#5751)
    Instead of using a scala-library-2.13.x, since Scala 3.8.0 the used
    scala-library has the same version as the Scala version.
    
    Fix https://github.com/com-lihaoyi/mill/issues/5739
    
    Co-authored-by: Natsu Kagami <nki@nkagami.me>

--- libs/scalalib/test/src/mill/scalalib/ScalaDotty213Tests.scala
+++ libs/scalalib/test/src/mill/scalalib/ScalaDotty213Tests.scala
@@ -9,8 +9,9 @@ object ScalaDotty213Tests extends TestSuite {
   object Dotty213 extends TestRootModule {
     object foo extends ScalaModule {
       def scalaVersion = "0.18.1-RC1"
-      override def mvnDeps =
-        Seq(mvn"org.scala-lang.modules::scala-xml:1.2.0".withDottyCompat(scalaVersion()))
+      override def mvnDeps = Seq(
+        mvn"org.scala-lang.modules::scala-xml:1.2.0".withDottyCompat(scalaVersion())
+      )
     }
 
     lazy val millDiscover = Discover[this.type]
