fd16f6ac7c4cf95afcdcdca5abb6817074c9b89a
Tobias Roeser <le.petit.fou@web.de>
2025-08-28 14:28:46 +0200

    Add missing experimental annotations

--- libs/kotlinlib/src/mill/kotlinlib/ksp/GeneratedKspSources.scala
+++ libs/kotlinlib/src/mill/kotlinlib/ksp/GeneratedKspSources.scala
@@ -3,6 +3,7 @@ package mill.kotlinlib.ksp
 import mill.api.PathRef
 import upickle.default.ReadWriter
 
+@mill.api.experimental
 case class GeneratedKspSources(
     java: PathRef,
     kotlin: PathRef,
--- libs/kotlinlib/src/mill/kotlinlib/ksp/KspModuleMode.scala
+++ libs/kotlinlib/src/mill/kotlinlib/ksp/KspModuleMode.scala
@@ -1,5 +1,6 @@
 package mill.kotlinlib.ksp
 
+@mill.api.experimental
 enum KspModuleMode {
   case Ksp1 // in program
 //  case Ksp2 // In program
