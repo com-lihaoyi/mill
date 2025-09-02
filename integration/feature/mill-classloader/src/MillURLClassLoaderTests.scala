e0e008fbfebea0a74f8f795e8f3e39c94a917208
Tobias Roeser <le.petit.fou@web.de>
2025-09-02 09:55:35 +0200

    Add integration test

---
+++ integration/feature/mill-classloader/src/MillURLClassLoaderTests.scala
@@ -0,0 +1,17 @@
+package mill.integration
+
+import mill.testkit.UtestIntegrationTestSuite
+import utest.*
+
+class MillURLClassLoaderTests extends UtestIntegrationTestSuite {
+
+  val tests: Tests = Tests {
+    test("test") - integrationTest { tester =>
+      val result1 = tester.eval(("show" , "graphDependencies"))
+      // The expected output shows, that we could successfully load the `scala.meta` package
+      assert(result1.isSuccess)
+      assert(result1.out.contains(""""object Main extends App { print(\"Hello!\") }""""))
+
+    }
+  }
+}
