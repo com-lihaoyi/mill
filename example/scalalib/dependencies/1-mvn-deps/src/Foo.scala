424135c71abaf0afad327b9884ce48e2d2a1818e
Li Haoyi <haoyi.sg@gmail.com>
2025-09-02 09:33:09 +0800

    merge

--- example/scalalib/dependencies/1-mvn-deps/src/Foo.scala
+++ example/scalalib/dependencies/1-mvn-deps/src/Foo.scala
@@ -3,6 +3,6 @@ package foo
 object Foo {
   def main(args: Array[String]): Unit = {
     println("pretty-printed using PPrint: " + pprint.PPrinter.BlackWhite.apply(args))
-    println("serialized using uPickle: " + upickle.write(args))
+    println("serialized using uPickle: " + upickle.default.write(args))
   }
 }
