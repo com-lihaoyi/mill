4add373756af0a53362ded6e42bbd33fddef2855
Alex Archambault <alexandre.archambault@gmail.com>
2025-09-03 16:16:44 +0200

    Tweak IT, add one more and ensure all fails on main

--- integration/feature/cs-env-vars/src/CsEnvVarsTests.scala
+++ integration/feature/cs-env-vars/src/CsEnvVarsTests.scala
@@ -1,5 +1,6 @@
 package mill.integration
 
+import coursier.cache.FileCache
 import mill.testkit.UtestIntegrationTestSuite
 import utest._
 
@@ -7,24 +8,67 @@ import java.io.File
 
 object CsEnvVarsTests extends UtestIntegrationTestSuite {
   val tests: Tests = Tests {
-    test("env vars") - integrationTest { tester =>
+    test("cache") - integrationTest { tester =>
       import tester._
 
-      def check(cache: os.Path): Unit = {
+      def check(cacheOpt: Option[os.Path]): Unit = {
+        val env = cacheOpt.toSeq.map { cache =>
+          "COURSIER_CACHE" -> cache.toString
+        }
         val res = eval(
           ("printRunClasspath"),
-          env = Map("COURSIER_CACHE" -> cache.toString)
+          env = env.toMap
         )
         assert(res.exitCode == 0)
 
-        pprint.err.log(res.out)
-        val cp = res.out.split(File.pathSeparator).map(os.Path(_))
-        assert(cp.exists(p => p.startsWith(cache) && p.last.startsWith("slf4j-api-")))
+        val cp = res.out.split(File.pathSeparator).filter(_.nonEmpty).map(os.Path(_))
+
+        val actualCache = cacheOpt.getOrElse(os.Path(FileCache().location))
+        assert(cp.exists(p => p.startsWith(actualCache) && p.last.startsWith("slf4j-api-")))
       }
 
-      check(workspacePath / "cache-0")
-      check(workspacePath / "cache-1")
-      check(workspacePath / "cache-2")
+      check(None)
+      check(Some(workspacePath / "cache-0"))
+      check(Some(workspacePath / "cache-1"))
+    }
+
+    test("mirrors") - integrationTest { tester =>
+      import tester._
+
+      def check(mirrorFileOpt: Option[os.Path]): Unit = {
+        val env = mirrorFileOpt.toSeq.map { file =>
+          "COURSIER_MIRRORS" -> file.toString
+        }
+        val res = eval(
+          ("printRunClasspath"),
+          env = env.toMap
+        )
+        assert(res.exitCode == 0)
+
+        val cacheRoot = os.Path(FileCache().location)
+        val cp = res.out.split(File.pathSeparator)
+          .filter(_.nonEmpty)
+          .map(os.Path(_))
+          .filter(_.startsWith(cacheRoot))
+          .map(_.relativeTo(cacheRoot).asSubPath)
+
+        val centralReplaced = cp.forall { f =>
+          f.startsWith(os.sub / "https/repo.maven.apache.org/maven2")
+        }
+        assert(cp.exists(f => f.last.startsWith("scala-library-") && f.last.endsWith(".jar")))
+        assert(mirrorFileOpt.nonEmpty == centralReplaced)
+      }
+
+      check(None)
+
+      val mirrorFile = workspacePath / "mirror.properties"
+      os.write(
+        mirrorFile,
+        """central.from=https://repo1.maven.org/maven2
+          |central.to=https://repo.maven.apache.org/maven2/
+          |""".stripMargin
+      )
+      check(Some(mirrorFile))
     }
   }
 }
