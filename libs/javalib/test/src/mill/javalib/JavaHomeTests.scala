8c28f2a0046f3ef5d33c305ce0b62cc36957fe51
Li Haoyi <haoyi.sg@gmail.com>
2025-09-16 16:17:34 +0800

    .

--- libs/javalib/test/src/mill/javalib/JavaHomeTests.scala
+++ libs/javalib/test/src/mill/javalib/JavaHomeTests.scala
@@ -6,100 +6,55 @@ import mill.testkit.{TestRootModule, UnitTester}
 import mill.util.TokenReaders.*
 import utest.*
 
-object JavaHomeTests extends TestSuite {
+object JavaHome11Tests extends JavaHomeTests("temurin:11.0.24", "11.", Seq[Byte](0, 0, 0, 55))
+object JavaHome17Tests extends JavaHomeTests("temurin:17.0.9", "17.", Seq[Byte](0, 0, 0, 61))
+trait JavaHomeTests(jvmId0: String, expectedPrefix: String, expectedBytes: Seq[Byte]) extends TestSuite {
 
   object HelloJavaJavaHome11Override extends TestRootModule {
     object core extends JavaModule {
-      def jvmId = "temurin:11.0.24"
+      def jvmId = jvmId0
       override def docJarUseArgsFile = false
       object test extends JavaTests with TestModule.Junit4
     }
 
-    lazy val millDiscover = Discover[this.type]
-  }
-
-  object HelloJavaJavaHome17Override extends TestRootModule {
-
-    object core extends JavaModule {
-      def jvmId = "temurin:17.0.9"
-      override def docJarUseArgsFile = false
-      object test extends JavaTests with TestModule.Junit4
-    }
     lazy val millDiscover = Discover[this.type]
   }
 
   val resourcePath = os.Path(sys.env("MILL_TEST_RESOURCE_DIR")) / "hello-java"
 
   def tests: Tests = Tests {
-
     test("javaHome") {
-      test("jvm11") {
-        UnitTester(HelloJavaJavaHome11Override, resourcePath).scoped { eval =>
+      UnitTester(HelloJavaJavaHome11Override, resourcePath).scoped { eval =>
 
-          val Right(result) = eval.apply(HelloJavaJavaHome11Override.core.compile): @unchecked
+        val Right(result) = eval.apply(HelloJavaJavaHome11Override.core.compile): @unchecked
 
-          val coreClassFile = os.walk(result.value.classes.path).find(_.last == "Core.class")
+        val coreClassFile = os.walk(result.value.classes.path).find(_.last == "Core.class")
 
-          assert(
-            coreClassFile.isDefined,
+        assert(
+          coreClassFile.isDefined,
 
-            // The first eight bytes are magic numbers followed by two bytes for major version and two bytes for minor version
-            // We are overriding to java 11 which corresponds to class file version 55
-            os.read.bytes(coreClassFile.get, 4, 4).toSeq == Seq[Byte](0, 0, 0, 55)
-          )
+          // The first eight bytes are magic numbers followed by two bytes for major version and two bytes for minor version
+          // We are overriding to java 11 which corresponds to class file version 55
+          os.read.bytes(coreClassFile.get, 4, 4).toSeq == expectedBytes
+        )
 
-          val path = eval.evaluator.workspace / "java.version"
-          val Right(_) =
-            eval.apply(HelloJavaJavaHome11Override.core.run(Task.Anon(Args(path)))): @unchecked
+        val path = eval.evaluator.workspace / "java.version"
+        val Right(_) =
+          eval.apply(HelloJavaJavaHome11Override.core.run(Task.Anon(Args(path)))): @unchecked
 
-          assert(
-            os.read(path).startsWith("11.")
-          )
+        assert(
+          os.read(path).startsWith(expectedPrefix)
+        )
 
-          val Left(ExecResult.Failure(_)) =
-            eval.apply(HelloJavaJavaHome11Override.core.test.testForked()): @unchecked
+        val Left(ExecResult.Failure(_)) =
+          eval.apply(HelloJavaJavaHome11Override.core.test.testForked()): @unchecked
 
-          //        assert(
-          //          v1._2(0).fullyQualifiedName == "hello.MyCoreTests.java11Test",
-          //          v1._2(0).status == "Success",
-          //          v1._2(1).fullyQualifiedName == "hello.MyCoreTests.java17Test",
-          //          v1._2(1).status == "Failure"
-          //        )
-        }
-      }
-      test("jvm17") {
-        UnitTester(HelloJavaJavaHome17Override, resourcePath).scoped { eval =>
-
-          val Right(result) = eval.apply(HelloJavaJavaHome17Override.core.compile): @unchecked
-
-          val coreClassFile = os.walk(result.value.classes.path).find(_.last == "Core.class")
-
-          assert(
-            coreClassFile.isDefined,
-
-            // The first eight bytes are magic numbers followed by two bytes for major version and two bytes for minor version
-            // We are overriding to java 17 which corresponds to class file version 67
-            os.read.bytes(coreClassFile.get, 4, 4).toSeq == Seq[Byte](0, 0, 0, 61)
-          )
-
-          val path = eval.evaluator.workspace / "java.version"
-          val Right(_) =
-            eval.apply(HelloJavaJavaHome17Override.core.run(Task.Anon(Args(path)))): @unchecked
-
-          assert(
-            os.read(path).startsWith("17.")
-          )
-
-          val Left(ExecResult.Failure(_)) =
-            eval.apply(HelloJavaJavaHome17Override.core.test.testForked()): @unchecked
-
-          //        assert(
-          //          v1._2(0).fullyQualifiedName == "hello.MyCoreTests.java11Test",
-          //          v1._2(0).status == "Failure",
-          //          v1._2(1).fullyQualifiedName == "hello.MyCoreTests.java17Test",
-          //          v1._2(1).status == "Success"
-          //        )
-        }
+        //        assert(
+        //          v1._2(0).fullyQualifiedName == "hello.MyCoreTests.java11Test",
+        //          v1._2(0).status == "Success",
+        //          v1._2(1).fullyQualifiedName == "hello.MyCoreTests.java17Test",
+        //          v1._2(1).status == "Failure"
+        //        )
       }
     }
   }
