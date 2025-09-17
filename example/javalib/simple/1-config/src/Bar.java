04324ceb3d03740cf88186002cb822cf9088e561
Li Haoyi <haoyi.sg@gmail.com>
2025-09-17 13:01:04 +0800

    config module non script

---
+++ example/javalib/simple/1-config/src/Bar.java
@@ -0,0 +1,6 @@
+
+public class Bar {
+  public static void main(String[] args) {
+    System.out.println("Hello Graal Native: " + System.getProperty("java.version"));
+  }
+}
---
+++ example/scalalib/simple/1-config/src/Bar.scala
@@ -0,0 +1,6 @@
+
+object Bar {
+  def main(args: Array[String]): Unit = {
+    println("Hello Graal Native: " + System.getProperty("java.version"))
+  }
+}
