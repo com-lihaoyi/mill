2d9d73c7c230d6e73787d1123971e621e6bf0f8e
Li Haoyi <haoyi.sg@gmail.com>
2025-09-17 13:40:49 +0800

    simple/2-module-deps

---
+++ example/javalib/simple/2-module-deps/bar/test/src/bar/BarTests.java
@@ -0,0 +1,20 @@
+package bar;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BarTests {
+
+  @Test
+  public void simple() {
+    String result = Bar.generateHtml("hello");
+    assertEquals("<h1>hello</h1>", result);
+  }
+
+  @Test
+  public void escaping() {
+    String result = Bar.generateHtml("<hello>");
+    assertEquals("<h1>&lt;hello&gt;</h1>", result);
+  }
+}
