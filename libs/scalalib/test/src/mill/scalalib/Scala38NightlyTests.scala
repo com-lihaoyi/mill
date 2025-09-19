9b9354249ceca13e1547c3ecb2bf5a74ed938761
Tobias Roeser <le.petit.fou@web.de>
2025-09-19 07:47:50 +0200

    Maintain a list of known coursier repositories under CoursierModule

--- libs/scalalib/test/src/mill/scalalib/Scala38NightlyTests.scala
+++ libs/scalalib/test/src/mill/scalalib/Scala38NightlyTests.scala
@@ -8,15 +8,13 @@ import utest.*
 // TODO: Once Scala 3.8.0 is out, we can change this test to use 3.8.0 and remove the extra repo
 object Scala38NightlyTests extends TestSuite {
 
-  val repo = "https://repo.scala-lang.org/artifactory/maven-nightlies"
-
   object Scala38Nightly extends TestRootModule {
     object JvmWorker extends JvmWorkerModule {
-      override def repositories = super.repositories() ++ Seq(repo)
+      override def repositories = super.repositories() ++ Seq(CoursierModule.KnownRepositories.ScalaLangNightlies)
     }
     object foo extends ScalaModule {
       override def jvmWorker: ModuleRef[JvmWorkerModule] = ModuleRef(JvmWorker)
-      override def repositories = super.repositories() ++ Seq(repo)
+      override def repositories = super.repositories() ++ Seq(CoursierModule.KnownRepositories.ScalaLangNightlies)
       override def scalaVersion = "3.8.0-RC1-bin-20250825-ee2f641-NIGHTLY"
       override def mvnDeps = Seq(
         mvn"org.scala-lang.modules::scala-xml:2.4.0"
