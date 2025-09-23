bc989f81567f9d8651c623bcbe7854b185de8f70
Tobias Roeser <le.petit.fou@web.de>
2025-09-23 21:50:02 +0200

    Update changelog and documentation for release 0.12.16 (#5905)
    Pull request: https://github.com/com-lihaoyi/mill/pull/5905

--- mill-build/src/millbuild/Settings.scala
+++ mill-build/src/millbuild/Settings.scala
@@ -18,7 +18,7 @@ object Settings {
   )
   val docTags: Seq[String] = Seq(
     "0.11.13",
-    "0.12.14",
+    "0.12.16",
     "1.0.5"
   )
   val mimaBaseVersions: Seq[String] = Seq("1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5")
