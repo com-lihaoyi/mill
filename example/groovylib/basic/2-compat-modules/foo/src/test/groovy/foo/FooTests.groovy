13c7ec9be8a4a3d35cca55e97b5effd7d3373636
Marc Schlegel <marc.schlegel@gmx.de>
2025-09-23 15:43:35 +0200

    samples

---
+++ example/groovylib/basic/2-compat-modules/foo/src/test/groovy/foo/FooTests.groovy
@@ -0,0 +1,13 @@
+package foo
+
+import org.junit.jupiter.api.Test
+
+class FooTest {
+
+    @Test
+    void "hello should print correct greeting"() {
+        // Groovy creates an implicit class for the script named after the file
+        def foo = new Foo()
+        assert foo.hello() == "Hello World, Earth"
+    }
+}
---
+++ example/groovylib/testing/2-integration-suite/qux/src/qux/Qux.groovy
@@ -0,0 +1,11 @@
+package qux
+
+class Qux {
+    static void main(String[] args){
+        println(hello())
+    }
+
+    String hello(){
+        "Hello World"
+    }
+}
