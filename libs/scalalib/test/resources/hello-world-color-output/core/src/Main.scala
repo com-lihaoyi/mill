eaac0f9631b0bf46834d252b1221026dc8567372
Tobias Roeser <le.petit.fou@web.de>
2025-10-01 10:17:07 +0200

    Fix test

--- libs/scalalib/test/resources/hello-world-color-output/core/src/Main.scala
+++ libs/scalalib/test/resources/hello-world-color-output/core/src/Main.scala
@@ -3,10 +3,10 @@ package example
 trait Show[A]
 
 object Show {
-  implicit def option[A](using s: Show[A]): Show[Option[A]] = ???
+  implicit def option[A](implicit s: Show[A]): Show[Option[A]] = ???
 }
 
 object Example {
   def main(args: Array[String]): Unit =
-    println(summon[Show[Option[String]]])
+    println(implicitly[Show[Option[String]]])
 }
