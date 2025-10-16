faa4b3f1f4e136839af3dae28e2c02da7a475ebf
Li Haoyi <haoyi.sg@gmail.com>
2025-10-17 00:15:13 +0800

    Fixes for `--jshell` (#5989)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- runner/launcher/src/mill/launcher/MillLauncherMain.java
+++ runner/launcher/src/mill/launcher/MillLauncherMain.java
@@ -27,8 +27,8 @@ public class MillLauncherMain {
   public static void main(String[] args) throws Exception {
     var needParsedConfig = Arrays.stream(args)
         .anyMatch(f -> f.startsWith("-") && !f.startsWith("--") && f.contains("i"));
-    for (var token :
-        Arrays.asList("--interactive", "--no-server", "--no-daemon", "--repl", "--bsp", "--help")) {
+    for (var token : Arrays.asList(
+        "--interactive", "--no-server", "--no-daemon", "--jshell", "--repl", "--bsp", "--help")) {
       if (Arrays.stream(args).anyMatch(f -> f.equals(token))) needParsedConfig = true;
     }
 
