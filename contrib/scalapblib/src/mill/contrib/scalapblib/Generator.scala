b64875f84a4e4a20c9c9ed129b26f19842087880
Tobias Roeser <le.petit.fou@web.de>
2025-10-18 14:03:48 +0200

    Include contrib modules in API docs and improve `scalapblib` docs (#6001)
    We now include all contrib modules in our API documentation. This
    removes the need to document the available configuration task in two
    places, the code and the readme files. For convenience, contrib readmes
    should point to their API docs. I already did this for `scalapblib`, to
    test it out. If it works reasonable, we can rework the other plugins as
    well.
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/6001

--- contrib/scalapblib/src/mill/contrib/scalapblib/Generator.scala
+++ contrib/scalapblib/src/mill/contrib/scalapblib/Generator.scala
@@ -8,8 +8,14 @@ import upickle.ReadWriter
  * @param supportsScalaPbOptions `true` if the [[ScalaPBModule.scalaPBOptions]] should be used to read options.
  */
 enum Generator(val generator: String, val supportsScalaPbOptions: Boolean) derives ReadWriter {
+
+  /** Generate Scala code. */
   case ScalaGen extends Generator("--scala_out", true)
+
+  /** Generate Java code. */
   case JavaGen extends Generator("--java_out", false)
+
+  /** Can be used to set up a custom generator. */
   case CustomGen(override val generator: String, override val supportsScalaPbOptions: Boolean)
       extends Generator(generator, supportsScalaPbOptions)
 }
