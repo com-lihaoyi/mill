87a0e8780b0cb58b7845c79cf1d8b3733a64d4d7
Li Haoyi <haoyi.sg@gmail.com>
2025-10-21 15:10:03 +0800

    .

--- runner/bsp/worker/src/mill/bsp/worker/SyntheticRootBspBuildTargetData.scala
+++ runner/bsp/worker/src/mill/bsp/worker/SyntheticRootBspBuildTargetData.scala
@@ -43,11 +43,11 @@ object SyntheticRootBspBuildTargetData {
       workspaceDir: os.Path
   ): Option[SyntheticRootBspBuildTargetData] = {
     def containsWorkspaceDir(path: Option[os.Path]) = path.exists(workspaceDir.startsWith)
-    /*if (
+    if (
       existingModules.exists { m =>
         containsWorkspaceDir(m.bspBuildTarget.baseDirectory.map(os.Path(_)))
       }
     ) None
-    else */Some(new SyntheticRootBspBuildTargetData(workspaceDir))
+    else Some(new SyntheticRootBspBuildTargetData(workspaceDir))
   }
 }
