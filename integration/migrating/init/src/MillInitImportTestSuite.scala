72afe310d4958b1ed8a99ed1346ad0a9bd3ece7f
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-10-22 14:24:49 +0530

    Reduced duplication in test method

--- integration/migrating/init/src/MillInitImportTestSuite.scala
+++ integration/migrating/init/src/MillInitImportTestSuite.scala
@@ -33,57 +33,42 @@ trait MillInitImportTestSuite extends UtestIntegrationTestSuite {
       assert(initRes.isSuccess)
 
       if (configsGoldenFile != null) {
-        val repositories = eval(("show", "__.repositories")).out
-        val jvmId = eval(("show", "__.jvmId")).out
-        val mvnDeps = eval(("show", "__.mvnDeps")).out
-        val compileMvnDeps = eval(("show", "__.compileMvnDeps")).out
-        val runMvnDeps = eval(("show", "__.runMvnDeps")).out
-        val bomMvnDeps = eval(("show", "__.bomMvnDeps")).out
-        val showModuleDeps = eval("__.showModuleDeps").out
-        val javacOptions = eval(("show", "__.javacOptions")).out
-        val pomParentProject = eval(("show", "__.pomParentProject")).out
-        val pomSettings = eval(("show", "__.pomSettings")).out
-        val publishVersion = eval(("show", "__.publishVersion")).out
-        val versionScheme = eval(("show", "__.versionScheme")).out
-        val publishProperties = eval(("show", "__.publishProperties")).out
-        val errorProneVersion = eval(("show", "__.errorProneVersion")).out
-        val errorProneDeps = eval(("show", "__.errorProneDeps")).out
-        val errorProneJavacEnableOptions = eval(("show", "errorProneJavacEnableOptions")).out
-        val errorProneOptions = eval(("show", "__.errorProneOptions")).out
-        val scalaVersion = eval(("show", "__.scalaVersion")).out
-        val scalacOptions = eval(("show", "__.scalacOptions")).out
-        val scalacPluginMvnDeps = eval(("show", "__.scalacPluginMvnDeps")).out
-        val scalaJSVersion = eval(("show", "__.scalaJSVersion")).out
-        val moduleKind = eval(("show", "__.moduleKind")).out
-        val scalaNativeVersion = eval(("show", "__.scalaNativeVersion")).out
-        val testParallelism = eval(("show", "__.testParallelism")).out
-        val testSandboxWorkingDir = eval(("show", "__.testSandboxWorkingDir")).out
-        val actualConfigs = s"""$repositories
-                               |$jvmId
-                               |$mvnDeps
-                               |$compileMvnDeps
-                               |$runMvnDeps
-                               |$bomMvnDeps
-                               |$showModuleDeps
-                               |$javacOptions
-                               |$pomParentProject
-                               |$pomSettings
-                               |$publishVersion
-                               |$versionScheme
-                               |$publishProperties
-                               |$errorProneVersion
-                               |$errorProneDeps
-                               |$errorProneJavacEnableOptions
-                               |$errorProneOptions
-                               |$scalaVersion
-                               |$scalacOptions
-                               |$scalacPluginMvnDeps
-                               |$scalaJSVersion
-                               |$moduleKind
-                               |$scalaNativeVersion
-                               |$testParallelism
-                               |$testSandboxWorkingDir
-                               |""".stripMargin
+        val taskNames = Seq(
+          "repositories",
+          "jvmId",
+          "mvnDeps",
+          "compileMvnDeps",
+          "runMvnDeps",
+          "bomMvnDeps",
+          "javacOptions",
+          "pomParentProject",
+          "pomSettings",
+          "publishVersion",
+          "versionScheme",
+          "publishProperties",
+          "errorProneVersion",
+          "errorProneDeps",
+          "errorProneJavacEnableOptions",
+          "errorProneOptions",
+          "scalaVersion",
+          "scalacOptions",
+          "scalacPluginMvnDeps",
+          "scalaJSVersion",
+          "moduleKind",
+          "scalaNativeVersion",
+          "testParallelism",
+          "testSandboxWorkingDir"
+        )
+        val showModuleDepsOut = eval("__.showModuleDeps").out
+        val actualConfigs = taskNames
+          .map(task => eval(("show", s"__.$task")).out)
+          .mkString(
+            s"""$showModuleDepsOut
+               |""".stripMargin,
+            s"""
+               |""".stripMargin,
+            ""
+          )
         val configsFile = resources / configsGoldenFile
         assertGoldenFile(actualConfigs, configsFile.wrapped)
       }
@@ -93,60 +78,5 @@ trait MillInitImportTestSuite extends UtestIntegrationTestSuite {
       val failingTasks0 = failingTasks.filterNot(eval(_).isSuccess)
       assertGoldenLiteral(failingTasks0, failingTasks)
     } finally tester.close()
-  }
-
-  def renderAllImportedConfigurations(tester: IntegrationTester) = {
-    import tester.eval
-    val repositories = eval(("show", "__.repositories")).out
-    val jvmId = eval(("show", "__.jvmId")).out
-    val mvnDeps = eval(("show", "__.mvnDeps")).out
-    val compileMvnDeps = eval(("show", "__.compileMvnDeps")).out
-    val runMvnDeps = eval(("show", "__.runMvnDeps")).out
-    val bomMvnDeps = eval(("show", "__.bomMvnDeps")).out
-    val showModuleDeps = eval("__.showModuleDeps").out
-    val javacOptions = eval(("show", "__.javacOptions")).out
-    val pomParentProject = eval(("show", "__.pomParentProject")).out
-    val pomSettings = eval(("show", "__.pomSettings")).out
-    val publishVersion = eval(("show", "__.publishVersion")).out
-    val versionScheme = eval(("show", "__.versionScheme")).out
-    val publishProperties = eval(("show", "__.publishProperties")).out
-    val errorProneVersion = eval(("show", "__.errorProneVersion")).out
-    val errorProneDeps = eval(("show", "__.errorProneDeps")).out
-    val errorProneJavacEnableOptions = eval(("show", "errorProneJavacEnableOptions")).out
-    val errorProneOptions = eval(("show", "__.errorProneOptions")).out
-    val scalaVersion = eval(("show", "__.scalaVersion")).out
-    val scalacOptions = eval(("show", "__.scalacOptions")).out
-    val scalacPluginMvnDeps = eval(("show", "__.scalacPluginMvnDeps")).out
-    val scalaJSVersion = eval(("show", "__.scalaJSVersion")).out
-    val moduleKind = eval(("show", "__.moduleKind")).out
-    val scalaNativeVersion = eval(("show", "__.scalaNativeVersion")).out
-    val testParallelism = eval(("show", "__.testParallelism")).out
-    val testSandboxWorkingDir = eval(("show", "__.testSandboxWorkingDir")).out
-    s"""$repositories
-       |$jvmId
-       |$mvnDeps
-       |$compileMvnDeps
-       |$runMvnDeps
-       |$bomMvnDeps
-       |$showModuleDeps
-       |$javacOptions
-       |$pomParentProject
-       |$pomSettings
-       |$publishVersion
-       |$versionScheme
-       |$publishProperties
-       |$errorProneVersion
-       |$errorProneDeps
-       |$errorProneJavacEnableOptions
-       |$errorProneOptions
-       |$scalaVersion
-       |$scalacOptions
-       |$scalacPluginMvnDeps
-       |$scalaJSVersion
-       |$moduleKind
-       |$scalaNativeVersion
-       |$testParallelism
-       |$testSandboxWorkingDir
-       |""".stripMargin
   }
 }
