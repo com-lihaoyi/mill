0feb2b6deaada69fb0bf4154ce27353f6032478f
Li Haoyi <haoyi.sg@gmail.com>
2025-10-23 20:01:17 +0800

    Update Foo.scala

--- example/scalalib/basic/1-script/Foo.scala
+++ example/scalalib/basic/1-script/Foo.scala
@@ -3,16 +3,15 @@
 //| - "com.lihaoyi::scalatags:0.13.1"
 //| - "com.lihaoyi::mainargs:0.7.7"
 import scalatags.Text.all.*
-import mainargs.{main, Parser}
 
 def generateHtml(text: String) = {
   h1(text).toString
 }
 
-@main
+@mainargs.main
 def main(text: String) = {
   println("Jvm Version: " + System.getProperty("java.version"))
   println(generateHtml(text))
 }
 
-def main(args: Array[String]): Unit = Parser(this).runOrExit(args)
+def main(args: Array[String]): Unit = mainargs.Parser(this).runOrExit(args)
