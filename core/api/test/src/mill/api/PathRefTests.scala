6bcdda130e8740f3f71776c1f2738f7a0eac54e8
Tobias Roeser <le.petit.fou@web.de>
2025-10-23 21:20:32 +0200

    Use path mapping when json-serializing PathRefs

--- core/api/test/src/mill/api/PathRefTests.scala
+++ core/api/test/src/mill/api/PathRefTests.scala
@@ -81,26 +81,73 @@ object PathRefTests extends TestSuite {
     }
 
     test("json") {
-      def check(quick: Boolean) = withTmpDir { tmpDir =>
-        val file = tmpDir / "foo.txt"
-        os.write(file, "hello")
-        val pr = PathRef(file, quick)
-        val prFile = pr.path.toString().replace("\\", "\\\\")
-        val json = upickle.write(pr)
-        if (quick) {
-          assert(json.startsWith(""""qref:v0:"""))
-          assert(json.endsWith(s""":${prFile}""""))
-        } else {
-          val hash = if (Properties.isWin) "86df6a6a" else "4c7ef487"
-          val expected = s""""ref:v0:${hash}:${prFile}""""
-          assert(json == expected)
+      def check(quick: Boolean) = withTmpDir { outDir =>
+        PathRef.outPathOverride.withValue(Some(outDir)) {
+          withTmpDir { tmpDir =>
+            val file = tmpDir / "foo.txt"
+            os.write(file, "hello")
+            val pr = PathRef(file, quick)
+            val prFile = pr.path.toString().replace("\\", "\\\\")
+            val json = upickle.write(pr)
+            if (quick) {
+              assert(json.startsWith(""""qref:v0:"""))
+              assert(json.endsWith(s""":${prFile}""""))
+            } else {
+              val hash = if (Properties.isWin) "86df6a6a" else "4c7ef487"
+              val expected = s""""ref:v0:${hash}:${prFile}""""
+              assert(json == expected)
+            }
+            val pr1 = upickle.read[PathRef](json)
+            assert(pr == pr1)
+          }
         }
-        val pr1 = upickle.read[PathRef](json)
-        assert(pr == pr1)
       }
 
       test("qref") - check(quick = true)
       test("ref") - check(quick = false)
+    }
+
+    test("encode") {
+      withTmpDir { tmpDir =>
+        val workspaceDir = tmpDir / "workspace"
+        BuildCtx.workspaceRoot0.withValue(workspaceDir) {
+          val outDir = workspaceDir / "out"
+          PathRef.outPathOverride.withValue(Some(outDir)) {
+
+            def check(file: os.Path, contains: Seq[String], containsNot: Seq[String]) = {
+              val pr = PathRef(file)
+              val enc = PathRef.encodeKnownRoots(pr)
+              val dec = PathRef.decodeKnownRoots(enc)
+              assert(pr.toString == dec)
+              contains.foreach(s => enc.containsSlice(s))
+              containsNot.foreach(s => !enc.containsSlice(s))
+
+              file -> enc
+            }
+
+            val file1 = tmpDir / "file1"
+            val file2 = workspaceDir / "file2"
+            val file3 = outDir / "file3"
+
+            assert(
+              PathRef.encodeKnownRoots(PathRef(file1)).containsSlice("ref:"),
+              !PathRef.encodeKnownRoots(PathRef(file1)).containsSlice("$WORKSPACE"),
+              !PathRef.encodeKnownRoots(PathRef(file1)).containsSlice("$MILL_OUT"),
+              PathRef.encodeKnownRoots(PathRef(file2)).containsSlice("$WORKSPACE/file2"),
+              !PathRef.encodeKnownRoots(PathRef(file2)).containsSlice("$MILL_OUT"),
+              PathRef.encodeKnownRoots(PathRef(file3)).containsSlice("$MILL_OUT/file3"),
+              !PathRef.encodeKnownRoots(PathRef(file3)).containsSlice("WORKSPACE")
+            )
+
+            Seq(
+              "mapping" -> PathRef.knownRoots,
+              check(file1, Seq("ref:v0:", file1.toString), Seq("$WORKSPACE", "$MILL_OUT")),
+                check (file2, Seq("ref:v0:", "$WORKSPACE/file2"), Seq("$MILL_OUT")),
+                check (file3, Seq("ref:v0:", "$MILL_OUT/file3"), Seq("$WORKSPACE")),
+            )
+          }
+        }
+      }
     }
   }
 
