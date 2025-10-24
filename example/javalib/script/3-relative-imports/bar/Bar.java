7adfbe89b13f79d39cdee3c91af50ab26e1d33c3
Li Haoyi <haoyi.sg@gmail.com>
2025-10-24 14:14:49 +0800

    wip

---
+++ example/javalib/script/3-relative-imports/bar/Bar.java
@@ -0,0 +1,14 @@
+//| mvnDeps:
+//| - org.thymeleaf:thymeleaf:3.1.1.RELEASE
+package bar;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+public class Bar {
+  public static String generateHtml(String text) {
+    Context context = new Context();
+    context.setVariable("text", text);
+    return new TemplateEngine().process("<h1 th:text=\"${text}\"></h1>", context);
+  }
+}
