ff66a59ce8ca461622795785c8b66ff502f8f0cc
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-10-27 11:35:33 +0200

    Docs + example enrichment

--- example/springboot/kotlin/1-web-initializr/src/main/kotlin/com/example/demokotlin/DemoKotlinApplication.kt
+++ example/springboot/kotlin/1-web-initializr/src/main/kotlin/com/example/demokotlin/DemoKotlinApplication.kt
@@ -3,17 +3,16 @@ package com.example.demokotlin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class DemoKotlinApplication
+@RestController
+class DemoKotlinApplication {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String? = String.format("Hello, %s!", name)
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoKotlinApplication>(*args)
-}
-
-@RestController
-class HelloController {
-    @GetMapping("/")
-    fun hello(): String = "<h1>Hello, World!</h1>"
 }
