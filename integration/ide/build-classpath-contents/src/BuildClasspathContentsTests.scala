5de36b7be259f3591a8c3ea348c95a3ac906a29d
Tobias Roeser <le.petit.fou@web.de>
2025-10-27 22:07:04 +0100

    Ajust reading of json files

--- integration/ide/build-classpath-contents/src/BuildClasspathContentsTests.scala
+++ integration/ide/build-classpath-contents/src/BuildClasspathContentsTests.scala
@@ -1,4 +1,4 @@
-import mill.api.BuildCtx
+import mill.api.{BuildCtx, PathRef}
 import mill.testkit.UtestIntegrationTestSuite
 import utest.*
 
@@ -6,56 +6,60 @@ object BuildClasspathContentsTests extends UtestIntegrationTestSuite {
 
   val tests: Tests = Tests {
     test("test") - integrationTest { tester =>
-      val result1 =
-        tester.eval(("--meta-level", "1", "show", "compileClasspath"), stderr = os.Inherit)
-      val deserialized = upickle.read[Seq[mill.api.PathRef]](result1.out)
-      val millPublishedJars = deserialized
-        .map(_.path.last)
-        .filter(_.startsWith("mill-"))
-        .sorted
-      val millLocalClasspath = deserialized
-        .map(_.path)
-        .filter(_.startsWith(BuildCtx.workspaceRoot))
-        .map(_.subRelativeTo(BuildCtx.workspaceRoot))
-        .filter(!_.startsWith("out/integration"))
-        .filter(!_.startsWith("out/dist/localRepo.dest"))
-        .map(_.toString)
-        .sorted
-      if (sys.env("MILL_INTEGRATION_IS_PACKAGED_LAUNCHER") == "true") {
-        assertGoldenLiteral(
-          millPublishedJars,
-          List(
-            "mill-core-api-daemon_3-SNAPSHOT.jar",
-            "mill-core-api_3-SNAPSHOT.jar",
-            "mill-core-constants-SNAPSHOT.jar",
-            "mill-libs-androidlib-databinding_3-SNAPSHOT.jar",
-            "mill-libs-androidlib_3-SNAPSHOT.jar",
-            "mill-libs-daemon-client-SNAPSHOT.jar",
-            "mill-libs-daemon-server_3-SNAPSHOT.jar",
-            "mill-libs-javalib-api_3-SNAPSHOT.jar",
-            "mill-libs-javalib-testrunner-entrypoint-SNAPSHOT.jar",
-            "mill-libs-javalib-testrunner_3-SNAPSHOT.jar",
-            "mill-libs-javalib_3-SNAPSHOT.jar",
-            "mill-libs-javascriptlib_3-SNAPSHOT.jar",
-            "mill-libs-kotlinlib-api_3-SNAPSHOT.jar",
-            "mill-libs-kotlinlib-ksp2-api_3-SNAPSHOT.jar",
-            "mill-libs-kotlinlib_3-SNAPSHOT.jar",
-            "mill-libs-pythonlib_3-SNAPSHOT.jar",
-            "mill-libs-rpc_3-SNAPSHOT.jar",
-            "mill-libs-scalajslib-api_3-SNAPSHOT.jar",
-            "mill-libs-scalajslib_3-SNAPSHOT.jar",
-            "mill-libs-scalalib_3-SNAPSHOT.jar",
-            "mill-libs-scalanativelib-api_3-SNAPSHOT.jar",
-            "mill-libs-scalanativelib_3-SNAPSHOT.jar",
-            "mill-libs-script_3-SNAPSHOT.jar",
-            "mill-libs-util_3-SNAPSHOT.jar",
-            "mill-libs_3-SNAPSHOT.jar",
-            "mill-moduledefs_3-0.11.10.jar"
-          )
-        )
-        assert(millLocalClasspath == Nil)
-      } else {
-        sys.error("This test must be run in `packaged` mode, not `local`")
+      PathRef.mappedRoots.withMapping(Seq(
+        "HOME" -> os.home,
+        "WORKSPACE" -> tester.workspacePath
+      )) {
+        val result1 =
+          tester.eval(("--meta-level", "1", "show", "compileClasspath"), stderr = os.Inherit)
+        val deserialized = upickle.read[Seq[mill.api.PathRef]](result1.out)
+        val millPublishedJars = deserialized
+          .map(_.path.last)
+          .filter(_.startsWith("mill-"))
+          .sorted
+        val millLocalClasspath = deserialized
+          .map(_.path)
+          .filter(_.startsWith(BuildCtx.workspaceRoot))
+          .map(_.subRelativeTo(BuildCtx.workspaceRoot))
+          .filter(!_.startsWith("out/integration"))
+          .filter(!_.startsWith("out/dist/localRepo.dest"))
+          .map(_.toString)
+          .sorted
+        if (sys.env("MILL_INTEGRATION_IS_PACKAGED_LAUNCHER") == "true") {
+          assertGoldenLiteral(
+            millPublishedJars,
+            List(
+              "mill-core-api-daemon_3-SNAPSHOT.jar",
+              "mill-core-api_3-SNAPSHOT.jar",
+              "mill-core-constants-SNAPSHOT.jar",
+              "mill-libs-androidlib-databinding_3-SNAPSHOT.jar",
+              "mill-libs-androidlib_3-SNAPSHOT.jar",
+              "mill-libs-daemon-client-SNAPSHOT.jar",
+              "mill-libs-daemon-server_3-SNAPSHOT.jar",
+              "mill-libs-javalib-api_3-SNAPSHOT.jar",
+              "mill-libs-javalib-testrunner-entrypoint-SNAPSHOT.jar",
+              "mill-libs-javalib-testrunner_3-SNAPSHOT.jar",
+              "mill-libs-javalib_3-SNAPSHOT.jar",
+              "mill-libs-javascriptlib_3-SNAPSHOT.jar",
+              "mill-libs-kotlinlib-api_3-SNAPSHOT.jar",
+              "mill-libs-kotlinlib-ksp2-api_3-SNAPSHOT.jar",
+              "mill-libs-kotlinlib_3-SNAPSHOT.jar",
+              "mill-libs-pythonlib_3-SNAPSHOT.jar",
+              "mill-libs-rpc_3-SNAPSHOT.jar",
+              "mill-libs-scalajslib-api_3-SNAPSHOT.jar",
+              "mill-libs-scalajslib_3-SNAPSHOT.jar",
+              "mill-libs-scalalib_3-SNAPSHOT.jar",
+              "mill-libs-scalanativelib-api_3-SNAPSHOT.jar",
+              "mill-libs-scalanativelib_3-SNAPSHOT.jar",
+              "mill-libs-script_3-SNAPSHOT.jar",
+              "mill-libs-util_3-SNAPSHOT.jar",
+              "mill-libs_3-SNAPSHOT.jar",
+              "mill-moduledefs_3-0.11.10.jar"
+            )
+            assert(millLocalClasspath == Nil)
+          } else {
+            sys.error("This test must be run in `packaged` mode, not `local`")
+          }
       }
     }
   }
