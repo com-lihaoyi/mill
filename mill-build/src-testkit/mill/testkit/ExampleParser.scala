14a2282ab31804181aa329586aa84fd6a53ddc0b
Li Haoyi <haoyi.sg@gmail.com>
2025-11-05 16:02:29 +0800

    .

--- mill-build/src-testkit/mill/testkit/ExampleParser.scala
+++ mill-build/src-testkit/mill/testkit/ExampleParser.scala
@@ -1,10 +1,30 @@
 package mill.testkit
 
-object ExampleParser {
-  def apply(testRepoRoot: os.Path): Seq[(String, String)] = {
+enum Chunk derives upickle.default.ReadWriter {
+  case Yaml(lines: Seq[String])
+  case Example(lines: Seq[String])
+  case See(path: String, lines: Seq[String])
+  case Scala(lines: Seq[String])
+  case Comment(lines: Seq[String])
+}
 
-    val states = collection.mutable.Buffer("yaml")
-    val chunks = collection.mutable.Buffer(collection.mutable.Buffer.empty[String])
+object ExampleParser {
+  def apply(testRepoRoot: os.Path): Seq[Chunk] = {
+    val result = collection.mutable.Buffer[Chunk]()
+
+    def appendLine(line: String): Unit = {
+      if (result.nonEmpty) {
+        val last = result.last
+        val updated = last match {
+          case Chunk.Yaml(lines) => Chunk.Yaml(lines :+ line)
+          case Chunk.Example(lines) => Chunk.Example(lines :+ line)
+          case Chunk.See(path, lines) => Chunk.See(path, lines :+ line)
+          case Chunk.Scala(lines) => Chunk.Scala(lines :+ line)
+          case Chunk.Comment(lines) => Chunk.Comment(lines :+ line)
+        }
+        result(result.length - 1) = updated
+      }
+    }
 
     val rootBuildFileNames = Seq("build.mill")
     val buildFile = rootBuildFileNames.map(testRepoRoot / _)
@@ -14,26 +34,46 @@ object ExampleParser {
           s"No build file named ${rootBuildFileNames.mkString("/")} found in $testRepoRoot"
         )
       )
+
     for (line <- os.read.lines(buildFile)) {
-      val (newState, restOpt) = line match {
-        case s"/** Usage" => ("example", None)
+      val (newChunkType, lineToAdd) = line match {
+        case s"/** Usage" => (Chunk.Example(Vector()), None)
         case s"/** See Also: $path */" =>
-          (s"see:$path", Some(os.read(os.Path(path, testRepoRoot))))
-        case s"*/" => ("scala", None)
-        case line @ s"//|$_" if states.last == "yaml" => ("yaml", Some(line))
-        case s"//$rest" if !rest.startsWith("|") => ("comment", Some(rest.stripPrefix(" ")))
+          (Chunk.See(path, Vector()), Some(os.read(os.Path(path, testRepoRoot))))
+        case s"*/" => (Chunk.Scala(Vector()), None)
+        case line @ s"//|$_" if result.nonEmpty && result.last.isInstanceOf[Chunk.Yaml] =>
+          (Chunk.Yaml(Vector()), Some(line))
+        case s"//$rest" if !rest.startsWith("|") =>
+          (Chunk.Comment(Vector()), Some(rest.stripPrefix(" ")))
         case l =>
-          (if (Seq("comment", "yaml").contains(states.last)) "scala" else states.last, Some(l))
+          val chunkType = if (result.nonEmpty) {
+            result.last match {
+              case _: Chunk.Comment | _: Chunk.Yaml => Chunk.Scala(Vector())
+              case other => other
+            }
+          } else Chunk.Yaml(Vector()) // initial state
+          (chunkType, Some(l))
       }
 
-      if (newState != states.last) {
-        states.append(newState)
-        chunks.append(collection.mutable.Buffer.empty[String])
+      if (result.isEmpty || newChunkType.getClass != result.last.getClass) {
+        result.append(newChunkType)
       }
 
-      restOpt.foreach(r => chunks.last.append(r))
+      lineToAdd.foreach(appendLine)
     }
 
-    states.zip(chunks.map(_.mkString("\n").trim)).filter(_._2.nonEmpty).toSeq
+    result.filter {
+      case Chunk.Yaml(lines) => lines.mkString("\n").trim.nonEmpty
+      case Chunk.Example(lines) => lines.mkString("\n").trim.nonEmpty
+      case Chunk.See(_, lines) => lines.mkString("\n").trim.nonEmpty
+      case Chunk.Scala(lines) => lines.mkString("\n").trim.nonEmpty
+      case Chunk.Comment(lines) => lines.mkString("\n").trim.nonEmpty
+    }.map {
+      case Chunk.Yaml(lines) => Chunk.Yaml(lines.mkString("\n").trim.linesIterator.toVector)
+      case Chunk.Example(lines) => Chunk.Example(lines.mkString("\n").trim.linesIterator.toVector)
+      case Chunk.See(path, lines) => Chunk.See(path, lines.mkString("\n").trim.linesIterator.toVector)
+      case Chunk.Scala(lines) => Chunk.Scala(lines.mkString("\n").trim.linesIterator.toVector)
+      case Chunk.Comment(lines) => Chunk.Comment(lines.mkString("\n").trim.linesIterator.toVector)
+    }.toSeq
   }
 }
