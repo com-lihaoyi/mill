d4e7087f408bd38abb7e785ff36603a3ad1ebe86
Li Haoyi <haoyi.sg@gmail.com>
2025-11-06 10:22:25 +0800

    fix

--- core/api/daemon/src/mill/api/daemon/SpawnThread.scala
+++ core/api/daemon/src/mill/api/daemon/SpawnThread.scala
@@ -4,6 +4,7 @@ object StartThread {
   def apply[T](name: String, daemon: Boolean = false)(block: => T) = {
     val t = new Thread((() => block): Runnable, name)
     if (daemon) t.setDaemon(true)
+    t.start()
     t
   }
 }
