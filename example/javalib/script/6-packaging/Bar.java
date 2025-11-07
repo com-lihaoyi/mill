138cda3104109f05c371512502a538a29e4c79e3
Tobias Roeser <le.petit.fou@web.de>
2025-11-07 15:15:42 +0100

    Fix opts in YAML

--- example/javalib/script/6-packaging/Bar.java
+++ example/javalib/script/6-packaging/Bar.java
@@ -1,5 +1,5 @@
 //| jvmId: "graalvm-community:17"
-//| nativeImageOptions: ["--no-fallback"]
+//| nativeImageOptions: [["--no-fallback"]]
 public class Bar {
   public static void main(String[] args) {
     System.out.println("Hello World");
--- example/kotlinlib/script/6-packaging/Bar.kt
+++ example/kotlinlib/script/6-packaging/Bar.kt
@@ -1,6 +1,6 @@
 //| kotlinVersion: 2.0.20
 //| jvmId: "graalvm-community:17"
-//| nativeImageOptions: ["--no-fallback"]
+//| nativeImageOptions: [["--no-fallback"]]
 fun main(args: Array<String>) {
     println("Hello World")
 }
