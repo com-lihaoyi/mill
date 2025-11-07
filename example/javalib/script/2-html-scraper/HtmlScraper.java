5f5880f675b5ee67aed8e81b23b4cc2fb96f50e5
Li Haoyi <haoyi.sg@gmail.com>
2025-11-07 15:39:50 +0800

    wip

--- example/javalib/script/2-html-scraper/HtmlScraper.java
+++ example/javalib/script/2-html-scraper/HtmlScraper.java
@@ -1,20 +1,15 @@
 //| mvnDeps: [org.jsoup:jsoup:1.7.2]
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.*;
 
 public class HtmlScraper {
   static List<String> fetchLinks(String title) throws IOException {
-    String url = "https://en.wikipedia.org/wiki/" + title;
-    Document doc = Jsoup.connect(url)
-      .header("User-Agent", "Mozilla/5.0 (compatible; JsoupBot/1.0; +https://example.com/bot)")
-      .get();
+    var url = "https://en.wikipedia.org/wiki/" + title;
+    var doc = Jsoup.connect(url).header("User-Agent", "My Scraper").get();
 
-    List<String> links = new ArrayList<>();
-    for (Element a : doc.select("main p a")) {
+    var links = new ArrayList<String>();
+    for (var a : doc.select("main p a")) {
       var href = a.attr("href");
       if (href.startsWith("/wiki/")) links.add(href.substring(6));
     }
@@ -22,18 +17,11 @@ public class HtmlScraper {
   }
 
   public static void main(String[] args) throws Exception {
-    if (args.length < 2) {
-      System.err.println("Usage: java WikiCrawler <startArticle> <depth>");
-      System.exit(1);
-    }
+    if (args.length < 2) throw new Exception("WikiCrawler.java <start> <depth>");
 
-    var startArticle = args[0];
-    var depth = Integer.parseInt(args[1]);
-
-    var seen = new HashSet<String>(Set.of(startArticle));
-    var current = new HashSet<String>(Set.of(startArticle));
-
-    for (int i = 0; i < depth; i++) {
+    var seen = new HashSet<>(Set.of(args[0]));
+    var current = new HashSet<>(Set.of(args[0]));
+    for (int i = 0; i < Integer.parseInt(args[1]); i++) {
       var next = new HashSet<String>();
       for (String article : current) {
         for (String link : fetchLinks(article)) {
@@ -42,7 +30,7 @@ public class HtmlScraper {
       }
       current = next;
     }
-
     for (String s : seen) System.out.println(s);
   }
 }
+
