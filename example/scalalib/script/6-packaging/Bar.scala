410dee89b5eb7a9855470d1df83e4118025eaa01
Tobias Roeser <le.petit.fou@web.de>
2025-11-08 17:26:16 +0100

    Revert "Fix opts in YAML"
    This reverts commit 138cda3104109f05c371512502a538a29e4c79e3.

--- example/scalalib/script/6-packaging/Bar.scala
+++ example/scalalib/script/6-packaging/Bar.scala
@@ -1,6 +1,6 @@
 //| scalaVersion: 3.7.3
 //| jvmId: "graalvm-community:17"
-//| nativeImageOptions: [["--no-fallback"]]
+//| nativeImageOptions: ["--no-fallback"]
 @main def main() = {
   println("Hello World")
 }
