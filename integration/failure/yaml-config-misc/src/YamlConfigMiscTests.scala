92bdeda57ae66f7312b68058bc83361f2ccd89e0
Li Haoyi <haoyi.sg@gmail.com>
2025-11-14 08:16:06 -0600

    wip

--- integration/failure/yaml-config-misc/src/YamlConfigMiscTests.scala
+++ integration/failure/yaml-config-misc/src/YamlConfigMiscTests.scala
@@ -6,7 +6,7 @@ import utest._
 
 // Basic tests for errors in build.mill.yaml an package.mill.yaml files. The errors don't
 // look great, but for now just assert on them and make sure they don't regress
-object YamlBuildTests extends UtestIntegrationTestSuite {
+object YamlConfigMiscTests extends UtestIntegrationTestSuite {
   val tests: Tests = Tests {
     integrationTest { tester =>
       val res = tester.eval("version")
@@ -14,12 +14,6 @@ object YamlBuildTests extends UtestIntegrationTestSuite {
       assert(res.err.contains("trait package_ extends mill.javalib.JavaModuleTypod"))
       assert(res.err.contains("type JavaModuleTypod is not a member of mill.javalib"))
 
-      os.remove.all(tester.workspacePath / "mispelledextends")
-
-      val res2 = tester.eval("version")
-      assert(res2.err.replace('\\', '/').contains("invalidtaskname/package.mill.yaml"))
-//      assert(res2.err.contains("override def mvnDepsTypo = Task.Stub()"))
-//      assert(res2.err.contains("method mvnDepsTypo overrides nothing"))
     }
   }
 }
