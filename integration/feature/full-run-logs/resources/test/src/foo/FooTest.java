813bfd839fb164488029238b352d3091ec7bede1
Li Haoyi <haoyi.sg@gmail.com>
2025-11-16 05:52:14 -0500

    wip

--- integration/feature/full-run-logs/resources/test/src/foo/FooTest.java
+++ integration/feature/full-run-logs/resources/test/src/foo/FooTest.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 
 
 public class FooTest {
+  public static void main(String[] args){
+    printAll();
+  }
   @Test
   public void testSimple() {
+    printAll();
+  }
+  public static void printAll(){
     final String RED = "\u001b[31m";
     final String GREEN = "\u001B[32m";
     final String BLUE = "\u001b[34m";
@@ -16,8 +22,8 @@ public class FooTest {
     final String[] colors = {RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW};
     String out = "";
     int m = 0;
-    int n = 10;
-    String prefix = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    int n = 3;
+    String prefix = "01234567890123456789012345678901234567890123456789012345678901234567890123456789";
     for(String color : colors){
       out += color;
       for(int i = 0; i < n; i += 1) {
