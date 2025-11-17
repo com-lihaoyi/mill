2d028a64452f9488355a772a8d7a65962a5dab69
Tobias Roeser <le.petit.fou@web.de>
2025-11-17 15:06:02 +0100

    .

--- example/scalalib/module/16-scala-nightlies/foo/src/Hello.scala
+++ example/scalalib/module/16-scala-nightlies/foo/src/Hello.scala
@@ -1,6 +1,6 @@
 @main
 def main(me: String*) = {
   println(
-    s"Hello ${me.headOption.getOrElse("world")}, this is Scala '${scala.util.Properties.versionNumberString}'"
+    s"Hello ${me.headOption.getOrElse("world")}!"
   )
 }
