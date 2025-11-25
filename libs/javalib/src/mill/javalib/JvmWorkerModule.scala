ebe4aef7be807949da8fdab388e12aefb20658e3
Li Haoyi <haoyi.sg@gmail.com>
2025-11-25 17:30:40 +0800

    wip

--- libs/javalib/src/mill/javalib/JvmWorkerModule.scala
+++ libs/javalib/src/mill/javalib/JvmWorkerModule.scala
@@ -8,7 +8,8 @@ import mill.javalib.CoursierModule.Resolver
 import mill.javalib.api.JvmWorkerUtil.{isBinaryBridgeAvailable, isDotty, isDottyOrScala3}
 import mill.javalib.api.internal.InternalJvmWorkerApi as InternalJvmWorkerApi
 import mill.javalib.api.{JvmWorkerApi, JvmWorkerUtil, Versions}
-import mill.javalib.internal.{JvmWorkerArgs, ZincCompilerBridgeProvider}
+import mill.javalib.api.{JvmWorkerArgs}
+import mill.javalib.api.internal.{ZincCompilerBridgeProvider}
 
 /**
  * A default implementation of [[JvmWorkerModule]]
