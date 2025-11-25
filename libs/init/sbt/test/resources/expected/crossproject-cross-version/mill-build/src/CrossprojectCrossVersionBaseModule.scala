602cbf86c5ebadb89f974a85aed48ea4d1740cc2
Tobias Roeser <le.petit.fou@web.de>
2025-11-25 11:11:48 +0100

    Update test expectations

--- libs/init/sbt/test/resources/expected/crossproject-cross-version/mill-build/src/CrossprojectCrossVersionBaseModule.scala
+++ libs/init/sbt/test/resources/expected/crossproject-cross-version/mill-build/src/CrossprojectCrossVersionBaseModule.scala
@@ -1,5 +1,7 @@
 package millbuild
 
+import mill.api.*
+import mill.api.opt.*
 import mill.javalib.*
 import mill.javalib.publish.*
 import mill.scalalib.*
@@ -9,15 +11,15 @@ trait CrossprojectCrossVersionBaseModule
 
   def mvnDeps = super.mvnDeps() ++ Seq(Deps.upickle)
 
-  def scalacOptions = super.scalacOptions() ++ Seq("-deprecation") ++
+  def scalacOptions = super.scalacOptions() ++ Opts("-deprecation") ++
     (scalaVersion() match {
-      case "2.12.20" => Seq(
+      case "2.12.20" => Opts(
           "-Xlint:_,-unused",
           "-Ywarn-numeric-widen",
           "-Ywarn-unused:_,-nowarn,-privates"
         )
-      case "2.13.14" => Seq("-Xlint:_,-unused", "-Wnumeric-widen", "-Wunused")
-      case "3.7.1"   => Seq("-Wunused")
+      case "2.13.14" => Opts("-Xlint:_,-unused", "-Wnumeric-widen", "-Wunused")
+      case "3.7.1"   => Opts("-Wunused")
       case _         => Nil
     })
 
