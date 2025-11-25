7d29c3f9582b3257bd9c93322227a470363e8138
Li Haoyi <haoyi.sg@gmail.com>
2025-11-25 20:21:43 +0800

    .

--- mill-build/src/millbuild/Java11Module.scala
+++ mill-build/src/millbuild/Java11Module.scala
@@ -4,4 +4,8 @@ trait MillJava11ScalaModule extends MillPublishScalaModule {
   def scalaVersion = Deps.scalaVersionJava11
 
   def jvmId = "11"
+
+  override object test extends MillScalaTests {
+    def scalaVersion = Deps.scalaVersion
+  }
 }
