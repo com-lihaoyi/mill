e7150400d7807cee4da9d38e29d2657163126627
Li Haoyi <haoyi.sg@gmail.com>
2025-11-26 13:47:44 +0800

    .

---
+++ example/javalib/script/11-jvm-version/Foo.java
@@ -0,0 +1,7 @@
+//| jvmId: 11.0.20
+
+public class Foo {
+  public static void main(String[] args) {
+    System.out.println("java.version " + System.getProperty("java.version"));
+  }
+}
---
+++ example/kotlinlib/script/11-jvm-version/Foo.kt
@@ -0,0 +1,6 @@
+//| jvmId: 11.0.20
+package foo
+
+fun main() {
+    println("java.version " + java.lang.System.getProperty("java.version"))
+}
