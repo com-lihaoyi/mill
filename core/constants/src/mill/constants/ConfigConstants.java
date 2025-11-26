b5bd66e00b566d9f2dbab7e1fc71e56d8f31f11d
Li Haoyi <haoyi.sg@gmail.com>
2025-11-26 15:35:55 +0800

    fix

--- core/constants/src/mill/constants/ConfigConstants.java
+++ core/constants/src/mill/constants/ConfigConstants.java
@@ -1,6 +1,7 @@
 package mill.constants;
 
 public class ConfigConstants {
+  public static final String millBuild = "mill-build";
   public static final String millVersion = "mill-version";
   public static final String millJvmVersion = "mill-jvm-version";
   public static final String millJvmIndexVersion = "mill-jvm-index-version";
@@ -8,6 +9,6 @@ public class ConfigConstants {
   public static final String millOpts = "mill-opts";
 
   public static String[] all() {
-    return new String[] {millVersion, millJvmVersion, millJvmIndexVersion, millJvmOpts, millOpts};
+    return new String[] {millBuild, millVersion, millJvmVersion, millJvmIndexVersion, millJvmOpts, millOpts};
   }
 }
