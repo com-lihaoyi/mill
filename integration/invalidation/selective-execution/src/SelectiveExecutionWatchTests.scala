d3ea9c07c886bddf7e6c7c351b7d2bb8bf702771
Li Haoyi <haoyi.sg@gmail.com>
2025-11-29 12:12:41 +0800

    remove-wrapper

--- integration/invalidation/selective-execution/src/SelectiveExecutionWatchTests.scala
+++ integration/invalidation/selective-execution/src/SelectiveExecutionWatchTests.scala
@@ -10,99 +10,97 @@ object SelectiveExecutionWatchTests extends UtestIntegrationTestSuite {
   implicit val retryInterval: RetryInterval = RetryInterval(1.seconds)
   val tests: Tests = Tests {
 
-    test("watch") {
-      test("changed-inputs") - retry(1) {
-        integrationTest { tester =>
-          import tester._
-          val spawned = spawn(("--watch", "{foo.fooCommand,bar.barCommand}"))
+    test("changed-inputs") - retry(1) {
+      integrationTest { tester =>
+        import tester._
+        val spawned = spawn(("--watch", "{foo.fooCommand,bar.barCommand}"))
 
-          assertEventually {
-            spawned.out.text().contains("Computing fooCommand") &&
-            spawned.out.text().contains("Computing barCommand")
-          }
+        assertEventually {
+          spawned.out.text().contains("Computing fooCommand") &&
+          spawned.out.text().contains("Computing barCommand")
+        }
 
-          // Make sure editing each individual input results in the corresponding downstream
-          // command being re-run, and watches on both are maintained even if in a prior run
-          // one set of tasks was ignored.
-          spawned.clear()
-          modifyFile(workspacePath / "bar/bar.txt", _ + "!")
-          assertEventually {
-            !spawned.out.text().contains("Computing fooCommand") &&
-            spawned.out.text().contains("Computing barCommand")
-          }
+        // Make sure editing each individual input results in the corresponding downstream
+        // command being re-run, and watches on both are maintained even if in a prior run
+        // one set of tasks was ignored.
+        spawned.clear()
+        modifyFile(workspacePath / "bar/bar.txt", _ + "!")
+        assertEventually {
+          !spawned.out.text().contains("Computing fooCommand") &&
+          spawned.out.text().contains("Computing barCommand")
+        }
 
-          // Test for a bug where modifying the sources 2nd time would run tasks from both modules.
-          spawned.clear()
-          modifyFile(workspacePath / "bar/bar.txt", _ + "!")
-          assertEventually {
-            !spawned.out.text().contains("Computing fooCommand") &&
-            spawned.out.text().contains("Computing barCommand")
-          }
+        // Test for a bug where modifying the sources 2nd time would run tasks from both modules.
+        spawned.clear()
+        modifyFile(workspacePath / "bar/bar.txt", _ + "!")
+        assertEventually {
+          !spawned.out.text().contains("Computing fooCommand") &&
+          spawned.out.text().contains("Computing barCommand")
+        }
 
-          spawned.clear()
-          modifyFile(workspacePath / "foo/foo.txt", _ + "!")
-          assertEventually {
-            spawned.out.text().contains("Computing fooCommand") &&
-            !spawned.out.text().contains("Computing barCommand")
-          }
+        spawned.clear()
+        modifyFile(workspacePath / "foo/foo.txt", _ + "!")
+        assertEventually {
+          spawned.out.text().contains("Computing fooCommand") &&
+          !spawned.out.text().contains("Computing barCommand")
         }
       }
-      test("show-changed-inputs") - retry(1) {
-        integrationTest { tester =>
-          import tester._
-          val spawned = spawn(("--watch", "show", "{foo.fooCommand,bar.barCommand}"))
+    }
+    test("show-changed-inputs") - retry(1) {
+      integrationTest { tester =>
+        import tester._
+        val spawned = spawn(("--watch", "show", "{foo.fooCommand,bar.barCommand}"))
 
-          assertEventually {
-            spawned.err.text().contains("Computing fooCommand") &&
-            spawned.err.text().contains("Computing barCommand")
-          }
+        assertEventually {
+          spawned.err.text().contains("Computing fooCommand") &&
+          spawned.err.text().contains("Computing barCommand")
+        }
 
-          spawned.clear()
-          modifyFile(workspacePath / "bar/bar.txt", _ + "!")
-          assertEventually {
-            !spawned.err.text().contains("Computing fooCommand") &&
-            spawned.err.text().contains("Computing barCommand")
-          }
+        spawned.clear()
+        modifyFile(workspacePath / "bar/bar.txt", _ + "!")
+        assertEventually {
+          !spawned.err.text().contains("Computing fooCommand") &&
+          spawned.err.text().contains("Computing barCommand")
+        }
 
-          spawned.clear()
-          modifyFile(workspacePath / "foo/foo.txt", _ + "!")
-          assertEventually {
-            spawned.err.text().contains("Computing fooCommand") &&
-            !spawned.err.text().contains("Computing barCommand")
-          }
+        spawned.clear()
+        modifyFile(workspacePath / "foo/foo.txt", _ + "!")
+        assertEventually {
+          spawned.err.text().contains("Computing fooCommand") &&
+          !spawned.err.text().contains("Computing barCommand")
         }
       }
+    }
 
-      test("changed-code") - retry(1) {
-        integrationTest { tester =>
-          import tester._
+    test("changed-code") - retry(1) {
+      integrationTest { tester =>
+        import tester._
 
-          val spawned = spawn(("--watch", "{foo.fooCommand,bar.barCommand}"))
+        val spawned = spawn(("--watch", "{foo.fooCommand,bar.barCommand}"))
 
-          assertEventually {
-            spawned.out.text().contains(
-              "Computing fooCommand"
-            ) && spawned.out.text().contains("Computing barCommand")
-          }
+        assertEventually {
+          spawned.out.text().contains(
+            "Computing fooCommand"
+          ) && spawned.out.text().contains("Computing barCommand")
+        }
 
-          // Check method body code changes correctly trigger downstream evaluation
-          spawned.clear()
-          modifyFile(workspacePath / "build.mill", _.replace("\"barHelper \"", "\"barHelper! \""))
-          assertEventually {
-            !spawned.out.text().contains("Computing fooCommand") &&
-            spawned.out.text().contains("Computing barCommand")
-          }
+        // Check method body code changes correctly trigger downstream evaluation
+        spawned.clear()
+        modifyFile(workspacePath / "build.mill", _.replace("\"barHelper \"", "\"barHelper! \""))
+        assertEventually {
+          !spawned.out.text().contains("Computing fooCommand") &&
+          spawned.out.text().contains("Computing barCommand")
+        }
 
-          // Check module body code changes correctly trigger downstream evaluation
-          spawned.clear()
-          modifyFile(
-            workspacePath / "build.mill",
-            _.replace("object foo extends Module {", "object foo extends Module { println(123)")
-          )
-          assertEventually {
-            spawned.out.text().contains("Computing fooCommand") &&
-            !spawned.out.text().contains("Computing barCommand")
-          }
+        // Check module body code changes correctly trigger downstream evaluation
+        spawned.clear()
+        modifyFile(
+          workspacePath / "build.mill",
+          _.replace("object foo extends Module {", "object foo extends Module { println(123)")
+        )
+        assertEventually {
+          spawned.out.text().contains("Computing fooCommand") &&
+          !spawned.out.text().contains("Computing barCommand")
         }
       }
     }
