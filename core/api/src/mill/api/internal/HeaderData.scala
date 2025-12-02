201d5c94e1bdfac709050fd55ffe3fe40e7a589b
Li Haoyi <haoyi.sg@gmail.com>
2025-12-02 10:07:02 +0800

    move headerData to its own file

--- core/api/src/mill/api/internal/HeaderData.scala
+++ core/api/src/mill/api/internal/HeaderData.scala
@@ -1,12 +1,12 @@
 package mill.api.internal
 
 private[mill] case class HeaderData(
-                                     `extends`: Located[Seq[Located[String]]] = Located(null, -1, Nil),
-                                     moduleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
-                                     compileModuleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
-                                     runModuleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
-                                     @upickle.implicits.flatten rest: Map[String, upickle.core.BufferedValue]
-                                   )
+    `extends`: Located[Seq[Located[String]]] = Located(null, -1, Nil),
+    moduleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
+    compileModuleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
+    runModuleDeps: Located[Seq[Located[String]]] = Located(null, -1, Nil),
+    @upickle.implicits.flatten rest: Map[String, upickle.core.BufferedValue]
+)
 private[mill] object HeaderData {
 
   import upickle.core.BufferedValue
