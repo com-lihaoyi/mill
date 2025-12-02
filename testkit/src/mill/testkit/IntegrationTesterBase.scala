com-lihaoyi/mill

commit 6287f4cc0ff50dd97a2f2e17241627b8bbdd5c80

Clean up integration test setup (#6283)
- Only use `compile().dest` in the test classpath so stray `out/`
folders in the integration test `resources/` dir doesn't cause blowups
during test discovery

- Wipe out the `out/` folder during integration test setup in all modes
except `.shared`, to try harder to ensure leftover `out/` folders from
`dist.run` don't interfere with test execution

integration/package.mill
@@ -22,11 +22,11 @@ object `package` extends mill.Module {
   // test it in the `test` CrossModule. We pass `test`'s sources to `lib` to
   // and pass `lib`'s compile output back to `test`.
 
-  def localOutFolder = Task.Worker {
+  def sharedOutFolder = Task.Worker {
     val counter = new java.util.concurrent.atomic.AtomicInteger(0)
     new mill.util.CachedFactory[Unit, os.Path] {
       def setup(key: Unit): os.Path = {
-        val res = Task.dest / s"local-${counter.getAndIncrement()}" / "out"
+        val res = Task.dest / s"shared-${counter.getAndIncrement()}" / "out"
         os.remove.all(res)
         os.makeDir.all(res)
         res
@@ -51,12 +51,14 @@ object `package` extends mill.Module {
     def forkEnv: T[Map[String, String]]
     def forkArgs: T[Seq[String]]
     def testExclusive = false
-
+    def testClasspath: T[Seq[PathRef]]
     trait ModeModule extends MillBaseTestsModule {
+      override def testClasspath: T[Seq[PathRef]] = IntegrationTestModule.this.testClasspath()
+      def useSharedOut = false
       override def enableBsp = false
       override def testForked(args: String*) = Task.Command(exclusive = testExclusive) {
-        val useSharedOut = moduleSegments.parts.contains("local")
-        localOutFolder().withValue(()) { sharedOutFolder =>
+
+        sharedOutFolder().withValue(()) { sharedOutFolder =>
           val testModuleUtil = new mill.javalib.TestModuleUtil(
             testUseArgsFile(),
             forkArgs(),
@@ -117,10 +119,11 @@ object `package` extends mill.Module {
     override def defaultTask(): String = "testForked"
     def testForked(args: String*): Task.Command[(msg: String, results: Seq[TestResult])] =
       Task.Command {
-        packaged.daemon.testForked(args*)()
+        shared.daemon.testForked(args*)()
       }
 
-    object local extends IntegrationLauncherModule {
+    object shared extends IntegrationLauncherModule {
+      def useSharedOut = true
       def millIntegrationLauncher = build.dist.launcher()
       def millIntegrationIsPackagedLauncher = Task(false)
     }
@@ -156,6 +159,7 @@ object `package` extends mill.Module {
   object bootstrap extends Cross[IdeIntegrationCrossModule](build.listCross)
   object migrating extends Cross[IdeIntegrationCrossModule](build.listCross)
   trait IntegrationCrossModule extends MillScalaModule with IntegrationTestModule {
+    override def testClasspath = Seq(compile().classes)
     override lazy val test: MillScalaTests = new MillScalaTests {
       // This `test` module isn't used, since `this` contains the test code we want to
       // compile and the `ModeModule`s contain the entrypoints to run that code

testkit/src/mill/testkit/IntegrationTesterBase.scala
@@ -47,21 +47,25 @@ trait IntegrationTesterBase {
   def initWorkspace(): Unit = {
     println(s"Preparing integration test in $workspacePath")
     os.makeDir.all(workspacePath)
-    if (!sys.env.contains("MILL_TEST_SHARED_OUTPUT_DIR"))
+    if (!sys.env.contains("MILL_TEST_SHARED_OUTPUT_DIR")) {
       Retry(logger = Retry.printStreamLogger(System.err)) {
         val tmp = os.temp.dir()
         val outDir = os.Path(out, workspacePath)
         if (os.exists(outDir)) os.move.into(outDir, tmp)
         os.remove.all(tmp)
       }
-
-    os.list(workspacePath).foreach { p =>
-      if (p.last != "out") os.remove.all(p)
+      for (p <- os.list(workspacePath)) os.remove.all(p)
+    } else {
+      // if `MILL_TEST_SHARED_OUTPUT_DIR` is provided, keep `out/` intact
+      // to re-use the daemon
+      for (p <- os.list(workspacePath) if p.last != "out") os.remove.all(p)
     }
+
     val outRelPathOpt = os.FilePath(out) match {
       case relPath: os.RelPath if relPath.ups == 0 => Some(relPath)
       case _ => None
     }
+
     os.list(workspaceSourcePath)
       .filter(
         outRelPathOpt match {

example/package.mill
@@ -220,7 +220,7 @@ object `package` extends Module {
     // disable scalafix because these example modules don't have sources causing it to misbehave
     def testRepoSourceRoot: T[PathRef] = Task.Source(moduleDir)
     def testRepoRoot: T[PathRef] = Task { testRepoSourceRoot() }
-
+    def testClasspath: T[Seq[PathRef]] = Nil
     def sources0 = Task.Sources(moduleDir)
     def sources = Task {
       sources0()

.github/workflows/run-tests.yml
@@ -21,7 +21,7 @@
 #      - uses: sbt/setup-sbt@v1
 #
 #
-#      - run:  ./mill 'integration.migrating[init].local.daemon.testOnly' mill.integration.MillInitSbtGatlingTests
+#      - run:  ./mill 'integration.migrating[init].shared.daemon.testOnly' mill.integration.MillInitSbtGatlingTests
 #
 
 
@@ -142,16 +142,16 @@ jobs:
             millargs: "'libs.{scalajslib,scalanativelib,kotlinlib,pythonlib,javascriptlib}.__.test'"
 
           - java-version: 17
-            millargs: "example.kotlinlib.__.local.daemon"
+            millargs: "example.kotlinlib.__.shared.daemon"
 
           - java-version: 17
-            millargs: "example.scalalib.__.local.daemon"
+            millargs: "example.scalalib.__.shared.daemon"
 
           - java-version: 21
-            millargs: "example.javalib.__.local.daemon"
+            millargs: "example.javalib.__.shared.daemon"
 
           - java-version: 17
-            millargs: "'example.androidlib.__.local.daemon'"
+            millargs: "'example.androidlib.__.shared.daemon'"
             setup-android: true
 
           - java-version: 17
@@ -159,14 +159,14 @@ jobs:
             setup-android: true
 
           - java-version: 24
-            millargs: "'example.migrating.scalalib.__.local.daemon'"
+            millargs: "'example.migrating.scalalib.__.shared.daemon'"
             install-sbt: true
 
           - java-version: 24
-            millargs: "'example.{pythonlib,javascriptlib}.__.local.daemon'"
+            millargs: "'example.{pythonlib,javascriptlib}.__.shared.daemon'"
 
           - java-version: 21
-            millargs: "'example.{cli,fundamentals,depth,extending,large}.__.local.daemon'"
+            millargs: "'example.{cli,fundamentals,depth,extending,large}.__.shared.daemon'"
 
           - java-version: 21
             millargs: "'integration.{feature,ide}.__.packaged.daemon'"

commit b954b325cf0c9f597ea6140b6de4ad358489aaf5

Fix re-use of out folder in shared tests (#6288)
This regressed in https://github.com/com-lihaoyi/mill/pull/6283 and has
been slowing down our CI considerably

integration/package.mill
@@ -52,9 +52,10 @@ object `package` extends mill.Module {
     def forkArgs: T[Seq[String]]
     def testExclusive = false
     def testClasspath: T[Seq[PathRef]]
+
     trait ModeModule extends MillBaseTestsModule {
       override def testClasspath: T[Seq[PathRef]] = IntegrationTestModule.this.testClasspath()
-      def useSharedOut = false
+      def useSharedOut: Boolean
       override def enableBsp = false
       override def testForked(args: String*) = Task.Command(exclusive = testExclusive) {
 
@@ -123,7 +124,7 @@ object `package` extends mill.Module {
       }
 
     object shared extends IntegrationLauncherModule {
-      def useSharedOut = true
+      override def useSharedOut = true
       def millIntegrationLauncher = build.dist.launcher()
       def millIntegrationIsPackagedLauncher = Task(false)
     }
@@ -136,14 +137,17 @@ object `package` extends mill.Module {
       def millIntegrationIsPackagedLauncher = Task(true)
     }
     trait IntegrationLauncherModule extends Module {
+      def useSharedOut = false
       def millIntegrationLauncher: T[PathRef]
       def millIntegrationIsPackagedLauncher: Task[Boolean]
       object nodaemon extends ModeModule {
+        override def useSharedOut = IntegrationLauncherModule.this.useSharedOut
         def millIntegrationLauncher = IntegrationLauncherModule.this.millIntegrationLauncher
         def millIntegrationIsPackagedLauncher =
           IntegrationLauncherModule.this.millIntegrationIsPackagedLauncher
       }
       object daemon extends ModeModule {
+        override def useSharedOut = IntegrationLauncherModule.this.useSharedOut
         def millIntegrationLauncher = IntegrationLauncherModule.this.millIntegrationLauncher
         def millIntegrationIsPackagedLauncher =
           IntegrationLauncherModule.this.millIntegrationIsPackagedLauncher

testkit/src/mill/testkit/IntegrationTesterBase.scala
@@ -48,6 +48,7 @@ trait IntegrationTesterBase {
     println(s"Preparing integration test in $workspacePath")
     os.makeDir.all(workspacePath)
     if (!sys.env.contains("MILL_TEST_SHARED_OUTPUT_DIR")) {
+      println("Clearing out folder")
       Retry(logger = Retry.printStreamLogger(System.err)) {
         val tmp = os.temp.dir()
         val outDir = os.Path(out, workspacePath)
@@ -56,6 +57,7 @@ trait IntegrationTesterBase {
       }
       for (p <- os.list(workspacePath)) os.remove.all(p)
     } else {
+      println("Re-using out folder")
       // if `MILL_TEST_SHARED_OUTPUT_DIR` is provided, keep `out/` intact
       // to re-use the daemon
       for (p <- os.list(workspacePath) if p.last != "out") os.remove.all(p)