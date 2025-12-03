006508889a24490d016e2fe5a3ddfdee48606c1b
Li Haoyi <haoyi.sg@gmail.com>
2025-12-03 15:16:03 +0800

    Replace `JvmWorkerImpl`'s `CachedFactoryWithInitData` with `RefCountedCache` (#6295)
    Fixes https://github.com/com-lihaoyi/mill/issues/6299
    
    `CachedFactoryWithInitData` is not the right abstraction because it
    instantiates a new `Value` for every concurrent thread. In this case
    these `Value`s point at the same `ZincWorkerMain` daemon due to the
    filesystem path being the same, but that results in the `teardown` logic
    being called more than once on the same daemon, causing problems with
    multiple threads having the `processId` or `daemonLock` files open and
    thus any one thread being unable to delete them without coordinating
    with all the other threads to first release their file references
    
    `RefCountedCache` re-uses the same `Value` instance across all threads,
    which more accurately models the lifecycle of how the `ZincWorkerMain`
    processes are managed. As a result any `ZincWorkerMain` process will
    only be torn down _once_, after _all_ threads have released it.
    
    Furthermore, we must make sure we `.close` the filesystem locks before
    we remove the file, because if we don't on windows removing the file
    will fail due to an open file handle even though it's all in the same
    process
    
    Tested locally, this seems to allow bootstrapped `./mill-assembly.jar
    dist.executable + core.api.test.compile + testkit.compile` to complete,
    where the previous implementation would fail due to conflicting
    teardowns causing deadlocks or hangs. And since we now correctly only
    tear down once all references to a daemon are released, hopefully that
    will avoid the nondeterministic failures we were getting before that may
    be caused by the daemon being killed by some other thread

--- libs/javalib/worker/src/mill/javalib/worker/JvmWorkerImpl.scala
+++ libs/javalib/worker/src/mill/javalib/worker/JvmWorkerImpl.scala
@@ -8,7 +8,7 @@ import mill.constants.DaemonFiles
 import mill.javalib.api.internal.*
 import mill.javalib.api.JvmWorkerArgs
 import mill.javalib.zinc.{ZincApi, ZincWorker}
-import mill.util.{CachedFactoryWithInitData, Jvm}
+import mill.util.{Jvm, RefCountedCache}
 import sbt.internal.util.ConsoleOut
 
 import java.nio.file.FileSystemException
@@ -55,21 +55,14 @@ class JvmWorkerImpl(args: JvmWorkerArgs) extends InternalJvmWorkerApi with AutoC
     close0() // make sure this is invoked last as it closes the classloader that we need for other `.close` calls
   }
 
-  private val subprocessCache = new CachedFactoryWithInitData[
+  private val subprocessCache: RefCountedCache[
+    SubprocessZincApi.Key,
     SubprocessZincApi.Key,
     SubprocessZincApi.Initialize,
     SubprocessZincApi.Value
-  ] {
-    override def maxCacheSize: Int = jobs
-
-    override def cacheEntryStillValid(
-        key: SubprocessZincApi.Key,
-        initData: => SubprocessZincApi.Initialize,
-        value: SubprocessZincApi.Value
-    ): Boolean = value.launchedServer.isAlive
-
-    private var memoryLocksByDaemonDir = Map.empty[os.Path, MemoryLock]
-    private def memLockFor(daemonDir: os.Path): MemoryLock = {
+  ] = {
+    var memoryLocksByDaemonDir = Map.empty[os.Path, MemoryLock]
+    def memLockFor(daemonDir: os.Path): MemoryLock = {
       memoryLocksByDaemonDir.synchronized {
         memoryLocksByDaemonDir.get(daemonDir) match {
           case Some(lock) => lock
@@ -81,80 +74,87 @@ class JvmWorkerImpl(args: JvmWorkerArgs) extends InternalJvmWorkerApi with AutoC
       }
     }
 
-    override def setup(
-        key: SubprocessZincApi.Key,
-        init: SubprocessZincApi.Initialize
-    ): SubprocessZincApi.Value = {
+    new RefCountedCache[
+      SubprocessZincApi.Key,
+      SubprocessZincApi.Key,
+      SubprocessZincApi.Initialize,
+      SubprocessZincApi.Value
+    ](
+      convertKey = identity,
+      setup = (key, _, init) => {
+        val workerDir = init.taskDest / "zinc-worker" / key.hashCode.toString
+        val daemonDir = workerDir / "daemon"
 
-      val workerDir = init.taskDest / "zinc-worker" / key.hashCode.toString
-      val daemonDir = workerDir / "daemon"
+        os.makeDir.all(daemonDir)
+        os.write.over(workerDir / "java-home", key.javaHome.map(_.toString).getOrElse("<default>"))
+        os.write.over(workerDir / "java-runtime-options", key.runtimeOptions.mkString("\n"))
 
-      os.makeDir.all(daemonDir)
-      os.write.over(workerDir / "java-home", key.javaHome.map(_.toString).getOrElse("<default>"))
-      os.write.over(workerDir / "java-runtime-options", key.runtimeOptions.mkString("\n"))
-
-      val mainClass = "mill.javalib.zinc.ZincWorkerMain"
-      val locks = {
+        val mainClass = "mill.javalib.zinc.ZincWorkerMain"
         val fileLocks = Locks.files(daemonDir.toString)
-        Locks(
-          // File locks are non-reentrant, so we need to lock on the memory lock first.
-          //
-          // We can get multiple lock acquisitions when we compile several modules in parallel,
-          DoubleLock(memLockFor(daemonDir), fileLocks.launcherLock),
-          // We never take the daemon lock, just check if it's already taken
-          fileLocks.daemonLock
-        )
-      }
-
-      val launched = ServerLauncher.launchOrConnectToServer(
-        locks,
-        daemonDir.toNIO,
-        10 * 1000,
-        () => {
-          val process = Jvm.spawnProcess(
-            mainClass = mainClass,
-            mainArgs = Seq(daemonDir.toString, jobs.toString),
-            javaHome = key.javaHome,
-            jvmArgs = key.runtimeOptions,
-            classPath = classPath
+        val locks = {
+          Locks(
+            // File locks are non-reentrant, so we need to lock on the memory lock first.
+            //
+            // We can get multiple lock acquisitions when we compile several modules in parallel,
+            DoubleLock(memLockFor(daemonDir), fileLocks.launcherLock),
+            // We never take the daemon lock, just check if it's already taken
+            fileLocks.daemonLock
           )
-          LaunchedServer.OsProcess(process.wrapped.toHandle)
-        },
-        processDied =>
-          throw IllegalStateException(
-            s"""Failed to launch '$mainClass' for:
-               |  javaHome = ${key.javaHome}
-               |  runtimeOptions = ${key.runtimeOptions.mkString(",")}
-               |  daemonDir = $daemonDir
-               |
-               |Failure:
-               |$processDied
-               |""".stripMargin
-          ),
-        _ => (),
-        false // openSocket
-      )
-
-      SubprocessZincApi.Value(launched.port, daemonDir, launched.launchedServer)
-    }
-
-    override def teardown(key: SubprocessZincApi.Key, value: SubprocessZincApi.Value): Unit = {
-      os.remove(value.daemonDir / DaemonFiles.processId)
-      while (value.launchedServer.isAlive) Thread.sleep(1)
-
-      // On Windows it takes some time until the file handles are released, so we have
-      // to wait for that as well.
-      if (scala.util.Properties.isWin) {
-        val daemonLock = value.daemonDir / DaemonFiles.daemonLock
-
-        def tryRemoving(): Boolean = {
-          try { os.remove(daemonLock); true }
-          catch { case _: FileSystemException => false }
         }
 
-        while (!tryRemoving()) Thread.sleep(10)
+        val launched = ServerLauncher.launchOrConnectToServer(
+          locks,
+          daemonDir.toNIO,
+          10 * 1000,
+          () => {
+            val process = Jvm.spawnProcess(
+              mainClass = mainClass,
+              mainArgs = Seq(daemonDir.toString, jobs.toString),
+              javaHome = key.javaHome,
+              jvmArgs = key.runtimeOptions,
+              classPath = classPath
+            )
+            LaunchedServer.OsProcess(process.wrapped.toHandle)
+          },
+          processDied =>
+            throw IllegalStateException(
+              s"""Failed to launch '$mainClass' for:
+                 |  javaHome = ${key.javaHome}
+                 |  runtimeOptions = ${key.runtimeOptions.mkString(",")}
+                 |  daemonDir = $daemonDir
+                 |
+                 |Failure:
+                 |$processDied
+                 |""".stripMargin
+            ),
+          _ => (),
+          false // openSocket
+        )
+
+        SubprocessZincApi.Value(launched.port, daemonDir, launched.launchedServer, locks)
+      },
+      closeValue = value => {
+        os.remove(value.daemonDir / DaemonFiles.processId)
+        while (value.launchedServer.isAlive) Thread.sleep(1)
+
+        try value.lock.close()
+        catch { case _ => () }
+        // On Windows it takes some time until the file handles are released, so we have
+        // to wait for that as well.
+        if (scala.util.Properties.isWin) {
+          val daemonLock = value.daemonDir / DaemonFiles.daemonLock
+
+          def tryRemoving(): Boolean = {
+            try { os.remove.all(daemonLock); true }
+            catch {
+              case _: FileSystemException => false
+            }
+          }
+
+          while (!tryRemoving()) Thread.sleep(10)
+        }
       }
-    }
+    )
   }
 
   /** Gives you API for the [[zincLocalWorker]] instance. */
