c37a1a6c9955dacd1ce54cbdcc36fe7a88ab756d
Li Haoyi <haoyi.sg@gmail.com>
2025-12-03 13:30:34 +0800

    .

--- libs/daemon/client/src/mill/client/ServerLauncher.java
+++ libs/daemon/client/src/mill/client/ServerLauncher.java
@@ -97,7 +97,6 @@ public abstract class ServerLauncher {
   public static class Launched implements AutoCloseable {
     public int port;
     public Socket socket;
-    public Locks locks;
     public LaunchedServer launchedServer;
 
     @Override
@@ -155,7 +154,6 @@ public abstract class ServerLauncher {
             } else if (result instanceof ServerLaunchResult.AlreadyRunning) {
               launched.launchedServer = ((ServerLaunchResult.AlreadyRunning) result).server;
             }
-            launched.locks = locks;
             return Optional.of(launched);
           } else {
             var processDied = (ServerLaunchResult.ServerDied) result;
