81fab4fedd93bed8c7d7976499da3be472502545
Li Haoyi <haoyi.sg@gmail.com>
2025-12-04 13:12:09 +0800

    Add `mill.api.Debug` helper (#6311)
    Easier to use than constantly writing
    `mill.constants.DebugLog.println("foo " + pprint.apply(foo))`
    
    Also add a check that `DebugLog.println` isn't run in CI, so we don't
    accidentally forget to remove some calls before publishing

---
+++ core/api/src/mill/api/Debug.scala
@@ -0,0 +1,33 @@
+package mill.api
+
+import mill.api.daemon.experimental
+
+import java.io.{ByteArrayOutputStream, PrintStream}
+
+@experimental
+object Debug {
+  def apply[T](
+      x: sourcecode.Text[T],
+      tag: String = "",
+      width: Int = 100,
+      height: Int = 500,
+      indent: Int = 2,
+      escapeUnicode: Boolean = true,
+      showFieldNames: Boolean = true
+  )(implicit line: sourcecode.Line, fileName: sourcecode.FileName): T = {
+
+    val baos = new ByteArrayOutputStream()
+    pprint.logTo(
+      x,
+      tag,
+      width,
+      height,
+      indent,
+      escapeUnicode,
+      showFieldNames,
+      new PrintStream(baos)
+    )
+    mill.constants.DebugLog.println(baos.toString)
+    x.value
+  }
+}
