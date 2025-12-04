3c591c9d3f7c73478f14c3be70a6c0425d2a0dde
Li Haoyi <haoyi.sg@gmail.com>
2025-12-04 16:00:47 +0800

    .

--- integration/ide/gen-idea/src/GenIdeaUtils.scala
+++ integration/ide/gen-idea/src/GenIdeaUtils.scala
@@ -7,6 +7,11 @@ import scala.util.Try
 
 object GenIdeaUtils {
 
+  private lazy val coursierVersion = sys.props.getOrElse(
+    "mill.integration.coursier-version",
+    sys.error("Java property mill.integration.coursier-version not set")
+  )
+
   /**
    * The resource content will be loaded from the classpath and matched against the file.
    * It may contain the `<!-- IGNORE -->` String, to simulate wildcard-matches.
@@ -73,6 +78,7 @@ object GenIdeaUtils {
       .replace("//$USER_HOME$/AppData/Local/Coursier/cache/", "//$USER_HOME$/COURSIER_CACHE/")
       .replace("//$USER_HOME$/.cache/coursier/", "//$USER_HOME$/COURSIER_CACHE/")
       .replace("//$USER_HOME$/Library/Caches/Coursier/", "//$USER_HOME$/COURSIER_CACHE/")
+      .replace("-$coursierVersion.jar", ".jar")
   }
 
   val ignoreString = "<!-- IGNORE -->"
