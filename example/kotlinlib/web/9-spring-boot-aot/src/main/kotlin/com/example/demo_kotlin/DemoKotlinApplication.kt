5603aadb5309d31b2fb5fe2017bf85fb75ac07b2
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-12-04 12:00:12 +0200

    Spring AOT example with Kotlin

---
+++ example/kotlinlib/web/9-spring-boot-aot/src/main/kotlin/com/example/demo_kotlin/DemoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoKotlinApplication>(*args)
+}
---
+++ example/kotlinlib/web/9-spring-boot-aot/src/test/kotlin/com/example/demo_kotlin/DemoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
