ce8866c9414934cbb219a39a6b0d8f03a38f64bd
Li Haoyi <haoyi.sg@gmail.com>
2025-12-04 12:53:51 +0800

    ..

--- core/api/src/mill/api/Debug.scala
+++ core/api/src/mill/api/Debug.scala
@@ -6,16 +6,16 @@ import java.io.{ByteArrayOutputStream, PrintStream}
 
 @experimental
 object Debug {
-  def debugLog[T](x: sourcecode.Text[T],
-                  tag: String = "",
-                  width: Int = 100,
-                  height: Int = 500,
-                  indent: Int = 2,
-                  escapeUnicode: Boolean = true,
-                  showFieldNames: Boolean = true)
-                 (implicit line: sourcecode.Line,
-                  fileName: sourcecode.FileName): T = {
-    
+  def apply[T](x: sourcecode.Text[T],
+               tag: String = "",
+               width: Int = 100,
+               height: Int = 500,
+               indent: Int = 2,
+               escapeUnicode: Boolean = true,
+               showFieldNames: Boolean = true)
+              (implicit line: sourcecode.Line,
+               fileName: sourcecode.FileName): T = {
+
     val baos = new ByteArrayOutputStream()
     pprint.logTo(x, tag, width, height, indent, escapeUnicode, showFieldNames, new PrintStream(baos))
     mill.constants.DebugLog.println(baos.toString)
