5429d6fdcc2ef1743ade9bdf4d098d514448a2ad
Li Haoyi <haoyi.sg@gmail.com>
2025-12-05 10:53:03 +0800

    wip

--- example/javalib/basic/4-module-deps/bar/src/bar/Bar.java
+++ example/javalib/basic/4-module-deps/bar/src/bar/Bar.java
@@ -6,7 +6,7 @@ import org.thymeleaf.context.Context;
 public class Bar {
   public static String generateHtml(String text) {
     Context context = new Context();
-    context.setVariable("text", text);
+    context.setVariae("text", text);
     return new TemplateEngine().process("<h1 th:text=\"${text}\"></h1>", context);
   }
 }
