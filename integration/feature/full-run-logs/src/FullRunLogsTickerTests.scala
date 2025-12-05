12ad9bc4ae806f5044148b1dd00d4bfbab0c7aa9
Li Haoyi <haoyi.sg@gmail.com>
2025-12-05 12:20:13 +0800

    .

---
+++ integration/feature/full-run-logs/src/FullRunLogsTickerTests.scala
@@ -0,0 +1,118 @@
+package mill.integration
+import mill.constants.OutFiles.OutFiles
+import mill.testkit.UtestIntegrationTestSuite
+import utest.*
+
+// Run simple commands on a simple build and check their entire output and some
+// metadata files, ensuring we don't get spurious warnings or logging messages
+// slipping in and the important parts of the logs and output files are present
+object FullRunLogsTickerTests extends UtestIntegrationTestSuite {
+
+  import FullRunLogsUtils.normalize
+
+  def tests: Tests = Tests {
+    test("noticker") - integrationTest { tester =>
+      import tester.*
+
+      val res = eval(("--ticker", "false", "run", "--text", "hello"), propagateEnv = false)
+
+      res.isSuccess ==> true
+      assert(res.out == "<h1>hello</h1>")
+      val normalized = normalize(res.result.err.text())
+
+      assertGoldenLiteral(
+        normalized,
+        List(
+          "compiling 3 Scala sources to out/mill-build/compile.dest/classes ...",
+          "done compiling",
+          "compiling 1 Java source to out/compile.dest/classes ...",
+          "done compiling"
+        )
+      )
+    }
+
+    test("ticker") - integrationTest { tester =>
+      import tester.*
+
+      val res = eval(("--ticker", "true", "run", "--text", "hello"), propagateEnv = false)
+      res.isSuccess ==> true
+
+      assertGoldenLiteral(
+        normalize(res.out),
+        List("<h1>hello</h1>")
+      )
+
+      assertGoldenLiteral(
+        normalize(res.result.err.text()),
+        // Should have no colors because we called it programmatically
+        List(
+          "============================== run --text hello ==============================",
+          "build.mill-<digits>] compile compiling 3 Scala sources to out/mill-build/compile.dest/classes ...",
+          "build.mill-<digits>] done compiling",
+          "<digits>] compile compiling 1 Java source to out/compile.dest/classes ...",
+          "<digits>] done compiling",
+          "<digits>] run",
+          "63/<digits>] ============================== run --text hello =============================="
+        )
+      )
+    }
+    test("exclusive") - integrationTest { tester =>
+      import tester.*
+
+      val res = eval(
+        ("--ticker", "true", "exclusives.printingC"),
+        mergeErrIntoOut = true
+      )
+      assert(res.isSuccess)
+
+      // Make sure when running `exclusive` tasks, we always print the name of the task
+      // before it starts, we turn off the ticker and otherwise there's no way to know what
+      // task each section of logs belongs to
+      assertGoldenLiteral(
+        normalize(res.result.out.text()),
+        List(
+          "============================== exclusives.printingC ==============================",
+          "build.mill-<digits>] compile compiling 3 Scala sources to out/mill-build/compile.dest/classes ...",
+          "build.mill-<digits>] done compiling",
+          "<digits>] exclusives.printingA",
+          "Hello A",
+          "<digits>] exclusives.empty",
+          "<digits>] exclusives.printingB",
+          "Hello B",
+          "World B",
+          "<digits>] exclusives.printingC",
+          "Hello C",
+          "World C",
+          "Exclusive C",
+          "4/<digits>] ============================== exclusives.printingC =============================="
+        )
+      )
+    }
+    test("logging") - integrationTest { tester =>
+      import tester.*
+
+      val res = eval(
+        ("--ticker", "true", "logging"),
+        mergeErrIntoOut = true
+      )
+      assert(res.isSuccess)
+
+      // Make sure when running `exclusive` tasks, we always print the name of the task
+      // before it starts, we turn off the ticker and otherwise there's no way to know what
+      // task each section of logs belongs to
+      assertGoldenLiteral(
+        normalize(res.result.out.text()),
+        List(
+          "============================== logging ==============================",
+          "build.mill-<digits>] compile compiling 3 Scala sources to out/mill-build/compile.dest/classes ...",
+          "build.mill-<digits>] done compiling",
+          "<digits>] logging MY PRINTLN",
+          "<digits>] MY INFO LOGS",
+          "<digits>] MY WARN LOGS",
+          "<digits>] MY WARN LOGS",
+          "1/<digits>] ============================== logging =============================="
+        )
+      )
+    }
+  }
+}
---
+++ integration/feature/full-run-logs/src/FullRunLogsUtils.scala
@@ -0,0 +1,30 @@
+package mill.integration
+import mill.constants.OutFiles.OutFiles
+import mill.testkit.UtestIntegrationTestSuite
+import utest.*
+
+// Run simple commands on a simple build and check their entire output and some
+// metadata files, ensuring we don't get spurious warnings or logging messages
+// slipping in and the important parts of the logs and output files are present
+object FullRunLogsUtils  {
+
+  def normalize(s: String) = s
+    .replace(fansi.Attr.Reset.escape, "(Z)")
+    .replace(fansi.Color.Reset.escape, "(X)")
+    .replace(fansi.Color.Red.escape, "(R)")
+    .replace(fansi.Color.Green.escape, "(G)")
+    .replace(fansi.Color.Blue.escape, "(B)")
+    .replace(fansi.Color.Cyan.escape, "(C)")
+    .replace(fansi.Color.Magenta.escape, "(M)")
+    .replace(fansi.Color.Yellow.escape, "(Y)")
+    .replace('\\', '/')
+    .replaceAll("\\(([a-zA-Z.]+):\\d+\\)", "($1:<digits>)")
+    .replaceAll("\\d+]", "<digits>]")
+    .replaceAll("\\d+]", "<digits>]")
+    .replaceAll("\\d+/\\d+", ".../...")
+    .replaceAll("\\d+\\.\\d+", ".../...")
+    .replaceAll(" \\d+s", "")
+    .linesIterator
+    .toList
+
+}
