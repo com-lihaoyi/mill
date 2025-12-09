cc57cba01d850d6a5ce7f28afa342996d6abbc6e
Li Haoyi <haoyi.sg@gmail.com>
2025-12-09 12:57:40 +0800

    .

--- integration/feature/jvm-worker-reuse/src/JvmWorkerReuseTests.scala
+++ integration/feature/jvm-worker-reuse/src/JvmWorkerReuseTests.scala
@@ -18,25 +18,18 @@ object JvmWorkerReuseTests extends UtestIntegrationTestSuite {
 
       tester.eval("foo.compile", check = true)
       assert(getProcessIds().size == 1)
-//      tester.eval("foo.run", check = true)
-//      pprint.log(getProcessId())
+
       tester.eval("bar.compile", check = true)
       // bar has same jvmId as foo, ZincWorkerMain should be reused
       assert(getProcessIds().size == 1)
-//      tester.eval("bar.run", check = true)
-//      pprint.log(getProcessId())
+
       tester.eval("qux.compile", check = true)
       // qux has different jvmId as bar, ZincWorkerMain should be re-created
       assert(getProcessIds().size == 2)
-//      pprint.log(getProcessId())
-//      tester.eval("qux.run", check = true)
-//      pprint.log(getProcessId())
+
       tester.eval("baz.compile", check = true)
       // baz has same jvmId as qux, ZincWorkerMain should be reused
       assert(getProcessIds().size == 2)
-//      pprint.log(getProcessId())
-//      tester.eval("baz.run", check = true)
-//      pprint.log(getProcessId())
     }
   }
 }
