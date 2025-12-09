973543413f5e3e1550477a103e4d4b3a72f1e526
Tobias Roeser <le.petit.fou@web.de>
2025-12-09 21:42:30 +0100

    Update Scala 2.13 to 2.13.18 (#6358)
    Pull request: https://github.com/com-lihaoyi/mill/pull/6358

--- mill-build/src/millbuild/Deps.scala
+++ mill-build/src/millbuild/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   val scalaVersion = "3.8.0-RC3"
 
   val scalaVersionJava11 = "3.7.4"
-  val scala2Version = "2.13.15"
+  val scala2Version = "2.13.18"
   // The Scala 2.12.x version to use for some workers
   val workerScalaVersion212 = "2.12.20"
   val sbtScalaVersion212 = workerScalaVersion212
