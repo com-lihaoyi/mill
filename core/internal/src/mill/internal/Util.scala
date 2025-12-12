fa2ffb6935cb7a6c471ad074b327aa095dbc5503
Li Haoyi <haoyi.sg@gmail.com>
2025-12-12 21:53:14 +0800

    .

--- core/internal/src/mill/internal/Util.scala
+++ core/internal/src/mill/internal/Util.scala
@@ -208,18 +208,8 @@ object Util {
               node match {
                 case scalar: ScalarNode =>
                   val value = scalar.getValue
-                  val tag = scalar.getTag.getValue
-                  tag match {
-                    case "tag:yaml.org,2002:null" => v.visitNull(index)
-                    case "tag:yaml.org,2002:bool" =>
-                      if (value == "true") v.visitTrue(index)
-                      else v.visitFalse(index)
-                    case "tag:yaml.org,2002:int" =>
-                      v.visitFloat64StringParts(value, -1, -1, index)
-                    case "tag:yaml.org,2002:float" =>
-                      v.visitFloat64StringParts(value, -1, -1, index)
-                    case _ => v.visitString(value, index)
-                  }
+                  scalar.getTag.getValue
+                  v.visitString(value, index)
 
                 case mapping: MappingNode =>
                   val objVisitor =
