ec87745d659c9069093742dc57ac101e9f5f1900
Li Haoyi <haoyi.sg@gmail.com>
2025-12-26 11:32:46 +0800

    Rebootstrap (#6445)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- libs/javalib/src/mill/javalib/Lib.scala
+++ libs/javalib/src/mill/javalib/Lib.scala
@@ -114,8 +114,10 @@ object Lib {
       )
 
   def scalaConsoleMvnDeps(scalaOrganization: String, scalaVersion: String): Seq[Dep] = {
-    if (scalaVersion.startsWith("3.8.") ||
-        mill.util.Version.isAtLeast(scalaVersion, "3.8")(using mill.util.Version.MavenOrdering)) {
+    if (
+      scalaVersion.startsWith("3.8.") ||
+      mill.util.Version.isAtLeast(scalaVersion, "3.8")(using mill.util.Version.MavenOrdering)
+    ) {
       // Since Scala 3.8, the repl is no longer part of the compiler jar
       Seq(mvn"$scalaOrganization::scala3-repl:$scalaVersion")
     } else {
