aaa1cf6c8e5879759a370f3215c7d5e1efb68e60
Li Haoyi <haoyi.sg@gmail.com>
2026-01-03 13:56:50 +0800

    .

--- example/pythonlib/web/2-todo-flask/todo/src/app.py
+++ example/pythonlib/web/2-todo-flask/todo/src/app.py
@@ -94,5 +94,5 @@ def toggle_all(state):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port=port)
