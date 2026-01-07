cb0a499f785c766043efe79b618dfbf2107b9dd5
Li Haoyi <haoyi.sg@gmail.com>
2026-01-07 18:54:51 +0800

    .

---
+++ libs/javalib/test/resources/jvm-worker-reuse/qux/src/qux/Qux.java
@@ -0,0 +1,7 @@
+package qux;
+
+public class Qux {
+  public static void main(String[] args) {
+    System.out.println("Qux");
+  }
+}
---
+++ libs/scalalib/test/resources/compiled-classes-semanticdb/test/src/app/tests/MyTest.scala
@@ -0,0 +1,5 @@
+package app.tests
+
+class MyTest {
+  val hello = (new app.Hello).world
+}
