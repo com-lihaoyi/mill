b25c84468c033ee9ca05218bb8c97a6eb2e9b03f
Li Haoyi <haoyi.sg@gmail.com>
2026-01-09 09:25:30 +0800

    .

--- core/api/src/mill/api/internal/Located.scala
+++ core/api/src/mill/api/internal/Located.scala
@@ -35,60 +35,17 @@ object Appendable {
   }
 
   /**
-   * Upickle reader for Appendable[T] that detects the `!append` marker object
-   * and extracts the append flag along with the actual value.
+   * Upickle reader for Appendable[T] that buffers the value, checks for the
+   * `!append` marker object, and deserializes accordingly.
    */
   class UpickleReader[T](implicit r: upickle.Reader[T])
-      extends DelegatingReader[T, Appendable[T]](r) {
-
-    protected def wrap(index: Int, v: T): Appendable[T] = Appendable(v)
-
-    // Override object handling to detect !append marker
-    override def visitObject(length: Int, jsonableKeys: Boolean, index: Int)
-        : upickle.core.ObjVisitor[Any, Appendable[T]] = {
-      new upickle.core.ObjVisitor[Any, Appendable[T]] {
-        private var currentKey: String = ""
-        private var hasAppendMarker = false
-        private var valuesResult: Option[T] = None
-        private val startIndex = index
-
-        private lazy val delegate = r.visitObject(length, jsonableKeys, index)
-        private val seenKeys = scala.collection.mutable.Set[String]()
-
-        def subVisitor: upickle.core.Visitor[?, ?] = currentKey match {
-          case AppendMarkerKey => upickle.core.NoOpVisitor
-          case ValuesMarkerKey => r
-          case _ => delegate.subVisitor
-        }
-
-        def visitKey(index: Int): upickle.core.Visitor[?, ?] = upickle.core.StringVisitor
-
-        def visitKeyValue(s: Any): Unit = {
-          currentKey = s.toString
-          seenKeys += currentKey
-          if (currentKey != AppendMarkerKey && currentKey != ValuesMarkerKey) {
-            delegate.visitKeyValue(s)
-          }
-        }
-
-        def visitValue(v: Any, index: Int): Unit = {
-          currentKey match {
-            case AppendMarkerKey => hasAppendMarker = true
-            case ValuesMarkerKey => valuesResult = Some(v.asInstanceOf[T])
-            case _ => delegate.visitValue(v, index)
-          }
-        }
-
-        def visitEnd(idx: Int): Appendable[T] = {
-          if (hasAppendMarker && valuesResult.isDefined) {
-            Appendable(valuesResult.get, append = true)
-          } else if (seenKeys == Set(AppendMarkerKey, ValuesMarkerKey)) {
-            Appendable(r.visitArray(0, startIndex).visitEnd(startIndex), append = false)
-          } else {
-            Appendable(delegate.visitEnd(idx))
-          }
-        }
-      }
+      extends upickle.Reader.MapReader[BufferedValue, BufferedValue, Appendable[T]](
+        BufferedValue.Builder
+      ) {
+    def mapNonNullsFunction(buffered: BufferedValue): Appendable[T] = {
+      val (actualValue, append) = unwrapAppendMarker(buffered)
+      val deserialized = BufferedValue.transform(actualValue, r)
+      Appendable(deserialized, append)
     }
   }
 }
@@ -97,60 +54,50 @@ object Located {
 
   /** Upickle reader for Located[T] that wraps values with their source location */
   class UpickleReader[T](path: os.Path)(implicit r: upickle.Reader[T])
-      extends DelegatingReader[T, Located[T]](r) {
+      extends upickle.Reader[Located[T]] {
 
-    protected def wrap(index: Int, v: T): Located[T] = Located(path, index, v)
-  }
-}
+    private def wrap(index: Int, v: T): Located[T] = Located(path, index, v)
 
-/**
- * Base class for upickle readers that delegate to an inner reader and wrap results.
- * Subclasses only need to implement `wrap` to transform inner values to outer values.
- */
-abstract class DelegatingReader[T, O](r: upickle.Reader[T]) extends upickle.Reader[O] {
-
-  /** Wrap an inner value with its parse index into the outer type */
-  protected def wrap(index: Int, v: T): O
-
-  def visitArray(length: Int, index: Int): upickle.core.ArrVisitor[Any, O] = {
-    val delegate = r.visitArray(length, index)
-    new upickle.core.ArrVisitor[Any, O] {
-      def subVisitor = delegate.subVisitor
-      def visitValue(v: Any, index: Int): Unit = delegate.visitValue(v, index)
-      def visitEnd(idx: Int) = wrap(index, delegate.visitEnd(idx))
+    def visitArray(length: Int, index: Int): upickle.core.ArrVisitor[Any, Located[T]] = {
+      val delegate = r.visitArray(length, index)
+      new upickle.core.ArrVisitor[Any, Located[T]] {
+        def subVisitor = delegate.subVisitor
+        def visitValue(v: Any, index: Int): Unit = delegate.visitValue(v, index)
+        def visitEnd(idx: Int) = wrap(index, delegate.visitEnd(idx))
+      }
     }
-  }
 
-  def visitObject(
-      length: Int,
-      jsonableKeys: Boolean,
-      index: Int
-  ): upickle.core.ObjVisitor[Any, O] = {
-    val delegate = r.visitObject(length, jsonableKeys, index)
-    new upickle.core.ObjVisitor[Any, O] {
-      def subVisitor = delegate.subVisitor
-      def visitKey(index: Int) = delegate.visitKey(index)
-      def visitKeyValue(s: Any): Unit = delegate.visitKeyValue(s)
-      def visitValue(v: Any, index: Int): Unit = delegate.visitValue(v, index)
-      def visitEnd(idx: Int) = wrap(index, delegate.visitEnd(idx))
+    def visitObject(
+        length: Int,
+        jsonableKeys: Boolean,
+        index: Int
+    ): upickle.core.ObjVisitor[Any, Located[T]] = {
+      val delegate = r.visitObject(length, jsonableKeys, index)
+      new upickle.core.ObjVisitor[Any, Located[T]] {
+        def subVisitor = delegate.subVisitor
+        def visitKey(index: Int) = delegate.visitKey(index)
+        def visitKeyValue(s: Any): Unit = delegate.visitKeyValue(s)
+        def visitValue(v: Any, index: Int): Unit = delegate.visitValue(v, index)
+        def visitEnd(idx: Int) = wrap(index, delegate.visitEnd(idx))
+      }
     }
-  }
 
-  def visitNull(index: Int) = wrap(index, r.visitNull(index))
-  def visitFalse(index: Int) = wrap(index, r.visitFalse(index))
-  def visitTrue(index: Int) = wrap(index, r.visitTrue(index))
-  def visitFloat64StringParts(s: CharSequence, decIndex: Int, expIndex: Int, index: Int) =
-    wrap(index, r.visitFloat64StringParts(s, decIndex, expIndex, index))
-  def visitFloat64(d: Double, index: Int) = wrap(index, r.visitFloat64(d, index))
-  def visitFloat32(d: Float, index: Int) = wrap(index, r.visitFloat32(d, index))
-  def visitInt32(i: Int, index: Int) = wrap(index, r.visitInt32(i, index))
-  def visitInt64(i: Long, index: Int) = wrap(index, r.visitInt64(i, index))
-  def visitUInt64(i: Long, index: Int) = wrap(index, r.visitUInt64(i, index))
-  def visitFloat64String(s: String, index: Int) = wrap(index, r.visitFloat64String(s, index))
-  def visitString(s: CharSequence, index: Int) = wrap(index, r.visitString(s, index))
-  def visitChar(s: Char, index: Int) = wrap(index, r.visitChar(s, index))
-  def visitBinary(bytes: Array[Byte], offset: Int, len: Int, index: Int) =
-    wrap(index, r.visitBinary(bytes, offset, len, index))
-  def visitExt(tag: Byte, bytes: Array[Byte], offset: Int, len: Int, index: Int) =
-    wrap(index, r.visitExt(tag, bytes, offset, len, index))
+    def visitNull(index: Int) = wrap(index, r.visitNull(index))
+    def visitFalse(index: Int) = wrap(index, r.visitFalse(index))
+    def visitTrue(index: Int) = wrap(index, r.visitTrue(index))
+    def visitFloat64StringParts(s: CharSequence, decIndex: Int, expIndex: Int, index: Int) =
+      wrap(index, r.visitFloat64StringParts(s, decIndex, expIndex, index))
+    def visitFloat64(d: Double, index: Int) = wrap(index, r.visitFloat64(d, index))
+    def visitFloat32(d: Float, index: Int) = wrap(index, r.visitFloat32(d, index))
+    def visitInt32(i: Int, index: Int) = wrap(index, r.visitInt32(i, index))
+    def visitInt64(i: Long, index: Int) = wrap(index, r.visitInt64(i, index))
+    def visitUInt64(i: Long, index: Int) = wrap(index, r.visitUInt64(i, index))
+    def visitFloat64String(s: String, index: Int) = wrap(index, r.visitFloat64String(s, index))
+    def visitString(s: CharSequence, index: Int) = wrap(index, r.visitString(s, index))
+    def visitChar(s: Char, index: Int) = wrap(index, r.visitChar(s, index))
+    def visitBinary(bytes: Array[Byte], offset: Int, len: Int, index: Int) =
+      wrap(index, r.visitBinary(bytes, offset, len, index))
+    def visitExt(tag: Byte, bytes: Array[Byte], offset: Int, len: Int, index: Int) =
+      wrap(index, r.visitExt(tag, bytes, offset, len, index))
+  }
 }
