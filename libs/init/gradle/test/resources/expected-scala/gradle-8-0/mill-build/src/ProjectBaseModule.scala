8157558b78955a204e65d3c7882493aedee8a836
Tobias Roeser <le.petit.fou@web.de>
2026-01-12 15:04:58 +0100

    Add Gradle tests

---
+++ libs/init/gradle/test/resources/expected-scala/gradle-8-0/mill-build/src/ProjectBaseModule.scala
@@ -0,0 +1,31 @@
+package millbuild
+
+import mill.*
+import mill.javalib.*
+import mill.javalib.errorprone.ErrorProneModule
+
+trait ProjectBaseModule extends MavenModule {
+
+  def depManagement = Seq(mvn"org.apache.commons:commons-text:1.9")
+
+  def javacOptions = Seq("-source", "11", "-target", "11")
+
+  trait ProjectBaseTests extends MavenTests {
+
+    def mvnDeps = Seq(mvn"org.junit.jupiter:junit-jupiter:5.9.1")
+
+    def runMvnDeps = Seq(mvn"org.junit.platform:junit-platform-launcher")
+
+    def bomMvnDeps = Seq(mvn"org.junit:junit-bom:5.9.1")
+
+    def javacOptions = Seq("-source", "11", "-target", "11")
+
+    def forkWorkingDir = moduleDir
+
+    def testParallelism = false
+
+    def testSandboxWorkingDir = false
+
+  }
+
+}
