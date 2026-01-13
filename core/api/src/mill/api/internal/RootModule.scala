1d992876e8318c453f7b6c91d99680369bbedc31
Li Haoyi <haoyi.sg@gmail.com>
2026-01-13 13:03:54 +0800

    .

--- core/api/src/mill/api/internal/RootModule.scala
+++ core/api/src/mill/api/internal/RootModule.scala
@@ -61,11 +61,11 @@ object RootModule {
      * dummy build to allow paths to be injected at runtime rather than compile time.
      */
     class FromEnv extends Info(
-      sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_)),
-      sys.env.get(mill.constants.EnvVars.MILL_OUTPUT_DIR).fold(
-        sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_)) / "out"
-      )(os.Path(_)),
-      sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_))
-    )
+          sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_)),
+          sys.env.get(mill.constants.EnvVars.MILL_OUTPUT_DIR).fold(
+            sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_)) / "out"
+          )(os.Path(_)),
+          sys.env.get(mill.constants.EnvVars.MILL_WORKSPACE_ROOT).fold(os.pwd)(os.Path(_))
+        )
   }
 }
