026ee5742504d40e90bc14e757c80fead55c9d41
Li Haoyi <haoyi.sg@gmail.com>
2026-01-14 14:24:32 +0800

    .

--- core/api/src/mill/api/Evaluator.scala
+++ core/api/src/mill/api/Evaluator.scala
@@ -35,7 +35,7 @@ trait Evaluator extends AutoCloseable with EvaluatorApi {
   private[mill] def useFileLocks: Boolean = false
   private[mill] def staticBuildOverrides: Map[String, Located[internal.Appendable[BufferedValue]]] =
     Map()
-  private[mill] def invalidateAllHashes: Int
+  private[mill] def invalidateAllHashes: Int = 0
   def withBaseLogger(newBaseLogger: Logger): Evaluator
 
   def resolveSegments(
