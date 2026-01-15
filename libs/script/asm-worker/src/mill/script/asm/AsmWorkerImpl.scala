com-lihaoyi/mill

commit e4193ae7bb448dde206fc1a2b621f7563833fa9c

Prefix all main classes with `Mill` to make it easier to spot them in `jps` (#6587)
This way it's clear that all the `Mill*` process belong to Mill in some
shape or form.

Also improved the naming of Scala script main classes, so
`foo-bar.scala`'s classname changed from `foo$minusbar_MillScriptMain`
to `MillScriptMain_foo_bar_scala`, which follow the `Mill*` prefix
convention of other Mill main classes and avoids having `$encoded`
characters in the name

libs/script/asm-worker/src/mill/script/asm/AsmWorkerImpl.scala
@@ -5,11 +5,11 @@ import org.objectweb.asm
 object AsmWorkerImpl {
 
   def generateSyntheticClasses(classesDir: java.nio.file.Path, mainMethods: Array[String]): Unit = {
-    // Find the *_MillScriptMain class name to forward to
+    // Find the MillScriptMain_ class name to forward to
     val targetClassName = os.list(os.Path(classesDir))
-      .find(_.last.endsWith("_MillScriptMain$.class"))
-      .map(_.last.stripSuffix("$.class"))
-      .getOrElse("_MillScriptMain")
+      .map(_.last)
+      .collectFirst { case s"MillScriptMain_${s}$$.class" => s }
+      .getOrElse("MillScriptMain_")
 
     mainMethods.foreach { methodName =>
       generateSyntheticMainClass(
@@ -24,10 +24,10 @@ object AsmWorkerImpl {
   def findMainArgsMethods(classesDir: java.nio.file.Path): Array[String] = {
     val mainMethods = collection.mutable.ArrayBuffer[String]()
 
-    // Look for *_MillScriptMain$ classes which contain the mainargs.Parser code
-    // The class name is prefixed with the script name (e.g., Multi_MillScriptMain$)
+    // Look for MillScriptMain_ classes which contain the mainargs.Parser code
+    // The class name is prefixed with the script name (e.g., MillScriptMain_Multi)
     val millScriptMainClasses = os.list(os.Path(classesDir))
-      .filter(_.last.endsWith("_MillScriptMain$.class"))
+      .filter(p => p.last.startsWith("MillScriptMain_") && p.last.endsWith("$.class"))
 
     for (classFile <- millScriptMainClasses) {
       val reader = new asm.ClassReader(os.read.bytes(classFile))
@@ -130,7 +130,7 @@ object AsmWorkerImpl {
               descriptor: String,
               isInterface: Boolean
           ): Unit = {
-            // Replace TemplateMainClass.main call with the actual *_MillScriptMain.main
+            // Replace TemplateMainClass.main call with the actual MillScriptMain_.main
             if (owner == s"mill/script/asm/$templateClassName" && name == "main") {
               super.visitMethodInsn(opcode, targetClassName, name, descriptor, isInterface)
             } else {

libs/script/src/ScalaModule.scala
@@ -19,6 +19,8 @@ class ScalaModule(scriptConfig: ScriptModule.Config) extends ScalaModule.Raw(scr
   override def allSourceFiles = Task {
     val original = scriptSource().path
     val modified = Task.dest / original.last
+    val sanitizedName = original.last.map(c => if (Character.isJavaIdentifierPart(c)) c else '_')
+    val selfReference = s"${sanitizedName}_millScriptMainSelf"
     os.write(
       modified,
       s"//SOURCECODE_ORIGINAL_FILE_PATH=$original\n" +
@@ -26,7 +28,7 @@ class ScalaModule(scriptConfig: ScriptModule.Config) extends ScalaModule.Raw(scr
         os.read(original) +
         System.lineSeparator +
         // Squeeze this onto one line so as not to affect line counts too much
-        s"type main = mainargs.main; private def `${original.baseName}_millScriptMainSelf` = this; object `${original.baseName}_MillScriptMain` { def main(args: Array[String]): Unit = this.getClass.getMethods.find(m => m.getName == \"main\" && m.getParameters.map(_.getType) == Seq(classOf[Array[String]]) && m.getReturnType == classOf[Unit]) match{ case Some(m) => m.invoke(`${original.baseName}_millScriptMainSelf`, args); case None => mainargs.Parser(`${original.baseName}_millScriptMainSelf`).runOrExit(args) }}"
+        s"type main = mainargs.main; private def $selfReference = this; object MillScriptMain_${sanitizedName} { def main(args: Array[String]): Unit = this.getClass.getMethods.find(m => m.getName == \"main\" && m.getParameters.map(_.getType) == Seq(classOf[Array[String]]) && m.getReturnType == classOf[Unit]) match{ case Some(m) => m.invoke($selfReference, args); case None => mainargs.Parser($selfReference).runOrExit(args) }}"
     )
 
     Seq(PathRef(modified))
@@ -73,11 +75,11 @@ class ScalaModule(scriptConfig: ScriptModule.Config) extends ScalaModule.Raw(scr
     // Never consider the `syntheticMainargsMainClasses` when choosing a default main class,
     // since those are purely for compatibility when run directly via `runMain`
     //
-    // If multiple main methods are present, skip the synthetic `_MillScriptMain` one
+    // If multiple main methods are present, skip the synthetic `MillScriptMain_` one
     // and use the user-provided main method instead
     super.allLocalMainClasses().filter(!syntheticMainargsMainClasses().contains(_)) match {
       case Seq(single) => Seq(single)
-      case multiple => multiple.filter(!_.endsWith("_MillScriptMain"))
+      case multiple => multiple.filter(!_.startsWith("MillScriptMain_"))
     }
   }
 }

integration/feature/script-main-forwarder-classes/src/ScriptMainForwarderClassesTests.scala
@@ -12,13 +12,13 @@ object ScriptMainForwarderClassesTests extends UtestIntegrationTestSuite {
     test("test") - integrationTest { tester =>
       import tester.*
       // When using `run`, we ignore the synthetic main classes when picking a main method,
-      // so in this case we run the default `_MillScriptMain` method which delegates
+      // so in this case we run the default `MillScriptMain_` method which delegates
       // to the relevant method internally based on the first token
       val res0 = eval(("Multi.scala:run", "main1", "--text", "HELLO"))
       assert(res0.out == "HELLO123")
 
       // Multi-main script have forwarder classes synthesized for each @mainargs.main method,
-      // which forward to `_MillScriptMain` but pass the name as the first param to disambiguate
+      // which forward to `MillScriptMain_` but pass the name as the first param to disambiguate
       val res1 = eval(("Multi.scala:runMain", "main1", "--text", "hello"))
       assert(res1.out == "hello123")
 
@@ -33,11 +33,11 @@ object ScriptMainForwarderClassesTests extends UtestIntegrationTestSuite {
       val res4 = eval(("Single.scala:runMain", "main1", "--text", "iamcow"))
       assert(res4.out == "iamcowXYZ")
 
-      // scala.main method takes priority over synthetic _MillScriptMain method
+      // scala.main method takes priority over synthetic MillScriptMain_ method
       val res5 = eval(("ScalaMain.scala:run", "hearmemoo"))
       assert(res5.out == "hearmemooABC")
 
-      // `def main(args: Array[String]): Unit` method takes priority over synthetic _MillScriptMain method
+      // `def main(args: Array[String]): Unit` method takes priority over synthetic MillScriptMain_ method
       val res6 = eval(("RawMainSignature.scala:run", "iweightwiceasmuchasyou"))
       assert(res6.out == "iweightwiceasmuchasyouOMG")
     }

libs/init/gradle/src/mill/main/gradle/MillGradleBuildGenMain.scala
@@ -11,7 +11,7 @@ import java.util.Properties
 import java.util.concurrent.TimeUnit
 import scala.util.Using
 
-object GradleBuildGenMain {
+object MillGradleBuildGenMain {
 
   def main(args: Array[String]): Unit = mainargs.Parser(this).runOrExit(args.toSeq)
 

libs/init/maven/src/mill/main/maven/MillMavenBuildGenMain.scala
@@ -6,7 +6,7 @@ import org.apache.maven.model.{Developer as MvnDeveloper, License as MvnLicense,
 
 import scala.jdk.CollectionConverters.*
 
-object MavenBuildGenMain {
+object MillMavenBuildGenMain {
 
   def main(args: Array[String]): Unit = mainargs.Parser(this).runOrExit(args.toSeq)
 

libs/init/sbt/src/mill/main/sbt/MillSbtBuildGenMain.scala
@@ -8,7 +8,7 @@ import pprint.Util.literalize
 import scala.util.Properties.isWin
 import scala.util.Using
 
-object SbtBuildGenMain {
+object MillSbtBuildGenMain {
 
   def main(args: Array[String]): Unit = mainargs.Parser(this).runOrExit(args.toSeq)
 

libs/init/src/mill/init/InitGradleModule.scala
@@ -12,5 +12,5 @@ object InitGradleModule extends ExternalModule with BuildGenModule {
     Dep.millProjectModule("mill-libs-init-gradle")
   )
 
-  def buildGenMainClass: T[String] = "mill.main.gradle.GradleBuildGenMain"
+  def buildGenMainClass: T[String] = "mill.main.gradle.MillGradleBuildGenMain"
 }

libs/init/src/mill/init/InitMavenModule.scala
@@ -12,5 +12,5 @@ object InitMavenModule extends ExternalModule with BuildGenModule {
     Dep.millProjectModule("mill-libs-init-maven")
   )
 
-  def buildGenMainClass: T[String] = "mill.main.maven.MavenBuildGenMain"
+  def buildGenMainClass: T[String] = "mill.main.maven.MillMavenBuildGenMain"
 }

commit c92b2c04dd762064aa5c7526b76dd7d9204e508b

Fix synthetic main class generation in scala scripts (#6591)
Was broken in https://github.com/com-lihaoyi/mill/pull/6587

libs/script/asm-worker/src/mill/script/asm/AsmWorkerImpl.scala
@@ -6,9 +6,10 @@ object AsmWorkerImpl {
 
   def generateSyntheticClasses(classesDir: java.nio.file.Path, mainMethods: Array[String]): Unit = {
     // Find the MillScriptMain_ class name to forward to
+    // Match on $.class but return without $ since static forwarders are in the non-$ class
     val targetClassName = os.list(os.Path(classesDir))
       .map(_.last)
-      .collectFirst { case s"MillScriptMain_${s}$$.class" => s }
+      .collectFirst { case s"MillScriptMain_${s}$$.class" => s"MillScriptMain_${s}" }
       .getOrElse("MillScriptMain_")
 
     mainMethods.foreach { methodName =>

libs/script/src/ScalaModule.scala
@@ -79,7 +79,7 @@ class ScalaModule(scriptConfig: ScriptModule.Config) extends ScalaModule.Raw(scr
     // and use the user-provided main method instead
     super.allLocalMainClasses().filter(!syntheticMainargsMainClasses().contains(_)) match {
       case Seq(single) => Seq(single)
-      case multiple => multiple.filter(!_.startsWith("MillScriptMain_"))
+      case multiple => multiple.filter(!_.contains("MillScriptMain_"))
     }
   }
 }