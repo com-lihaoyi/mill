4d5d5777f87a0d6362b828cb834a85e743c7bd1e
Li Haoyi <haoyi.sg@gmail.com>
2026-01-15 09:52:30 +0800

    .

--- core/api/daemon/src/mill/api/daemon/CliImports.scala
+++
@@ -1,8 +0,0 @@
-package mill.api.daemon
-
-import scala.util.DynamicVariable
-
-/**
- * Hold additional runtime dependencies given via the `--import` cli option.
- */
-object CliImports extends DynamicVariable[Seq[String]](Seq.empty)
