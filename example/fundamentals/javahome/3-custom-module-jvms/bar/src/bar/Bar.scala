630966331221b9e4813c8d8ce802bbd0ad3f7dff
Li Haoyi <haoyi.sg@gmail.com>
2026-01-18 11:54:13 +0800

    .

--- example/fundamentals/javahome/3-custom-module-jvms/bar/src/bar/Bar.scala
+++
@@ -1,7 +0,0 @@
-package bar
-
-object Bar {
-  def main(args: Array[String]): Unit = {
-    println(s"Bar running on Java ${sys.props("java.version")}")
-  }
-}
--- example/fundamentals/javahome/3-custom-module-jvms/foo/src/foo/Foo.java
+++
@@ -1,8 +0,0 @@
-package foo;
-
-public class Foo {
-
-  public static void main(String[] args) throws Exception {
-    System.out.println("Foo running on Java " + System.getProperty("java.version"));
-  }
-}
