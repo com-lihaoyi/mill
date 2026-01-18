6003b3fb1aa64c9f31725e2bc0d34825d0664bf9
Li Haoyi <haoyi.sg@gmail.com>
2026-01-18 12:45:29 +0800

    .

---
+++ example/fundamentals/javahome/4-custom-module-jvms/foo/test/src/foo/FooTest.java
@@ -0,0 +1,13 @@
+package foo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FooTest {
+  @Test
+  public void testSimple() {
+    System.out.println("Testing with JVM version: " + System.getProperty("java.version"));
+    assertEquals(System.getProperty("java.version"), "11.0.21");
+  }
+}
