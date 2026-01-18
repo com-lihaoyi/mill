557966bb847710bab091219fe13fc65db0109a7e
Li Haoyi <haoyi.sg@gmail.com>
2026-01-18 09:31:58 +0800

    compiles

--- integration/ide/gen-idea/src/GenIdeaUtils.scala
+++ integration/ide/gen-idea/src/GenIdeaUtils.scala
@@ -34,19 +34,19 @@ object GenIdeaUtils {
         .replace("jansi-2.4.1.jar", "jansi.jar")
         // Normalize coursier cache references which contain the user-specific home folder
         .replaceAll(
-          "path=\"[a-zA-Z0-9._/]+/maven2/",
+          "path=\"[a-zA-Z0-9._/-]+/maven2/",
           "path=\".../"
         )
         .replaceAll(
-          "path=\"[a-zA-Z0-9._/]+/out/",
+          "path=\"[a-zA-Z0-9._/-]+/out/",
           "path=\".../out/"
         )
         .replaceAll(
-          "-Xplugin:/[a-zA-Z0-9._/]+/maven2/",
+          "-Xplugin:/[a-zA-Z0-9._/-]+/maven2/",
           "-Xplugin:.../"
         )
         .replaceAll(
-          "-Xplugin:/[a-zA-Z0-9._/]+/out/",
+          "-Xplugin:/[a-zA-Z0-9._/-]+/out/",
           "-Xplugin:.../"
         )
         // Normalize third-party dependency versions in jar paths like "artifact/1.2.3/artifact-1.2.3.jar"
