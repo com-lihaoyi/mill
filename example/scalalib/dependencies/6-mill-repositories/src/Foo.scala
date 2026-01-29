23c58be53fc7ff5392992e661e520376204e43b2
Li Haoyi <haoyi.sg@gmail.com>
2026-01-29 12:06:51 +0800

    .

--- example/scalalib/dependencies/6-mill-repositories/src/Foo.scala
+++ example/scalalib/dependencies/6-mill-repositories/src/Foo.scala
@@ -1,12 +1,7 @@
 package foo
 
-import mainargs.{main, arg, ParserForMethods, Flag}
-
 object Foo {
-  @main
-  def run(@arg(name = "text") text: String) = {
+  def main(args: Array[String]): Unit = {
     println(s"text: $text, Scala version: ${scala.util.Properties.versionString}")
   }
-
-  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
 }
