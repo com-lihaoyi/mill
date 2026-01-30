1b10a67872ad9e23b77577dd710259d2835b696c
Li Haoyi <haoyi.sg@gmail.com>
2026-01-30 09:40:38 +0800

    .

--- ci/publish-docs.sh
+++ ci/publish-docs.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # Build the pages
-./mill website.githubPages
+./mill -i website.githubPages
 
 # Prepare ssh-key for git actions
 echo $REPO_DEPLOY_KEY | base64 --decode > deploy_key
--- ci/test-dist-run.sh
+++ ci/test-dist-run.sh
@@ -9,14 +9,14 @@ rm -rf $EXAMPLE/out
 test ! -d $EXAMPLE/out/foo/3.3.6/compile.dest
 test ! -f $EXAMPLE/out/bar/2.13.16/assembly.dest/out.jar
 
-./mill dist.run $EXAMPLE -i "foo[3.3.6].run"
+./mill -i dist.run $EXAMPLE -i "foo[3.3.6].run"
 
 test -d $EXAMPLE/out/foo/3.3.6/compile.dest
 
-./mill dist.run $EXAMPLE show "bar[2.13.16].assembly"
+./mill -i dist.run $EXAMPLE show "bar[2.13.16].assembly"
 
 test -f $EXAMPLE/out/bar/2.13.16/assembly.dest/out.jar
 
-./mill dist.run $EXAMPLE
+./mill -i dist.run $EXAMPLE shutdown
 
 echo "Seq.tabulate(1000)(identity).sum" | ./mill dist.run scratch repl | grep 499500
