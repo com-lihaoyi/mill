9d7dd61055d1d4d7577bdc77000fe484f3444896
Li Haoyi <haoyi.sg@gmail.com>
2026-01-31 21:58:47 +0800

    .

--- core/constants/src/mill/constants/DebugLog.java
+++ core/constants/src/mill/constants/DebugLog.java
@@ -14,13 +14,8 @@ public class DebugLog {
   }
 
   public static synchronized void println(String s) {
-    String workspaceRoot = System.getenv("MILL_WORKSPACE_ROOT");
-    Path path = workspaceRoot == null || workspaceRoot.isEmpty()
-        ? Paths.get(System.getProperty("user.home"), "mill-debug-log.txt")
-        : Paths.get(workspaceRoot, "mill-debug-log.txt");
+    Path path = Paths.get(System.getProperty("user.home"), "mill-debug-log.txt");
     try {
-      Path parent = path.getParent();
-      if (parent != null) Files.createDirectories(parent);
       if (!Files.exists(path)) Files.createFile(path);
       Files.writeString(path, s + "\n", StandardOpenOption.APPEND);
     } catch (IOException e) {
