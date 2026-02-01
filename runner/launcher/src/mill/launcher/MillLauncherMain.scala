4a27d85f84d3ffbf79faf8b46ff1abc7442b1dbd
Li Haoyi <haoyi.sg@gmail.com>
2026-02-01 20:53:31 +0800

    Update MillLauncherMain.scala

--- runner/launcher/src/mill/launcher/MillLauncherMain.scala
+++ runner/launcher/src/mill/launcher/MillLauncherMain.scala
@@ -14,7 +14,7 @@ import java.time.{Instant, ZoneId}
  * Mill launcher main entry point.
  */
 object MillLauncherMain {
-
+  val x = 123
   def main(args: Array[String]): Unit = {
     System.exit(main0(args, None, sys.env, os.pwd))
   }
