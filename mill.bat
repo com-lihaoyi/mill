d60533715fee5d3e604605b953f0a059517ea416
Li Haoyi <haoyi.sg@gmail.com>
2026-02-07 22:53:24 +0800

    .

--- mill.bat
+++ mill.bat
@@ -2,7 +2,7 @@
 
 setlocal enabledelayedexpansion
 
-if [!DEFAULT_MILL_VERSION!]==[] ( set "DEFAULT_MILL_VERSION=1.1.1" )
+if [!DEFAULT_MILL_VERSION!]==[] ( set "DEFAULT_MILL_VERSION=1.1.1-16-8f959f" )
 
 if [!MILL_GITHUB_RELEASE_CDN!]==[] ( set "MILL_GITHUB_RELEASE_CDN=" )
 
