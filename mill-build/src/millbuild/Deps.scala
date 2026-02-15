23b6a9b82763ed1946916436386118c5fe14f6e0
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2026-02-15 17:32:12 +0200

    Cleanup

--- mill-build/src/millbuild/Deps.scala
+++ mill-build/src/millbuild/Deps.scala
@@ -192,7 +192,6 @@ object Deps {
   val springBootTools = mvn"org.springframework.boot:spring-boot-loader-tools:3.5.5"
   val quarkusAppModel = mvn"io.quarkus:quarkus-bootstrap-app-model:3.31.1"
   val quarkusBootstrapCore = mvn"io.quarkus:quarkus-bootstrap-core:3.31.1"
-  val quarkusBootstrapMavenResolver = mvn"io.quarkus:quarkus-bootstrap-maven-resolver:3.31.1"
   val quarkusCoreDeployment = mvn"io.quarkus:quarkus-core-deployment:3.31.1"
   val upickle = mvn"com.lihaoyi::upickle:4.4.2"
   val upickleNamedTuples = mvn"com.lihaoyi::upickle-implicits-named-tuples:${upickle.version}"
