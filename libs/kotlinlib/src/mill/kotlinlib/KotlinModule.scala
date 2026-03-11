1c04e68bb345b20fa7dd9e81d057311f41eaa0d6
Souvlakia <souvlakia@vaslabs.io>
2026-03-11 18:45:01 +0200

    move to `KotlinIdeaModule`

--- libs/kotlinlib/src/mill/kotlinlib/KotlinModule.scala
+++ libs/kotlinlib/src/mill/kotlinlib/KotlinModule.scala
@@ -21,7 +21,6 @@ import mill.*
 import java.io.File
 import mainargs.Flag
 import mill.api.daemon.internal.bsp.{BspBuildTarget, BspModuleApi}
-import mill.api.daemon.internal.idea.{JavaFacet, Element}
 import mill.javalib.api.internal.{JavaCompilerOptions, ZincOp}
 
 /**
@@ -497,65 +496,6 @@ trait KotlinModule extends JavaModule with KotlinModuleApi { outer =>
         dokkaCliClasspath() ++
         dokkaPluginsClasspath()
     ).distinct
-  }
-
-  override def ideaJavaModuleFacets(ideaConfigVersion: Int): Task[Seq[JavaFacet]] = Task.Anon {
-    def stringArgElement(name: String, arg: String): Element =
-      Element(
-        "stringArg",
-        attributes = Map(
-          "name" -> name,
-          "arg" -> arg
-        )
-      )
-
-    val facets = ideaConfigVersion match {
-      case 4 => {
-        Seq(
-          JavaFacet(
-            "kotlin-language",
-            "Kotlin",
-            Element(
-              "configuration",
-              attributes = Map(
-                "version" -> "5"
-              ),
-              childs = Seq(
-                Element(
-                  "compilerSettings",
-                  childs = Seq(
-                    Element(
-                      "option",
-                      attributes = Map(
-                        "name" -> "additionalArguments",
-                        "value" -> allKotlincOptions().mkString(" ")
-                      )
-                    )
-                  )
-                ),
-                Element(
-                  "compilerArguments",
-                  childs = Seq(
-                    Element(
-                      "stringArguments",
-                      childs =
-                          Seq(
-                            stringArgElement("apiVersion", kotlinApiVersion()),
-                            stringArgElement("languageVersion", kotlinLanguageVersion())
-                          ) ++ when(jvmVersion().nonEmpty)(
-                            stringArgElement("jvmTarget", jvmVersion())
-                          )
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
-      }
-      case _ => Seq.empty
-    }
-    super.ideaJavaModuleFacets(ideaConfigVersion)() ++ facets
   }
 
   /**
