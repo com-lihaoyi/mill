b855d3a5c1ad45e1b51f819e7818cecb0d17f765
Souvlakia <souvlakia@vaslabs.io>
2026-03-16 18:41:11 +0200

    spring: add configuration processor example

---
+++ example/javalib/springboot/7-configuration-processor/src/com/example/MyProperties.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "my.example")
+public class MyProperties {
+    /**
+     * A message to be displayed.
+     */
+    private String message = "Default message";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
