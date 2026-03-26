37e1a6612c32720fcd24d1cda48a06220a409b4a
Alex Archambault <alexandre.archambault@gmail.com>
2026-03-26 16:34:27 +0100

    Tweaking

--- libs/javalib/src/mill/javalib/TestModule.scala
+++ libs/javalib/src/mill/javalib/TestModule.scala
@@ -633,7 +633,7 @@ object TestModule {
   /**
    * TestModule that uses Weaver to run tests.
    * You can override the [[weaverVersion]] task or provide the Weaver-dependency yourself.
-   * https://github.com/typelevel/
+   * https://github.com/typelevel/weaver-test
    */
   trait Weaver extends TestModule {
 
@@ -644,17 +644,14 @@ object TestModule {
       super.mandatoryMvnDeps() ++
         Seq(weaverVersion())
           .filter(!_.isBlank())
-          .map({ v =>
-            val trimmed = v.trim()
-            trimmed.split("\\.")
-              .toList
-              .take(2)
-              .flatMap(_.toIntOption) match
-              case 0 :: n :: _ if (n <= 8) =>
-                mvn"com.disneystreaming::weaver-scalacheck::$trimmed"
+          .map(_.trim())
+          .map { v =>
+            v.split('.').toSeq.take(2).flatMap(_.toIntOption) match
+              case Seq(0, n) if n <= 8 =>
+                mvn"com.disneystreaming::weaver-scalacheck::$v"
               case _ =>
-                mvn"org.typelevel::weaver-cats::$trimmed"
-          })
+                mvn"org.typelevel::weaver-cats::$v"
+          }
     }
   }
 
