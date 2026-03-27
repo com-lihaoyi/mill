30c48c29cbe70ed7cc4867d9f3f04a0beea08efb
Jiri Hausner <jiri.hausner.j@gmail.com>
2026-03-27 16:57:11 +0100

    Add Docker config, Docker credential helper and Google ADC credential configurations for Jib builder in contrib/docker

--- contrib/docker/src/mill/contrib/docker/JibTypes.scala
+++ contrib/docker/src/mill/contrib/docker/JibTypes.scala
@@ -7,6 +7,27 @@ enum JibImageFormat derives ReadWriter {
   case Docker, OCI
 }
 
+sealed trait JibCredentialConfig
+
+object JibCredentialConfig {
+  case class EnvironmentVariables(usernameEnv: String, passwordEnv: String)
+      extends JibCredentialConfig
+  object EnvironmentVariables {
+    def apply(credentialsEnvironment: (String, String)): EnvironmentVariables =
+      EnvironmentVariables(credentialsEnvironment._1, credentialsEnvironment._2)
+  }
+  case class DockerConfig(path: os.Path = os.home / ".docker" / "config.json")
+      extends JibCredentialConfig
+  case class DockerCredentialHelper(helper: String) extends JibCredentialConfig
+  case class GoogleADC() extends JibCredentialConfig
+
+  given ReadWriter[EnvironmentVariables] = macroRW
+  given ReadWriter[DockerConfig] = macroRW
+  given ReadWriter[DockerCredentialHelper] = macroRW
+  given ReadWriter[GoogleADC] = macroRW
+  given ReadWriter[JibCredentialConfig] = macroRW
+}
+
 sealed trait JibSourceImage
 sealed trait JibTargetImage {
   def qualifiedName: String
@@ -20,14 +41,46 @@ object JibImage {
   ) extends JibTargetImage
   case class DockerDaemonImage(
       qualifiedName: String,
-      credentialsEnvironment: Option[(String, String)] = None
+      credentials: Seq[JibCredentialConfig] = Seq.empty
   ) extends JibTargetImage
-      with JibSourceImage
+      with JibSourceImage {
+    def credentialsEnvironment: Option[(String, String)] =
+      credentials.collectFirst {
+        case JibCredentialConfig.EnvironmentVariables(usernameEnv, passwordEnv) =>
+          (usernameEnv, passwordEnv)
+      }
+  }
+  object DockerDaemonImage {
+    def apply(
+        qualifiedName: String,
+        credentialsEnvironment: Option[(String, String)]
+    ): DockerDaemonImage =
+      DockerDaemonImage(
+        qualifiedName,
+        credentialsEnvironment.map(JibCredentialConfig.EnvironmentVariables(_)).toSeq
+      )
+  }
   case class RegistryImage(
       qualifiedName: String,
-      credentialsEnvironment: Option[(String, String)] = None
+      credentials: Seq[JibCredentialConfig] = Seq.empty
   ) extends JibTargetImage
-      with JibSourceImage
+      with JibSourceImage {
+    def credentialsEnvironment: Option[(String, String)] =
+      credentials.collectFirst {
+        case JibCredentialConfig.EnvironmentVariables(usernameEnv, passwordEnv) =>
+          (usernameEnv, passwordEnv)
+      }
+  }
+  object RegistryImage {
+    def apply(
+        qualifiedName: String,
+        credentialsEnvironment: Option[(String, String)]
+    ): RegistryImage =
+      RegistryImage(
+        qualifiedName,
+        credentialsEnvironment.map(JibCredentialConfig.EnvironmentVariables(_)).toSeq
+      )
+  }
 
   given ReadWriter[SourceTarFile] = macroRW
   given ReadWriter[TargetTarFile] = macroRW
