40cb071f98cacb92945b722a8ee25186bcb64486
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2026-04-17 14:20:27 +0300

    Android: Fixes androidMainActivity to only run when there's no passed activity (#6992)
    Fixes an issue introduced in https://github.com/com-lihaoyi/mill/pull/6980
    
    
    ```
    1/1, 1 FAILED] mill show Jetcaster.tv.androidRun --activity com.example.jetcaster.tv.MainActivity
    Error: 1-099] [error] Jetcaster.tv.androidMainActivity No launchable main activity found in the manifest.
    ```
    
    Runs the androidMainActivity only if it's not passed as an argument,
    avoiding the non-existing error
    
    cc: @souvlakias 
    
    Verification:
    https://github.com/vaslabs-ltd/compose-samples-with-mill/actions/runs/24177395330
    and
    https://github.com/vaslabs-ltd/compose-samples-with-mill/actions/runs/24390871608
    
    Also fixes the auto-detection of Android TV android activities
    
    <img width="1909" height="946" alt="Screenshot From 2026-04-14 12-09-09"
    src="https://github.com/user-attachments/assets/d7bc4d31-bdae-40ce-8849-86aac342f610"
    />
    
    Pull request: https://github.com/com-lihaoyi/mill/pull/6992

--- libs/androidlib/src/mill/androidlib/AndroidAppModule.scala
+++ libs/androidlib/src/mill/androidlib/AndroidAppModule.scala
@@ -335,6 +335,7 @@ trait AndroidAppModule extends AndroidModule { outer =>
     val androidNs = "http://schemas.android.com/apk/res/android"
     val actionMain = "android.intent.action.MAIN"
     val categoryLauncher = "android.intent.category.LAUNCHER"
+    val leanback_launcher = "android.intent.category.LEANBACK_LAUNCHER"
 
     def nodeNameOpt(node: Node): Option[String] = {
       node.attribute(androidNs, "name").map(_.text)
@@ -350,7 +351,10 @@ trait AndroidAppModule extends AndroidModule { outer =>
       hasName && filters.exists { filter =>
         val hasMainAction = (filter \ "action").exists(nodeNameOpt(_).contains(actionMain))
         val hasLauncherCategory =
-          (filter \ "category").exists(nodeNameOpt(_).contains(categoryLauncher))
+          (filter \ "category").exists(launcher => {
+            val launcherNode = nodeNameOpt(launcher)
+            launcherNode.contains(categoryLauncher) || launcherNode.contains(leanback_launcher)
+          })
 
         hasMainAction && hasLauncherCategory
       }
@@ -797,28 +801,32 @@ trait AndroidAppModule extends AndroidModule { outer =>
    *                 If not provided, runs the main activity detected from the manifest.
    * @return
    */
-  def androidRun(activity: Option[String] = None): Command[Vector[String]] =
-    Task.Command(exclusive = true) {
-      val emulator = runningEmulator()
+  def androidRun(activity: Option[String] = None): Command[Vector[String]] = activity match {
+    case Some(providedActivity) =>
+      Task.Command(exclusive = true) { androidRunWith(Task.Anon(providedActivity))() }
+    case None =>
+      Task.Command(exclusive = true) { androidRunWith(androidMainActivity)() }
+  }
 
-      val activity0 = activity.getOrElse(androidMainActivity())
+  private def androidRunWith(activity: Task[String]): Task[Vector[String]] = Task.Anon {
+    val emulator = runningEmulator()
 
-      Task.log.info(s"Starting activity $activity0 on emulator $emulator")
+    Task.log.info(s"Starting activity ${activity()} on emulator $emulator")
 
-      os.call(
-        (
-          androidSdkModule().adbExe().path,
-          "-s",
-          emulator,
-          "shell",
-          "am",
-          "start",
-          "-n",
-          s"${androidApplicationId}/${activity0}",
-          "-W"
-        )
-      ).out.lines()
-    }
+    os.call(
+      (
+        androidSdkModule().adbExe().path,
+        "-s",
+        emulator,
+        "shell",
+        "am",
+        "start",
+        "-n",
+        s"${androidApplicationId}/${activity()}",
+        "-W"
+      )
+    ).out.lines()
+  }
 
   /**
    * Default os.Path to the keystore file, derived from `androidReleaseKeyName()`.

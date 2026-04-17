com-lihaoyi/mill

commit 35d18f6d2267ade57c1ce32c91f4898b0f28f15e

Android: Detect and run app's MainActivity with `run` (#6980)
- Detect MainActivity from MergedManifest
- Makes `androidRun` default to the MainActivity
- Makes `run` install and run the app


https://github.com/user-attachments/assets/feefa2ba-d8cc-4b4d-9d74-405ecf9a0a0f

libs/androidlib/src/mill/androidlib/AndroidAppModule.scala
@@ -328,6 +328,55 @@ trait AndroidAppModule extends AndroidModule { outer =>
   }
 
   /**
+   * Finds the main activity from the merged manifest
+   * @return The fully qualified name of the main activity, e.g. `com.package.name.ActivityName`
+   */
+  def androidMainActivity: T[String] = Task {
+    val androidNs = "http://schemas.android.com/apk/res/android"
+    val actionMain = "android.intent.action.MAIN"
+    val categoryLauncher = "android.intent.category.LAUNCHER"
+
+    def nodeNameOpt(node: Node): Option[String] = {
+      node.attribute(androidNs, "name").map(_.text)
+    }
+
+    /**
+     * Checks if the given activity node is launchable by looking for an intent filter that contains both
+     */
+    def isLaunchable(activity: Node): Boolean = {
+      val hasName = nodeNameOpt(activity).isDefined
+      val filters = activity \ "intent-filter"
+
+      hasName && filters.exists { filter =>
+        val hasMainAction = (filter \ "action").exists(nodeNameOpt(_).contains(actionMain))
+        val hasLauncherCategory =
+          (filter \ "category").exists(nodeNameOpt(_).contains(categoryLauncher))
+
+        hasMainAction && hasLauncherCategory
+      }
+    }
+
+    val root: Elem = XML.loadFile(androidMergedManifest().path.toString)
+
+    val activities = root \ "application" \ "activity"
+
+    activities.collectFirst {
+      case activity if isLaunchable(activity) =>
+        val rawName = nodeNameOpt(activity).get
+
+        val qualifiedName = {
+          if (rawName.startsWith(".")) s"$androidApplicationId$rawName"
+          else rawName
+        }
+
+        qualifiedName
+    } match {
+      case Some(activity) => activity
+      case None => Task.fail("No launchable main activity found in the manifest.")
+    }
+  }
+
+  /**
    * Optimizes the APK using the `zipalign` tool for better performance.
    *
    * For more details on the zipalign tool, refer to:
@@ -730,31 +779,46 @@ trait AndroidAppModule extends AndroidModule { outer =>
   }
 
   /**
-   * Run your application by providing the activity.
-   *
-   * E.g. `com.package.name.ActivityName`
-   *
-   * See also [[https://developer.android.com/tools/adb#am]] and [[https://developer.android.com/tools/adb#IntentSpec]]
-   * @param activity
+   * Installs the app to the [[runningEmulator]] and runs the main activity.
+   * @param args
    * @return
    */
-  def androidRun(activity: String): Command[Vector[String]] = Task.Command(exclusive = true) {
-    val emulator = runningEmulator()
+  override def run(args: Task[Args] = Task.Anon(Args())): Task.Command[Unit] =
+    Task.Command(exclusive = true) {
+      androidInstall()()
+      androidRun()()
+    }
 
-    os.call(
-      (
-        androidSdkModule().adbExe().path,
-        "-s",
-        emulator,
-        "shell",
-        "am",
-        "start",
-        "-n",
-        s"${androidApplicationId}/${activity}",
-        "-W"
-      )
-    ).out.lines()
-  }
+  /**
+   * Run the specified activity on the [[runningEmulator]]
+   *
+   * See also [[https://developer.android.com/tools/adb#am]] and [[https://developer.android.com/tools/adb#IntentSpec]]
+   * @param activity The fully qualified name of the activity to start. E.g. `com.package.name.ActivityName`.
+   *                 If not provided, runs the main activity detected from the manifest.
+   * @return
+   */
+  def androidRun(activity: Option[String] = None): Command[Vector[String]] =
+    Task.Command(exclusive = true) {
+      val emulator = runningEmulator()
+
+      val activity0 = activity.getOrElse(androidMainActivity())
+
+      Task.log.info(s"Starting activity $activity0 on emulator $emulator")
+
+      os.call(
+        (
+          androidSdkModule().adbExe().path,
+          "-s",
+          emulator,
+          "shell",
+          "am",
+          "start",
+          "-n",
+          s"${androidApplicationId}/${activity0}",
+          "-W"
+        )
+      ).out.lines()
+    }
 
   /**
    * Default os.Path to the keystore file, derived from `androidReleaseKeyName()`.

example/androidlib/java/1-hello-world/build.mill
@@ -129,9 +129,19 @@ object app extends AndroidAppModule {
 > ./mill show app.adbDevices
 ...emulator-...device...
 
+*/
+
+// To install the APK on the emulator:
+/** Usage
+
 > ./mill show app.androidInstall
 ...All files should be loaded. Notifying the device...
 
+*/
+
+// You can also use the `run` command which combines the installation and launching of the app in one step. It will automatically trigger the `androidInstall` task and then launch the main activity of the app on the emulator.
+/** Usage
+> ./mill show app.run
 */
 
 // The android tests (existing typically in androidTest directory, aka instrumented tests)

example/androidlib/kotlin/1-hello-kotlin/build.mill
@@ -131,9 +131,19 @@ object app extends AndroidAppKotlinModule {
 > ./mill show app.adbDevices
 ...emulator-...device...
 
+*/
+
+// To install the APK on the emulator:
+/** Usage
+
 > ./mill show app.androidInstall
 ...All files should be loaded. Notifying the device...
 
+*/
+
+// You can also use the `run` command which combines the installation and launching of the app in one step. It will automatically trigger the `androidInstall` task and then launch the main activity of the app on the emulator.
+/** Usage
+> ./mill show app.run
 */
 
 // The android tests (existing typically in androidTest directory, aka instrumented tests)

example/androidlib/kotlin/2-compose/build.mill
@@ -74,7 +74,7 @@ object app extends AndroidAppKotlinModule {
 > ./mill show app.androidInstall
 ...All files should be loaded. Notifying the device...
 
-> ./mill show app.androidRun --activity com.example.composetest.MainActivity
+> ./mill show app.androidRun
 [
   "Starting: Intent { cmp=com.example.composetest/.MainActivity }",
   "Status: ok",

example/thirdparty/android-compose-samples/build.mill
@@ -346,7 +346,7 @@ object Jetchat extends mill.api.Module {
 > ./mill show JetNews.app.androidInstall
 ...All files should be loaded. Notifying the device...
 
-> ./mill show JetNews.app.androidRun --activity com.example.jetnews.ui.MainActivity
+> ./mill show JetNews.app.androidRun
 [
   "Starting: Intent { cmp=com.example.jetnews/.ui.MainActivity }",
   "Status: ok",

example/thirdparty/android-endless-tunnel/build.mill
@@ -63,7 +63,7 @@ object `endless-tunnel` extends mill.api.Module {
 > ./mill show endless-tunnel.app.androidInstall
 ...All files should be loaded. Notifying the device...
 
-> ./mill show endless-tunnel.app.androidRun --activity android.app.NativeActivity
+> ./mill show endless-tunnel.app.androidRun
 [
   "Starting: Intent { cmp=com.google.sample.tunnel/android.app.NativeActivity }",
   "Status: ok",

example/thirdparty/androidtodo/build.mill
@@ -270,7 +270,7 @@ object `shared-test` extends AndroidKotlinModule, AndroidHiltSupport, KotlinIdea
 > ./mill show app.androidInstall
 ...All files should be loaded. Notifying the device...
 
-> ./mill show app.androidRun --activity com.example.android.architecture.blueprints.todoapp.TodoActivity
+> ./mill show app.androidRun
 [
   "Starting: Intent { cmp=com.example.android.architecture.blueprints.main/com.example.android.architecture.blueprints.todoapp.TodoActivity }",
   "Status: ok",

commit 40cb071f98cacb92945b722a8ee25186bcb64486

Android: Fixes androidMainActivity to only run when there's no passed activity (#6992)
Fixes an issue introduced in https://github.com/com-lihaoyi/mill/pull/6980


```
1/1, 1 FAILED] mill show Jetcaster.tv.androidRun --activity com.example.jetcaster.tv.MainActivity
Error: 1-099] [error] Jetcaster.tv.androidMainActivity No launchable main activity found in the manifest.
```

Runs the androidMainActivity only if it's not passed as an argument,
avoiding the non-existing error

cc: @souvlakias 

Verification:
https://github.com/vaslabs-ltd/compose-samples-with-mill/actions/runs/24177395330
and
https://github.com/vaslabs-ltd/compose-samples-with-mill/actions/runs/24390871608

Also fixes the auto-detection of Android TV android activities

<img width="1909" height="946" alt="Screenshot From 2026-04-14 12-09-09"
src="https://github.com/user-attachments/assets/d7bc4d31-bdae-40ce-8849-86aac342f610"
/>

Pull request: https://github.com/com-lihaoyi/mill/pull/6992

libs/androidlib/src/mill/androidlib/AndroidAppModule.scala
@@ -335,6 +335,7 @@ trait AndroidAppModule extends AndroidModule { outer =>
     val androidNs = "http://schemas.android.com/apk/res/android"
     val actionMain = "android.intent.action.MAIN"
     val categoryLauncher = "android.intent.category.LAUNCHER"
+    val leanback_launcher = "android.intent.category.LEANBACK_LAUNCHER"
 
     def nodeNameOpt(node: Node): Option[String] = {
       node.attribute(androidNs, "name").map(_.text)
@@ -350,7 +351,10 @@ trait AndroidAppModule extends AndroidModule { outer =>
       hasName && filters.exists { filter =>
         val hasMainAction = (filter \ "action").exists(nodeNameOpt(_).contains(actionMain))
         val hasLauncherCategory =
-          (filter \ "category").exists(nodeNameOpt(_).contains(categoryLauncher))
+          (filter \ "category").exists(launcher => {
+            val launcherNode = nodeNameOpt(launcher)
+            launcherNode.contains(categoryLauncher) || launcherNode.contains(leanback_launcher)
+          })
 
         hasMainAction && hasLauncherCategory
       }
@@ -797,28 +801,32 @@ trait AndroidAppModule extends AndroidModule { outer =>
    *                 If not provided, runs the main activity detected from the manifest.
    * @return
    */
-  def androidRun(activity: Option[String] = None): Command[Vector[String]] =
-    Task.Command(exclusive = true) {
-      val emulator = runningEmulator()
+  def androidRun(activity: Option[String] = None): Command[Vector[String]] = activity match {
+    case Some(providedActivity) =>
+      Task.Command(exclusive = true) { androidRunWith(Task.Anon(providedActivity))() }
+    case None =>
+      Task.Command(exclusive = true) { androidRunWith(androidMainActivity)() }
+  }
 
-      val activity0 = activity.getOrElse(androidMainActivity())
+  private def androidRunWith(activity: Task[String]): Task[Vector[String]] = Task.Anon {
+    val emulator = runningEmulator()
 
-      Task.log.info(s"Starting activity $activity0 on emulator $emulator")
+    Task.log.info(s"Starting activity ${activity()} on emulator $emulator")
 
-      os.call(
-        (
-          androidSdkModule().adbExe().path,
-          "-s",
-          emulator,
-          "shell",
-          "am",
-          "start",
-          "-n",
-          s"${androidApplicationId}/${activity0}",
-          "-W"
-        )
-      ).out.lines()
-    }
+    os.call(
+      (
+        androidSdkModule().adbExe().path,
+        "-s",
+        emulator,
+        "shell",
+        "am",
+        "start",
+        "-n",
+        s"${androidApplicationId}/${activity()}",
+        "-W"
+      )
+    ).out.lines()
+  }
 
   /**
    * Default os.Path to the keystore file, derived from `androidReleaseKeyName()`.