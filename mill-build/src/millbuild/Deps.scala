59ed77320d5a4e58c7941cd84a645db3737997ee
Tobias Roeser <le.petit.fou@web.de>
2026-04-30 09:05:45 +0200

    Update snakeyaml to 2.6 (#7044)
    Pull request: https://github.com/com-lihaoyi/mill/pull/7044

--- mill-build/src/millbuild/Deps.scala
+++ mill-build/src/millbuild/Deps.scala
@@ -339,7 +339,7 @@ object Deps {
     Deps.gson,
     mvn"com.google.protobuf:protobuf-java:4.33.5",
     mvn"com.google.guava:guava:33.4.0-jre",
-    mvn"org.yaml:snakeyaml:2.5",
+    mvn"org.yaml:snakeyaml:2.6",
     mvn"org.apache.commons:commons-compress:1.28.0"
   )
 
