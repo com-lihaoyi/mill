68e72c1fc218cb187c08da3b1cc4f2add8c2d7a8
Souvlakia <souvlakia@vaslabs.io>
2026-05-13 15:58:14 +0300

    buildgen

--- libs/init/buildgen/src/mill/main/buildgen/BuildGenYaml.scala
+++ libs/init/buildgen/src/mill/main/buildgen/BuildGenYaml.scala
@@ -96,6 +96,9 @@ object BuildGenYaml extends BuildGen {
       "def errorProneJavacEnableOptions",
       errorProneJavacEnableOptions
     ).foreach(lines += _)
+    springBootPlatformVersion.base.foreach(v =>
+      lines += s"""def springBootPlatformVersion = "$v""""
+    )
     renderScalaOptValues("def scalacOptions", scalacOptions).foreach(lines += _)
     renderScalaMvnDepValues("def scalacPluginMvnDeps", scalacPluginMvnDeps).foreach(lines += _)
     testParallelism.base.foreach(v => lines += s"def testParallelism = $v")
