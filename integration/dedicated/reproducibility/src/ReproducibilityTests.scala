707782c4e83ab2899ea0df7b2f5946485620c1b3
Tobias Roeser <le.petit.fou@web.de>
2026-06-22 22:21:23 +0200

    Fix deprecations

--- integration/dedicated/reproducibility/src/ReproducibilityTests.scala
+++ integration/dedicated/reproducibility/src/ReproducibilityTests.scala
@@ -72,7 +72,7 @@ object ReproducibilityTests extends UtestIntegrationTestSuite {
   // Whether `task` was recomputed (`"cached": false` in `mill-profile.json`) rather than served
   // from a cache, in the most recent invocation in `tester`'s workspace.
   def evaluated(tester: IntegrationTester, task: String): Boolean = {
-    val profile = os.read(tester.workspacePath / "out" / mill.constants.OutFiles.millProfile)
+    val profile = os.read(tester.workspacePath / "out" / mill.constants.OutFiles.OutFiles.millProfile)
     ujson.read(profile).arr.exists { e =>
       e("label").str == task && e.obj.get("cached").flatMap(_.boolOpt).contains(false)
     }
@@ -87,7 +87,7 @@ object ReproducibilityTests extends UtestIntegrationTestSuite {
 
   def metaBuildCached(tester: IntegrationTester, task: String): Boolean =
     cachedInProfile(
-      tester.workspacePath / "out" / "mill-build" / mill.constants.OutFiles.millProfile,
+      tester.workspacePath / "out" / "mill-build" / mill.constants.OutFiles.OutFiles.millProfile,
       task
     )
 
