391fe6e7783c252c37ec10ea034885b15007340d
Scala Steward <scala_steward@virtuslab.com>
2026-07-22 15:53:52 +0000

    Update jarjar-abrams-core to 1.17.0

--- mill-build/src/millbuild/Deps.scala
+++ mill-build/src/millbuild/Deps.scala
@@ -231,7 +231,7 @@ object Deps {
   val gson = mvn"com.google.code.gson:gson:2.14.0"
   val fansi = mvn"com.lihaoyi::fansi:0.5.1"
   val javaparser = mvn"com.github.javaparser:javaparser-core:3.28.2"
-  val jarjarabrams = mvn"com.eed3si9n.jarjarabrams::jarjar-abrams-core:1.16.0"
+  val jarjarabrams = mvn"com.eed3si9n.jarjarabrams::jarjar-abrams-core:1.17.0"
   val requests = mvn"com.lihaoyi::requests:0.9.3"
   val logback = mvn"ch.qos.logback:logback-classic:1.5.37"
   val sonatypeCentralClient = mvn"com.lumidion::sonatype-central-client-requests:0.6.0"
