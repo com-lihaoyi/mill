fdb6900e43a5f25696fa2e274107a4ca16af9eae
Tobias Roeser <le.petit.fou@web.de>
2026-08-03 23:16:14 +0200

    .

--- libs/javalib/src/mill/javalib/SemanticDbJavaModule.scala
+++ libs/javalib/src/mill/javalib/SemanticDbJavaModule.scala
@@ -235,23 +235,25 @@ object SemanticDbJavaModule extends ExternalModule with CoursierModule {
   ): Seq[String] = {
     val isNewEnough =
       Version.isAtLeast(semanticDbJavaVersion, "0.8.10")(using Version.IgnoreQualifierOrdering)
-    val buildTool = if(isNewEnough) "mill" else "sbt"
+    val buildTool = if (isNewEnough) "mill" else "sbt"
 
     // https://github.com/scalameta/scalameta/blob/main/semanticdb/guide.md#javac-compiler-plugin
     javacOptions ++ Seq(
-      // enable the plugin
-      s"-Xplugin:semanticdb",
-      // set sourceroot option
-      s"-sourceroot:${ctx.workspace}".replace(" ", "\\ "),
-      // set targetroot option
-      s"-targetroot:${ctx.dest / "classes"}".replace(" ", "\\ "),
-      // set build-tool option
-      s"-build-tool:${buildTool}",
-      // set verbose option
-      if (ctx.log.debugEnabled) Seq("-verbose") else Seq()
+      Seq(
+        // enable the plugin
+        s"-Xplugin:semanticdb",
+        // set sourceroot option
+        s"-sourceroot:${ctx.workspace}".replace(" ", "\\ "),
+        // set targetroot option
+        s"-targetroot:${ctx.dest / "classes"}".replace(" ", "\\ "),
+        // set build-tool option
+        s"-build-tool:${buildTool}",
+        // set verbose option
+        if (ctx.log.debugEnabled) "-verbose" else ""
+      )
+        // all args must be set as a single Javac parameter
+        .mkString(" ")
     )
-      // all args must be set as a single Javac parameters
-      .mkString(" ")
 
   }
 
