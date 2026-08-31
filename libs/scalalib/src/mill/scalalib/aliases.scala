a1e435d599829ca54c0e6de05f28ab7fbcce8cca
Li Haoyi <haoyi.sg@gmail.com>
2026-09-01 07:44:09 +0800

    Restore scalalib PublishModule external resolution (backport #7518)
    The scalalib PublishModule term was only a Scala export of the javalib
    external module. External-module discovery requires a physical object in
    the requested package, so mill.scalalib.PublishModule selectors failed
    as unknown extensions.
    
    Add a physical ExternalModule alias, retain the source type alias and
    exported members, and emit the former JVM forwarder for binary
    compatibility. Integration coverage resolves the selector and default
    task, while MiMa validates the published API.
    
    Backport of pull request: https://github.com/com-lihaoyi/mill/pull/7518

--- libs/scalalib/src/mill/scalalib/aliases.scala
+++ libs/scalalib/src/mill/scalalib/aliases.scala
@@ -3,5 +3,8 @@ package mill.scalalib
 object Dependency extends mill.api.ExternalModule.Alias(mill.javalib.Dependency)
 object MavenPublishModule
     extends mill.api.ExternalModule.Alias(mill.javalib.MavenPublishModule)
+object PublishModule extends mill.api.ExternalModule.Alias(mill.javalib.PublishModule) {
+  export mill.javalib.PublishModule.*
+}
 object SonatypeCentralPublishModule
     extends mill.api.ExternalModule.Alias(mill.javalib.SonatypeCentralPublishModule)
