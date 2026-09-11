c712437351dd2988946c077a5ed25c70ee09eb78
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2026-09-11 19:34:58 +0300

    workaround the timeout

--- libs/javalib/src/mill/javalib/OsDetectorModule.scala
+++ libs/javalib/src/mill/javalib/OsDetectorModule.scala
@@ -32,7 +32,9 @@ trait OsDetectorModule extends CoursierModule {
    * See [[OsDetector]].
    */
   def osDetectedMvnProperties: T[Map[String, String]] = Task {
-    OsDetectorModule.detect()()
+    // TODO using the OsDetectorModule.detect command seems to cause a timeout
+    // If the cause is found, this can be switched to reuse the command.
+    OsDetector.detect()
   }
 }
 
