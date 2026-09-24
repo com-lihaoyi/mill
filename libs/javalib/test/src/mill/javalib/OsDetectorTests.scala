com-lihaoyi/mill

commit 21bbe1ae1c020f6d0bc378f2cb7ef5c791f9327a

Preserve Quarkus executable dependency metadata (#7519)
Quarkus gRPC code generation requires platform-specific protoc artifacts
selected by type and classifier. Mill omitted executable artifacts and
modeled every resolved file as an unclassified jar.

Include executable artifacts in Quarkus resolution, preserve their exact
coordinates in the application model, avoid scanning executables as
jars, and pass Quarkus its build directory. Tests verify classifier
preservation and execute gRPC source generation.

libs/javalib/src/mill/javalib/quarkus/QuarkusModule.scala
@@ -3,7 +3,7 @@ package mill.javalib.quarkus
 import coursier.core.VariantSelector.ConfigurationBased
 import mill.api.PathRef
 import mill.{T, Task}
-import mill.javalib.{Dep, DepSyntax, JavaModule, PublishModule}
+import mill.javalib.{CoursierModule, Dep, DepSyntax, JavaModule, PublishModule}
 import mill.util.Jvm
 import upickle.default.ReadWriter.join
 
@@ -174,8 +174,13 @@ trait QuarkusModule extends JavaModule { outer =>
       ConfigurationBased(coursier.core.Configuration.compile)
     )
 
+    val quarkusArtifactTypes = Some(artifactTypes() + coursier.Type("exe"))
+
+    def resolveArtifacts[T: CoursierModule.Resolvable](deps: Seq[T]) =
+      millResolver().artifacts(deps, sources = false, artifactTypes = quarkusArtifactTypes)
+
     val runtimeDeps =
-      millResolver().artifacts(Seq(mill.javalib.BoundDep(depRuntime, force = false)))
+      resolveArtifacts(Seq(mill.javalib.BoundDep(depRuntime, force = false)))
 
     def qualifier(d: coursier.core.Dependency) =
       s"${d.module.organization.value}:${d.module.name.value}"
@@ -186,21 +191,51 @@ trait QuarkusModule extends JavaModule { outer =>
     def isDirectDep(d: coursier.core.Module): Boolean =
       mvnDeps().exists(dep => dep.dep.module == d)
 
+    def toQuarkusDependency(
+        artifact: (
+            coursier.core.Dependency,
+            Either[coursier.core.VariantPublication, coursier.core.Publication],
+            coursier.util.Artifact,
+            File
+        ),
+        isRuntime: Boolean,
+        isDeployment: Boolean,
+        hasExtension: Boolean
+    ): ApplicationModelWorker.Dependency = {
+      val (dependency, publication, _, file) = artifact
+      val attributes = publication.fold(
+        variantPublication =>
+          dependency.attributes.withClassifier(
+            variantPublication.classifier.getOrElse(dependency.attributes.classifier)
+          ),
+        _.attributes
+      )
+      val artifactType =
+        if (attributes.`type`.isEmpty) coursier.Type.jar.value else attributes.`type`.value
+      ApplicationModelWorker.Dependency(
+        groupId = dependency.module.organization.value,
+        artifactId = dependency.module.name.value,
+        version = dependency.versionConstraint.asString,
+        artifactType = artifactType,
+        classifier = attributes.classifier.value,
+        resolvedPath = os.Path(file),
+        isRuntime = isRuntime,
+        isDeployment = isDeployment,
+        isTopLevelArtifact = isDirectDep(dependency.module),
+        hasExtension = hasExtension
+      )
+    }
+
     val runtimeDepSet = runtimeDeps.detailedArtifacts0.map(da => qualifier(da._1)).toSet
 
-    val quarkusPrecomputedRuntimeDeps = runtimeDeps.detailedArtifacts0.map {
-      case (dependency, _, _, file) =>
-        ApplicationModelWorker.Dependency(
-          groupId = dependency.module.organization.value,
-          artifactId = dependency.module.name.value,
-          version = dependency.versionConstraint.asString,
-          resolvedPath = os.Path(file),
-          isRuntime = true,
-          isDeployment = false,
-          isTopLevelArtifact = isDirectDep(dependency.module),
-          hasExtension = false
-        )
-    }
+    val quarkusPrecomputedRuntimeDeps = runtimeDeps.detailedArtifacts0.map(artifact =>
+      toQuarkusDependency(
+        artifact,
+        isRuntime = true,
+        isDeployment = false,
+        hasExtension = false
+      )
+    )
 
     val depsWithExtensions = quarkusApplicationModelWorker().quarkusDeploymentDependencies(
       quarkusPrecomputedRuntimeDeps
@@ -213,24 +248,18 @@ trait QuarkusModule extends JavaModule { outer =>
       mvn"${d.groupId}:${d.artifactId}-deployment:${d.version}"
     )
 
-    val deploymentDeps = millResolver().artifacts(
-      deploymentMvnDeps
-    )
+    val deploymentDeps = resolveArtifacts(deploymentMvnDeps)
 
     val deploymentDepsSet = deploymentDeps.detailedArtifacts0.map(da => qualifier(da._1)).toSet
 
-    val quarkusDeploymentDeps = deploymentDeps.detailedArtifacts0.map {
-      case (dependency, _, _, file) =>
-        ApplicationModelWorker.Dependency(
-          groupId = dependency.module.organization.value,
-          artifactId = dependency.module.name.value,
-          version = dependency.versionConstraint.asString,
-          resolvedPath = os.Path(file),
-          isRuntime = runtimeDepSet.contains(qualifier(dependency)),
-          isDeployment = true,
-          isTopLevelArtifact = isDirectDep(dependency.module),
-          hasExtension = extensionDepsSet.contains(qualifier(dependency))
-        )
+    val quarkusDeploymentDeps = deploymentDeps.detailedArtifacts0.map { artifact =>
+      val dependency = artifact._1
+      toQuarkusDependency(
+        artifact,
+        isRuntime = runtimeDepSet.contains(qualifier(dependency)),
+        isDeployment = true,
+        hasExtension = extensionDepsSet.contains(qualifier(dependency))
+      )
     }
 
     val quarkusRuntimeDeps = quarkusPrecomputedRuntimeDeps.filterNot(d =>
@@ -238,26 +267,21 @@ trait QuarkusModule extends JavaModule { outer =>
     )
 
     val compileDeps =
-      millResolver().artifacts(Seq(mill.javalib.BoundDep(depCompile, force = false)))
+      resolveArtifacts(Seq(mill.javalib.BoundDep(depCompile, force = false)))
 
     val quarkusCompileDeps =
       compileDeps.detailedArtifacts0.filterNot {
         da =>
           val q = qualifier(da._1)
           runtimeDepSet.contains(q) || deploymentDepsSet.contains(q) || extensionDepsSet.contains(q)
-      }.map {
-        case (dependency, _, _, file) =>
-          ApplicationModelWorker.Dependency(
-            groupId = dependency.module.organization.value,
-            artifactId = dependency.module.name.value,
-            version = dependency.versionConstraint.asString,
-            resolvedPath = os.Path(file),
-            isRuntime = false,
-            isDeployment = false,
-            isTopLevelArtifact = isDirectDep(dependency.module),
-            hasExtension = false
-          )
-      }
+      }.map(artifact =>
+        toQuarkusDependency(
+          artifact,
+          isRuntime = false,
+          isDeployment = false,
+          hasExtension = false
+        )
+      )
 
     quarkusRuntimeDeps ++ quarkusCompileDeps ++ quarkusDeploymentDeps
   }

libs/javalib/test/src/mill/javalib/quarkus/QuarkusModuleTests.scala
@@ -1,0 +1,93 @@
+package mill.javalib.quarkus
+
+import mill.api.Discover
+import mill.javalib.{DepSyntax, MavenModule}
+import mill.testkit.{TestRootModule, UnitTester}
+import mill.util.TokenReaders.*
+import utest.*
+
+object QuarkusModuleTests extends TestSuite {
+
+  object TestCase extends TestRootModule {
+    object grpc extends QuarkusModule, MavenModule {
+      def quarkusPlatformVersion = "3.31.3"
+
+      def artifactGroupId = "com.lihaoyi.test"
+      def artifactId = "quarkus-grpc-test"
+      def artifactVersion = "0.0.1"
+
+      def mvnDeps = Seq(
+        mvn"io.quarkus:quarkus-grpc"
+      )
+    }
+
+    lazy val millDiscover = Discover[this.type]
+  }
+
+  private val resourcePath = os.Path(sys.env("MILL_TEST_RESOURCE_DIR")) / "quarkus-grpc"
+
+  val tests = Tests {
+    test("dependencyCoordinates") {
+      UnitTester(TestCase, resourcePath).scoped { eval =>
+        val result = eval(TestCase.grpc.quarkusDependencies).runtimeChecked.fold(
+          _.throwException,
+          identity
+        )
+        val dependencies = result.value
+
+        val platformClassifiers = Set(
+          "linux-aarch_64",
+          "linux-ppcle_64",
+          "linux-s390_64",
+          "linux-x86_32",
+          "linux-x86_64",
+          "osx-aarch_64",
+          "osx-x86_64",
+          "windows-x86_32",
+          "windows-x86_64"
+        )
+
+        val protoc = dependencies.filter(d =>
+          d.groupId == "com.google.protobuf" && d.artifactId == "protoc"
+        )
+        val grpcPlugin = dependencies.filter(d =>
+          d.groupId == "io.grpc" && d.artifactId == "protoc-gen-grpc-java"
+        )
+        val quarkusPlugin = dependencies.find(d =>
+          d.groupId == "io.quarkus" && d.artifactId == "quarkus-grpc-protoc-plugin"
+        ).get
+        val ordinaryJar = dependencies.find(d =>
+          d.groupId == "io.quarkus" && d.artifactId == "quarkus-grpc-codegen"
+        ).get
+
+        assert(
+          protoc.size == platformClassifiers.size,
+          protoc.map(_.classifier).toSet == platformClassifiers,
+          protoc.forall(_.artifactType == "exe"),
+          grpcPlugin.size == platformClassifiers.size,
+          grpcPlugin.map(_.classifier).toSet == platformClassifiers,
+          grpcPlugin.forall(_.artifactType == "exe"),
+          quarkusPlugin.artifactType == "jar",
+          quarkusPlugin.classifier == "shaded",
+          ordinaryJar.artifactType == "jar",
+          ordinaryJar.classifier.isEmpty
+        )
+      }
+    }
+
+    test("grpcCodeGeneration") {
+      UnitTester(TestCase, resourcePath).scoped { eval =>
+        val result = eval(TestCase.grpc.quarkusCodeGen).runtimeChecked.fold(
+          _.throwException,
+          identity
+        )
+        val generatedFiles = os.walk(result.value.path).filter(os.isFile).map(_.last).toSet
+
+        assert(
+          generatedFiles.contains("Ping.java"),
+          generatedFiles.contains("Pinger.java")
+        )
+      }
+    }
+  }
+}

libs/javalib/api/src/mill/javalib/quarkus/ApplicationModelWorker.scala
@@ -61,10 +61,13 @@ object ApplicationModelWorker {
       appMode: AppMode
   ) derives ReadWriter
 
+  @experimental
   case class Dependency(
       groupId: String,
       artifactId: String,
       version: String,
+      artifactType: String,
+      classifier: String,
       resolvedPath: os.Path,
       isRuntime: Boolean,
       isDeployment: Boolean,

libs/javalib/package.mill
@@ -45,6 +45,7 @@ object `package` extends MillStableScalaModule {
       `pgp-worker`,
       `classgraph-worker`,
       `jarjarabrams-worker`,
+      `quarkus-worker`,
       `spotless-worker`,
       `scalameta-worker`
     )

libs/javalib/quarkus-worker/src/mill/javalib/quarkus/ApplicationModelWorkerImpl.scala
@@ -135,6 +135,8 @@ class ApplicationModelWorkerImpl extends ApplicationModelWorker {
         .setResolvedPath(dep.resolvedPath.toNIO)
         .setGroupId(dep.groupId)
         .setArtifactId(dep.artifactId)
+        .setType(dep.artifactType)
+        .setClassifier(dep.classifier)
         .setVersion(dep.version)
 
       if (appModel.appMode == Test) {
@@ -286,7 +288,7 @@ class ApplicationModelWorkerImpl extends ApplicationModelWorker {
       Files.exists(quarkusDescr)
     }
 
-    dep.isRuntime && metaInfPathExists
+    dep.isRuntime && dep.artifactType == ArtifactCoords.TYPE_JAR && metaInfPathExists
   }
 
   private def processQuarkusDir(
@@ -380,17 +382,15 @@ class ApplicationModelWorkerImpl extends ApplicationModelWorker {
 
     val sourcesPathCollection: PathCollection = PathList.of(sourcesDir.map(_.toNIO)*)
     val generatedSourcesPath: Path = generatedSourcesDir.toNIO
-    val buildPropertiesPath: Path = buildProperties.toNIO
 
     val sourceRegistrar: Consumer[Path] = _ => ()
-    curatedApplication.getApplicationModel
 
     initAndRun.invoke(
       null,
       deploymentClassLoader,
       sourcesPathCollection,
       generatedSourcesPath,
-      buildPropertiesPath,
+      buildDir.toNIO,
       sourceRegistrar,
       applicationModel,
       props,

libs/javalib/src/mill/javalib/CoursierModule.scala
@@ -331,6 +331,17 @@ object CoursierModule {
     def artifacts[T: CoursierModule.Resolvable](
         deps: IterableOnce[T],
         sources: Boolean = false
+    )(using ctx: mill.api.TaskCtx): coursier.Artifacts.Result =
+      artifacts(deps, sources, artifactTypes = None)
+
+    /**
+     * Raw artifact results for the passed dependencies, including only the requested artifact
+     * types when `artifactTypes` is defined.
+     */
+    def artifacts[T: CoursierModule.Resolvable](
+        deps: IterableOnce[T],
+        sources: Boolean,
+        artifactTypes: Option[Set[coursier.Type]]
     )(using ctx: mill.api.TaskCtx): coursier.Artifacts.Result = {
       val deps0 = deps
         .iterator
@@ -340,6 +351,7 @@ object CoursierModule {
         repositories,
         deps0.map(_.dep),
         sources = sources,
+        artifactTypes = artifactTypes,
         ctx = Some(ctx),
         checkGradleModules = checkGradleModules,
         resolutionParams = resolutionParams,

libs/javalib/test/resources/quarkus-grpc/grpc/src/main/java/Marker.java
@@ -1,0 +1,1 @@
+final class Marker {}

libs/javalib/test/resources/quarkus-grpc/grpc/src/main/proto/ping.proto
@@ -1,0 +1,18 @@
+syntax = "proto3";
+
+option java_multiple_files = true;
+option java_package = "com.lihaoyi.test.grpc";
+
+package ping;
+
+service Pinger {
+  rpc Send (Ping) returns (Pong) {}
+}
+
+message Ping {
+  string message = 1;
+}
+
+message Pong {
+  string message = 1;
+}

commit 6ba0f242f62643d8bb50479db4d974b4772b7b33

Fix/quarkus exe artifact resolution and dependencies with missing os placeholders (backport #7552)
fixes https://github.com/com-lihaoyi/mill/issues/7331 and other
dependency issues found in quarkus quick examples

Made with initial vibes and then some handcrafted changes

Fixes two dependency-resolution regressions in `QuarkusModule`
introduced by #7519
("Preserve Quarkus executable dependency metadata"), both surfacing
while running
`example.migrating.javalib.6-maven-quarkus` and while building
real-world Quarkus
projects (e.g. the `quarkus-quickstarts` repo) with Mill:

1. Coursier fails to resolve dependencies whose Maven coordinates rely
on
`${os.detected.classifier}`-style properties (e.g.
`io.netty:netty-tcnative`, a
transitive, optional dependency of `io.netty:netty-handler`), because
that
property is normally injected by the `os-maven-plugin`/`osdetector`
Maven/Gradle
   build extensions, which Coursier has no equivalent for.
2. Dependencies published with a Maven packaging type other than the
literal `jar`
(e.g. `jakarta.ws.rs:jakarta.ws.rs-api`, packaged as an OSGi `bundle`)
were
silently dropped from Quarkus's runtime/deployment classloaders during
`quarkusCodeGen`, causing
`ClassNotFoundException`/`NoClassDefFoundError` for
   basically any Quarkus app using `quarkus-rest`.
3. Enabled all quarkus examples (removed the `rm
grpc-plain-text-quickstart/package.mill.yaml
grpc-tls-quickstart/package.mill.yaml` . They are all in a working state
now

`io.netty:netty-handler`'s published POM declares its native TLS
dependency as:

```xml
<dependency>
  <groupId>${project.groupId}</groupId>
  <artifactId>${tcnative.artifactId}</artifactId>
  <classifier>${tcnative.classifier}</classifier>
  <optional>true</optional>
</dependency>
```

where `tcnative.classifier` is itself defined as
`${os.detected.classifier}` - a
property that real Maven builds only get a concrete value for because
the
`os-maven-plugin` build extension injects it before dependency

example/migrating/javalib/6-maven-quarkus/build.mill
@@ -6,8 +6,6 @@
 
 > ./mill init
 
-> rm grpc-plain-text-quickstart/package.mill.yaml  grpc-tls-quickstart/package.mill.yaml # skip subprojects with quarkus codegen. Not yet implemented in mill.
-
 > ./mill {getting-started-reactive,kafka-avro-schema-quickstart}.compile
 
 > ./mill __.compile # scheduled linux

libs/javalib/src/mill/javalib/OsDetector.scala
@@ -1,0 +1,55 @@
+package mill.javalib
+
+trait OsDetector {
+
+  /** The `os.detected.*` properties for the JVM currently running Mill. */
+  def detect(): Map[String, String] =
+    detect(sys.props.getOrElse("os.name", ""), sys.props.getOrElse("os.arch", ""))
+
+  private[javalib] def detect(rawOsName: String, rawOsArch: String): Map[String, String] = {
+    val name = normalizeOsName(rawOsName)
+    val arch = normalizeArch(rawOsArch)
+    val bitness = if (arch.endsWith("_64")) "64" else "32"
+    Map(
+      "os.detected.name" -> name,
+      "os.detected.arch" -> arch,
+      "os.detected.bitness" -> bitness,
+      "os.detected.classifier" -> s"$name-$arch"
+    )
+  }
+
+  private def normalizeOsName(raw: String): String = {
+    val name = raw.toLowerCase(java.util.Locale.ROOT)
+    if (name.contains("windows")) "windows"
+    else if (name.contains("mac") || name.contains("darwin")) "osx"
+    else if (name.contains("linux")) "linux"
+    else if (name.contains("sunos") || name.contains("solaris")) "sunos"
+    else if (name.contains("aix")) "aix"
+    else if (name.contains("hp-ux") || name.contains("hpux")) "hpux"
+    else if (name.contains("os/400") || name.contains("os400")) "os400"
+    else if (name.contains("freebsd")) "freebsd"
+    else if (name.contains("openbsd")) "openbsd"
+    else if (name.contains("netbsd")) "netbsd"
+    else if (name.contains("z/os") || name.contains("zos")) "zos"
+    else name.replaceAll("[^a-z0-9]+", "")
+  }
+
+  private def normalizeArch(raw: String): String =
+    raw.toLowerCase(java.util.Locale.ROOT) match {
+      case "x86_64" | "amd64" | "ia32e" | "em64t" | "x64" => "x86_64"
+      case "x86_32" | "x86" | "i386" | "i486" | "i586" | "i686" | "ia32" | "x32" => "x86_32"
+      case "aarch64" | "arm64" => "aarch_64"
+      case "arm" | "arm32" => "arm_32"
+      case "ppc" | "ppc32" => "ppc_32"
+      case "ppcle" | "ppc32le" => "ppcle_32"
+      case "ppc64" => "ppc_64"
+      case "ppc64le" | "ppcle64" => "ppcle_64"
+      case "s390" => "s390_32"
+      case "s390x" => "s390_64"
+      case "riscv64" => "riscv64"
+      case "riscv32" | "riscv" => "riscv32"
+      case other => other.replaceAll("[^a-z0-9_]+", "")
+    }
+}
+
+object OsDetector extends OsDetector

libs/javalib/src/mill/javalib/OsDetectorModule.scala
@@ -1,0 +1,57 @@
+package mill.javalib
+
+import coursier.params.ResolutionParams
+import mill.{T, Command}
+import mill.api.{DefaultTaskModule, Discover, ExternalModule, Task}
+
+/**
+ * Detects the current platform using the same conventions as the `os-maven-plugin`/`osdetector`
+ * Maven and Gradle build extensions (`os.detected.name`, `os.detected.arch`,
+ * `os.detected.classifier`, `os.detected.bitness`).
+ *
+ * A number of published artifacts (e.g. `com.google.protobuf:protoc`,
+ * `io.grpc:protoc-gen-grpc-java`, `io.netty:netty-tcnative*`) reference a platform-specific
+ * classifier via a `${os.detected.classifier}`-style placeholder in their consumers' POMs,
+ * expecting one of those extensions to inject the property at build time. Coursier has no
+ * equivalent extension mechanism, so without injecting the same properties ourselves, such
+ * placeholders are left as literal, unresolvable text and dependency resolution fails.
+ */
+trait OsDetectorModule extends CoursierModule {
+
+  override def resolutionParams: Task[ResolutionParams] = Task.Anon {
+    super.resolutionParams().addProperties(osDetectedMvnProperties().toSeq*)
+  }
+
+  /**
+   * The `os.detected.*` properties (`os.detected.name`, `os.detected.arch`,
+   * `os.detected.classifier`, `os.detected.bitness`) for the platform Mill is currently running
+   * on, made available to Coursier so that `${os.detected.*}` placeholders in dependency POMs -
+   * normally injected by the `os-maven-plugin`/`osdetector` build extensions - resolve to the
+   * current platform instead of being left as literal, unresolvable text.
+   *
+   * See [[OsDetector]].
+   */
+  def osDetectedMvnProperties: T[Map[String, String]] = Task.Uncached {
+    // TODO using the OsDetectorModule.detect command seems to cause a timeout
+    // If the cause is found, this can be switched to reuse the command.
+    OsDetector.detect()
+  }
+}
+
+/**
+ * Outputs this Operating System's related properties such as `os.detected.name`, `os.detected.arch`,
+ * `os.detected.classifier`, `os.detected.bitness` , of this system. Useful
+ * when debugging placeholder issues for POMs.
+ */
+object OsDetectorModule extends ExternalModule, DefaultTaskModule {
+  lazy val millDiscover: Discover = Discover[this.type]
+
+  def defaultTask(): String = "detect"
+
+  /**
+   * Outputs the `os.detected.*` properties for the JVM currently running Mill.
+   */
+  def detect(): Command[Map[String, String]] = Task.Command {
+    OsDetector.detect()
+  }
+}

libs/javalib/src/mill/javalib/quarkus/QuarkusModule.scala
@@ -3,7 +3,7 @@ package mill.javalib.quarkus
 import coursier.core.VariantSelector.ConfigurationBased
 import mill.api.PathRef
 import mill.{T, Task}
-import mill.javalib.{CoursierModule, Dep, DepSyntax, JavaModule, PublishModule}
+import mill.javalib.{CoursierModule, Dep, DepSyntax, JavaModule, OsDetectorModule, PublishModule}
 import mill.util.Jvm
 import upickle.default.ReadWriter.join
 
@@ -13,7 +13,7 @@ import java.util.Properties
 import scala.util.Using
 
 @mill.api.experimental
-trait QuarkusModule extends JavaModule { outer =>
+trait QuarkusModule extends JavaModule, OsDetectorModule { outer =>
 
   /**
    * The version of the quarkus platform (e.g. 3.31.2). Used for
@@ -174,7 +174,8 @@ trait QuarkusModule extends JavaModule { outer =>
       ConfigurationBased(coursier.core.Configuration.compile)
     )
 
-    val quarkusArtifactTypes = Some(artifactTypes() + coursier.Type("exe"))
+    val jarLikeArtifactTypes = artifactTypes()
+    val quarkusArtifactTypes = Some(jarLikeArtifactTypes + coursier.Type("exe"))
 
     def resolveArtifacts[T: CoursierModule.Resolvable](deps: Seq[T]) =
       millResolver().artifacts(deps, sources = false, artifactTypes = quarkusArtifactTypes)
@@ -210,8 +211,20 @@ trait QuarkusModule extends JavaModule { outer =>
           ),
         _.attributes
       )
+      // Coursier treats several Maven packaging types (`bundle`, `eclipse-plugin`, `hk2`,
+      // `orbit`, `scala-jar`, `klib`, `maven-plugin`, see `artifactTypes`/`Resolution.defaultTypes`)
+      // as ordinary jars for classpath purposes - e.g. plenty of OSGi-packaged libraries
+      // (like `jakarta.ws.rs:jakarta.ws.rs-api`) declare `<packaging>bundle</packaging>` despite
+      // being perfectly normal jars. Quarkus's own `ArtifactCoords.isJar()` only recognizes the
+      // literal type `jar`/`test-jar` when deciding what to add to its classloaders, so anything
+      // else - including these jar-equivalent types - would silently be dropped from the
+      // classpath. Normalize them to `jar` here, and only pass through types Quarkus doesn't
+      // already treat as jar-like, such as the `exe` type used for platform-specific tool
+      // executables (e.g. `protoc`).
       val artifactType =
-        if (attributes.`type`.isEmpty) coursier.Type.jar.value else attributes.`type`.value
+        if (attributes.`type`.isEmpty || jarLikeArtifactTypes(attributes.`type`))
+          coursier.Type.jar.value
+        else attributes.`type`.value
       ApplicationModelWorker.Dependency(
         groupId = dependency.module.organization.value,
         artifactId = dependency.module.name.value,

libs/javalib/test/src/mill/javalib/OsDetectorTests.scala
@@ -1,0 +1,51 @@
+package mill.javalib
+
+import utest.*
+
+object OsDetectorTests extends TestSuite {
+
+  val tests = Tests {
+    test("name") {
+      assert(OsDetector.detect("Linux", "amd64")("os.detected.name") == "linux")
+      assert(OsDetector.detect("Windows 11", "amd64")("os.detected.name") == "windows")
+      assert(OsDetector.detect("Mac OS X", "aarch64")("os.detected.name") == "osx")
+    }
+
+    test("arch") {
+      assert(OsDetector.detect("Linux", "amd64")("os.detected.arch") == "x86_64")
+      assert(OsDetector.detect("Linux", "x86_64")("os.detected.arch") == "x86_64")
+      assert(OsDetector.detect("Linux", "aarch64")("os.detected.arch") == "aarch_64")
+      assert(OsDetector.detect("Mac OS X", "aarch64")("os.detected.arch") == "aarch_64")
+      assert(OsDetector.detect("Linux", "i686")("os.detected.arch") == "x86_32")
+      assert(OsDetector.detect("Linux", "ppc64le")("os.detected.arch") == "ppcle_64")
+      assert(OsDetector.detect("Linux", "s390x")("os.detected.arch") == "s390_64")
+    }
+
+    test("bitness") {
+      assert(OsDetector.detect("Linux", "amd64")("os.detected.bitness") == "64")
+      assert(OsDetector.detect("Linux", "i686")("os.detected.bitness") == "32")
+    }
+
+    test("classifier") {
+      assert(OsDetector.detect("Linux", "amd64")("os.detected.classifier") == "linux-x86_64")
+      assert(OsDetector.detect(
+        "Windows 11",
+        "amd64"
+      )("os.detected.classifier") == "windows-x86_64")
+      assert(OsDetector.detect(
+        "Mac OS X",
+        "aarch64"
+      )("os.detected.classifier") == "osx-aarch_64")
+    }
+
+    test("liveDetection") {
+      // Sanity check the properties derived from the JVM actually running this test are populated
+      val props = OsDetector.detect()
+      assert(props("os.detected.name").nonEmpty)
+      assert(props("os.detected.arch").nonEmpty)
+      assert(props(
+        "os.detected.classifier"
+      ) == s"${props("os.detected.name")}-${props("os.detected.arch")}")
+    }
+  }
+}

libs/javalib/test/src/mill/javalib/quarkus/QuarkusModuleTests.scala
@@ -17,7 +17,10 @@ object QuarkusModuleTests extends TestSuite {
       def artifactVersion = "0.0.1"
 
       def mvnDeps = Seq(
-        mvn"io.quarkus:quarkus-grpc"
+        mvn"io.quarkus:quarkus-grpc",
+        // Pulls in jakarta.ws.rs:jakarta.ws.rs-api, published with
+        // `<packaging>bundle</packaging>`, exercised by the `bundlePackagedJar` assertion below.
+        mvn"io.quarkus:quarkus-rest"
       )
     }
 
@@ -59,6 +62,14 @@ object QuarkusModuleTests extends TestSuite {
         val ordinaryJar = dependencies.find(d =>
           d.groupId == "io.quarkus" && d.artifactId == "quarkus-grpc-codegen"
         ).get
+        // Published with `<packaging>bundle</packaging>` - Coursier treats that as a jar-like
+        // type, but Quarkus's own `ArtifactCoords.isJar()` only recognizes the literal `jar`
+        // type, so this must be normalized or the dependency silently vanishes from Quarkus's
+        // classloaders (see the regression this guards against: jakarta.ws.rs.BadRequestException
+        // ClassNotFoundException during quarkusCodeGen).
+        val bundlePackagedJar = dependencies.find(d =>
+          d.groupId == "jakarta.ws.rs" && d.artifactId == "jakarta.ws.rs-api"
+        ).get
 
         assert(
           protoc.size == platformClassifiers.size,
@@ -70,7 +81,8 @@ object QuarkusModuleTests extends TestSuite {
           quarkusPlugin.artifactType == "jar",
           quarkusPlugin.classifier == "shaded",
           ordinaryJar.artifactType == "jar",
-          ordinaryJar.classifier.isEmpty
+          ordinaryJar.classifier.isEmpty,
+          bundlePackagedJar.artifactType == "jar"
         )
       }
     }