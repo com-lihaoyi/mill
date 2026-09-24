b62f2b73afbcae359d24b5db986982a30fab2cf1
Tobias Roeser <le.petit.fou@web.de>
2026-09-24 10:07:23 +0200

    Add release 1.1.7, changelog and docs (backport #7302)
    Backport of pull request: https://github.com/com-lihaoyi/mill/pull/7302

--- mill-build/src/millbuild/Settings.scala
+++ mill-build/src/millbuild/Settings.scala
@@ -20,7 +20,7 @@ object Settings {
     "0.11.13",
     "0.12.17",
     "1.0.6",
-    "1.1.6"
+    "1.1.7"
   )
   val mimaBaseVersions: Seq[String] = Seq(
     "1.0.0",
@@ -36,7 +36,8 @@ object Settings {
     "1.1.3",
     "1.1.4",
     "1.1.5",
-    "1.1.6"
+    "1.1.6",
+    "1.1.7"
   )
 
   val graalvmJvmId = "graalvm-community:23.0.1"
